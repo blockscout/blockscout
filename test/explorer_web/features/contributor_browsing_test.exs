ed6791d2e02281a315ca4e7a0e111ab02ec8dc7c
CJ Bryan and Matt Olenick <pair+cj+matto@ministryofvelocity.com>
2018-01-17 10:52:10 -0800

    The logo is now a link to the homepage.

--- test/explorer_web/features/contributor_browsing_test.exs
+++ test/explorer_web/features/contributor_browsing_test.exs
@@ -1,11 +1,15 @@
 defmodule ExplorerWeb.UserListTest do
   use ExplorerWeb.FeatureCase, async: true
 
-  import Wallaby.Query, only: [css: 2]
+  import Wallaby.Query, only: [css: 1, css: 2]
+
+  @logo css("img.header__logo")
 
   test "browses the home page", %{session: session} do
     session
     |> visit("/")
     |> assert_has(css(".header__title", text: "POA Network Explorer"))
+    |> click(@logo)
+    |> assert_has(css("main", text: "Welcome to our blockchain explorer."))
   end
 end
