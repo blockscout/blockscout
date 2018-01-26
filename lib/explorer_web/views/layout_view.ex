7f879c272b3b64bef7f8922fb7e8e25f4a2bb555
Doc Ritezel <doc@ministryofvelocity.com>
2018-01-25 19:39:43 -0800

    Add breakpoints to the layout

--- lib/explorer_web/views/layout_view.ex
+++ lib/explorer_web/views/layout_view.ex
@@ -1,11 +1,9 @@
 defmodule ExplorerWeb.LayoutView do
   use ExplorerWeb, :view
-  alias ExplorerWeb.Endpoint
 
-  def logo_image(conn, alt: alt) do
+  def logo_image(conn, alt: alt, class: class) do
     conn
-    |> static_path("/images/logo.png")
-    |> img_tag(class: "header__logo", alt: alt)
-    |> link(to: Endpoint.url(), class: "header__logo-link")
+    |> static_path("/images/logo.svg")
+    |> img_tag(class: class, alt: alt)
   end
 end
