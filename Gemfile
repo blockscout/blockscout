06f580a0cf73e58910bd08b6434a624886bd3b35
Doc Ritezel <doc@ministryofvelocity.com>
2018-01-28 17:24:18 -0800

    Add license_finder to the build process

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+group :development do
+  gem 'license_finder', require: false
+  gem 'pivotal_git_scripts', require: false
+end
--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -10,15 +10,15 @@ exports.config = {
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
       // joinTo: {
-      //   "js/app.js": /^js/,
-      //   "js/vendor.js": /^(?!js)/
+      //   'js/app.js': /^js/,
+      //   'js/vendor.js': /^(?!js)/
       // }
       //
       // To change the order of concatenation of files, explicitly mention here
       // order: {
       //   before: [
-      //     "vendor/js/jquery-2.1.1.js",
-      //     "vendor/js/bootstrap.min.js"
+      //     'vendor/js/jquery-2.1.1.js',
+      //     'vendor/js/bootstrap.min.js'
       //   ]
       // }
     },
@@ -34,8 +34,8 @@ exports.config = {
 
   conventions: {
     // This option sets where we should place non-css and non-js assets in.
-    // By default, we set this to "/assets/static". Files in this directory
-    // will be copied to `paths.public`, which is "priv/static" by default.
+    // By default, we set this to '/assets/static'. Files in this directory
+    // will be copied to `paths.public`, which is 'priv/static' by default.
     assets: /^(static)/
   },
 
@@ -59,7 +59,7 @@ exports.config = {
       precision: 8,
       allowCache: true,
       options: {
-        includePaths: ["node_modules/normalize-scss/sass"]
+        includePaths: ['node_modules/normalize-scss/sass']
       }
     }
   },
