492ddd1ceeb41dc9ce37e5f853685ca365cdf12a
Doc Ritezel <ritezel@gmail.com>
2018-02-10 10:49:35 -0800

    Do not load blocks in pending transactions query

--- lib/explorer_web/controllers/pending_transaction_controller.ex
+++ lib/explorer_web/controllers/pending_transaction_controller.ex
@@ -10,13 +10,11 @@ defmodule ExplorerWeb.PendingTransactionController do
   def index(conn, params) do
     query = from transaction in Transaction,
       left_join: block_transaction in assoc(transaction, :block_transaction),
-      left_join: block in assoc(block_transaction, :block),
-      left_join: to_address_join in assoc(transaction, :to_address_join),
-      left_join: to_address in assoc(to_address_join, :address),
-      left_join: from_address_join in assoc(transaction, :from_address_join),
-      left_join: from_address in assoc(from_address_join, :address),
+      join: to_address_join in assoc(transaction, :to_address_join),
+      join: to_address in assoc(to_address_join, :address),
+      join: from_address_join in assoc(transaction, :from_address_join),
+      join: from_address in assoc(from_address_join, :address),
       preload: [
-        block: block,
         to_address: to_address,
         from_address: from_address
       ],
