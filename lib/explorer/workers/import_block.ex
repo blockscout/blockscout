1abbf61404c8df2306db12483982feb367a948ac
Doc Ritezel <doc@ministryofvelocity.com>
2018-02-15 22:29:40 -0800

    Allow the latest block worker to re-enqueue an actual block number

--- lib/explorer/workers/import_block.ex
+++ lib/explorer/workers/import_block.ex
@@ -1,16 +1,23 @@
 defmodule Explorer.Workers.ImportBlock do
+  @moduledoc "Imports blocks by web3 conventions."
+
+  import Ethereumex.HttpClient, only: [eth_block_number: 0]
+
   alias Explorer.BlockImporter
 
-  @moduledoc "Imports blocks by web3 conventions."
+  @dialyzer {:nowarn_function, perform: 1}
+  def perform("latest") do
+    case eth_block_number() do
+      {:ok, number} -> perform_later(number)
+      _ -> nil
+    end
+  end
 
   @dialyzer {:nowarn_function, perform: 1}
   def perform(number), do: BlockImporter.import("#{number}")
 
-  @dialyzer {:nowarn_function, perform: 0}
-  def perform, do: perform("latest")
-
-  def perform_later("latest") do
-    Exq.enqueue(Exq.Enqueuer, "blocks", __MODULE__, ["latest"], max_retries: 0)
+  def perform_later("0x" <> number) when is_binary(number) do
+    number |> String.to_integer(16) |> perform_later()
   end
 
   def perform_later(number) do
