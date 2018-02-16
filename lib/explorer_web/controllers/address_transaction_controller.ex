a1e5bb1f9911ec398fbd4a2a41c2542801287b07
Doc Ritezel <doc@ministryofvelocity.com>
2018-02-15 19:43:31 -0800

    Look up addresses before scanning for their transactions

--- lib/explorer_web/controllers/address_transaction_controller.ex
+++ lib/explorer_web/controllers/address_transaction_controller.ex
@@ -3,12 +3,17 @@ defmodule ExplorerWeb.AddressTransactionController do
 
   import Ecto.Query
 
+  alias Explorer.Address
   alias Explorer.Repo.NewRelic, as: Repo
   alias Explorer.Transaction
   alias Explorer.TransactionForm
 
   def index(conn, %{"address_id" => address_id} = params) do
     hash = String.downcase(address_id)
+    address = Repo.one(from address in Address,
+      where: fragment("lower(?)", address.hash) == ^hash,
+      limit: 1)
+    address_id = address.id
     query = from transaction in Transaction,
       join: block in assoc(transaction, :block),
       join: receipt in assoc(transaction, :receipt),
@@ -16,8 +21,7 @@ defmodule ExplorerWeb.AddressTransactionController do
       join: to_address in assoc(transaction, :to_address),
       preload: [:block, :receipt, :to_address, :from_address],
       order_by: [desc: transaction.inserted_at],
-      where: fragment("lower(?)", to_address.hash) == ^hash or
-        fragment("lower(?)", from_address.hash) == ^hash
+      where: to_address.id == ^address_id or from_address.id == ^address_id
     page = Repo.paginate(query, params)
     entries = Enum.map(page.entries, &TransactionForm.build_and_merge/1)
     render(conn, "index.html", transactions: Map.put(page, :entries, entries))
