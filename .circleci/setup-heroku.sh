f71e542f467cf9d9ab4d2c1edf7e760373c31b6d
Luke Imhoff <luke.imhoff@dockyard.com>
2018-04-03 10:56:18 -0500

    Migrate from CircleCI 1.0 to 2.0
    Note: deployment jobs are filtered by branch, so deploy_staging won't be
    tested until the merge to master and deploy_production won't be tested
    until the merge to production.

---
+++ .circleci/setup-heroku.sh
@@ -0,0 +1,16 @@
+ #!/bin/bash
+ wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+ sudo mkdir -p /usr/local/lib /usr/local/bin
+ sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+ sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+
+ cat > ~/.netrc << EOF
+ machine api.heroku.com
+   login $HEROKU_LOGIN
+   password $HEROKU_API_KEY
+ EOF
+
+ cat >> ~/.ssh/config << EOF
+ VerifyHostKeyDNS yes
+ StrictHostKeyChecking no
+ EOF
