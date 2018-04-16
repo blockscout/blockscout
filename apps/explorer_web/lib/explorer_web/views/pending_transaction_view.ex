5c69c523da63ebc68ad235ee220057f3348e558d
Luke Imhoff <luke.imhoff@dockyard.com>
2018-04-16 10:23:00 -0500

    Replace PendingTransactionForm with proper PendingTransactionView functions

--- apps/explorer_web/lib/explorer_web/views/pending_transaction_view.ex
+++ apps/explorer_web/lib/explorer_web/views/pending_transaction_view.ex
@@ -1,4 +1,27 @@
 defmodule ExplorerWeb.PendingTransactionView do
   use ExplorerWeb, :view
+
+  alias Explorer.Chain.{Address, Transaction}
+
   @dialyzer :no_match
+
+  # Functions
+
+  def from_address_hash(%Transaction{from_address: from_address}) do
+    case from_address do
+      %Address{hash: hash} -> hash
+      _ -> nil
+    end
+  end
+
+  def last_seen(%Transaction{updated_at: updated_at}) do
+    Timex.from_now(updated_at)
+  end
+
+  def to_address_hash(%Transaction{to_address: to_address}) do
+    case to_address do
+      %Address{hash: hash} -> hash
+      _ -> nil
+    end
+  end
 end
