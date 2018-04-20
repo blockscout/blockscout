5d9f155582a2547b63b37e4b869f6218c59b5f17
Tim Mecklem <timothy@mecklem.com>
2018-04-20 13:48:19 -0400

    Move block overview above transactions list on block details page
    Co-authored-by: jimmay5469 <jimmay5469@gmail.com>

--- apps/explorer_web/lib/explorer_web/controllers/block_controller.ex
+++ apps/explorer_web/lib/explorer_web/controllers/block_controller.ex
@@ -9,14 +9,7 @@ defmodule ExplorerWeb.BlockController do
     render(conn, "index.html", blocks: blocks)
   end
 
-  def show(conn, %{"id" => number}) do
-    case Chain.number_to_block(number) do
-      {:ok, block} ->
-        block_transaction_count = Chain.block_to_transaction_count(block)
-        render(conn, "show.html", block: block, block_transaction_count: block_transaction_count)
-
-      {:error, :not_found} ->
-        not_found(conn)
-    end
+  def show(conn, %{"id" => number, "locale" => locale}) do
+    redirect(conn, to: block_transaction_path(conn, :index, locale, number))
   end
 end
