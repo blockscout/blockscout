a5277b5103beeee090ce4a85e2a021a25baceb02
Luke Imhoff <luke.imhoff@dockyard.com>
2018-05-03 13:35:32 -0500

    Fix dialyzer warnings

--- apps/explorer/lib/explorer/chain/address.ex
+++ apps/explorer/lib/explorer/chain/address.ex
@@ -63,6 +63,7 @@ defmodule Explorer.Chain.Address do
     |> unique_constraint(:hash)
   end
 
+  @spec hash_set_to_changes_list(MapSet.t(Hash.Truncated.t())) :: [%{hash: Hash.Truncated.t()}]
   def hash_set_to_changes_list(hash_set) do
     Enum.map(hash_set, &hash_to_changes/1)
   end
