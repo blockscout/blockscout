92989beffee44b47a00bea4f5eeded6dfbdd9a15
Luke Imhoff <luke.imhoff@dockyard.com>
2018-05-08 17:34:21 -0500

    Cover pure Explorer.JSONRPC.Receipt(s)

--- apps/explorer/lib/explorer/jsonrpc/receipts.ex
+++ apps/explorer/lib/explorer/jsonrpc/receipts.ex
@@ -12,11 +12,100 @@ defmodule Explorer.JSONRPC.Receipts do
   @type elixir :: [Receipt.elixir()]
   @type t :: [Receipt.t()]
 
+  @doc """
+  Extracts logs from `t:elixir/0`
+
+      iex> Explorer.JSONRPC.Receipts.elixir_to_logs([
+      ...>   %{
+      ...>     "blockHash" => "0xf6b4b8c88df3ebd252ec476328334dc026cf66606a84fb769b3d3cbccc8471bd",
+      ...>     "blockNumber" => 37,
+      ...>     "contractAddress" => nil,
+      ...>     "cumulativeGasUsed" => 50450,
+      ...>     "gasUsed" => 50450,
+      ...>     "logs" => [
+      ...>       %{
+      ...>         "address" => "0x8bf38d4764929064f2d4d3a56520a76ab3df415b",
+      ...>         "blockHash" => "0xf6b4b8c88df3ebd252ec476328334dc026cf66606a84fb769b3d3cbccc8471bd",
+      ...>         "blockNumber" => 37,
+      ...>         "data" => "0x000000000000000000000000862d67cb0773ee3f8ce7ea89b328ffea861ab3ef",
+      ...>         "logIndex" => 0,
+      ...>         "topics" => ["0x600bcf04a13e752d1e3670a5a9f1c21177ca2a93c6f5391d4f1298d098097c22"],
+      ...>         "transactionHash" => "0x53bd884872de3e488692881baeec262e7b95234d3965248c39fe992fffd433e5",
+      ...>         "transactionIndex" => 0,
+      ...>         "transactionLogIndex" => 0,
+      ...>         "type" => "mined"
+      ...>       }
+      ...>     ],
+      ...>     "logsBloom" => "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000200000000000000000000020000000000000000200000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+      ...>     "root" => nil,
+      ...>     "status" => :ok,
+      ...>     "transactionHash" => "0x53bd884872de3e488692881baeec262e7b95234d3965248c39fe992fffd433e5",
+      ...>     "transactionIndex" => 0
+      ...>   }
+      ...> ])
+      [
+        %{
+          "address" => "0x8bf38d4764929064f2d4d3a56520a76ab3df415b",
+          "blockHash" => "0xf6b4b8c88df3ebd252ec476328334dc026cf66606a84fb769b3d3cbccc8471bd",
+          "blockNumber" => 37,
+          "data" => "0x000000000000000000000000862d67cb0773ee3f8ce7ea89b328ffea861ab3ef",
+          "logIndex" => 0,
+          "topics" => ["0x600bcf04a13e752d1e3670a5a9f1c21177ca2a93c6f5391d4f1298d098097c22"],
+          "transactionHash" => "0x53bd884872de3e488692881baeec262e7b95234d3965248c39fe992fffd433e5",
+          "transactionIndex" => 0,
+          "transactionLogIndex" => 0,
+          "type" => "mined"
+        }
+      ]
+
+  """
   @spec elixir_to_logs(elixir) :: Logs.elixir()
   def elixir_to_logs(elixir) when is_list(elixir) do
     Enum.flat_map(elixir, &Receipt.elixir_to_logs/1)
   end
 
+  @doc """
+  Converts each element of `t:elixir/0` to params used by `Explorer.Chain.Receipt.changeset/2`.
+
+      iex> Explorer.JSONRPC.Receipts.elixir_to_params([
+      ...>   %{
+      ...>     "blockHash" => "0xf6b4b8c88df3ebd252ec476328334dc026cf66606a84fb769b3d3cbccc8471bd",
+      ...>     "blockNumber" => 37,
+      ...>     "contractAddress" => nil,
+      ...>     "cumulativeGasUsed" => 50450,
+      ...>     "gasUsed" => 50450,
+      ...>     "logs" => [
+      ...>       %{
+      ...>         "address" => "0x8bf38d4764929064f2d4d3a56520a76ab3df415b",
+      ...>         "blockHash" => "0xf6b4b8c88df3ebd252ec476328334dc026cf66606a84fb769b3d3cbccc8471bd",
+      ...>         "blockNumber" => 37,
+      ...>         "data" => "0x000000000000000000000000862d67cb0773ee3f8ce7ea89b328ffea861ab3ef",
+      ...>         "logIndex" => 0,
+      ...>         "topics" => ["0x600bcf04a13e752d1e3670a5a9f1c21177ca2a93c6f5391d4f1298d098097c22"],
+      ...>         "transactionHash" => "0x53bd884872de3e488692881baeec262e7b95234d3965248c39fe992fffd433e5",
+      ...>         "transactionIndex" => 0,
+      ...>         "transactionLogIndex" => 0,
+      ...>         "type" => "mined"
+      ...>       }
+      ...>     ],
+      ...>     "logsBloom" => "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000200000000000000000000020000000000000000200000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+      ...>     "root" => nil,
+      ...>     "status" => :ok,
+      ...>     "transactionHash" => "0x53bd884872de3e488692881baeec262e7b95234d3965248c39fe992fffd433e5",
+      ...>     "transactionIndex" => 0
+      ...>   }
+      ...> ])
+      [
+        %{
+          cumulative_gas_used: 50450,
+          gas_used: 50450,
+          status: :ok,
+          transaction_hash: "0x53bd884872de3e488692881baeec262e7b95234d3965248c39fe992fffd433e5",
+          transaction_index: 0
+        }
+      ]
+
+  """
   @spec elixir_to_params(elixir) :: [map]
   def elixir_to_params(elixir) when is_list(elixir) do
     Enum.map(elixir, &Receipt.elixir_to_params/1)
@@ -44,6 +133,67 @@ defmodule Explorer.JSONRPC.Receipts do
     end
   end
 
+  @doc """
+  Converts stringly typed fields to native Elixir types.
+
+      iex> Explorer.JSONRPC.Receipts.to_elixir([
+      ...>   %{
+      ...>     "blockHash" => "0xf6b4b8c88df3ebd252ec476328334dc026cf66606a84fb769b3d3cbccc8471bd",
+      ...>     "blockNumber" => "0x25",
+      ...>     "contractAddress" => nil,
+      ...>     "cumulativeGasUsed" => "0xc512",
+      ...>     "gasUsed" => "0xc512",
+      ...>     "logs" => [
+      ...>       %{
+      ...>         "address" => "0x8bf38d4764929064f2d4d3a56520a76ab3df415b",
+      ...>         "blockHash" => "0xf6b4b8c88df3ebd252ec476328334dc026cf66606a84fb769b3d3cbccc8471bd",
+      ...>         "blockNumber" => "0x25",
+      ...>         "data" => "0x000000000000000000000000862d67cb0773ee3f8ce7ea89b328ffea861ab3ef",
+      ...>         "logIndex" => "0x0",
+      ...>         "topics" => ["0x600bcf04a13e752d1e3670a5a9f1c21177ca2a93c6f5391d4f1298d098097c22"],
+      ...>         "transactionHash" => "0x53bd884872de3e488692881baeec262e7b95234d3965248c39fe992fffd433e5",
+      ...>         "transactionIndex" => "0x0",
+      ...>         "transactionLogIndex" => "0x0",
+      ...>         "type" => "mined"
+      ...>       }
+      ...>     ],
+      ...>     "logsBloom" => "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000200000000000000000000020000000000000000200000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+      ...>     "root" => nil,
+      ...>     "status" => "0x1",
+      ...>     "transactionHash" => "0x53bd884872de3e488692881baeec262e7b95234d3965248c39fe992fffd433e5",
+      ...>     "transactionIndex" => "0x0"
+      ...>   }
+      ...> ])
+      [
+        %{
+          "blockHash" => "0xf6b4b8c88df3ebd252ec476328334dc026cf66606a84fb769b3d3cbccc8471bd",
+          "blockNumber" => 37,
+          "contractAddress" => nil,
+          "cumulativeGasUsed" => 50450,
+          "gasUsed" => 50450,
+          "logs" => [
+            %{
+              "address" => "0x8bf38d4764929064f2d4d3a56520a76ab3df415b",
+              "blockHash" => "0xf6b4b8c88df3ebd252ec476328334dc026cf66606a84fb769b3d3cbccc8471bd",
+              "blockNumber" => 37,
+              "data" => "0x000000000000000000000000862d67cb0773ee3f8ce7ea89b328ffea861ab3ef",
+              "logIndex" => 0,
+              "topics" => ["0x600bcf04a13e752d1e3670a5a9f1c21177ca2a93c6f5391d4f1298d098097c22"],
+              "transactionHash" => "0x53bd884872de3e488692881baeec262e7b95234d3965248c39fe992fffd433e5",
+              "transactionIndex" => 0,
+              "transactionLogIndex" => 0,
+              "type" => "mined"
+            }
+          ],
+          "logsBloom" => "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000200000000000000000000020000000000000000200000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+          "root" => nil,
+          "status" => :ok,
+          "transactionHash" => "0x53bd884872de3e488692881baeec262e7b95234d3965248c39fe992fffd433e5",
+          "transactionIndex" => 0
+        }
+      ]
+
+  """
   @spec to_elixir(t) :: elixir
   def to_elixir(receipts) when is_list(receipts) do
     Enum.map(receipts, &Receipt.to_elixir/1)
---
+++ apps/explorer/test/explorer/jsonrpc/receipts_test.exs
@@ -0,0 +1,5 @@
+defmodule Explorer.JSONRPC.ReceiptsTest do
+  use ExUnit.Case, async: true
+
+  doctest Explorer.JSONRPC.Receipts
+end
