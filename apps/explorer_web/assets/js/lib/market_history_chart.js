0b6b338819aab4a51a4f7e4b1fbe89984842b668
jimmay5469 <jimmay5469@gmail.com>
2018-05-09 10:46:31 -0400

    Display Price, Market Cap, and 24h Volume

--- apps/explorer_web/assets/js/lib/market_history_chart.js
+++ apps/explorer_web/assets/js/lib/market_history_chart.js
@@ -1,36 +1,44 @@
 import $ from 'jquery'
 import Chart from 'chart.js'
+import humps from 'humps'
+import moment from 'moment'
 import numeral from 'numeral'
 import sassVariables from '../../css/app.scss'
 
-function formatPrice(price) {
+function formatPrice (price) {
   return '$' + price.toFixed(2)
 }
 
-function formatMarketCap(marketCap) {
+function formatMarketCap (marketCap) {
   return numeral(marketCap).format('($0,0a)')
 }
 
-$('[data-chart="marketHistoryChart"]').each((i, ctx)=> {
-  const marketHistoryData = JSON.parse(ctx.dataset.market_history_data)
-  const availableSupply = JSON.parse(ctx.dataset.available_supply)
+function createMarketHistoryChart (ctx) {
+  const currentExchangeRate = ctx.dataset.current_exchange_rate
+  const availableSupply = ctx.dataset.available_supply
+  const marketHistoryData = humps.camelizeKeys(JSON.parse(ctx.dataset.market_history_data))
+  const today = moment().format('YYYY-MM-DD')
+  const currentMarketHistoryData = marketHistoryData.map(({date, closingPrice}) => ({
+    date,
+    closingPrice: date === today ? currentExchangeRate : closingPrice
+  }))
 
-  var myChart = new Chart(ctx, {
+  return new Chart(ctx, {
     type: 'line',
     responsive: true,
     data: {
       datasets: [{
         label: 'Price',
         yAxisID: 'price',
-        data: marketHistoryData.map(({ date, closing_price }) => ({x: date, y: closing_price})),
+        data: currentMarketHistoryData.map(({ date, closingPrice }) => ({x: date, y: closingPrice})),
         fill: false,
         pointRadius: 0,
         backgroundColor: sassVariables.primary,
         borderColor: sassVariables.primary
-      },{
+      }, {
         label: 'Market Cap',
         yAxisID: 'marketCap',
-        data: marketHistoryData.map(({ date, closing_price }) => ({x: date, y: closing_price * availableSupply})),
+        data: currentMarketHistoryData.map(({ date, closingPrice }) => ({x: date, y: closingPrice * availableSupply})),
         fill: false,
         pointRadius: 0.5,
         backgroundColor: sassVariables.secondary,
@@ -54,7 +62,7 @@ $('[data-chart="marketHistoryChart"]').each((i, ctx)=> {
         yAxes: [{
           id: 'price',
           ticks: {
-            beginAtZero:true,
+            beginAtZero: true,
             callback: (value, index, values) => formatPrice(value),
             maxTicksLimit: 6
           }
@@ -73,9 +81,9 @@ $('[data-chart="marketHistoryChart"]').each((i, ctx)=> {
         callbacks: {
           label: ({datasetIndex, yLabel}, {datasets}) => {
             const label = datasets[datasetIndex].label
-            if(datasets[datasetIndex].label === 'Price') {
+            if (datasets[datasetIndex].label === 'Price') {
               return `${label}: ${formatPrice(yLabel)}`
-            } else if(datasets[datasetIndex].label === 'Market Cap') {
+            } else if (datasets[datasetIndex].label === 'Market Cap') {
               return `${label}: ${formatMarketCap(yLabel)}`
             } else {
               return yLabel
@@ -85,4 +93,6 @@ $('[data-chart="marketHistoryChart"]').each((i, ctx)=> {
       }
     }
   })
-})
+}
+
+$('[data-chart="marketHistoryChart"]').each((i, ctx) => createMarketHistoryChart(ctx))
