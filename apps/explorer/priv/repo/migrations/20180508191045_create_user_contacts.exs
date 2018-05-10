be7dfa5f121cdb3d8bd139f06408ad5a1807aeb7
Alex Garibay <alex.garibay@dockyard.com>
2018-05-10 15:54:34 -0500

    Delete user_contacts on user delete

--- apps/explorer/priv/repo/migrations/20180508191045_create_user_contacts.exs
+++ apps/explorer/priv/repo/migrations/20180508191045_create_user_contacts.exs
@@ -4,7 +4,7 @@ defmodule Explorer.Repo.Migrations.CreateUserContacts do
   def change do
     create table(:user_contacts) do
       add :email, :string, null: false
-      add :user_id, references(:users)
+      add :user_id, references(:users, on_delete: :delete_all), null: false
       add :primary, :boolean, default: false
       add :verified, :boolean, default: false
 
