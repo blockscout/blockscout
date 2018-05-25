g-cl/poa-explorer

Show Contract code in the Address page

apps/explorer_web/lib/explorer_web/controllers/address_contract_controller.ex
@@ -1,0 +1,28 @@
+defmodule ExplorerWeb.AddressContractController do
+  use ExplorerWeb, :controller
+
+  import ExplorerWeb.AddressController, only: [transaction_count: 1]
+
+  alias Explorer.{Chain, Market}
+  alias Explorer.ExchangeRates.Token
+
+  def index(conn, %{"address_id" => address_hash_string}) do
+    with {:ok, address_hash} <- Chain.string_to_address_hash(address_hash_string),
+         {:ok, address} <- Chain.hash_to_address(address_hash) do
+
+      render(
+        conn,
+        "index.html",
+        address: address,
+        exchange_rate: Market.get_exchange_rate(Explorer.coin()) || Token.null(),
+        transaction_count: transaction_count(address)
+      )
+    else
+      :error ->
+        not_found(conn)
+
+      {:error, :not_found} ->
+        not_found(conn)
+    end
+  end
+end

apps/explorer_web/lib/explorer_web/views/address_internal_transaction_view.ex
@@ -1,6 +1,8 @@
 defmodule ExplorerWeb.AddressInternalTransactionView do
   use ExplorerWeb, :view
 
+  import ExplorerWeb.AddressView, only: [is_contract?: 1]
+
   def format_current_filter(filter) do
     case filter do
       "to" -> gettext("To")

apps/explorer_web/lib/explorer_web/views/address_transaction_view.ex
@@ -1,7 +1,10 @@
 defmodule ExplorerWeb.AddressTransactionView do
   use ExplorerWeb, :view
 
+  import ExplorerWeb.AddressView, only: [is_contract?: 1]
+
   alias ExplorerWeb.TransactionView
+
 
   def format_current_filter(filter) do
     case filter do

blockscout/blockscout

apps/explorer_web/lib/explorer_web/controllers/address_contract_controller.ex
@@ -1,0 +1,27 @@
+defmodule ExplorerWeb.AddressContractController do
+  use ExplorerWeb, :controller
+
+  import ExplorerWeb.AddressController, only: [transaction_count: 1]
+
+  alias Explorer.{Chain, Market}
+  alias Explorer.ExchangeRates.Token
+
+  def index(conn, %{"address_id" => address_hash_string}) do
+    with {:ok, address_hash} <- Chain.string_to_address_hash(address_hash_string),
+         {:ok, address} <- Chain.find_contract_address(address_hash) do
+      render(
+        conn,
+        "index.html",
+        address: address,
+        exchange_rate: Market.get_exchange_rate(Explorer.coin()) || Token.null(),
+        transaction_count: transaction_count(address)
+      )
+    else
+      :error ->
+        not_found(conn)
+
+      {:error, :not_found} ->
+        not_found(conn)
+    end
+  end
+end

apps/explorer_web/lib/explorer_web/views/address_internal_transaction_view.ex
@@ -1,6 +1,8 @@
 defmodule ExplorerWeb.AddressInternalTransactionView do
   use ExplorerWeb, :view
 
+  import ExplorerWeb.AddressView, only: [contract?: 1]
+
   def format_current_filter(filter) do
     case filter do
       "to" -> gettext("To")

apps/explorer_web/lib/explorer_web/views/address_transaction_view.ex
@@ -1,6 +1,8 @@
 defmodule ExplorerWeb.AddressTransactionView do
   use ExplorerWeb, :view
 
+  import ExplorerWeb.AddressView, only: [contract?: 1]
+
   alias ExplorerWeb.TransactionView
 
   def format_current_filter(filter) do