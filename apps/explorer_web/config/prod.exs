38b01c3a42dc324ba22a593a56be42fb5762473f
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-06-04 18:46:21 -0400

    [deployment] fix handling of SSL in prod config

--- apps/explorer_web/config/prod.exs
+++ apps/explorer_web/config/prod.exs
@@ -15,9 +15,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :explorer_web, ExplorerWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  load_from_system_env: true,
+  force_ssl: false,
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  http: [port: System.get_env("PORT")],
   url: [
     scheme: "http",
     port: System.get_env("PORT")
