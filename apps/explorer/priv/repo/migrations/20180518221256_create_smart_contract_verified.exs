g-cl/poa-explorer

Add smart contract verification using the solidity cli to process code

apps/explorer/priv/repo/migrations/20180518221256_create_smart_contract_verified.exs
@@ -1,0 +1,17 @@
+defmodule Explorer.Repo.Migrations.CreateSmartContractVerified do
+  use Ecto.Migration
+
+  def change do
+    create table(:smart_contracts) do
+      add(:name, :string, null: false)
+      add(:compiler_version, :string, null: false)
+      add(:optimization, :boolean, null: false)
+      add(:contract_code, :text, null: false)
+      add(:abi, :jsonb, null: false)
+
+      add(:address_hash, references(:addresses, column: :hash, on_delete: :delete_all, type: :bytea), null: false)
+
+      timestamps()
+    end
+  end
+end

blockscout/blockscout

apps/explorer/priv/repo/migrations/20180518221256_create_smart_contract_verified.exs
@@ -1,0 +1,19 @@
+defmodule Explorer.Repo.Migrations.CreateSmartContractVerified do
+  use Ecto.Migration
+
+  def change do
+    create table(:smart_contracts) do
+      add(:name, :string, null: false)
+      add(:compiler_version, :string, null: false)
+      add(:optimization, :boolean, null: false)
+      add(:contract_source_code, :text, null: false)
+      add(:abi, :jsonb, null: false)
+
+      add(:address_hash, references(:addresses, column: :hash, on_delete: :delete_all, type: :bytea), null: false)
+
+      timestamps()
+    end
+
+    create(unique_index(:smart_contracts, :address_hash))
+  end
+end