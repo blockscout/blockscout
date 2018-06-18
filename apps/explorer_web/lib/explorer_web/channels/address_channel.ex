3a50823ef562f83b7100853093e460928162c615
Tim Mecklem <timothy@mecklem.com>
2018-06-18 14:29:50 -0400

    Localize transactions sent through the channel

--- apps/explorer_web/lib/explorer_web/channels/address_channel.ex
+++ apps/explorer_web/lib/explorer_web/channels/address_channel.ex
@@ -8,6 +8,7 @@ defmodule ExplorerWeb.AddressChannel do
   end
 
   def handle_out("transaction", %{transaction: transaction}, socket) do
+    Gettext.put_locale(ExplorerWeb.Gettext, socket.assigns.locale)
     rendered = Phoenix.View.render_to_string(ExplorerWeb.AddressTransactionView, "_transaction.html", locale: socket.assigns.locale, transaction: transaction)
     push socket, "transaction", %{transaction: rendered}
     {:noreply, socket}
