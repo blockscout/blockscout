62dc04a75b6986221f7524fea46560f3ddec16e7
jimmay5469 <jimmay5469@gmail.com>
2018-06-22 15:13:11 -0400

    Handle reload for address's first transaction
    Co-authored-by: Stamates <stamates@hotmail.com>

--- apps/explorer_web/assets/js/pages/address.js
+++ apps/explorer_web/assets/js/pages/address.js
@@ -7,6 +7,13 @@ if (window.page === 'address') {
     .receive('ok', resp => { console.log('Joined successfully', `addresses:${window.addressHash}`, resp) })
     .receive('error', resp => { console.log('Unable to join', `addresses:${window.addressHash}`, resp) })
 
+  const $emptyTransactionsList = $('[data-selector="empty-transactions-list"]')
+  if ($emptyTransactionsList) {
+    channel.on('transaction', () => {
+      window.location.reload()
+    })
+  }
+
   const $transactionsList = $('[data-selector="transactions-list"]')
   if ($transactionsList) {
     channel.on('transaction', (msg) => {
