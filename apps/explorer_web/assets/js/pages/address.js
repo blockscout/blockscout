42b262667b9511148113d440b93fab2f58b1b1e9
jimmay5469 <jimmay5469@gmail.com>
2018-06-25 10:49:44 -0400

    Handle channel disconnection
    Co-authored-by: Stamates <stamates@hotmail.com>

--- apps/explorer_web/assets/js/pages/address.js
+++ apps/explorer_web/assets/js/pages/address.js
@@ -5,9 +5,13 @@ import router from '../router'
 
 router.when('/addresses/:addressHash').then(({ addressHash, blockNumber, filter }) => {
   const channel = socket.channel(`addresses:${addressHash}`, {})
+  const $channelDisconnected = $('[data-selector="channel-disconnected-message"]')
   channel.join()
     .receive('ok', resp => { console.log('Joined successfully', `addresses:${addressHash}`, resp) })
     .receive('error', resp => { console.log('Unable to join', `addresses:${addressHash}`, resp) })
+  channel.onError(() => {
+    $channelDisconnected.show()
+  })
 
   if (!blockNumber) {
     const $emptyTransactionsList = $('[data-selector="empty-transactions-list"]')
@@ -20,6 +24,10 @@ router.when('/addresses/:addressHash').then(({ addressHash, blockNumber, filter 
     const $transactionsList = $('[data-selector="transactions-list"]')
     if ($transactionsList) {
       channel.on('transaction', (msg) => {
+        if($channelDisconnected.is(':visible')) {
+          return
+        }
+
         const {
           toAddressHash,
           fromAddressHash,
