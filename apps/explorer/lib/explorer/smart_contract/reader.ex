7474187a21f23ee5df933860f03852068f10e792
Lucas Narciso <lucasnar@gmail.com>
2018-06-25 18:05:56 -0300

    Create Explorer.SmartContract.Reader
    This new module is the main entry point for getting results of a smart
    contract function. It accepts a smart contract hash and a list of
    functions names and its arguments (when applicable) in order to execute
    those functions and return their results.
    
    Before actually making the call to the blockchain, the function name is
    encrypted and the arguments are encoded. The EthereumJSONRPC deals with
    this, encrypting the name of the function using the Keccak256 algorithm
    and encoding the arguments based on their type.
    
    The call to the blockchain is also made by EthereumJSONRPC.
    
    The result is then decoded - as it comes in the same type of encoding
    that has been sent - by the EthereumJSONRPC lib as well, and returned.
    The return is a list of results of calling the functions in the blockchain.
    Note that each function may return more than one result, so for each
    functions there is a list of results.

---
+++ apps/explorer/lib/explorer/smart_contract/reader.ex
@@ -0,0 +1,51 @@
+defmodule Explorer.SmartContract.Reader do
+  @moduledoc """
+  Reads Smart Contract functions from the blockchain.
+
+  For information on smart contract's Application Binary Interface (ABI), visit the
+  [wiki](https://github.com/ethereum/wiki/wiki/Ethereum-Contract-ABI).
+  """
+
+  alias Explorer.Chain
+  alias EthereumJSONRPC.Encoder
+
+  @doc """
+  Queries a contract function on the blockchain and returns the call result.
+
+  ## Examples
+
+  Note that for this example to work the database must be up to date with the
+  information available in the blockchain.
+
+  Explorer.SmartContract.Reader.query_contract(
+    "0x7e50612682b8ee2a8bb94774d50d6c2955726526",
+    %{"sum" => [20, 22]}
+  )
+  # => %{"sum" => [42]}
+  """
+  @spec query_contract(String.t(), %{String.t() => [term()]}) :: map()
+  def query_contract(contract_address, functions) do
+    abi =
+      contract_address
+      |> Chain.find_smart_contract()
+      |> Map.get(:abi)
+
+    abi
+    |> Encoder.encode_abi(functions)
+    |> Enum.map(&setup_call_payload(&1, contract_address))
+    |> EthereumJSONRPC.execute_contract_functions()
+    |> Encoder.decode_abi_results(abi, functions)
+  end
+
+  @doc """
+  Given the encoded data that references a function and its arguments in the blockchain, as well as the contract address, returns what EthereumJSONRPC.execute_contract_functions expects.
+  """
+  @spec setup_call_payload({%ABI.FunctionSelector{}, [term()]}, String.t()) :: map()
+  def setup_call_payload({function_name, data}, contract_address) do
+    %{
+      contract_address: contract_address,
+      data: data,
+      id: function_name
+    }
+  end
+end
