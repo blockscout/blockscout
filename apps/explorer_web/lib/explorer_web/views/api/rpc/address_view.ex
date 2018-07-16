cc059dac1eb4a06dd8646d9f0c9f21285abdd025
Sebastian Abondano <sabondano1@gmail.com>
2018-07-16 07:55:15 -0400

    Enhance API account balance endpoint
    Why:
    
    * Currently, the API account balance endpoint allows users to get the
    ether balance for a single address. This commit introduces support for
    getting the ether balance for multiple addresses (maximum of 20) in a
    single batch.
    * Issue link: https://github.com/poanetwork/poa-explorer/issues/138
    
    This change addresses the need by:
    
    * Adding `Chain.hashes_to_addresses/1` to get multiple addresses from
    the DB, given a list of hashes.
    * Editing `balance/2` in `API.RPC.AddressController` to support multiple
    addresses. Previously it only supported a single address.
    * Editing `API.RPC.AddressView` to handle a single address differently
    than multiple addresses. This is to mimic Etherscan's API.

--- apps/explorer_web/lib/explorer_web/views/api/rpc/address_view.ex
+++ apps/explorer_web/lib/explorer_web/views/api/rpc/address_view.ex
@@ -3,13 +3,28 @@ defmodule ExplorerWeb.API.RPC.AddressView do
 
   alias ExplorerWeb.API.RPC.RPCView
 
-  def render("balance.json", %{address: address}) do
-    ether_balance = format_wei_value(address.fetched_balance, :ether, include_unit_label: false)
-    data = ether_balance
+  def render("balance.json", %{addresses: [address]}) do
+    ether_balance = wei_to_ether(address.fetched_balance)
+    RPCView.render("show.json", data: ether_balance)
+  end
+
+  def render("balance.json", %{addresses: addresses}) do
+    data =
+      Enum.map(addresses, fn address ->
+        %{
+          "account" => "#{address.hash}",
+          "balance" => wei_to_ether(address.fetched_balance)
+        }
+      end)
+
     RPCView.render("show.json", data: data)
   end
 
   def render("error.json", %{error: error}) do
     RPCView.render("error.json", error: error)
+  end
+
+  defp wei_to_ether(wei) do
+    format_wei_value(wei, :ether, include_unit_label: false)
   end
 end
