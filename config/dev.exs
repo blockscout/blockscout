657edda04678d86178417ddedc81f47e1c247b87
Felipe Renan <felipe.renan@plataformatec.com.br>
2018-07-20 19:37:13 -0300

    Fix log in development environment
    IMHO, it's very helpful to see which query is being generated by Ecto
    and how long it's taking. For some reason it was disabled, I'm not sure
    if it was intentional or a mistake.
    
    If it was intentional, we can close this PR and keep just as reference
    for developers that want to enabled these logs.

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,4 @@
 use Mix.Config
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
-config :logger, level: :debug
+config :logger, :console, format: "[$level] $message\n", level: :debug
