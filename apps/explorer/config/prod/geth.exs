d9e37c44a62beafb8b6b77a8c2d66ee93f5383a4
Peter Romfeld <peter.romfeld.hk@gmail.com>
2018-07-25 15:18:21 +0800

    fix formatting

--- apps/explorer/config/prod/geth.exs
+++ apps/explorer/config/prod/geth.exs
@@ -5,9 +5,7 @@ config :explorer,
     transport: EthereumJSONRPC.HTTP,
     transport_options: [
       http: EthereumJSONRPC.HTTP.HTTPoison,
-      url:
-        System.get_env("ETHEREUM_JSONRPC_HTTP_URL") ||
-          "https://mainnet.infura.io/8lTvJTKmHPCHazkneJsY",
+      url: System.get_env("ETHEREUM_JSONRPC_HTTP_URL") || "https://mainnet.infura.io/8lTvJTKmHPCHazkneJsY",
       http_options: [recv_timeout: 60_000, timeout: 60_000, hackney: [pool: :ethereum_jsonrpc]]
     ],
     variant: EthereumJSONRPC.Geth
--- apps/indexer/config/dev/geth.exs
+++ apps/indexer/config/dev/geth.exs
@@ -6,9 +6,7 @@ config :indexer,
     transport: EthereumJSONRPC.HTTP,
     transport_options: [
       http: EthereumJSONRPC.HTTP.HTTPoison,
-      url:
-        System.get_env("ETHEREUM_JSONRPC_HTTP_URL") ||
-          "https://mainnet.infura.io/8lTvJTKmHPCHazkneJsY",
+      url: System.get_env("ETHEREUM_JSONRPC_HTTP_URL") || "https://mainnet.infura.io/8lTvJTKmHPCHazkneJsY",
       http_options: [recv_timeout: 60_000, timeout: 60_000, hackney: [pool: :ethereum_jsonrpc]]
     ],
     variant: EthereumJSONRPC.Geth
