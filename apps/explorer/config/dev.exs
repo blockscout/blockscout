2ddc1b9747d1078e37a8c678d165d3ae2d1c840b
Peter Romfeld <peter.romfeld.hk@gmail.com>
2018-07-25 14:36:10 +0800

    get geth_url from environment and fix `variant` for {dev,prod}.exs

--- apps/explorer/config/dev.exs
+++ apps/explorer/config/dev.exs
@@ -12,7 +12,15 @@ config :explorer, Explorer.Repo,
 
 import_config "dev.secret.exs"
 
-variant = System.get_env("ETHEREUM_JSONRPC_VARIANT") || "parity"
+variant =
+  if is_nil(System.get_env("ETHEREUM_JSONRPC_VARIANT")) do
+    "parity"
+  else
+    System.get_env("ETHEREUM_JSONRPC_VARIANT")
+    |> String.split(".")
+    |> List.last()
+    |> String.downcase()
+  end
 
 # Import variant specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
--- apps/explorer/config/dev/geth.exs
+++ apps/explorer/config/dev/geth.exs
@@ -5,7 +5,7 @@ config :explorer,
     transport: EthereumJSONRPC.HTTP,
     transport_options: [
       http: EthereumJSONRPC.HTTP.HTTPoison,
-      url: "https://mainnet.infura.io/8lTvJTKmHPCHazkneJsY",
+      url: System.get_env("GETH_URL") || "https://mainnet.infura.io/8lTvJTKmHPCHazkneJsY",
       http_options: [recv_timeout: 60_000, timeout: 60_000, hackney: [pool: :ethereum_jsonrpc]]
     ],
     variant: EthereumJSONRPC.Geth
