aec8c5da84820161dc55f8429abb380a4601e642
Felipe Renan <felipe.renan@plataformatec.com.br>
2018-07-26 16:55:28 -0300

    Format the amount of token transfers in the view

--- apps/explorer_web/lib/explorer_web/views/address_transaction_view.ex
+++ apps/explorer_web/lib/explorer_web/views/address_transaction_view.ex
@@ -21,4 +21,27 @@ defmodule ExplorerWeb.AddressTransactionView do
   def transaction_from_or_to_current_address?(transaction, address_hash) do
     transaction.from_address_hash == address_hash || transaction.to_address_hash == address_hash
   end
+
+  @doc """
+  Formats the given amount according to given decimals.
+
+  ## Examples
+
+  iex> ExplorerWeb.AddressTransactionView.formatted_token_amount(Decimal.new(20500000), 5)
+  "205"
+
+  iex> ExplorerWeb.AddressTransactionView.formatted_token_amount(Decimal.new(20500000), 7)
+  "2.05"
+
+  iex> ExplorerWeb.AddressTransactionView.formatted_token_amount(Decimal.new(205000), 12)
+  "0.000000205"
+
+  """
+  @spec formatted_token_amount(Decimal.t(), non_neg_integer()) :: String.t()
+  def formatted_token_amount(%Decimal{sign: sign, coef: coef, exp: exp}, decimals) do
+    sign
+    |> Decimal.new(coef, exp - decimals)
+    |> Decimal.reduce()
+    |> Decimal.to_string(:normal)
+  end
 end
