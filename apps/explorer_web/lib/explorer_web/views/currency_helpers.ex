265e5ff18817da2a8aa0f4a6a2ef2e08b51c00c6
Amanda Sposito <amanda.sposito@plataformatec.com.br>
2018-07-30 17:54:58 -0300

    Move formatted_token_amount to currency_helpers and rename it

--- apps/explorer_web/lib/explorer_web/views/currency_helpers.ex
+++ apps/explorer_web/lib/explorer_web/views/currency_helpers.ex
@@ -33,4 +33,34 @@ defmodule ExplorerWeb.CurrencyHelpers do
       _ -> nil
     end
   end
+
+  @spec format_integer_to_currency(non_neg_integer()) :: String.t()
+  def format_integer_to_currency(value) do
+    {:ok, formatted} = Cldr.Number.to_string(value, format: "#,##0")
+
+    formatted
+  end
+
+  @doc """
+  Formats the given amount according to given decimals.
+
+  ## Examples
+
+  iex> format_according_to_decimals(Decimal.new(20500000), 5)
+  "205"
+
+  iex> format_according_to_decimals(Decimal.new(20500000), 7)
+  "2.05"
+
+  iex> format_according_to_decimals(Decimal.new(205000), 12)
+  "0.000000205"
+
+  """
+  @spec format_according_to_decimals(Decimal.t(), non_neg_integer()) :: String.t()
+  def format_according_to_decimals(%Decimal{sign: sign, coef: coef, exp: exp}, decimals) do
+    sign
+    |> Decimal.new(coef, exp - decimals)
+    |> Decimal.reduce()
+    |> Decimal.to_string(:normal)
+  end
 end
