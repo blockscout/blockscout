3427873986466a247269a9d2c156cfb3722cf882
Luke Imhoff <luke.imhoff@dockyard.com>
2018-07-31 14:55:36 -0500

    {:ok, nil} -> {:error, :not_found} in handle_get_block_by_tag
    Fixes #490
    
    Compatibility with paritytech/parity-ethereum#8281, which fixed
    paritytech/parity-ethereum#8028 and made pending return be compliant
    with Ethereum docs.

---
+++ apps/ethereum_jsonrpc/test/ethereum_jsonrpc/mox_test.exs
@@ -0,0 +1,33 @@
+defmodule EthereumJSONRPC.MoxTest do
+  @moduledoc """
+  Tests that only work with `EthereumJSONRPC.Mox` because they need precise data back from the network that can't be
+  gotten reliably.
+  """
+
+  use ExUnit.Case, async: true
+
+  import EthereumJSONRPC.Case, only: [variant: 0]
+  import Mox
+
+  setup do
+    %{
+      json_rpc_named_arguments: [
+        transport: EthereumJSONRPC.Mox,
+        transport_options: [],
+        variant: variant()
+      ]
+    }
+  end
+
+  setup :verify_on_exit!
+
+  describe "fetch_block_number_by_tag/2" do
+    test "with pending with null result", %{json_rpc_named_arguments: json_rpc_named_arguments} do
+      expect(EthereumJSONRPC.Mox, :json_rpc, fn _json, _options ->
+        {:ok, nil}
+      end)
+
+      assert {:error, :not_found} = EthereumJSONRPC.fetch_block_number_by_tag("pending", json_rpc_named_arguments)
+    end
+  end
+end
