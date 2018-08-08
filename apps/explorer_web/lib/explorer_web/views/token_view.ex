e49079f833fb87cc607db363e8ae57fc0d20e636
Amanda Sposito <amanda.sposito@plataformatec.com.br>
2018-08-08 15:29:14 -0300

    Add total_supply? verification at Token Details Page

--- apps/explorer_web/lib/explorer_web/views/token_view.ex
+++ apps/explorer_web/lib/explorer_web/views/token_view.ex
@@ -1,9 +1,14 @@
 defmodule ExplorerWeb.TokenView do
   use ExplorerWeb, :view
 
-  def decimals?(nil), do: false
-  def decimals?(_), do: true
+  alias Explorer.Chain.Token
 
-  def token_name?(nil), do: false
-  def token_name?(_), do: true
+  def decimals?(%Token{decimals: nil}), do: false
+  def decimals?(%Token{decimals: _}), do: true
+
+  def token_name?(%Token{name: nil}), do: false
+  def token_name?(%Token{name: _}), do: true
+
+  def total_supply?(%Token{total_supply: nil}), do: false
+  def total_supply?(%Token{total_supply: _}), do: true
 end
