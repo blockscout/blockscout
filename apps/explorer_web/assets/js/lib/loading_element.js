189c6604f49595813a7c89c00738fe74fbf11ec6
katibest <katibest@gaslight.co>
2018-08-10 16:16:34 -0400

    Added spinner to verify publish button

---
+++ apps/explorer_web/assets/js/lib/loading_element.js
@@ -0,0 +1,9 @@
+import $ from 'jquery'
+
+$('button[data-loading="animation"]').click(event => {
+  const clickedButton = $(event.target)
+
+  clickedButton.addClass("d-none")
+  $('#loading').removeClass("d-none")
+
+})
