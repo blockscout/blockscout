3bc84fd597a23ea8742b5a3571245187be9c4805
Alex Garibay <alex.garibay@dockyard.com>
2018-08-16 12:03:25 -0500

    Add support for multiple return types in contracts

--- apps/block_scout_web/lib/block_scout_web/views/address_read_contract_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/address_read_contract_view.ex
@@ -6,4 +6,9 @@ defmodule BlockScoutWeb.AddressReadContractView do
   def queryable?(inputs), do: Enum.any?(inputs)
 
   def address?(type), do: type == "address"
+
+  def named_argument?(%{"name" => ""}), do: false
+  def named_argument?(%{"name" => nil}), do: false
+  def named_argument?(%{"name" => _}), do: true
+  def named_argument?(_), do: false
 end
