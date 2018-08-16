ad529737399b9d61a8c3de3dd9255448270792de
Amanda Sposito <amanda.sposito@plataformatec.com.br>
2018-08-16 11:40:12 -0300

    Alter loading_element.js to not hide the 'clickedElement'

--- apps/block_scout_web/assets/js/lib/loading_element.js
+++ apps/block_scout_web/assets/js/lib/loading_element.js
@@ -1,8 +1,5 @@
 import $ from 'jquery'
 
 $('button[data-loading="animation"]').click(event => {
-  const clickedButton = $(event.target)
-
-  clickedButton.addClass('d-none')
   $('#loading').removeClass('d-none')
 })
