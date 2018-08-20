063d7e7ab3ba38a6f929843bb84ff18ccbe2b7ad
Stamates <Stamates@users.noreply.github.com>
2018-08-20 10:34:47 -0400

    Add chain view test

--- apps/block_scout_web/test/block_scout_web/views/chain_view_test.exs
+++ apps/block_scout_web/test/block_scout_web/views/chain_view_test.exs
@@ -4,6 +4,20 @@ defmodule BlockScoutWeb.ChainViewTest do
   alias Explorer.ExchangeRates.Token
   alias BlockScoutWeb.ChainView
 
+  describe "encode_market_history_data/1" do
+    test "returns a JSON encoded market history data" do
+      market_history_data = [
+        %Explorer.Market.MarketHistory{
+          closing_price: Decimal.new("0.078"),
+          date: ~D[2018-08-20]
+        }
+      ]
+
+      assert "[{\"closing_price\":\"0.078\",\"date\":\"2018-08-20\"}]" ==
+               ChainView.encode_market_history_data(market_history_data)
+    end
+  end
+
   describe "format_exchange_rate/1" do
     test "returns a formatted usd value from a `Token`'s usd_value" do
       token = %Token{usd_value: Decimal.new(5.45)}
