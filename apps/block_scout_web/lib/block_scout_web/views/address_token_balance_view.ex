0ef7c4ce0c3b7a3c146b3d66ece533b97cf171e4
Felipe Renan <felipe.renan@plataformatec.com.br>
2018-08-28 09:21:41 -0300

    Refactor Token Balances dropdown to use database

--- apps/block_scout_web/lib/block_scout_web/views/address_token_balance_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/address_token_balance_view.ex
@@ -1,20 +1,20 @@
 defmodule BlockScoutWeb.AddressTokenBalanceView do
   use BlockScoutWeb, :view
 
-  def tokens_count_title(tokens) do
-    ngettext("%{count} token", "%{count} tokens", Enum.count(tokens))
+  def tokens_count_title(token_balances) do
+    ngettext("%{count} token", "%{count} tokens", Enum.count(token_balances))
   end
 
-  def filter_by_type(tokens, type) do
-    Enum.filter(tokens, &(&1.type == type))
+  def filter_by_type(token_balances, type) do
+    Enum.filter(token_balances, &(&1.token.type == type))
   end
 
   @doc """
   Sorts the given list of tokens in alphabetically order considering nil values in the bottom of
   the list.
   """
-  def sort_by_name(tokens) do
-    {unnamed, named} = Enum.split_with(tokens, &is_nil(&1.name))
-    Enum.sort_by(named, &String.downcase(&1.name)) ++ unnamed
+  def sort_by_name(token_balances) do
+    {unnamed, named} = Enum.split_with(token_balances, &is_nil(&1.token.name))
+    Enum.sort_by(named, &String.downcase(&1.token.name)) ++ unnamed
   end
 end
--- apps/block_scout_web/test/block_scout_web/views/address_token_balance_view_test.exs
+++ apps/block_scout_web/test/block_scout_web/views/address_token_balance_view_test.exs
@@ -5,26 +5,45 @@ defmodule BlockScoutWeb.AddressTokenBalanceViewTest do
 
   describe "sort_by_name/1" do
     test "sorts the given tokens by its name" do
-      token_a = build(:token, name: "token name")
-      token_b = build(:token, name: "token")
-      token_c = build(:token, name: "atoken")
+      token_balance_a = build(:token_balance, token: build(:token, name: "token name"))
+      token_balance_b = build(:token_balance, token: build(:token, name: "token"))
+      token_balance_c = build(:token_balance, token: build(:token, name: "atoken"))
 
-      assert AddressTokenBalanceView.sort_by_name([token_a, token_b, token_c]) == [token_c, token_b, token_a]
+      token_balances = [
+        token_balance_a,
+        token_balance_b,
+        token_balance_c
+      ]
+
+      expected = [token_balance_c, token_balance_b, token_balance_a]
+
+      assert AddressTokenBalanceView.sort_by_name(token_balances) == expected
     end
 
     test "considers nil values in the bottom of the list" do
-      token_a = build(:token, name: nil)
-      token_b = build(:token, name: "token name")
-      token_c = build(:token, name: "token")
+      token_balance_a = build(:token_balance, token: build(:token, name: nil))
+      token_balance_b = build(:token_balance, token: build(:token, name: "token name"))
+      token_balance_c = build(:token_balance, token: build(:token, name: "token"))
 
-      assert AddressTokenBalanceView.sort_by_name([token_a, token_b, token_c]) == [token_c, token_b, token_a]
+      token_balances = [
+        token_balance_a,
+        token_balance_b,
+        token_balance_c
+      ]
+
+      expected = [token_balance_c, token_balance_b, token_balance_a]
+
+      assert AddressTokenBalanceView.sort_by_name(token_balances) == expected
     end
 
     test "considers capitalization" do
-      token_a = build(:token, name: "Token")
-      token_b = build(:token, name: "atoken")
+      token_balance_a = build(:token_balance, token: build(:token, name: "Token"))
+      token_balance_b = build(:token_balance, token: build(:token, name: "atoken"))
 
-      assert AddressTokenBalanceView.sort_by_name([token_a, token_b]) == [token_b, token_a]
+      token_balances = [token_balance_a, token_balance_b]
+      expected = [token_balance_b, token_balance_a]
+
+      assert AddressTokenBalanceView.sort_by_name(token_balances) == expected
     end
   end
 end
