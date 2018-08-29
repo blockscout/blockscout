4a1031406e9a3bf591cbf077eb5f2b9ffca4fb97
Felipe Renan <felipe.renan@plataformatec.com.br>
2018-08-29 13:44:52 -0300

    Add tests to AddressTokenBalanceView functions

--- apps/block_scout_web/test/block_scout_web/views/address_token_balance_view_test.exs
+++ apps/block_scout_web/test/block_scout_web/views/address_token_balance_view_test.exs
@@ -3,6 +3,28 @@ defmodule BlockScoutWeb.AddressTokenBalanceViewTest do
 
   alias BlockScoutWeb.AddressTokenBalanceView
 
+  describe "tokens_count_title/1" do
+    test "returns the title pluralized" do
+      token_balances = [
+        build(:token_balance),
+        build(:token_balance)
+      ]
+
+      assert AddressTokenBalanceView.tokens_count_title(token_balances) == "2 tokens"
+    end
+  end
+
+  describe "filter_by_type/2" do
+    test "filter tokens by the given type" do
+      token_balance_a = build(:token_balance, token: build(:token, type: "ERC-20"))
+      token_balance_b = build(:token_balance, token: build(:token, type: "ERC-721"))
+
+      token_balances = [token_balance_a, token_balance_b]
+
+      assert AddressTokenBalanceView.filter_by_type(token_balances, "ERC-20") == [token_balance_a]
+    end
+  end
+
   describe "sort_by_name/1" do
     test "sorts the given tokens by its name" do
       token_balance_a = build(:token_balance, token: build(:token, name: "token name"))
