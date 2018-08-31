9e6198da1b576a5ce0086db8d64590853e2ed0c8
Luke Imhoff <luke.imhoff@dockyard.com>
2018-08-31 13:18:31 -0500

    Cover EthereumJSONRPC

--- apps/ethereum_jsonrpc/test/ethereum_jsonrpc_test.exs
+++ apps/ethereum_jsonrpc/test/ethereum_jsonrpc_test.exs
@@ -164,6 +164,217 @@ defmodule EthereumJSONRPCTest do
     end
   end
 
+  describe "fetch_block_by_hash/2" do
+    test "can fetch blocks", %{json_rpc_named_arguments: json_rpc_named_arguments} do
+      %{block_hash: block_hash, transaction_hash: transaction_hash} =
+        case Keyword.fetch!(json_rpc_named_arguments, :variant) do
+          EthereumJSONRPC.Parity ->
+            %{
+              block_hash: "0x29c850324e357f3c0c836d79860c5af55f7b651e5d7ee253c1af1b14908af49c",
+              transaction_hash: "0xa2e81bb56b55ba3dab2daf76501b50dfaad240cccb905dbf89d65c7a84a4a48e"
+            }
+
+          EthereumJSONRPC.Geth ->
+            %{
+              block_hash: "0xe065eed62c152c8c3dd14d6e5948e652c3e36a9cdb10b79853802ef9fa1d536c",
+              transaction_hash: "0x615506d9872bb07faa2ce17c02b902148eae88ccba0298902be6a0dbba1124de"
+            }
+        end
+
+      if json_rpc_named_arguments[:transport] == EthereumJSONRPC.Mox do
+        expect(EthereumJSONRPC.Mox, :json_rpc, fn _json, _options ->
+          block_number = "0x0"
+
+          {:ok,
+           [
+             %{
+               result: %{
+                 "difficulty" => "0x0",
+                 "gasLimit" => "0x0",
+                 "gasUsed" => "0x0",
+                 "hash" => block_hash,
+                 "miner" => "0x0",
+                 "number" => block_number,
+                 "parentHash" => "0x0",
+                 "size" => "0x0",
+                 "timestamp" => "0x0",
+                 "totalDifficulty" => "0x0",
+                 "transactions" => [
+                   %{
+                     "blockHash" => block_hash,
+                     "blockNumber" => block_number,
+                     "from" => "0x0",
+                     "gas" => "0x0",
+                     "gasPrice" => "0x0",
+                     "hash" => transaction_hash,
+                     "input" => "0x",
+                     "nonce" => "0x0",
+                     "r" => "0x0",
+                     "s" => "0x0",
+                     "to" => "0x0",
+                     "transactionIndex" => "0x0",
+                     "v" => "0x0",
+                     "value" => "0x0"
+                   }
+                 ]
+               }
+             }
+           ]}
+        end)
+      end
+
+      assert {:ok, %{blocks: [_ | _], transactions: [_ | _]}} =
+               EthereumJSONRPC.fetch_blocks_by_hash([block_hash], json_rpc_named_arguments)
+    end
+
+    test "returns errors with block hash in data", %{json_rpc_named_arguments: json_rpc_named_arguments} do
+      if json_rpc_named_arguments[:transport] == EthereumJSONRPC.Mox do
+        expect(EthereumJSONRPC.Mox, :json_rpc, fn _json, _options ->
+          {:ok,
+           [
+             %{
+               error: %{
+                 code: -32602,
+                 message: "Hash too short"
+               },
+               id: 0,
+               jsonrpc: "2.0"
+             }
+           ]}
+        end)
+      end
+
+      assert {:error, [%{data: %{hash: "0x0"}}]} =
+               EthereumJSONRPC.fetch_blocks_by_hash(["0x0"], json_rpc_named_arguments)
+    end
+
+    test "full batch errors are returned", %{json_rpc_named_arguments: json_rpc_named_arguments} do
+      # I'm not sure how to reliably trigger this on the real chains, so only do mox
+      moxed_json_rpc_named_arguments = Keyword.put(json_rpc_named_arguments, :transport, EthereumJSONRPC.Mox)
+
+      error = {:error, %{"message" => "methodNotSupported"}}
+
+      expect(EthereumJSONRPC.Mox, :json_rpc, fn _json, _options ->
+        error
+      end)
+
+      assert EthereumJSONRPC.fetch_blocks_by_hash(["0x0"], moxed_json_rpc_named_arguments) == error
+    end
+  end
+
+  describe "fetch_block_by_range/2" do
+    test "returns errors with block number in data", %{json_rpc_named_arguments: json_rpc_named_arguments} do
+      if json_rpc_named_arguments[:transport] == EthereumJSONRPC.Mox do
+        expect(EthereumJSONRPC.Mox, :json_rpc, fn _json, _options ->
+          {:ok,
+           [
+             %{
+               error: %{
+                 code: -32602,
+                 message: "Invalid params: Invalid block number: number too large to fit in target type."
+               },
+               id: 0,
+               jsonrpc: "2.0"
+             },
+             %{
+               error: %{
+                 code: -32602,
+                 message: "Invalid params: Invalid block number: number too large to fit in target type."
+               },
+               id: 1,
+               jsonrpc: "2.0"
+             }
+           ]}
+        end)
+      end
+
+      assert {:error,
+              [%{data: %{number: 1_000_000_000_000_000_000_001}}, %{data: %{number: 1_000_000_000_000_000_000_000}}]} =
+               EthereumJSONRPC.fetch_blocks_by_range(
+                 1_000_000_000_000_000_000_000..1_000_000_000_000_000_000_001,
+                 json_rpc_named_arguments
+               )
+    end
+
+    test "returns only errors if a mix of results and errors", %{json_rpc_named_arguments: json_rpc_named_arguments} do
+      # Can't be faked reliably on real chain
+      moxed_json_rpc_named_arguments = Keyword.put(json_rpc_named_arguments, :transport, EthereumJSONRPC.Mox)
+
+      expect(EthereumJSONRPC.Mox, :json_rpc, fn _json, _options ->
+        {:ok,
+         [
+           %{
+             error: %{
+               code: -32602,
+               message: "Invalid params: Invalid block number: number too large to fit in target type."
+             },
+             id: 0,
+             jsonrpc: "2.0"
+           },
+           %{
+             id: 1,
+             result: %{
+               "difficulty" => "0x0",
+               "gasLimit" => "0x0",
+               "gasUsed" => "0x0",
+               "hash" => "0x0",
+               "miner" => "0x0",
+               "number" => "0x0",
+               "parentHash" => "0x0",
+               "size" => "0x0",
+               "timestamp" => "0x0",
+               "totalDifficulty" => "0x0",
+               "transactions" => []
+             },
+             jsonrpc: "2.0"
+           }
+         ]}
+      end)
+
+      assert {:error, [%{data: %{number: 1_000_000_000_000_000_000_000}}]} =
+               EthereumJSONRPC.fetch_blocks_by_range(
+                 1_000_000_000_000_000_000_000..1_000_000_000_000_000_000_001,
+                 moxed_json_rpc_named_arguments
+               )
+    end
+
+    test "nil result indicated end-of-chain", %{json_rpc_named_arguments: json_rpc_named_arguments} do
+      # Can't be faked reliably on real chain
+      moxed_json_rpc_named_arguments = Keyword.put(json_rpc_named_arguments, :transport, EthereumJSONRPC.Mox)
+
+      expect(EthereumJSONRPC.Mox, :json_rpc, fn _json, _options ->
+        {:ok,
+         [
+           %{
+             id: 0,
+             result: %{
+               "difficulty" => "0x0",
+               "gasLimit" => "0x0",
+               "gasUsed" => "0x0",
+               "hash" => "0x0",
+               "miner" => "0x0",
+               "number" => "0x0",
+               "parentHash" => "0x0",
+               "size" => "0x0",
+               "timestamp" => "0x0",
+               "totalDifficulty" => "0x0",
+               "transactions" => []
+             },
+             jsonrpc: "2.0"
+           },
+           %{
+             result: nil,
+             id: 1,
+             jsonrpc: "2.0"
+           }
+         ]}
+      end)
+
+      assert {:ok, :end_of_chain, %{blocks: [_], transactions: []}} =
+               EthereumJSONRPC.fetch_blocks_by_range(0..1, moxed_json_rpc_named_arguments)
+    end
+  end
+
   describe "fetch_block_number_by_tag" do
     @tag capture_log: false
     test "with earliest", %{json_rpc_named_arguments: json_rpc_named_arguments} do
@@ -219,6 +430,113 @@ defmodule EthereumJSONRPCTest do
             assert number > 0
         end
       )
+    end
+
+    test "unknown errors are returned", %{json_rpc_named_arguments: json_rpc_named_arguments} do
+      # Can't be faked reliably on real chain
+      moxed_json_rpc_named_arguments = Keyword.put(json_rpc_named_arguments, :transport, EthereumJSONRPC.Mox)
+
+      unknown_error = {:error, %{"code" => 500, "message" => "Unknown error"}}
+
+      expect(EthereumJSONRPC.Mox, :json_rpc, fn _json, _options ->
+        unknown_error
+      end)
+
+      assert {:error, unknown_error} =
+               EthereumJSONRPC.fetch_block_number_by_tag("latest", moxed_json_rpc_named_arguments)
+    end
+  end
+
+  describe "fetch_pending_transactions/2" do
+    @tag :no_geth
+    test "pending transactions are returned", %{json_rpc_named_arguments: json_rpc_named_arguments} do
+      if json_rpc_named_arguments[:transport] == EthereumJSONRPC.Mox do
+        expect(EthereumJSONRPC.Mox, :json_rpc, fn _json, _options ->
+          {:ok,
+           [
+             %{
+               "blockHash" => nil,
+               "blockNumber" => nil,
+               "from" => "0x0",
+               "gas" => "0x0",
+               "gasPrice" => "0x0",
+               "hash" => "0x73c5599001f77bd570e32c4a5e63157200747910a502fae009821767c36b2ac9",
+               "input" => "0x",
+               "nonce" => "0x0",
+               "r" => "0x0",
+               "s" => "0x0",
+               "to" => "0x0",
+               "transactionIndex" => nil,
+               "v" => "0x0",
+               "value" => "0x0"
+             }
+           ]}
+        end)
+      end
+
+      assert {:ok, pending_transactions} = EthereumJSONRPC.fetch_pending_transactions(json_rpc_named_arguments)
+      # can't say more because there could be no pending transactions on test chains
+      assert is_list(pending_transactions)
+    end
+  end
+
+  describe "fetch_transaction_receipts/2" do
+    test "with invalid transaction hash", %{json_rpc_named_arguments: json_rpc_named_arguments} do
+      hash = "0x0000000000000000000000000000000000000000000000000000000000000000"
+
+      if json_rpc_named_arguments[:transport] == EthereumJSONRPC.Mox do
+        expect(EthereumJSONRPC.Mox, :json_rpc, fn _json, _options ->
+          {:ok, [%{id: 0, jsonrpc: "2.0", result: nil}]}
+        end)
+      end
+
+      assert {:error, [%{data: %{hash: ^hash}, message: "Not Found"}]} =
+               EthereumJSONRPC.fetch_transaction_receipts(
+                 [%{hash: hash, gas: "0x0"}],
+                 json_rpc_named_arguments
+               )
+    end
+
+    test "with valid transaction hash", %{json_rpc_named_arguments: json_rpc_named_arguments} do
+      hash =
+        case Keyword.fetch!(json_rpc_named_arguments, :variant) do
+          EthereumJSONRPC.Parity ->
+            "0xa2e81bb56b55ba3dab2daf76501b50dfaad240cccb905dbf89d65c7a84a4a48e"
+
+          EthereumJSONRPC.Geth ->
+            "0x615506d9872bb07faa2ce17c02b902148eae88ccba0298902be6a0dbba1124de"
+        end
+
+      if json_rpc_named_arguments[:transport] == EthereumJSONRPC.Mox do
+        expect(EthereumJSONRPC.Mox, :json_rpc, fn _json, _options ->
+          {:ok,
+           [
+             %{
+               id: 0,
+               jsonrpc: "2.0",
+               result: %{
+                 "blockHash" => "0x29c850324e357f3c0c836d79860c5af55f7b651e5d7ee253c1af1b14908af49c",
+                 "blockNumber" => "0x414911",
+                 "contractAddress" => nil,
+                 "cumulativeGasUsed" => "0x5208",
+                 "gasUsed" => "0x5208",
+                 "logs" => [],
+                 "logsBloom" =>
+                   "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+                 "root" => nil,
+                 "status" => "0x1",
+                 "transactionHash" => hash,
+                 "transactionIndex" => "0x0"
+               }
+             }
+           ]}
+        end)
+      end
+
+      assert {:ok, %{logs: logs, receipts: [_]}} =
+               EthereumJSONRPC.fetch_transaction_receipts([%{hash: hash, gas: "0x0"}], json_rpc_named_arguments)
+
+      assert is_list(logs)
     end
   end
 
@@ -376,6 +694,12 @@ defmodule EthereumJSONRPCTest do
       assert :ok = EthereumJSONRPC.unsubscribe(subscription)
 
       assert {:error, :not_found} = EthereumJSONRPC.unsubscribe(subscription)
+    end
+  end
+
+  describe "unique_request_id" do
+    test "returns integer" do
+      assert is_integer(EthereumJSONRPC.unique_request_id())
     end
   end
 
