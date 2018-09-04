b8716e21510a5d7c46a1b1f14ab2762be4e3878b
Igor Florian <igor.florian@plataformatec.com.br>
2018-09-04 15:52:27 -0300

    Remove locale from JS

--- apps/block_scout_web/assets/js/router.js
+++ apps/block_scout_web/assets/js/router.js
@@ -2,13 +2,10 @@ import Path from 'path-parser'
 import URI from 'urijs'
 import humps from 'humps'
 
-const { locale } = Path.createPath('/:locale').partialTest(window.location.pathname) || { locale: 'en' }
-
 export default {
-  locale,
   when (pattern, { exactPathMatch } = { exactPathMatch: false }) {
     return new Promise((resolve) => {
-      const path = Path.createPath(`/:locale${pattern}`)
+      const path = Path.createPath(pattern || '/')
       const match = exactPathMatch ? path.test(window.location.pathname) : path.partialTest(window.location.pathname)
       if (match) {
         const routeParams = humps.camelizeKeys(match)
