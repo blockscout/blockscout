9c1dc73912b64d32c206bde2f0b866169b8b23cd
jimmay5469 <jimmay5469@gmail.com>
2018-09-13 16:06:13 -0400

    Standardize internal transaction tiles

--- apps/block_scout_web/lib/block_scout_web/views/internal_transaction_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/internal_transaction_view.ex
@@ -1,17 +1,3 @@
 defmodule BlockScoutWeb.InternalTransactionView do
   use BlockScoutWeb, :view
-  @dialyzer :no_match
-
-  alias Explorer.Chain.{Address, InternalTransaction}
-
-  def create?(%InternalTransaction{type: :create}), do: true
-  def create?(_), do: false
-
-  # This is the address to be shown in the to field
-  def to_address_hash(%InternalTransaction{to_address_hash: nil, created_contract_address_hash: hash}), do: hash
-
-  def to_address_hash(%InternalTransaction{to_address_hash: hash}), do: hash
-
-  def to_address(%InternalTransaction{to_address: nil, created_contract_address: %Address{} = address}), do: address
-  def to_address(%InternalTransaction{to_address: %Address{} = address}), do: address
 end
