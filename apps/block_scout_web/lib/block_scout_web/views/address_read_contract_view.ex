3dee9d00317444feeaf5c76ad664c875a7b40c2c
Felipe Renan <felipe.renan@plataformatec.com.br>
2018-09-13 16:48:15 -0300

    Remove used partial and view helper functions
    This partial and these functions are not being used.

--- apps/block_scout_web/lib/block_scout_web/views/address_read_contract_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/address_read_contract_view.ex
@@ -6,12 +6,4 @@ defmodule BlockScoutWeb.AddressReadContractView do
   def queryable?(inputs), do: Enum.any?(inputs)
 
   def address?(type), do: type == "address"
-
-  def named_argument?(%{"name" => ""}), do: false
-  def named_argument?(%{"name" => nil}), do: false
-  def named_argument?(%{"name" => _}), do: true
-  def named_argument?(_), do: false
-
-  def values(values) when is_list(values), do: Enum.join(values, ",")
-  def values(value), do: value
 end
