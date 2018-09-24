931ff46fbc584d1ce1f6d18bb45959c05a49e2f9
mulili <mulili.nzuki@gmail.com>
2018-09-24 16:31:59 +0300

    fixed merge conflict and fixed number formatting

--- apps/block_scout_web/assets/js/lib/smart_contract/wei_ether_converter.js
+++ apps/block_scout_web/assets/js/lib/smart_contract/wei_ether_converter.js
@@ -8,18 +8,19 @@ const weiToEtherConverter = (element, event) => {
   const $conversionTextWei = $element.find('[data-conversion-text-wei]')
   const $conversionTextEth = $element.find('[data-conversion-text-eth]')
   const $conversionUnit = $element.find('[data-conversion-unit]')
-  let unitVal = new BigNumber($conversionUnit.html())
+  let unitVal = new BigNumber(numeral($conversionUnit.html()).value())
 
-  if (event.target.checked) {
-    $conversionTextWei.hide()
-    $conversionTextEth.show()
-    unitVal = unitVal.dividedBy(weiUnit).toNumber()
-    $conversionUnit.html(String(unitVal > 0 ? numeral(unitVal).format('0,0') : numeral(unitVal).format('0.0[00000]')))
-  } else {
-    $conversionTextWei.show()
-    $conversionTextEth.hide()
-    unitVal = unitVal.multipliedBy(weiUnit).toNumber()
-    $conversionUnit.html(String(numeral(unitVal).format('0,0')))
+    if (event.target.checked) {
+      $conversionTextWei.removeClass('d-inline-block').hide()
+      $conversionTextEth.removeClass('d-none').show()
+      unitVal = unitVal.dividedBy(weiUnit)
+      $conversionUnit.html(unitVal.toFixed() > 0 ? unitVal.toFixed() : numeral(unitVal).format('0[.000000000000000000]'))
+    } else {
+      $conversionTextWei.addClass('d-inline-block')
+      $conversionTextEth.addClass('d-none')
+      unitVal = unitVal.multipliedBy(weiUnit)
+      $conversionUnit.html(unitVal.toFixed())
+    }
   }
 }
 
