37800a6eefc944489c21d5fc1aacf565c21ebf8e
Felipe Renan <felipe.renan@plataformatec.com.br>
2018-10-02 14:24:45 -0300

    Refactor view to use configured envs
    Since we have different networks, this commit allows us to use the
    information below from the environmnet:
    
    * network title
    * subnetwork title
    * network icon
    * network logo
    
    If those configurations are not set in the env, we are going to use the
    default from POA.

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -3,6 +3,28 @@ defmodule BlockScoutWeb.LayoutView do
 
   alias BlockScoutWeb.SocialMedia
 
+  def network_icon_partial do
+    Keyword.get(application_config(), :network_icon) || "_network_icon.html"
+  end
+
+  def logo do
+    Keyword.get(application_config(), :logo) || "/images/poa_logo.svg"
+  end
+
+  def subnetwork_title do
+    Keyword.get(application_config(), :subnetwork) || "Sokol Testnet"
+  end
+
+  def app_title do
+    network_title = Keyword.get(application_config(), :network) || "POA"
+
+    gettext("%{subnetwork} %{network} Explorer", subnetwork: subnetwork_title(), network: network_title)
+  end
+
+  defp application_config do
+    Application.get_env(:block_scout_web, BlockScoutWeb.Chain)
+  end
+
   def configured_social_media_services do
     SocialMedia.links()
   end
--- apps/block_scout_web/test/block_scout_web/views/layout_view_test.exs
+++ apps/block_scout_web/test/block_scout_web/views/layout_view_test.exs
@@ -1,9 +1,69 @@
 defmodule BlockScoutWeb.LayoutViewTest do
-  use BlockScoutWeb.ConnCase, async: true
+  use BlockScoutWeb.ConnCase
 
   alias BlockScoutWeb.LayoutView
 
   test "configured_social_media_services/0" do
     assert length(LayoutView.configured_social_media_services()) > 0
+  end
+
+  setup do
+    on_exit(fn ->
+      Application.put_env(:block_scout_web, BlockScoutWeb.Chain, [])
+    end)
+  end
+
+  describe "network_icon_partial/0" do
+    test "use the enviroment icon when it's configured" do
+      Application.put_env(:block_scout_web, BlockScoutWeb.Chain, network_icon: "custom_icon")
+
+      assert LayoutView.network_icon_partial() == "custom_icon"
+    end
+
+    test "use the default icon when there is no env configured for it" do
+      assert LayoutView.network_icon_partial() == "_network_icon.html"
+    end
+  end
+
+  describe "logo/0" do
+    test "use the enviroment logo when it's configured" do
+      Application.put_env(:block_scout_web, BlockScoutWeb.Chain, logo: "custom/logo.png")
+
+      assert LayoutView.logo() == "custom/logo.png"
+    end
+
+    test "use the default logo when there is no env configured for it" do
+      assert LayoutView.logo() == "/images/poa_logo.svg"
+    end
+  end
+
+  describe "subnetwork_title/0" do
+    test "use the enviroment subnetwork title when it's configured" do
+      Application.put_env(:block_scout_web, BlockScoutWeb.Chain, subnetwork: "Subnetwork Test")
+
+      assert LayoutView.subnetwork_title() == "Subnetwork Test"
+    end
+
+    test "use the default subnetwork title when there is no env configured for it" do
+      assert LayoutView.subnetwork_title() == "Sokol Testnet"
+    end
+  end
+
+  describe "app_title/0" do
+    test "use the enviroment subnetwork title when it's configured" do
+      Application.put_env(:block_scout_web, BlockScoutWeb.Chain, subnetwork: "Subnetwork Test")
+
+      assert LayoutView.app_title() == "Subnetwork Test POA Explorer"
+    end
+
+    test "use the enviroment network title when it's configured" do
+      Application.put_env(:block_scout_web, BlockScoutWeb.Chain, network: "Custom")
+
+      assert LayoutView.app_title() == "Sokol Testnet Custom Explorer"
+    end
+
+    test "use the default subnetwork title when there is no env configured for it" do
+      assert LayoutView.app_title() == "Sokol Testnet POA Explorer"
+    end
   end
 end
