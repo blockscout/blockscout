3085edf6bd1b5a60d6880da8ba80c8abed055080
Felipe Renan <felipe.renan@plataformatec.com.br>
2018-10-03 16:45:06 -0300

    Create metatags to the Root page

--- apps/block_scout_web/lib/block_scout_web/views/chain_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/chain_view.ex
@@ -1,6 +1,8 @@
 defmodule BlockScoutWeb.ChainView do
   use BlockScoutWeb, :view
 
+  alias BlockScoutWeb.LayoutView
+
   def encode_market_history_data(market_history_data) do
     market_history_data
     |> Enum.map(fn day -> Map.take(day, [:closing_price, :date]) end)
