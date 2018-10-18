961a2fb03c9607fb091ce2f54cf598daa6de01e9
zachdaniel <zachary.s.daniel@gmail.com>
2018-10-18 12:14:13 -0400

    WIP: request coordinator + starting rolling window

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/application.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/application.ex
@@ -5,10 +5,19 @@ defmodule EthereumJSONRPC.Application do
 
   use Application
 
+  alias EthereumJSONRPC.{RollingWindow, TimeoutCounter, RequestCoordinator}
+
+  @rolling_window_opts [
+    bucket: :ethereum_jsonrpc_bucket,
+    window_length: :timer.seconds(10),
+    window_count: 6
+  ]
+
   @impl Application
   def start(_type, _args) do
     children = [
-      :hackney_pool.child_spec(:ethereum_jsonrpc, recv_timeout: 60_000, timeout: 60_000, max_connections: 1000)
+      :hackney_pool.child_spec(:ethereum_jsonrpc, recv_timeout: 60_000, timeout: 60_000, max_connections: 1000),
+      {RollingWindow, [RequestCoordinator.rolling_window_opts(), [name: TimeoutCounter]]}
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one, name: EthereumJSONRPC.Supervisor)
--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/request_coordinator.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/request_coordinator.ex
@@ -1,7 +1,35 @@
 defmodule EthereumJSONRPC.RequestCoordinator do
-  @failure_rate_limit_interval :timer.minutes(3)
-  @failure_rate_limit 30
+  @moduledoc """
+  Retries JSONRPC requests according to the provided retry_options
 
+  Leverages `EthereumJSONRPC.RollingWindow` to keep track of the count
+  of recent timeouts, and waits a small amount of time per timeout.
+
+  To see the rolling window options, see `EthereumJSONRPC.Application`
+  """
+  alias EthereumJSONRPC.{RollingWindow, TimeoutCounter}
+
+  @wait_per_timeout :timer.seconds(5)
+  @rolling_window_opts [
+    bucket: :ethereum_jsonrpc_bucket,
+    window_length: :timer.seconds(10),
+    window_count: 6
+  ]
+
+  @doc "Options used when initializing the RollingWindow used by this module."
+  @spec rolling_window_opts() :: Keyword.t()
+  def rolling_window_opts do
+    @rolling_window_opts
+  end
+
+  @doc """
+  Retries the request according to the provided retry_options
+
+  If none were provided, the request is not retried. In all cases, the request
+  waits an amount of time before proceeding based on the count of recent
+  failures.
+  """
+  @spec perform(term(), )
   def perform(request, named_arguments) do
     transport = Keyword.fetch!(named_arguments, :transport)
     transport_options = Keyword.fetch!(named_arguments, :transport_options)
@@ -11,37 +39,37 @@ defmodule EthereumJSONRPC.RequestCoordinator do
       retry_timeout = Keyword.get(retry_options, :retry_timeout, 5_000)
 
       fn ->
-        request_with_retry(transport, request, transport_options)
+        request(transport, request, transport_options, true)
       end
       |> Task.async()
-      |> Task.await()
+      |> Task.await(retry_timeout)
     else
-      request(transport, request, transport_options)
+      request(transport, request, transport_options, false)
     end
   end
 
-  defp request_with_retry(transport, request, transport_options) do
+  defp request(transport, request, transport_options, retry?) do
     key = something_that_uniquely_identifies_this_transport(transport, transport_options)
 
     sleep_if_too_many_recent_timeouts(key)
 
     case request(transport, request, transport_options) do
-      {:error, :timeout} ->
+      {:error, :timeout} = error ->
         increment_recent_timeouts(key)
 
-        request_with_retry(transport, request, transport_options)
+        if retry? do
+          request_with_retry(transport, request, transport_options)
+        else
+          error
+        end
 
       response ->
         response
     end
   end
 
-  defp request(transport, request, transport_options), do: transport.json_rpc(request, transport_options)
-
-  @spec increment_recent_timeouts(String.t()) :: :ok
   defp increment_recent_timeouts(key) do
-    # TODO: Call into rolling window rate limiter
-    # ExRated.check_rate(key, @failure_rate_limit_interval, @failure_rate_limit)
+    RollingWindow.inc(TimeoutCounter, key)
 
     :ok
   end
@@ -49,8 +77,7 @@ defmodule EthereumJSONRPC.RequestCoordinator do
   defp sleep_if_too_many_recent_timeouts(key) do
     wait_coefficient = count_of_recent_timeouts(key)
 
-    # TODO: Math TBD
-    :timer.sleep(:timer.seconds(wait_coefficient))
+    :timer.sleep(wait_coefficient * @wait_per_timeout)
   end
 
   defp something_that_uniquely_identifies_this_transport(transport, transport_options) do
@@ -58,11 +85,6 @@ defmodule EthereumJSONRPC.RequestCoordinator do
   end
 
   defp count_of_recent_timeouts(key) do
-    # if we are using ex_rated it looks like this:
-    # TODO: Call into rolling window rate limiter
-    # {count, _count_remaining, _ms_to_next_bucket, _created_at, _updated_at} =
-    #   ExRated.inspect_bucket(key, @failure_rate_limit_interval, @failure_rate_limit)
-
-    count
+    RollingWindow.count(TimeoutCounter, key)
   end
 end
