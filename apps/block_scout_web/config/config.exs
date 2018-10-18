4bf00363f42fd460fca546f231d23ee8e278ccf0
Andrew Cravenho <andrew@cravenho.me>
2018-10-18 17:14:02 -0400

    xDai Chain file changes

--- apps/block_scout_web/config/config.exs
+++ apps/block_scout_web/config/config.exs
@@ -14,7 +14,7 @@ config :block_scout_web, BlockScoutWeb.Chain,
   network: System.get_env("NETWORK"),
   subnetwork: System.get_env("SUBNETWORK"),
   network_icon: System.get_env("NETWORK_ICON"),
-  logo: System.get_env("LOGO")
+  logo: "/images/dai_logo.svg"
 
 # Configures the endpoint
 config :block_scout_web, BlockScoutWeb.Endpoint,
