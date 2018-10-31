67c68b6f416453eea691be66bbf9ef348f8449c2
Sebastian Abondano <sabondano1@gmail.com>
2018-10-31 14:54:46 -0400

    GraphQL API query to get addresses by hash
    Why:
    
    * We'd like to support GraphQL API queries for getting single or
    multiple addresses by hashes. API users could use this instead of the
    `balance` and `balancemulti` actions on the RPC API.
    
      Sample document:
      ```
       query ($hashes: [AddressHash!]!) {
         addresses(hashes: $hashes) {
           hash
           fetched_coin_balance
           fetched_coin_balance_block_number
           contract_code
         }
       }
      ```
    * Issue link: n/a
    
    This change addresses the need by:
    
    * Creating `BlockScoutWeb.Resolvers.Address` with a single resolver
    function that gets addresses by a list of hashes.
    * Adding `:data` scalar to `BlockScoutWeb.Schema.Scalars`.
    * Adding `address` object type to `BlockScoutWeb.Schema.Types`. Uses new
    `:data` scalar mentioned above.
    * Adding `addresses` field to query in `BlockScoutWeb.Schema`. Uses the
    new `address` object type and the resolver function mentioned above.
    * Editing `Abinthe.Plug` and `GraphiQL` in router to analyze complexity
    and set `max_complexity` to 50.

--- apps/block_scout_web/lib/block_scout_web/schema.ex
+++ apps/block_scout_web/lib/block_scout_web/schema.ex
@@ -3,11 +3,18 @@ defmodule BlockScoutWeb.Schema do
 
   use Absinthe.Schema
 
-  alias BlockScoutWeb.Resolvers.{Block, Transaction}
+  alias BlockScoutWeb.Resolvers.{Address, Block, Transaction}
 
   import_types(BlockScoutWeb.Schema.Types)
 
   query do
+    @desc "Gets addresses by address hash."
+    field :addresses, list_of(:address) do
+      arg(:hashes, non_null(list_of(non_null(:address_hash))))
+      resolve(&Address.get_by/3)
+      complexity(fn %{hashes: hashes}, child_complexity -> length(hashes) * child_complexity end)
+    end
+
     @desc "Gets a block by number."
     field :block, :block do
       arg(:number, non_null(:integer))
