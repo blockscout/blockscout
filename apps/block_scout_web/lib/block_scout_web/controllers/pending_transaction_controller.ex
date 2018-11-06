e39a776e19de6dcc44c9fe1b459f4b90044c186c
jimmay5469 <jimmay5469@gmail.com>
2018-11-06 15:24:26 -0500

    Infinite scroll on pending transactions page

--- apps/block_scout_web/lib/block_scout_web/controllers/pending_transaction_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/pending_transaction_controller.ex
@@ -3,7 +3,59 @@ defmodule BlockScoutWeb.PendingTransactionController do
 
   import BlockScoutWeb.Chain, only: [paging_options: 1, next_page_params: 3, split_list_by_page: 1]
 
+  alias BlockScoutWeb.TransactionView
   alias Explorer.Chain
+  alias Explorer.Chain.Hash
+  alias Phoenix.View
+
+  def index(conn, %{"type" => "JSON"} = params) do
+    full_options =
+      Keyword.merge(
+        [
+          necessity_by_association: %{
+            [from_address: :names] => :optional,
+            [to_address: :names] => :optional
+          }
+        ],
+        paging_options(params)
+      )
+
+    transactions_plus_one = Chain.recent_pending_transactions(full_options)
+
+    {transactions, next_page} = split_list_by_page(transactions_plus_one)
+
+    next_page_url =
+      case next_page_params(next_page, transactions, params) do
+        nil ->
+          nil
+
+        next_page_params ->
+          pending_transaction_path(
+            conn,
+            :index,
+            next_page_params
+          )
+      end
+
+    json(
+      conn,
+      %{
+        pending_transactions:
+          Enum.map(transactions, fn transaction ->
+            %{
+              transaction_hash: Hash.to_string(transaction.hash),
+              transaction_html:
+                View.render_to_string(
+                  TransactionView,
+                  "_tile.html",
+                  transaction: transaction
+                )
+            }
+          end),
+        next_page_url: next_page_url
+      }
+    )
+  end
 
   def index(conn, params) do
     full_options =
@@ -14,7 +66,7 @@ defmodule BlockScoutWeb.PendingTransactionController do
             [to_address: :names] => :optional
           }
         ],
-        paging_options(params)
+        paging_options(%{})
       )
 
     transactions_plus_one = Chain.recent_pending_transactions(full_options)
