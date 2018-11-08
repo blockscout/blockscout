8cdf2258ffcfe308e0945168dea4645c0570c693
Andrew Cravenho <andrew@cravenho.me>
2018-11-07 19:28:24 -0500

    Fix layout test

--- apps/block_scout_web/test/block_scout_web/views/layout_view_test.exs
+++ apps/block_scout_web/test/block_scout_web/views/layout_view_test.exs
@@ -33,7 +33,7 @@ defmodule BlockScoutWeb.LayoutViewTest do
     end
 
     test "use the default logo when there is no env configured for it" do
-      assert LayoutView.logo() == "/images/poa_logo.svg"
+      assert LayoutView.logo() == "/images/blockscout_logo.svg"
     end
   end
 
