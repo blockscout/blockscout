0c8c1eef6e419e839c558583232b05b09e7df01d
Andrew Cravenho <andrew@cravenho.me>
2018-11-12 15:33:11 -0500

    Kovan updates

--- apps/indexer/lib/indexer/block/catchup/fetcher.ex
+++ apps/indexer/lib/indexer/block/catchup/fetcher.ex
@@ -17,8 +17,8 @@ defmodule Indexer.Block.Catchup.Fetcher do
   # These are all the *default* values for options.
   # DO NOT use them directly in the code.  Get options from `state`.
 
-  @blocks_batch_size 10
-  @blocks_concurrency 10
+  @blocks_batch_size 3
+  @blocks_concurrency 3
   @sequence_name :block_catchup_sequencer
 
   defstruct blocks_batch_size: @blocks_batch_size,
