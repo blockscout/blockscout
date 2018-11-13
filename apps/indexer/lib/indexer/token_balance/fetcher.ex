92d528b1bb5f9f909e0d92fb060e42c0934ad795
Andrew Cravenho <andrew@cravenho.me>
2018-11-13 11:07:14 -0500

    Indexer update

--- apps/indexer/lib/indexer/token_balance/fetcher.ex
+++ apps/indexer/lib/indexer/token_balance/fetcher.ex
@@ -13,8 +13,8 @@ defmodule Indexer.TokenBalance.Fetcher do
 
   @defaults [
     flush_interval: 300,
-    max_batch_size: 100,
-    max_concurrency: 10,
+    max_batch_size: 20,
+    max_concurrency: 5,
     task_supervisor: Indexer.TokenBalance.TaskSupervisor
   ]
 
