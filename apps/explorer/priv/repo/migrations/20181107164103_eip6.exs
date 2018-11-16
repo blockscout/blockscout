5990a8a7958a070aa341b5d8b19a02ec23c74f64
Luke Imhoff <luke.imhoff@dockyard.com>
2018-11-16 10:56:52 -0600

    Separate schema and data migration for EIP-6
    Use `NOT VALID` on constraint and don't put data updates in schema
    migration.  Instead, migrate data using script based on @amandasposito's
    scripts.

--- apps/explorer/priv/repo/migrations/20181107164103_eip6.exs
+++ apps/explorer/priv/repo/migrations/20181107164103_eip6.exs
@@ -1,19 +1,26 @@
 defmodule Explorer.Repo.Migrations.EIP6 do
+  @moduledoc """
+  Use `priv/repo/migrations/scripts/20181107164103_eip6.sql` to migrate data and validate constraint
+
+  ```
+  mix ecto.migrate
+  psql -d $DATABASE -a -f priv/repo/migrations/scripts/20181107164103_eip6.sql
+  ```
+  """
+
   use Ecto.Migration
 
   def up do
     execute("ALTER TABLE internal_transactions DROP CONSTRAINT suicide_has_from_and_to_address_hashes")
 
-    create(
-      constraint(
-        :internal_transactions,
-        :selfdestruct_or_suicide_has_from_and_to_address_hashes,
-        check: """
-        (type != 'selfdestruct' AND type != 'suicide') OR
-        (from_address_hash IS NOT NULL AND gas IS NULL AND to_address_hash IS NOT NULL)
-        """
-      )
-    )
+    # `NOT VALID` skips checking pre-existing rows. Use `priv/repo/migrations/scripts/20181107164103_eip6.sql` to
+    # migrate data and validate constraints
+    execute("""
+    ALTER TABLE internal_transactions
+    ADD CONSTRAINT selfdestruct_has_from_and_to_address
+    CHECK (type != 'selfdestruct' OR (from_address_hash IS NOT NULL AND gas IS NULL AND to_address_hash IS NOT NULL))
+    NOT VALID
+    """)
   end
 
   def down do
