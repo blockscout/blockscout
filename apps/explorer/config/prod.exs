af4c17cff100d7d69bb38c9b066c3556698affb8
Andrew Cravenho <andrew@cravenho.me>
2018-11-18 13:54:37 -0500

    Goerli Production changes

--- apps/explorer/config/prod.exs
+++ apps/explorer/config/prod.exs
@@ -22,7 +22,7 @@ config :logger, :reading_token_functions,
 
 variant =
   if is_nil(System.get_env("ETHEREUM_JSONRPC_VARIANT")) do
-    "parity"
+    "geth"
   else
     System.get_env("ETHEREUM_JSONRPC_VARIANT")
     |> String.split(".")
