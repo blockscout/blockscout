2ad1042de208182bcc83e8f780966b019530b19c
Felipe Renan <felipe.renan@plataformatec.com.br>
2018-11-21 17:17:48 -0200

    Add script to migrate block_number to token transfers
    This script copy block_number from the transactions' table to token
    transfers' table in batches.

--- apps/explorer/priv/repo/migrations/20181121170616_add_block_number_to_token_transfers.exs
+++ apps/explorer/priv/repo/migrations/20181121170616_add_block_number_to_token_transfers.exs
@@ -1,4 +1,13 @@
 defmodule Explorer.Repo.Migrations.AddBlockNumberToTokenTransfers do
+  @moduledoc """
+  Use `priv/repo/migrations/scripts/20181121170616_token_transfers_update_block_number_in_batches.sql` to migrate data.
+
+  ```sh
+  mix ecto.migrate
+  psql -d $DATABASE -a -f priv/repo/migrations/scripts/20181121170616_token_transfers_update_block_number_in_batches.sql
+  ```
+  """
+
   use Ecto.Migration
 
   def change do
