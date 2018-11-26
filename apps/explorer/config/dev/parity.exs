c1d1ad996ccdb965cc45a324e2fe51e16dfda2c7
Andrew Cravenho <andrew@cravenho.me>
2018-11-26 14:21:02 -0500

    Change the sokol node to localhost

--- apps/explorer/config/dev/parity.exs
+++ apps/explorer/config/dev/parity.exs
@@ -5,11 +5,11 @@ config :explorer,
     transport: EthereumJSONRPC.HTTP,
     transport_options: [
       http: EthereumJSONRPC.HTTP.HTTPoison,
-      url: System.get_env("ETHEREUM_JSONRPC_HTTP_URL") || "http://54.234.12.105:8545",
+      url: System.get_env("ETHEREUM_JSONRPC_HTTP_URL") || "http://localhost:8545",
       method_to_url: [
-        eth_call: System.get_env("ETHEREUM_JSONRPC_TRACE_URL") || "http://54.234.12.105:8545",
-        eth_getBalance: System.get_env("ETHEREUM_JSONRPC_TRACE_URL") || "http://54.234.12.105:8545",
-        trace_replayTransaction: System.get_env("ETHEREUM_JSONRPC_TRACE_URL") || "http://54.234.12.105:8545"
+        eth_call: System.get_env("ETHEREUM_JSONRPC_TRACE_URL") || "http://localhost:8545",
+        eth_getBalance: System.get_env("ETHEREUM_JSONRPC_TRACE_URL") || "http://localhost:8545",
+        trace_replayTransaction: System.get_env("ETHEREUM_JSONRPC_TRACE_URL") || "http://localhost:8545"
       ],
       http_options: [recv_timeout: 60_000, timeout: 60_000, hackney: [pool: :ethereum_jsonrpc]]
     ],
@@ -19,7 +19,7 @@ config :explorer,
     transport: EthereumJSONRPC.WebSocket,
     transport_options: [
       web_socket: EthereumJSONRPC.WebSocket.WebSocketClient,
-      url: System.get_env("ETHEREUM_JSONRPC_WS_URL") || "ws://54.234.12.105:8546"
+      url: System.get_env("ETHEREUM_JSONRPC_WS_URL") || "ws://localhost:8546"
     ],
     variant: EthereumJSONRPC.Parity
   ]
