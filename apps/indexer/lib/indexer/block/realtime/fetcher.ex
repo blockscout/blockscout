6a4a4982320baeac666a7abbda51a151f8bd23c3
Luke Imhoff <luke.imhoff@dockyard.com>
2018-12-03 14:47:09 -0600

    Push realtime fetcher and step into logger metadata

--- apps/indexer/lib/indexer/block/realtime/fetcher.ex
+++ apps/indexer/lib/indexer/block/realtime/fetcher.ex
@@ -131,8 +131,14 @@ defmodule Indexer.Block.Realtime.Fetcher do
 
     for block_number_to_fetch <- start_at..number do
       args = [block_number_to_fetch, block_fetcher, reorg?(number, max_number_seen)]
-      Task.Supervisor.start_child(TaskSupervisor, __MODULE__, :fetch_and_import_block, args)
+      Task.Supervisor.start_child(TaskSupervisor, __MODULE__, :log_fetch_and_import_block, args)
     end
+  end
+
+  def log_fetch_and_import_block(block_number_to_fetch, block_fetcher, retry \\ 3) do
+    Indexer.Logger.metadata([fetcher: :block_realtime, block_number: block_number_to_fetch], fn ->
+      fetch_and_import_block(block_number_to_fetch, block_fetcher, retry)
+    end)
   end
 
   defp determine_start_at(number, nil, nil), do: number
@@ -156,13 +162,12 @@ defmodule Indexer.Block.Realtime.Fetcher do
   @reorg_delay 5_000
 
   @decorate trace(name: "fetch", resource: "Indexer.Block.Realtime.Fetcher.fetch_and_import_block/3", tracer: Tracer)
-  def fetch_and_import_block(block_number_to_fetch, block_fetcher, reorg?, retry \\ 3) do
+  def fetch_and_import_block(block_number_to_fetch, block_fetcher, reorg?, retry) do
     if reorg? do
       # give previous fetch attempt (for same block number) a chance to finish
       # before fetching again, to reduce block consensus mistakes
       :timer.sleep(@reorg_delay)
     end
-
     do_fetch_and_import_block(block_number_to_fetch, block_fetcher, retry)
   end
 
@@ -170,16 +175,12 @@ defmodule Indexer.Block.Realtime.Fetcher do
   defp do_fetch_and_import_block(block_number_to_fetch, block_fetcher, retry) do
     case fetch_and_import_range(block_fetcher, block_number_to_fetch..block_number_to_fetch) do
       {:ok, %{inserted: _, errors: []}} ->
-        Logger.debug(fn ->
-          ["realtime indexer fetched and imported block ", to_string(block_number_to_fetch)]
-        end)
+        Logger.debug(fn -> "fetched and imported" end)
 
       {:ok, %{inserted: _, errors: [_ | _] = errors}} ->
         Logger.error(fn ->
           [
-            "realtime indexer failed to fetch block",
-            to_string(block_number_to_fetch),
-            ": ",
+            "failed to fetch: ",
             inspect(errors),
             ".  Block will be retried by catchup indexer."
           ]
@@ -196,9 +197,7 @@ defmodule Indexer.Block.Realtime.Fetcher do
         if retry_fetch_and_import_block(params) == :ignore do
           Logger.error(fn ->
             [
-              "realtime indexer failed to validate for block ",
-              to_string(block_number_to_fetch),
-              ": ",
+              "failed to validate: ",
               inspect(changesets),
               ".  Block will be retried by catchup indexer."
             ]
@@ -208,28 +207,20 @@ defmodule Indexer.Block.Realtime.Fetcher do
       {:error, {step, reason}} ->
         Logger.error(fn ->
           [
-            "realtime indexer failed to fetch ",
-            to_string(step),
-            " for block ",
-            to_string(block_number_to_fetch),
-            ": ",
+            "failed step: ",
             inspect(reason),
             ".  Block will be retried by catchup indexer."
           ]
-        end)
+        end, step: step)
 
       {:error, {step, failed_value, _changes_so_far}} ->
         Logger.error(fn ->
           [
-            "realtime indexer failed to insert ",
-            to_string(step),
-            " for block ",
-            to_string(block_number_to_fetch),
-            ": ",
+            "failed to insert: ",
             inspect(failed_value),
             ".  Block will be retried by catchup indexer."
           ]
-        end)
+        end, step: step)
     end
   end
 
