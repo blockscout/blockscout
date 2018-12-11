0a430d93098a080aad1e2a4ff43e7e107e3e0039
William Sanches <william.sanches@plataformatec.com.br>
2018-12-11 18:14:47 -0200

    Update FetchedBeneficiary typespec

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/fetched_beneficiary.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/fetched_beneficiary.ex
@@ -3,6 +3,15 @@ defmodule EthereumJSONRPC.FetchedBeneficiary do
   A single balance request params for the beneficiary of a block.
   """
 
-  @type params :: %{address_hash: EthereumJSONRPC.hash(), block_number: non_neg_integer()}
+  alias Explorer.Chain.Block.Reward.AddressType
+  alias Explorer.Chain.Hash
+
+  @type params :: %{
+          address_hash: EthereumJSONRPC.hash(),
+          address_type: AddressType.t(),
+          block_hash: Hash.Full.t(),
+          block_number: non_neg_integer(),
+          reward: String.t()
+        }
   @type error :: %{code: integer(), message: String.t(), data: %{block_number: non_neg_integer()}}
 end
