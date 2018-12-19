616658adbf7d221461a562c0bdd2f7b548688295
fvictorio <victorio.franco@gmail.com>
2018-12-19 19:01:28 -0300

    Show token price in USD

--- apps/block_scout_web/lib/block_scout_web/controllers/tokens/read_contract_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/tokens/read_contract_controller.ex
@@ -1,7 +1,7 @@
 defmodule BlockScoutWeb.Tokens.ReadContractController do
   use BlockScoutWeb, :controller
 
-  alias Explorer.Chain
+  alias Explorer.{Chain, Market}
 
   def index(conn, %{"token_id" => address_hash_string}) do
     with {:ok, address_hash} <- Chain.string_to_address_hash(address_hash_string),
@@ -9,7 +9,7 @@ defmodule BlockScoutWeb.Tokens.ReadContractController do
       render(
         conn,
         "index.html",
-        token: token,
+        token: Market.add_price(token),
         holders_count_consolidation_enabled: Chain.token_holders_counter_consolidation_enabled?(),
         total_token_transfers: Chain.count_token_transfers_from_token_hash(address_hash),
         total_token_holders: Chain.count_token_holders_from_token_hash(address_hash)
