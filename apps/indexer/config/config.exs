b6cca3ea8fc1c6c895704a71061f0feb6689ae6c
Andrew Cravenho <andrew@cravenho.me>
2019-01-03 20:49:40 -0500

    Update memory usage

--- apps/indexer/config/config.exs
+++ apps/indexer/config/config.exs
@@ -9,7 +9,7 @@ config :indexer,
   ecto_repos: [Explorer.Repo],
   metadata_updater_days_interval: 7,
   # bytes
-  memory_limit: 10 <<< 30
+  memory_limit: 6 <<< 30
 
 config :indexer, Indexer.Tracer,
   service: :indexer,
