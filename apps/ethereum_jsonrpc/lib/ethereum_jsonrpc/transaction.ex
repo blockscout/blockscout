62777c078f3a145658a584e5c11eb52cd54798cb
Luke Imhoff <Kronic.Deth@gmail.com>
2019-01-09 21:35:35 +0200

    Update apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/transaction.ex
    Co-Authored-By: konstantinzolotarev <konstantin.zolotarev@gmail.com>

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/transaction.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/transaction.ex
@@ -312,7 +312,7 @@ defmodule EthereumJSONRPC.Transaction do
   end
 
   # as always ganache has it's own vision on JSON RPC standard
-  defp entry_to_elixir({key, quantity}) when key in ~w(r s v) and quantity == nil do
+  defp entry_to_elixir({key, nil}) when key in ~w(r s v) do
     {key, 0}
   end
 
