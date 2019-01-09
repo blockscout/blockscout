76ea93f735ae914cdf1ca8bf619328d1c511a6f4
zachdaniel <zachary.s.daniel@gmail.com>
2019-01-09 16:26:19 -0500

    feat: allow deferring js to specific pages

--- apps/block_scout_web/assets/webpack.config.js
+++ apps/block_scout_web/assets/webpack.config.js
@@ -1,56 +1,85 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const glob = require("glob");
 
-module.exports = {
-  entry: './js/app.js',
-  output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, '../priv/static/js')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          use: [{
-            loader: "css-loader"
-          }, {
-            loader: "postcss-loader"
-          }, {
-            loader: "sass-loader",
+const viewScripts = glob.sync('./js/view_specific/**/*.js');
+
+function transpileViewScript(file) {
+  return {
+    entry: file,
+    output: {
+      filename: file.replace('./js/view_specific/', ''),
+      path: path.resolve(__dirname, '../priv/static/js')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+      ]
+    }
+  }
+};
+
+
+
+const appJs =
+  {
+    entry: './js/app.js',
+    output: {
+      filename: 'app.js',
+      path: path.resolve(__dirname, '../priv/static/js')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            use: [{
+              loader: "css-loader"
+            }, {
+              loader: "postcss-loader"
+            }, {
+              loader: "sass-loader",
+              options: {
+                precision: 8,
+                includePaths: [
+                  'node_modules/bootstrap/scss',
+                  'node_modules/@fortawesome/fontawesome-free/scss'
+                ]
+              }
+            }],
+            fallback: 'style-loader'
+          })
+        }, {
+          test: /\.(svg|ttf|eot|woff|woff2)$/,
+          use: {
+            loader: 'file-loader',
             options: {
-              precision: 8,
-              includePaths: [
-                'node_modules/bootstrap/scss',
-                'node_modules/@fortawesome/fontawesome-free/scss'
-              ]
+              name: '[name].[ext]',
+              outputPath: '../fonts/',
+              publicPath: '../fonts/'
             }
-          }],
-          fallback: 'style-loader'
-        })
-      }, {
-        test: /\.(svg|ttf|eot|woff|woff2)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: '../fonts/',
-            publicPath: '../fonts/'
           }
         }
-      }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('../css/app.css'),
+      new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
     ]
-  },
-  plugins: [
-    new ExtractTextPlugin('../css/app.css'),
-    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
-  ]
-};
+  }
+
+module.exports = viewScripts.map(transpileViewScript).concat(appJs);
