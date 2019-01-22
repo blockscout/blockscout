fad8ec74dd67720260f3a738a1322d08ef663b36
Nam Chu Hoai <nambrot@googlemail.com>
2019-01-22 13:41:46 -0800

    Readd cowboy

--- apps/indexer/mix.exs
+++ apps/indexer/mix.exs
@@ -44,6 +44,7 @@ defmodule Indexer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:plug_cowboy, "~> 2.0"},
       # Optional dependency of `:spandex` for `Spandex.Decorators`
       {:decorator, "~> 1.2"},
       # JSONRPC access to Parity for `Explorer.Indexer`
