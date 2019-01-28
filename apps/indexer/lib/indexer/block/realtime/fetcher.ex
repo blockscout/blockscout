2d9f36f253f69ded14de0bf404283b394c467d92
Andrew Cravenho <andrew@cravenho.me>
2019-01-28 11:36:45 -0500

    Update polling max number

--- apps/indexer/lib/indexer/block/realtime/fetcher.ex
+++ apps/indexer/lib/indexer/block/realtime/fetcher.ex
@@ -32,7 +32,7 @@ defmodule Indexer.Block.Realtime.Fetcher do
 
   @behaviour Block.Fetcher
 
-  @minimum_safe_polling_period :timer.seconds(10)
+  @minimum_safe_polling_period :timer.seconds(60)
 
   @enforce_keys ~w(block_fetcher)a
   defstruct ~w(block_fetcher subscription previous_number max_number_seen timer)a
