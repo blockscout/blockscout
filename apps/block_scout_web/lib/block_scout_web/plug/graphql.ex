ecf2602eb65fce301995fc30212d9a36d16372f0
Ayrat Badykov <ayratin555@gmail.com>
2019-02-12 16:55:15 +0300

    fix credo

--- apps/block_scout_web/lib/block_scout_web/plug/graphql.ex
+++ apps/block_scout_web/lib/block_scout_web/plug/graphql.ex
@@ -1,4 +1,7 @@
 defmodule BlockScoutWeb.Plug.GraphQL do
+  @moduledoc """
+  Default query for GraphiQL interface.
+  """
   @default_transaction_hash "0x69e3923eef50eada197c3336d546936d0c994211492c9f947a24c02827568f9f"
 
   def default_query do
