72e38aab393a48f77cbe5d7147d96d03b062f810
Andrew Cravenho <andrew@cravenho.me>
2019-02-15 09:32:08 -0500

    Remove up/0 and down/0 add alter to change/0

--- apps/explorer/priv/repo/migrations/20190215105501_add_constructor_arguments_to_smart_contracts.exs
+++ apps/explorer/priv/repo/migrations/20190215105501_add_constructor_arguments_to_smart_contracts.exs
@@ -1,12 +1,8 @@
 defmodule Explorer.Repo.Migrations.AddConstructorArgumentsToSmartContracts do
   use Ecto.Migration
 
-  def up do
-    # Check for the existence of constructor arguments and remove
+  def change do
     execute("ALTER TABLE smart_contracts DROP COLUMN IF EXISTS constructor_arguments")
-  end
-
-  def down do
     alter table(:smart_contracts) do
       add(:constructor_arguments, :string, null: true)
     end
