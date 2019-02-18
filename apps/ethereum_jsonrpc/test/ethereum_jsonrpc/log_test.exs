d1ec793889de9d3cb1867aba7b6a03e7f293bcd7
Ayrat Badykov <ayratin555@gmail.com>
2019-02-18 10:39:28 +0300

    mix format

--- apps/ethereum_jsonrpc/test/ethereum_jsonrpc/log_test.exs
+++ apps/ethereum_jsonrpc/test/ethereum_jsonrpc/log_test.exs
@@ -14,9 +14,11 @@ defmodule EthereumJSONRPC.LogTest do
         "data" => "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563",
         "logIndex" => "0x0",
         "removed" => false,
-        "topics" => ["0xadc1e8a294f8415511303acc4a8c0c5906c7eb0bf2a71043d7f4b03b46a39130",
-                     "0x000000000000000000000000c15bf627accd3b054075c7880425f903106be72a",
-                     "0x000000000000000000000000a59eb37750f9c8f2e11aac6700e62ef89187e4ed"],
+        "topics" => [
+          "0xadc1e8a294f8415511303acc4a8c0c5906c7eb0bf2a71043d7f4b03b46a39130",
+          "0x000000000000000000000000c15bf627accd3b054075c7880425f903106be72a",
+          "0x000000000000000000000000a59eb37750f9c8f2e11aac6700e62ef89187e4ed"
+        ],
         "transactionHash" => "0xf9b663b4e9b1fdc94eb27b5cfba04eb03d2f7b3fa0b24eb2e1af34f823f2b89e",
         "transactionIndex" => "0x0"
       }
