f2821cd7f8d9bd292d19605cacd6943c38189508
Victor Baranov <baranov.viktor.27@gmail.com>
2019-02-22 13:23:30 +0300

    xDai v1.3.4

--- apps/indexer/config/config.exs
+++ apps/indexer/config/config.exs
@@ -35,7 +35,7 @@ config :indexer,
   # bytes
   memory_limit: 5 <<< 30
 
-# config :indexer, Indexer.ReplacedTransaction.Supervisor, disabled?: true
+config :indexer, Indexer.ReplacedTransaction.Supervisor, disabled?: true
 
 config :indexer, Indexer.Tracer,
   service: :indexer,
