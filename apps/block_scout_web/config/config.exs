66362ed673a6e610c02492a732dff8a8754d5427
a@a.ru <a@a.ru>
2019-02-25 18:54:32 +0300

    Update other explorers

--- apps/block_scout_web/config/config.exs
+++ apps/block_scout_web/config/config.exs
@@ -20,9 +20,9 @@ config :block_scout_web, BlockScoutWeb.Chain,
 config :block_scout_web,
   link_to_other_explorers: System.get_env("LINK_TO_OTHER_EXPLORERS") == "true",
   other_explorers: %{
-    "Etherscan" => "https://etherscan.io/",
-    "EtherChain" => "https://www.etherchain.org/",
-    "Bloxy" => "https://bloxy.info/"
+    "Etherscan" => "https://goerli.etherscan.io/",
+    "Goerli" => "https://explorer.goerli.net/",
+    "Ethstats" => "https://lite.ethstats.io/"
   },
   other_networks: [
     %{
