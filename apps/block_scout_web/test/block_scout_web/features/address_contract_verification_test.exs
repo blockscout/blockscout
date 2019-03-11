0f8d3a9d9af51807d444563211c7da23b49e5dbb
Ayrat Badykov <ayratin555@gmail.com>
2019-03-11 18:39:32 +0300

    fix view tests

--- apps/block_scout_web/test/block_scout_web/features/address_contract_verification_test.exs
+++ apps/block_scout_web/test/block_scout_web/features/address_contract_verification_test.exs
@@ -36,7 +36,8 @@ defmodule BlockScoutWeb.AddressContractVerificationTest do
       contract_name: name,
       version: version,
       optimization: false,
-      source_code: source_code
+      source_code: source_code,
+      evm_version: "byzantium"
     })
     |> ContractVerifyPage.verify_and_publish()
 
@@ -48,7 +49,13 @@ defmodule BlockScoutWeb.AddressContractVerificationTest do
 
     session
     |> ContractVerifyPage.visit_page("0x1e0eaa06d02f965be2dfe0bc9ff52b2d82133461")
-    |> ContractVerifyPage.fill_form(%{contract_name: "", version: nil, optimization: nil, source_code: ""})
+    |> ContractVerifyPage.fill_form(%{
+      contract_name: "",
+      version: nil,
+      optimization: nil,
+      source_code: "",
+      evm_version: "byzantium"
+    })
     |> ContractVerifyPage.verify_and_publish()
     |> assert_has(ContractVerifyPage.validation_error())
   end
