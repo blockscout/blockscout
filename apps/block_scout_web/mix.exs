3e619d0c21537690947fa361ddb18ee975c88c0e
Lior Rabin <lior@colu.com>
2019-03-12 17:21:24 +0200

    deps from https://github.com/poanetwork/blockscout/releases/tag/v1.3.4-beta

--- apps/block_scout_web/mix.exs
+++ apps/block_scout_web/mix.exs
@@ -63,9 +63,9 @@ defmodule BlockScoutWeb.Mixfile do
       # GraphQL toolkit
       {:absinthe, "~> 1.4"},
       # Integrates Absinthe subscriptions with Phoenix
-      {:absinthe_phoenix, git: "https://github.com/ayrat555/absinthe_phoenix.git", branch: "master", override: true},
+      {:absinthe_phoenix, "~> 1.4"},
       # Plug support for Absinthe
-      {:absinthe_plug, git: "https://github.com/ayrat555/absinthe_plug.git", branch: "ab-allow-to-set-default-query", override: true},
+      {:absinthe_plug, "~> 1.4"},
       # Absinthe support for the Relay framework
       {:absinthe_relay, "~> 1.4"},
       {:bypass, "~> 1.0", only: :test},
