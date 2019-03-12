11fad5aaeebaad1cae0463a24befb4dd29d01a4d
Victor Baranov <baranov.viktor.27@gmail.com>
2019-03-12 13:20:34 +0300

    Exact date and time for Transaction details page

--- apps/block_scout_web/assets/js/lib/from_now.js
+++ apps/block_scout_web/assets/js/lib/from_now.js
@@ -19,7 +19,14 @@ function tryUpdateAge (el) {
   if (timestamp.isValid()) updateAge(el, timestamp)
 }
 function updateAge (el, timestamp) {
-  const fromNow = timestamp.fromNow()
+  let fromNow = timestamp.fromNow()
+  //show the exact time only for transaction details page. Otherwise, short entry
+  if (window.location.pathname.startsWith('/tx/')) {
+  	const offset = moment().utcOffset() / 60
+  	const sign = offset && Math.sign(offset) ? '+' : '-';
+  	const formatDate = `MMMM-DD-YYYY hh:mm:ss A ${sign}${offset} UTC`
+  	fromNow = `${fromNow} (${timestamp.format(formatDate)})`
+  }
   if (fromNow !== el.innerHTML) el.innerHTML = fromNow
 }
 updateAllAges()
