1679a478105ce73566e0e6f21b8537999d0e089e
a@a.ru <a@a.ru>
2019-03-21 16:29:23 +0300

    disable the dropped/replaced fetcher

--- apps/indexer/config/config.exs
+++ apps/indexer/config/config.exs
@@ -35,7 +35,7 @@ config :indexer,
   # bytes
   memory_limit: 4 <<< 30
 
-# config :indexer, Indexer.ReplacedTransaction.Supervisor, disabled?: true
+config :indexer, Indexer.ReplacedTransaction.Supervisor, disabled?: true
 # config :indexer, Indexer.Block.Reward.Supervisor, disabled?: true 
 
 config :indexer, Indexer.Tracer,
