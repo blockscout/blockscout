6360ecf7997d61023ee3eeaa43b28d88ed4a8768
Ayrat Badykov <ayratin555@gmail.com>
2019-03-22 14:56:34 +0300

    fix tests

--- apps/explorer/lib/explorer/chain/transaction_count_cache.ex
+++ apps/explorer/lib/explorer/chain/transaction_count_cache.ex
@@ -3,12 +3,13 @@ defmodule Explorer.Chain.TransactionCountCache do
   Cache for estimated transaction count.
   """
 
+  require Logger
+
   use GenServer
 
   alias Explorer.Chain.Transaction
   alias Explorer.Repo
 
-  @tab :transaction_count_cache
   # 2 hours
   @cache_period 1_000 * 60 * 60 * 2
   @default_value 0
@@ -16,78 +17,97 @@ defmodule Explorer.Chain.TransactionCountCache do
   @name __MODULE__
 
   def start_link([params, gen_server_options]) do
-    GenServer.start_link(__MODULE__, params, name: gen_server_options[:name] || @name)
+    name = gen_server_options[:name] || @name
+    params_with_name = Keyword.put(params, :name, name)
+
+    GenServer.start_link(__MODULE__, params_with_name, name: name)
   end
 
   def init(params) do
     cache_period = params[:cache_period] || @cache_period
     current_value = params[:default_value] || @default_value
+    name = params[:name]
 
-    init_ets_table()
+    init_ets_table(name)
 
     schedule_cache_update()
 
-    {:ok, {{cache_period, current_value}, nil}}
+    {:ok, {{cache_period, current_value, name}, nil}}
   end
 
   def value(process_name \\ __MODULE__) do
     GenServer.call(process_name, :value)
   end
 
-  def handle_call(:value, _, {{cache_period, default_value}, task}) do
+  def handle_call(:value, _, {{cache_period, default_value, name}, task}) do
     {value, task} =
-      case cached_values() do
+      case cached_values(name) do
         nil ->
-          {default_value, update_cache(task)}
+          {default_value, update_cache(task, name)}
 
         {cached_value, timestamp} ->
           task =
             if current_time() - timestamp > cache_period do
-              update_cache(task)
+              update_cache(task, name)
             end
 
           {cached_value, task}
       end
 
-    {:reply, value, {{cache_period, default_value}, task}}
+    {:reply, value, {{cache_period, default_value, name}, task}}
   end
 
-  def update_cache(nil) do
-    async_update_cache()
+  def update_cache(nil, name) do
+    async_update_cache(name)
   end
 
-  def update_cache(task) do
+  def update_cache(task, _) do
     task
   end
 
-  def handle_cast({:update_cache, value}, {{cache_period, default_value}, _}) do
+  def handle_cast({:update_cache, value}, {{cache_period, default_value, name}, _}) do
     current_time = current_time()
     tuple = {value, current_time}
 
-    :ets.insert(@tab, {@key, tuple})
+    table_name = table_name(name)
 
-    {:noreply, {{cache_period, default_value}, nil}}
+    :ets.insert(table_name, {@key, tuple})
+
+    {:noreply, {{cache_period, default_value, name}, nil}}
   end
 
-  def handle_info({:DOWN, _, _, _, _}, {{cache_period, default_value}, _}) do
-    {:noreply, {{cache_period, default_value}, nil}}
+  def handle_info({:DOWN, _, _, _, _}, {{cache_period, default_value, name}, _}) do
+    {:noreply, {{cache_period, default_value, name}, nil}}
   end
 
-  def handle_info(_, {{cache_period, default_value}, _}) do
-    {:noreply, {{cache_period, default_value}, nil}}
+  def handle_info(_, {{cache_period, default_value, name}, _}) do
+    {:noreply, {{cache_period, default_value, name}, nil}}
   end
 
-  def async_update_cache do
+  defp table_name(name) do
+    name |> Atom.to_string() |> Macro.underscore() |> String.to_atom()
+  end
+
+  def async_update_cache(name) do
     Task.async(fn ->
-      result = Repo.aggregate(Transaction, :count, :hash, timeout: :infinity)
+      try do
+        result = Repo.aggregate(Transaction, :count, :hash, timeout: :infinity)
 
-      GenServer.cast(__MODULE__, {:update_cache, result})
+        GenServer.cast(name, {:update_cache, result})
+      rescue
+        e ->
+          Logger.debug([
+            "Coudn't update transaction count test #{inspect(e)}"
+          ])
+      end
     end)
   end
 
-  defp init_ets_table do
-    if :ets.whereis(@tab) == :undefined do
-      :ets.new(@tab, [
+  defp init_ets_table(name) do
+    table_name = table_name(name)
+
+    if :ets.whereis(table_name) == :undefined do
+      :ets.new(table_name, [
         :set,
         :named_table,
         :public,
@@ -96,8 +116,10 @@ defmodule Explorer.Chain.TransactionCountCache do
     end
   end
 
-  defp cached_values do
-    case :ets.lookup(@tab, @key) do
+  defp cached_values(name) do
+    table_name = table_name(name)
+
+    case :ets.lookup(table_name, @key) do
       [{_, cached_values}] -> cached_values
       _ -> nil
     end
--- apps/explorer/test/explorer/chain/transaction_count_cache_test.exs
+++ apps/explorer/test/explorer/chain/transaction_count_cache_test.exs
@@ -4,54 +4,50 @@ defmodule Explorer.Chain.TransactionCountCacheTest do
   alias Explorer.Chain.TransactionCountCache
 
   test "returns default transaction count" do
-    TransactionCountCache.start_link([[], []])
+    TransactionCountCache.start_link([[], [name: TestCache]])
 
-    result = TransactionCountCache.value()
+    result = TransactionCountCache.value(TestCache)
 
     assert result == 0
   end
 
   test "updates cache if initial value is zero" do
-    TransactionCountCache.start_link([[], []])
+    TransactionCountCache.start_link([[], [name: TestCache]])
 
     insert(:transaction)
     insert(:transaction)
 
-    result = TransactionCountCache.value()
+    _result = TransactionCountCache.value(TestCache)
 
-    assert result == 0
+    Process.sleep(1000)
 
-    Process.sleep(500)
-
-    updated_value = TransactionCountCache.value()
+    updated_value = TransactionCountCache.value(TestCache)
 
     assert updated_value == 2
   end
 
   test "does not update cache if cache period did not pass" do
-    TransactionCountCache.start_link([[], []])
+    TransactionCountCache.start_link([[], [name: TestCache]])
 
     insert(:transaction)
     insert(:transaction)
 
-    result = TransactionCountCache.value()
+    _result = TransactionCountCache.value(TestCache)
 
-    assert result == 0
+    Process.sleep(1000)
 
-    Process.sleep(500)
-
-    updated_value = TransactionCountCache.value()
+    updated_value = TransactionCountCache.value(TestCache)
 
     assert updated_value == 2
 
     insert(:transaction)
     insert(:transaction)
 
-    _updated_value = TransactionCountCache.value()
+    _updated_value = TransactionCountCache.value(TestCache)
 
-    Process.sleep(500)
+    Process.sleep(1000)
 
-    updated_value = TransactionCountCache.value()
+    updated_value = TransactionCountCache.value(TestCache)
 
     assert updated_value == 2
   end
