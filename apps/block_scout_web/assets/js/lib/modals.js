e72f8fbea7fdc026ddbfa1cf58a158d9afaefb64
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2019-04-03 16:58:26 -0300

    (add) validator info modal

--- apps/block_scout_web/assets/js/lib/modals.js
+++ apps/block_scout_web/assets/js/lib/modals.js
@@ -4,4 +4,8 @@ $(function () {
   $('.js-become-candidate').on('click', function () {
     $('#becomeCandidateModal').modal()
   })
+
+  $('.js-validator-info-modal').on('click', function () {
+    $('#validatorInfoModal').modal()
+  })
 })
