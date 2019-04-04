afcf48168dbf0f277dfbe1e34de76e7282d7af12
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2019-04-04 12:20:20 -0300

    (add) stakes progress modal

--- apps/block_scout_web/assets/js/lib/modals.js
+++ apps/block_scout_web/assets/js/lib/modals.js
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+import Chart from 'chart.js'
 
 $(function () {
   $('.js-become-candidate').on('click', function () {
@@ -8,4 +9,43 @@ $(function () {
   $('.js-validator-info-modal').on('click', function () {
     $('#validatorInfoModal').modal()
   })
+
+  $('.js-stake-stake').on('click', function () {
+    $('#stakeModal').modal()
+    const progress = parseInt($('.js-stakes-progress-data-progress').text())
+    const total = parseInt($('.js-stakes-progress-data-total').text())
+
+    setupStakesProgress(progress, total)
+  })
+
+  function setupStakesProgress (progress, total) {
+    const stakeProgress = $('#stakeProgress')
+    const primaryColor = $('.btn-full-primary').css('background-color')
+    const backgroundColors = [
+      primaryColor,
+      'rgba(202, 199, 226, 0.5)'
+    ]
+    const progressBackground = total - progress
+
+    let myChart = new Chart(stakeProgress, {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          data: [progress, progressBackground],
+          backgroundColor: backgroundColors,
+          hoverBackgroundColor: backgroundColors,
+          borderWidth: 0
+        }]
+      },
+      options: {
+        cutoutPercentage: 80,
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: false
+        }
+      }
+    })
+  }
 })
