64c75349d6922d2b61d175fd257b66c94293469c
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2019-04-10 10:28:01 -0300

    (fix) address several compiler warnings

--- apps/block_scout_web/lib/block_scout_web/controllers/common_components_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/common_components_controller.ex
@@ -1,14 +1,12 @@
 defmodule BlockScoutWeb.CommonComponentsController do
   use BlockScoutWeb, :controller
 
-  alias BlockScoutWeb.CommonComponentsView
-
   def index(conn, params) do
     []
     |> handle_render(conn, params)
   end
 
-  defp handle_render(full_options, conn, _params) do
+  defp handle_render(_full_options, conn, _params) do
     render(conn, "index.html")
   end
 end
