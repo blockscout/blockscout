75cc0df10037b4102d5051f7b1e988e8a9ad9704
a@a.ru <a@a.ru>
2019-04-15 13:24:22 +0300

    Increase memory limit - fix

--- apps/indexer/config/config.exs
+++ apps/indexer/config/config.exs
@@ -33,7 +33,7 @@ config :indexer,
   ecto_repos: [Explorer.Repo],
   metadata_updater_days_interval: 2,
   # bytes
-  memory_limit: 6 <<< 30,
+  memory_limit: 12 <<< 30,
   first_block: System.get_env("FIRST_BLOCK") || 0
 
 config :indexer, Indexer.ReplacedTransaction.Supervisor, disabled?: true
