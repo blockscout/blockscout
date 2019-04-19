f764f2ea6a0c671298da3713d34f1d72b2aa2ca9
Ayrat Badykov <ayratin555@gmail.com>
2019-04-19 13:01:40 +0300

    fix contructor arguments verification

--- apps/explorer/lib/explorer/smart_contract/verifier/constructor_arguments.ex
+++ apps/explorer/lib/explorer/smart_contract/verifier/constructor_arguments.ex
@@ -5,13 +5,13 @@ defmodule Explorer.SmartContract.Verifier.ConstructorArguments do
 
   alias Explorer.Chain
 
-  def verify(address_hash, bytecode, arguments_data) do
+  def verify(address_hash, arguments_data) do
     arguments_data = String.replace(arguments_data, "0x", "")
     creation_input_data = Chain.contract_creation_input_data(address_hash)
 
     expected_arguments_data =
       creation_input_data
-      |> String.split(bytecode)
+      |> String.split("0029")
       |> List.last()
       |> String.replace("0x", "")
 
