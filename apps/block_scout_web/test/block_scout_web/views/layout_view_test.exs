ae8b853901ce1356c44defa73e686e1618b0dfbc
Victor Baranov <baranov.viktor.27@gmail.com>
2019-05-12 22:32:16 +0300

    Add tests for SUPPORTED_CHAINS env var

--- apps/block_scout_web/test/block_scout_web/views/layout_view_test.exs
+++ apps/block_scout_web/test/block_scout_web/views/layout_view_test.exs
@@ -86,4 +86,119 @@ defmodule BlockScoutWeb.LayoutViewTest do
                 ~s(<a href="https://github.com/poanetwork/blockscout/releases/tag/v1.3.4-beta" class="footer-link" target="_blank">1.3.4</a>)}
     end
   end
+
+  @supported_chains_pattern ~s([ { "title": "RSK Mainnet", "url": "https://blockscout.com/rsk/mainnet", "other?": true }, { "title": "POA Sokol", "url": "https://blockscout.com/poa/sokol", "test_net?": true }, { "title": "POA Core", "url": "https://blockscout.com/poa/core" }, { "title": "LUKSO L14 testnet", "url": "https://blockscout.com/lukso/l14", "test_net?": true, "hide_in_dropdown?": true } ])
+
+  describe "other_networks/0" do
+    test "get networks list based on env variables" do
+      Application.put_env(:block_scout_web, :supported_chains, @supported_chains_pattern)
+
+      assert LayoutView.other_networks() == [
+               %{
+                 title: "POA Core",
+                 url: "https://blockscout.com/poa/core"
+               },
+               %{
+                 title: "POA Sokol",
+                 url: "https://blockscout.com/poa/sokol",
+                 test_net?: true
+               },
+               %{
+                 title: "RSK Mainnet",
+                 url: "https://blockscout.com/rsk/mainnet",
+                 other?: true
+               },
+               %{
+                 title: "LUKSO L14 testnet",
+                 url: "http://blockscout.com/lukso/l14",
+                 test_net?: true,
+                 hide_in_dropdown?: true
+               }
+             ]
+    end
+  end
+
+  describe "main_nets/1" do
+    test "get all dropdown networks list based on env variables" do
+      Application.put_env(:block_scout_web, :supported_chains, @supported_chains_pattern)
+
+      assert LayoutView.main_nets(LayoutView.other_networks()) == [
+               %{
+                 title: "POA Core",
+                 url: "https://blockscout.com/poa/core"
+               },
+               %{
+                 title: "LUKSO L14 testnet",
+                 url: "http://blockscout.com/lukso/l14",
+                 test_net?: true,
+                 hide_in_dropdown?: true
+               }
+             ]
+    end
+  end
+
+  describe "test_nets/1" do
+    test "get all dropdown networks list based on env variables" do
+      Application.put_env(:block_scout_web, :supported_chains, @supported_chains_pattern)
+
+      assert LayoutView.test_nets(LayoutView.other_networks()) == [
+               %{
+                 title: "POA Sokol",
+                 url: "https://blockscout.com/poa/sokol",
+                 test_net?: true
+               },
+               %{
+                 title: "LUKSO L14 testnet",
+                 url: "http://blockscout.com/lukso/l14",
+                 test_net?: true,
+                 hide_in_dropdown?: true
+               }
+             ]
+    end
+  end
+
+  describe "dropdown_nets/0" do
+    test "get all dropdown networks list based on env variables" do
+      Application.put_env(:block_scout_web, :supported_chains, @supported_chains_pattern)
+
+      assert LayoutView.dropdown_nets() == [
+               %{
+                 title: "POA Core",
+                 url: "https://blockscout.com/poa/core"
+               },
+               %{
+                 title: "RSK Mainnet",
+                 url: "https://blockscout.com/rsk/mainnet",
+                 other?: true
+               }
+             ]
+    end
+  end
+
+  describe "dropdown_head_main_nets/0" do
+    test "get dropdown all main networks except those of 'other' type list based on env variables" do
+      Application.put_env(:block_scout_web, :supported_chains, @supported_chains_pattern)
+
+      assert LayoutView.dropdown_head_main_nets() == [
+               %{
+                 title: "POA Core",
+                 url: "https://blockscout.com/poa/core"
+               }
+             ]
+    end
+  end
+
+  describe "dropdown_other_nets/0" do
+    test "get dropdown networks of 'other' type list based on env variables" do
+      Application.put_env(:block_scout_web, :supported_chains, @supported_chains_pattern)
+
+      assert LayoutView.dropdown_other_nets() == [
+               %{
+                 title: "RSK Mainnet",
+                 url: "https://blockscout.com/rsk/mainnet",
+                 other?: true
+               }
+             ]
+    end
+  end
 end
