d30523be524bc76d82a2f5536af512d35c413ea0
Ayrat Badykov <ayratin555@gmail.com>
2019-05-15 14:58:59 +0300

    add order to response

--- apps/block_scout_web/lib/block_scout_web/views/api/rpc/eth_rpc_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/api/rpc/eth_rpc_view.ex
@@ -1,19 +1,41 @@
 defmodule BlockScoutWeb.API.RPC.EthRPCView do
   use BlockScoutWeb, :view
 
+  defstruct [:result, :id, :error]
+
   def render("show.json", %{result: result, id: id}) do
-    %{
-      "jsonrpc" => "2.0",
-      "result" => result,
-      "id" => id
+    %__MODULE__{
+      result: result,
+      id: id
     }
   end
 
   def render("error.json", %{error: message, id: id}) do
-    %{
-      "jsonrpc" => "2.0",
-      "error" => message,
-      "id" => id
+    %__MODULE__{
+      error: message,
+      id: id
     }
+  end
+
+  defimpl Poison.Encoder, for: BlockScoutWeb.API.RPC.EthRPCView do
+    def encode(%BlockScoutWeb.API.RPC.EthRPCView{result: result, id: id, error: error}, _options) when is_nil(error) do
+      """
+      {
+      "jsonrpc": "2.0",
+      "result": "#{result}",
+      "id": #{id}
+      }\n
+      """
+    end
+
+    def encode(%BlockScoutWeb.API.RPC.EthRPCView{id: id, error: error}, _options) do
+      """
+      {
+      "jsonrpc": "2.0",
+      "error": #{error},
+      "id": #{id}
+      }\n
+      """
+    end
   end
 end
