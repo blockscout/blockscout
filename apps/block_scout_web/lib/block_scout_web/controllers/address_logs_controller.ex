bc533e01506869e44e3dfcc334a7aa79c8a72ef7
Ayrat Badykov <ayratin555@gmail.com>
2019-05-23 15:02:17 +0300

    make logs loading async

--- apps/block_scout_web/lib/block_scout_web/controllers/address_logs_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/address_logs_controller.ex
@@ -6,13 +6,15 @@ defmodule BlockScoutWeb.AddressLogsController do
   import BlockScoutWeb.AddressController, only: [transaction_count: 1, validation_count: 1]
   import BlockScoutWeb.Chain, only: [paging_options: 1, next_page_params: 3, split_list_by_page: 1]
 
+  alias BlockScoutWeb.AddressLogsView
   alias Explorer.{Chain, Market}
   alias Explorer.ExchangeRates.Token
   alias Indexer.Fetcher.CoinBalanceOnDemand
+  alias Phoenix.View
 
   use BlockScoutWeb, :controller
 
-  def index(conn, %{"address_id" => address_hash_string} = params) do
+  def index(conn, %{"address_id" => address_hash_string, "type" => "JSON"} = params) do
     with {:ok, address_hash} <- Chain.string_to_address_hash(address_hash_string),
          {:ok, address} <- Chain.hash_to_address(address_hash) do
       logs_plus_one = Chain.address_to_logs(address, paging_options(params))
@@ -24,19 +26,45 @@ defmodule BlockScoutWeb.AddressLogsController do
             nil
 
           next_page_params ->
-            address_logs_path(conn, :index, address, next_page_params)
+            address_logs_path(conn, :index, address, Map.delete(next_page_params, "type"))
         end
 
+      items =
+        results
+        |> Enum.map(fn log ->
+          View.render_to_string(
+            AddressLogsView,
+            "_logs.html",
+            log: log,
+            conn: conn
+          )
+        end)
+
+      json(
+        conn,
+        %{
+          items: items,
+          next_page_path: next_page_url
+        }
+      )
+    else
+      _ ->
+        not_found(conn)
+    end
+  end
+
+  def index(conn, %{"address_id" => address_hash_string}) do
+    with {:ok, address_hash} <- Chain.string_to_address_hash(address_hash_string),
+         {:ok, address} <- Chain.hash_to_address(address_hash) do
       render(
         conn,
         "index.html",
         address: address,
-        logs: results,
+        current_path: current_path(conn),
         coin_balance_status: CoinBalanceOnDemand.trigger_fetch(address),
         exchange_rate: Market.get_exchange_rate(Explorer.coin()) || Token.null(),
         transaction_count: transaction_count(address),
-        validation_count: validation_count(address),
-        next_page_url: next_page_url
+        validation_count: validation_count(address)
       )
     else
       _ ->
