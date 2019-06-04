f7868aac80452b51497952f0c30f3a467c33040a
Ayrat Badykov <ayratin555@gmail.com>
2019-06-04 14:48:06 +0300

    add function to detect token transfer type

--- apps/explorer/lib/explorer/chain.ex
+++ apps/explorer/lib/explorer/chain.ex
@@ -21,6 +21,7 @@ defmodule Explorer.Chain do
 
   import EthereumJSONRPC, only: [integer_to_quantity: 1]
 
+  alias ABI.TypeDecoder
   alias Ecto.Adapters.SQL
   alias Ecto.{Changeset, Multi}
 
@@ -2820,6 +2821,91 @@ defmodule Explorer.Chain do
     |> Repo.all()
   end
 
+  @spec transaction_token_transfer_type(Transaction.t()) ::
+          {:erc20, TokenTransfer.t()} | {:erc721, TokenTransfer.t()} | nil
+  def transaction_token_transfer_type(
+        %Transaction{
+          status: :ok,
+          created_contract_address_hash: nil,
+          input: input,
+          value: value
+        } = transaction
+      ) do
+    zero_wei = %Wei{value: Decimal.new(0)}
+
+    transaction =
+      if Ecto.assoc_loaded?(transaction.token_transfers),
+        do: transaction,
+        else: Repo.preload(transaction, :token_transfers)
+
+    # https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/token/ERC721/ERC721.sol#L35
+    case {to_string(input), value} do
+      # transferFrom(address,address,uint256)
+      {"0x23b872dd" <> params, ^zero_wei} ->
+        types = [:address, :address, {:uint, 256}]
+        [from_address, to_address, _value] = decode_params(params, types)
+
+        find_erc721_token_transfer(transaction.token_transfers, {from_address, to_address})
+
+      # safeTransferFrom(address,address,uint256)
+      {"0x42842e0e" <> params, ^zero_wei} ->
+        types = [:address, :address, {:uint, 256}]
+        [from_address, to_address, _value] = decode_params(params, types)
+
+        find_erc721_token_transfer(transaction.token_transfers, {from_address, to_address})
+
+      # safeTransferFrom(address,address,uint256,bytes)
+      {"0xb88d4fde" <> params, ^zero_wei} ->
+        types = [:address, :address, {:uint, 256}, :bytes]
+        [from_address, to_address, _value, _data] = decode_params(params, types)
+
+        find_erc721_token_transfer(transaction.token_transfers, {from_address, to_address})
+
+      # check for ERC 20 or for old ERC 721 token verions
+      {unquote(TokenTransfer.transfer_function_signature()) <> params, ^zero_wei} ->
+        types = [:address, {:uint, 256}]
+
+        [address, value] = decode_params(params, types)
+
+        decimal_value = Decimal.new(value)
+
+        find_erc721_or_erc20_token_transfer(transaction.token_transfers, {address, decimal_value})
+
+      _ ->
+        nil
+    end
+  rescue
+    _ -> nil
+  end
+
+  def transaction_token_transfer_type(_), do: nil
+
+  defp find_erc721_token_transfer(token_transfers, {from_address, to_address}) do
+    token_transfer =
+      Enum.find(token_transfers, fn token_transfer ->
+        token_transfer.from_address_hash.bytes == from_address && token_transfer.to_address_hash.bytes == to_address
+      end)
+
+    if token_transfer, do: {:erc721, token_transfer}
+  end
+
+  defp find_erc721_or_erc20_token_transfer(token_transfers, {address, decimal_value}) do
+    IO.inspect({address, decimal_value})
+
+    token_transfer =
+      Enum.find(token_transfers, fn token_transfer ->
+        token_transfer.to_address_hash.bytes == address && token_transfer.amount == decimal_value
+      end)
+
+    if token_transfer do
+      case token_from_address_hash(token_transfer.token_contract_address_hash) do
+        {:ok, %Token{type: "ERC-20"}} -> {:erc20, token_transfer}
+        {:ok, %Token{type: "ERC-721"}} -> {:erc721, token_transfer}
+        _ -> nil
+      end
+    end
+  end
+
   defp reject_decompiled_with_version(query, nil), do: query
 
   defp reject_decompiled_with_version(query, reject_version) do
@@ -2995,5 +3081,11 @@ defmodule Explorer.Chain do
       left_join: decompiled_code in subquery(has_decompiled_code_query),
       select_merge: %{has_decompiled_code?: decompiled_code.has_decompiled_code?}
     )
+  end
+
+  defp decode_params(params, types) do
+    params
+    |> Base.decode16!(case: :mixed)
+    |> TypeDecoder.decode_raw(types)
   end
 end
