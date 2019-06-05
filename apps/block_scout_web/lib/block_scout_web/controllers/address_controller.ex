9d91df48ab2f1005d7c046b01405e39157a2f545
a@a.ru <a@a.ru>
2019-06-05 17:43:34 +0300

    Override not merged file address_controller.ex

--- apps/block_scout_web/lib/block_scout_web/controllers/address_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/address_controller.ex
@@ -58,40 +58,7 @@ defmodule BlockScoutWeb.AddressController do
     )
   end
 
-  def index(conn, params) do
-    addresses =
-      params
-      |> paging_options()
-      |> Chain.list_top_addresses()
-
-    {addresses_page, next_page} = split_list_by_page(addresses)
-
-    cur_page_number =
-      cond do
-        !params["prev_page_number"] -> 1
-        params["next_page"] -> String.to_integer(params["prev_page_number"]) + 1
-        params["prev_page"] -> String.to_integer(params["prev_page_number"]) - 1
-      end
-
-    next_page_path =
-      case next_page_params(next_page, addresses_page, params) do
-        nil ->
-          nil
-
-        next_page_params ->
-          next_params =
-            next_page_params
-            |> Map.put("prev_page_path", cur_page_path(conn, params))
-            |> Map.put("next_page", true)
-            |> Map.put("prev_page_number", cur_page_number)
-
-          address_path(
-            conn,
-            :index,
-            next_params
-          )
-      end
-
+  def index(conn, _params) do
     render(conn, "index.html",
       current_path: current_path(conn),
       address_count: Chain.count_addresses_with_balance_from_cache()
@@ -109,16 +76,4 @@ defmodule BlockScoutWeb.AddressController do
   def validation_count(%Address{} = address) do
     Chain.address_to_validation_count(address)
   end
-
-  defp cur_page_path(conn, %{"hash" => _hash, "fetched_coin_balance" => _balance} = params) do
-    new_params = Map.put(params, "next_page", false)
-
-    address_path(
-      conn,
-      :index,
-      new_params
-    )
-  end
-
-  defp cur_page_path(conn, _), do: address_path(conn, :index)
 end
