ac64f843214d70c4af3168261fdd6565dd297c4d
Victor Baranov <baranov.viktor.27@gmail.com>
2019-06-10 13:52:31 +0300

    Mix credo test fix update

--- apps/block_scout_web/lib/block_scout_web/controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controller.ex
@@ -13,7 +13,7 @@ defmodule BlockScoutWeb.Controller do
     conn
     |> put_status(:not_found)
     |> put_view(BlockScoutWeb.PageNotFoundView)
-    |> Controller.render(:index)
+    |> render(:index)
     |> halt()
   end
 
