f6d9872babd4822c0056605bf0dc31ee53c87bff
Victor Baranov <baranov.viktor.27@gmail.com>
2019-06-14 01:13:01 +0300

    previous version of of fetcher to fix wrong transactions collation

--- apps/indexer/lib/indexer/temporary/blocks_transactions_mismatch.ex
+++ apps/indexer/lib/indexer/temporary/blocks_transactions_mismatch.ex
@@ -13,6 +13,7 @@ defmodule Indexer.Temporary.BlocksTransactionsMismatch do
 
   import Ecto.Query
 
+  alias Ecto.Multi
   alias EthereumJSONRPC.Blocks
   alias Explorer.Chain.Block
   alias Explorer.Repo
@@ -22,14 +23,13 @@ defmodule Indexer.Temporary.BlocksTransactionsMismatch do
 
   @defaults [
     flush_interval: :timer.seconds(3),
-    max_batch_size: 50,
-    max_concurrency: 1,
+    max_batch_size: 10,
+    max_concurrency: 4,
     task_supervisor: Indexer.Temporary.BlocksTransactionsMismatch.TaskSupervisor,
     metadata: [fetcher: :blocks_transactions_mismatch]
   ]
 
   @doc false
-  # credo:disable-for-next-line Credo.Check.Design.DuplicatedCode
   def child_spec([init_options, gen_server_options]) when is_list(init_options) do
     {state, mergeable_init_options} = Keyword.pop(init_options, :json_rpc_named_arguments)
 
@@ -51,7 +51,7 @@ defmodule Indexer.Temporary.BlocksTransactionsMismatch do
   def init(initial, reducer, _) do
     query =
       from(block in Block,
-        left_join: transactions in assoc(block, :transactions),
+        join: transactions in assoc(block, :transactions),
         where: block.consensus and block.refetch_needed,
         group_by: block.hash,
         select: {block, count(transactions.hash)}
@@ -81,44 +81,30 @@ defmodule Indexer.Temporary.BlocksTransactionsMismatch do
   defp run_blocks(%Blocks{blocks_params: []}, blocks_data), do: {:retry, blocks_data}
 
   defp run_blocks(
-         %Blocks{transactions_params: transactions_params, blocks_params: blocks_params},
+         %Blocks{transactions_params: transactions_params},
          blocks_data
        ) do
-    blocks_with_transactions_map =
+    found_blocks_map =
       transactions_params
       |> Enum.group_by(&Map.fetch!(&1, :block_hash))
       |> Map.new(fn {block_hash, trans_lst} -> {block_hash, Enum.count(trans_lst)} end)
-
-    found_blocks_map =
-      blocks_params
-      |> Map.new(&{Map.fetch!(&1, :hash), 0})
-      |> Map.merge(blocks_with_transactions_map)
 
     {found_blocks_data, missing_blocks_data} =
       Enum.split_with(blocks_data, fn {block, _trans_num} ->
         Map.has_key?(found_blocks_map, to_string(block.hash))
       end)
 
-    {matching_blocks_data, unmatching_blocks_data} =
-      Enum.split_with(found_blocks_data, fn {block, trans_num} ->
-        found_blocks_map[to_string(block.hash)] == trans_num
+    {:ok, _} =
+      found_blocks_data
+      |> Enum.reduce(Multi.new(), fn {block, trans_num}, multi ->
+        changes = %{
+          refetch_needed: false,
+          consensus: found_blocks_map[to_string(block.hash)] == trans_num
+        }
+
+        Multi.update(multi, block.hash, Block.changeset(block, changes))
       end)
-
-    unless Enum.empty?(matching_blocks_data) do
-      hashes = Enum.map(matching_blocks_data, fn {block, _trans_num} -> block.hash end)
-
-      Block
-      |> where([block], block.hash in ^hashes)
-      |> Repo.update_all(set: [refetch_needed: false])
-    end
-
-    unless Enum.empty?(unmatching_blocks_data) do
-      hashes = Enum.map(unmatching_blocks_data, fn {block, _trans_num} -> block.hash end)
-
-      Block
-      |> where([block], block.hash in ^hashes)
-      |> Repo.update_all(set: [refetch_needed: false, consensus: false])
-    end
+      |> Repo.transaction()
 
     if Enum.empty?(missing_blocks_data) do
       :ok
