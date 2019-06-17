38977a101c7b66ffbbc58176e1f811de731e1743
Victor Baranov <baranov.viktor.27@gmail.com>
2019-06-17 13:52:03 +0300

    BlocksTransactionsMismatch

--- apps/indexer/lib/indexer/supervisor.ex
+++ apps/indexer/lib/indexer/supervisor.ex
@@ -24,6 +24,7 @@ defmodule Indexer.Supervisor do
   }
 
   alias Indexer.Temporary.{
+    BlocksTransactionsMismatch,
     UncatalogedTokenTransfers,
     UnclesWithoutIndex
   }
@@ -124,6 +125,8 @@ defmodule Indexer.Supervisor do
         # Temporary workers
         {UncatalogedTokenTransfers.Supervisor, [[]]},
         {UnclesWithoutIndex.Supervisor,
+         [[json_rpc_named_arguments: json_rpc_named_arguments, memory_monitor: memory_monitor]]},
+        {BlocksTransactionsMismatch.Supervisor,
          [[json_rpc_named_arguments: json_rpc_named_arguments, memory_monitor: memory_monitor]]}
       ],
       strategy: :one_for_one
