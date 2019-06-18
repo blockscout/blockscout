50794d8a4357f1d2cf652956de835d71053ea84e
saneery <anatolyniky@gmail.com>
2019-06-18 08:39:09 +0300

    fix test

--- apps/indexer/test/indexer/block/fetcher/receipts_test.exs
+++ apps/indexer/test/indexer/block/fetcher/receipts_test.exs
@@ -87,8 +87,8 @@ defmodule Indexer.Block.Fetcher.ReceiptsTest do
                    },
                    %{
                      "address" => "0x8bf38d4764929064f2d4d3a56520a76ab3df415c",
-                     "blockHash" => "0xf6b4b8c88df3ebd252ec476328334dc026cf66606a84fb769b3d3cbccc8471bd",
-                     "blockNumber" => "0x25",
+                     "blockHash" => nil,
+                     "blockNumber" => nil,
                      "data" => "0x000000000000000000000000862d67cb0773ee3f8ce7ea89b328ffea861ab3ef",
                      "logIndex" => "0x1",
                      "topics" => ["0x600bcf04a13e752d1e3670a5a9f1c21177ca2a93c6f5391d4f1298d098097c22"],
