b81c468f902bafcb6423ee664c35cbeecdb0a204
saneery <anatolyniky@gmail.com>
2019-06-21 12:21:10 +0300

    Add pools controller and view helpers tests

---
+++ apps/block_scout_web/test/block_scout_web/controllers/pools_controller_test.exs
@@ -0,0 +1,38 @@
+defmodule BlockScoutWeb.PoolsControllerTest do
+  use BlockScoutWeb.ConnCase
+
+  describe "GET validators/2" do
+    test "returns page", %{conn: conn} do
+      conn = get(conn, validators_path(conn, :validators))
+      assert conn.status == 200
+    end
+
+    test "returns rendered table", %{conn: conn} do
+      address_hashes = Enum.map(1..4, fn _ -> insert(:staking_pool) end)
+
+      conn = get(conn, validators_path(conn, :validators, %{type: "JSON"}))
+      assert {:ok, %{"items" => items, "next_page_path" => _}} = Poison.decode(conn.resp_body)
+      assert Enum.count(items) == Enum.count(address_hashes)
+    end
+  end
+
+  describe "GET active_pools/2" do
+    test "returns rendered table", %{conn: conn} do
+      address_hashes = Enum.map(1..4, fn _ -> insert(:staking_pool) end)
+
+      conn = get(conn, active_pools_path(conn, :active_pools, %{type: "JSON"}))
+      assert {:ok, %{"items" => items, "next_page_path" => _}} = Poison.decode(conn.resp_body)
+      assert Enum.count(items) == Enum.count(address_hashes)
+    end
+  end
+
+  describe "GET inactive_pools/2" do
+    test "returns rendered table", %{conn: conn} do
+      address_hashes = Enum.map(1..4, fn _ -> insert(:staking_pool, is_active: false) end)
+
+      conn = get(conn, inactive_pools_path(conn, :inactive_pools, %{type: "JSON"}))
+      assert {:ok, %{"items" => items, "next_page_path" => _}} = Poison.decode(conn.resp_body)
+      assert Enum.count(items) == Enum.count(address_hashes)
+    end
+  end
+end
