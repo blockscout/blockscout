00d469b879c574061564a10869611a85a9556640
saneery <anatolyniky@gmail.com>
2019-06-23 16:33:48 +0300

    Become a candidate functionality

--- apps/block_scout_web/assets/js/lib/modals.js
+++ apps/block_scout_web/assets/js/lib/modals.js
@@ -1,76 +1,142 @@
 import $ from 'jquery'
-import Chart from 'chart.js'
+import {store} from '../pages/stakes.js'
 
-$(function () {
-  $('.js-become-candidate').on('click', function () {
-    $('#becomeCandidateModal').modal()
-  })
-
-  $('.js-validator-info-modal').on('click', function () {
-    $('#validatorInfoModal').modal()
-  })
-
-  $('.js-move-stake').on('click', function () {
-    $('#errorStatusModal').modal()
-  })
-
-  $('.js-remove-pool').on('click', function () {
-    $('#warningStatusModal').modal()
-  })
-
-  $('.js-copy-address').on('click', function () {
-    $('#successStatusModal').modal()
-  })
-
-  $('.js-stake-stake').on('click', function () {
-    const modal = '#stakeModal'
-    const progress = parseInt($(`${modal} .js-stakes-progress-data-progress`).text())
-    const total = parseInt($(`${modal} .js-stakes-progress-data-total`).text())
-
-    $(modal).modal()
-
-    setupStakesProgress(progress, total, modal)
-  })
-
-  $('.js-withdraw-stake').on('click', function () {
-    const modal = '#withdrawModal'
-    const progress = parseInt($(`${modal} .js-stakes-progress-data-progress`).text())
-    const total = parseInt($(`${modal} .js-stakes-progress-data-total`).text())
-
-    $(modal).modal()
-
-    setupStakesProgress(progress, total, modal)
-  })
-
-  function setupStakesProgress (progress, total, modal) {
-    const stakeProgress = $(`${modal} .js-stakes-progress`)
-    const primaryColor = $('.btn-full-primary').css('background-color')
-    const backgroundColors = [
-      primaryColor,
-      'rgba(202, 199, 226, 0.5)'
-    ]
-    const progressBackground = total - progress
-
-    // eslint-disable-next-line no-unused-vars
-    let myChart = new Chart(stakeProgress, {
-      type: 'doughnut',
-      data: {
-        datasets: [{
-          data: [progress, progressBackground],
-          backgroundColor: backgroundColors,
-          hoverBackgroundColor: backgroundColors,
-          borderWidth: 0
-        }]
-      },
-      options: {
-        cutoutPercentage: 80,
-        legend: {
-          display: false
-        },
-        tooltips: {
-          enabled: false
-        }
-      }
+window.openBecomeCandidateModal = function () {
+  const el = '#becomeCandidateModal'
+  if ($(el).length) {
+    $(`${el} form`).unbind('submit')
+    $(`${el} form`).submit(() => {
+      becomeCandidate(el)
+      return false
     })
+    $(el).modal()
+  } else {
+    const modal = '#warningStatusModal'
+    $(`${modal} .modal-status-title`).text('Unauthorized')
+    $(`${modal} .modal-status-text`).text('Please login with MetaMask')
+    $(modal).modal()
   }
-})
+}
+
+function lockModal (el) {
+  var $submitButton = $(`${el} .btn-add-full`)
+  $(`${el} .close-modal`).attr('disabled', true)
+  $(el).on('hide.bs.modal', e => {
+    e.preventDefault()
+    e.stopPropagation()
+  })
+  $submitButton.attr('disabled', true)
+  $submitButton.html(`
+    <span class="loading-spinner-small mr-2">
+      <span class="loading-spinner-block-1"></span>
+      <span class="loading-spinner-block-2"></span>
+    </span>`)
+}
+
+function unlockAndHideModal (el) {
+  var $submitButton = $(`${el} .btn-add-full`)
+  $(el).unbind()
+  $(el).modal('hide')
+  $(`${el} .close-modal`).attr('disabled', false)
+  $submitButton.attr('disabled', false)
+}
+
+function openErrorModal (title, text) {
+  $(`#errorStatusModal .modal-status-title`).text(title)
+  $(`#errorStatusModal .modal-status-text`).text(text)
+  $('#errorStatusModal').modal('show')
+}
+
+function openSuccessModal (title, text) {
+  $(`#successStatusModal .modal-status-title`).text(title)
+  $(`#successStatusModal .modal-status-text`).text(text)
+  $('#successStatusModal').modal('show')
+}
+
+async function becomeCandidate (el) {
+  const web3 = store.getState().web3
+  var $submitButton = $(`${el} .btn-add-full`)
+  const buttonText = $submitButton.html()
+  lockModal(el)
+
+  const stake = parseFloat($(`${el} [candidate-stake]`).val())
+  const address = $(`${el} [mining-address]`).val()
+  const contract = store.getState().stakingContract
+  const account = store.getState().account
+
+  if (!stake || stake < $(el).data('min-stake')) {
+    var min = $(el).data('min-stake')
+    unlockAndHideModal(el)
+    $submitButton.html(buttonText)
+    openErrorModal('Error', `You cannot stake less than ${min} POA20`)
+    return false
+  }
+
+  if (account === address || !web3.utils.isAddress(address)) {
+    unlockAndHideModal(el)
+    $submitButton.html(buttonText)
+    openErrorModal('Error', 'Invalid Mining Address')
+    return false
+  }
+
+  try {
+    var stakeAllowed = await contract.methods.areStakeAndWithdrawAllowed().call()
+    if (!stakeAllowed) {
+      unlockAndHideModal(el)
+      $submitButton.html(buttonText)
+      const blockContract = new web3.eth.Contract(
+        [{
+          'constant': true,
+          'inputs': [],
+          'name': 'isSnapshotting',
+          'outputs': [
+            {
+              'name': '',
+              'type': 'bool'
+            }
+          ],
+          'payable': false,
+          'stateMutability': 'view',
+          'type': 'function'
+        }],
+        '0x2000000000000000000000000000000000000001'
+      )
+      var isSnapshotting = await blockContract.methods.isSnapshotting().call()
+      if (isSnapshotting) {
+        openErrorModal('Error', 'Stakes are not allowed at the moment. Please try again in a few blocks')
+      } else {
+        const epochEndSec = $('[data-page="stakes"]').data('epoch-end-sec')
+        const hours = Math.trunc(epochEndSec / 3600)
+        const minutes = Math.trunc((epochEndSec % 3600) / 60)
+
+        openErrorModal('Error', `Since the current staking epoch is finishing now, you will be able to place a stake during the next staking epoch. Please try again in ${hours} hours ${minutes} minutes`)
+      }
+    } else {
+      contract.methods.addPool(stake * Math.pow(10, 18), address).send({
+        from: account,
+        gas: 400000,
+        gasPrice: 1000000000
+      })
+        .on('receipt', _receipt => {
+          unlockAndHideModal(el)
+          $submitButton.html(buttonText)
+          store.dispatch({ type: 'START_REQUEST' })
+          store.dispatch({ type: 'GET_USER' })
+          store.dispatch({ type: 'RELOAD_POOLS_LIST' })
+          openSuccessModal('Success', 'The transaction is created')
+        })
+        .catch(_err => {
+          unlockAndHideModal(el)
+          $submitButton.html(buttonText)
+          openErrorModal('Error', 'Something went wrong')
+        })
+    }
+  } catch (err) {
+    console.log(err)
+    unlockAndHideModal(el)
+    $submitButton.html(buttonText)
+    openErrorModal('Error', 'Something went wrong')
+  }
+
+  return false
+}
--- apps/block_scout_web/lib/block_scout_web/controllers/pools_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/pools_controller.ex
@@ -5,7 +5,7 @@ defmodule BlockScoutWeb.PoolsController do
   alias Explorer.Chain.{BlockNumberCache, Wei}
   alias Explorer.Counters.AverageBlockTime
   alias BlockScoutWeb.{PoolsView, StakesView}
-  alias Explorer.Staking.EpochCounter
+  alias Explorer.Staking.{EpochCounter, PoolsReader}
   alias Phoenix.View
 
   import BlockScoutWeb.Chain, only: [paging_options: 1, next_page_params: 3, split_list_by_page: 1]
@@ -46,10 +46,18 @@ defmodule BlockScoutWeb.PoolsController do
     end
   end
 
+  def staking_contract(conn, _) do
+    staking_address = PoolsReader.get_staking_address()
+    staking_abi = PoolsReader.get_staking_abi()
+
+    json(conn, %{abi: staking_abi, address: staking_address})
+  end
+
   defp render_template(_, conn, %{"type" => "JSON", "template" => "top"}) do
     epoch_number = EpochCounter.epoch_number() || 0
     epoch_end_block = EpochCounter.epoch_end_block() || 0
     block_number = BlockNumberCache.max_number()
+    stakes_setting = Application.get_env(:block_scout_web, :stakes)
 
     user =
       conn
@@ -61,7 +69,8 @@ defmodule BlockScoutWeb.PoolsController do
       epoch_end_in: epoch_end_block - block_number,
       block_number: block_number,
       user: user,
-      logged_in: user != nil
+      logged_in: user != nil,
+      min_candidate_stake: stakes_setting[:min_candidate_stake]
     ]
 
     content =
@@ -130,6 +139,7 @@ defmodule BlockScoutWeb.PoolsController do
     epoch_end_block = EpochCounter.epoch_end_block() || 0
     block_number = BlockNumberCache.max_number()
     average_block_time = AverageBlockTime.average_block_time()
+    stakes_setting = Application.get_env(:block_scout_web, :stakes)
 
     user =
       conn
@@ -144,7 +154,8 @@ defmodule BlockScoutWeb.PoolsController do
       current_path: current_path(conn),
       user: user,
       logged_in: user != nil,
-      average_block_time: average_block_time
+      average_block_time: average_block_time,
+      min_candidate_stake: stakes_setting[:min_candidate_stake]
     ]
 
     render(conn, "index.html", options)
