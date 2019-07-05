6f9638b3f5af5b0a2a4f3e66702ef57f944310e2
Anatoly Nikiforov <anatolyniky@gmail.com>
2019-07-05 22:31:07 +0300

    Render lists of validators and staking pools (#2215)
    Replace hard-coded placeholders with actual data from contracts.
    Three pages from the design are merged into one controller with a filter.

---
+++ apps/block_scout_web/lib/block_scout_web/controllers/pools_controller.ex
@@ -0,0 +1,97 @@
+defmodule BlockScoutWeb.PoolsController do
+  use BlockScoutWeb, :controller
+
+  alias BlockScoutWeb.PoolsView
+  alias Explorer.Chain
+  alias Explorer.Chain.BlockNumberCache
+  alias Explorer.Counters.AverageBlockTime
+  alias Explorer.Staking.ContractState
+  alias Phoenix.View
+
+  import BlockScoutWeb.Chain, only: [paging_options: 1, next_page_params: 3, split_list_by_page: 1]
+
+  def index(%{assigns: assigns} = conn, params) do
+    render_template(assigns.filter, conn, params)
+  end
+
+  defp render_template(filter, conn, %{"type" => "JSON"} = params) do
+    [paging_options: options] = paging_options(params)
+
+    last_index =
+      params
+      |> Map.get("position", "0")
+      |> String.to_integer()
+
+    pools_plus_one = Chain.staking_pools(filter, options)
+
+    {pools, next_page} = split_list_by_page(pools_plus_one)
+
+    next_page_path =
+      case next_page_params(next_page, pools, params) do
+        nil ->
+          nil
+
+        next_page_params ->
+          updated_page_params =
+            next_page_params
+            |> Map.delete("type")
+            |> Map.put("position", last_index + 1)
+
+          next_page_path(filter, conn, updated_page_params)
+      end
+
+    average_block_time = AverageBlockTime.average_block_time()
+
+    items =
+      pools
+      |> Enum.with_index(last_index + 1)
+      |> Enum.map(fn {pool, index} ->
+        View.render_to_string(
+          PoolsView,
+          "_rows.html",
+          pool: pool,
+          index: index,
+          average_block_time: average_block_time,
+          pools_type: filter
+        )
+      end)
+
+    json(
+      conn,
+      %{
+        items: items,
+        next_page_path: next_page_path
+      }
+    )
+  end
+
+  defp render_template(filter, conn, _) do
+    epoch_number = ContractState.get(:epoch_number, 0)
+    epoch_end_block = ContractState.get(:epoch_end_block, 0)
+    block_number = BlockNumberCache.max_number()
+    average_block_time = AverageBlockTime.average_block_time()
+
+    options = [
+      pools_type: filter,
+      epoch_number: epoch_number,
+      epoch_end_in: epoch_end_block - block_number,
+      block_number: block_number,
+      current_path: current_path(conn),
+      average_block_time: average_block_time
+    ]
+
+    render(conn, "index.html", options)
+  end
+
+  defp next_page_path(:validator, conn, params) do
+    validators_path(conn, :index, params)
+  end
+
+  defp next_page_path(:active, conn, params) do
+    active_pools_path(conn, :index, params)
+  end
+
+  defp next_page_path(:inactive, conn, params) do
+    inactive_pools_path(conn, :index, params)
+  end
+end
