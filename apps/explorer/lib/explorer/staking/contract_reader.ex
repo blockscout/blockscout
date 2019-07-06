dde80476bd8f7cd34051eb672c753df34465db94
Paul Tsupikoff <goodsoft@goodsoft.lv>
2019-07-06 17:14:51 +0300

    Consolidate POSDAO contract reading (#2371)
    Previously, staking contract reading was arbitrarily spread across
    Explorer.Staking.ContractState, Explorer.Staking.PoolsReader and
    Indexer.Fetcher.StakingPools. Using async fetcher infrastructure for staking
    contracts is inadequate, as blocks may arrive out of order, while we want
    fetching to be triggered by newer block arrival. Also, contract calls were
    not batched enough, and it was hard to follow their sequence across involved modules.
    
    Now, on each incoming block, which is newer than the last seen, we fully update
    not only global contract state, but all validators, pools and delegators.
    
    All requests are intelligently compiled into four batches.
    These are defined together to give better overview of performed actions.
    Chain.import infrastructure is still used for inserting data into DB
    to leverage smart batching of INSERT queries.
    
    Likelihood calculation is added: it zips results of getPoolsToBeElected
    and getPoolsLikelihood calls to make a map of addresses.
    
    Only POS_STAKING_CONTRACT env variable is required now.
    Other contract addresses are fetched from it during launch.
    ABIs are concatenated together during launch as well, as we don't support
    batch requests against distinct ABIs yet. This should be addressed in further PRs.
    
    Up-to-date ABIs copied from build artifacts of posdao-contracts repo.
    File names now correspond to contract names, README directs to their origin.
    
    * Fetch inactive delegators, mark pools and delegators deleted (#2205)
    * Fetch min stakes and token contract address from StakingAuRa (#2313)
    * Fetch block reward ratio using validatorRewardPercent getter (#2424)
    * Add missing `banned_until` field in ON CONFLICT clause for staking pools.

---
+++ apps/explorer/lib/explorer/staking/contract_reader.ex
@@ -0,0 +1,100 @@
+defmodule Explorer.Staking.ContractReader do
+  @moduledoc """
+  Routines for batched fetching of information from POSDAO contracts
+  """
+
+  alias Explorer.SmartContract.Reader
+
+  def global_requests do
+    [
+      token_contract_address: {:staking, "erc20TokenContract", []},
+      min_candidate_stake: {:staking, "candidateMinStake", []},
+      min_delegator_stake: {:staking, "delegatorMinStake", []},
+      epoch_number: {:staking, "stakingEpoch", []},
+      epoch_end_block: {:staking, "stakingEpochEndBlock", []},
+      active_pools: {:staking, "getPools", []},
+      inactive_pools: {:staking, "getPoolsInactive", []},
+      pools_likely: {:staking, "getPoolsToBeElected", []},
+      pools_likelihood: {:staking, "getPoolsLikelihood", []},
+      validators: {:validator_set, "getValidators", []}
+    ]
+  end
+
+  def pool_staking_requests(staking_address) do
+    [
+      mining_address_hash: {:validator_set, "miningByStakingAddress", [staking_address]},
+      is_active: {:staking, "isPoolActive", [staking_address]},
+      active_delegators: {:staking, "poolDelegators", [staking_address]},
+      inactive_delegators: {:staking, "poolDelegatorsInactive", [staking_address]},
+      staked_amount: {:staking, "stakeAmountTotalMinusOrderedWithdraw", [staking_address]},
+      self_staked_amount: {:staking, "stakeAmountMinusOrderedWithdraw", [staking_address, staking_address]},
+      block_reward: {:block_reward, "validatorRewardPercent", [staking_address]}
+    ]
+  end
+
+  def pool_mining_requests(mining_address) do
+    [
+      is_validator: {:validator_set, "isValidator", [mining_address]},
+      was_validator_count: {:validator_set, "validatorCounter", [mining_address]},
+      is_banned: {:validator_set, "isValidatorBanned", [mining_address]},
+      banned_until: {:validator_set, "bannedUntil", [mining_address]},
+      was_banned_count: {:validator_set, "banCounter", [mining_address]}
+    ]
+  end
+
+  def delegator_requests(pool_address, delegator_address) do
+    [
+      stake_amount: {:staking, "stakeAmount", [pool_address, delegator_address]},
+      ordered_withdraw: {:staking, "orderedWithdrawAmount", [pool_address, delegator_address]},
+      max_withdraw_allowed: {:staking, "maxWithdrawAllowed", [pool_address, delegator_address]},
+      max_ordered_withdraw_allowed: {:staking, "maxWithdrawOrderAllowed", [pool_address, delegator_address]},
+      ordered_withdraw_epoch: {:staking, "orderWithdrawEpoch", [pool_address, delegator_address]}
+    ]
+  end
+
+  def perform_requests(requests, contracts, abi) do
+    requests
+    |> generate_requests(contracts)
+    |> Reader.query_contracts(abi)
+    |> parse_responses(requests)
+  end
+
+  def perform_grouped_requests(requests, keys, contracts, abi) do
+    requests
+    |> List.flatten()
+    |> generate_requests(contracts)
+    |> Reader.query_contracts(abi)
+    |> parse_grouped_responses(keys, requests)
+  end
+
+  defp generate_requests(functions, contracts) do
+    Enum.map(functions, fn {_, {contract, function, args}} ->
+      %{
+        contract_address: contracts[contract],
+        function_name: function,
+        args: args
+      }
+    end)
+  end
+
+  defp parse_responses(responses, requests) do
+    requests
+    |> Enum.zip(responses)
+    |> Enum.into(%{}, fn {{key, _}, {:ok, response}} ->
+      case response do
+        [item] -> {key, item}
+        items -> {key, items}
+      end
+    end)
+  end
+
+  defp parse_grouped_responses(responses, keys, grouped_requests) do
+    {grouped_responses, _} = Enum.map_reduce(grouped_requests, responses, &Enum.split(&2, length(&1)))
+
+    [keys, grouped_requests, grouped_responses]
+    |> Enum.zip()
+    |> Enum.into(%{}, fn {key, requests, responses} ->
+      {key, parse_responses(responses, requests)}
+    end)
+  end
+end
