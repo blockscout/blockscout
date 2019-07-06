6a5916764db12f1397dd36a9e3edef90f26a1138
Paul Tsupikoff <goodsoft@goodsoft.lv>
2019-07-06 17:14:51 +0300

    Consolidate POSDAO contract reading (#2371)
    Previously, staking contract reading was arbitrarily spread across
    Explorer.Staking.ContractState, Explorer.Staking.PoolsReader and
    Indexer.Fetcher.StakingPools. Using async fetcher infrastructure for staking
    contracts is inadequate, as blocks may arrive out of order, while we want
    fetching to be triggered by newer block arrival. Also, contract calls were
    not batched enough, and it was hard to follow their sequence across involved modules.
    
    Now, on each incoming block, which is newer than the last seen, we fully update
    not only global contract state, but all validators, pools and delegators.
    
    All requests are intelligently compiled into four batches.
    These are defined together to give better overview of performed actions.
    Chain.import infrastructure is still used for inserting data into DB
    to leverage smart batching of INSERT queries.
    
    Likelihood calculation is added: it zips results of getPoolsToBeElected
    and getPoolsLikelihood calls to make a map of addresses.
    
    Only POS_STAKING_CONTRACT env variable is required now.
    Other contract addresses are fetched from it during launch.
    ABIs are concatenated together during launch as well, as we don't support
    batch requests against distinct ABIs yet. This should be addressed in further PRs.
    
    Up-to-date ABIs copied from build artifacts of posdao-contracts repo.
    File names now correspond to contract names, README directs to their origin.
    
    * Fetch inactive delegators, mark pools and delegators deleted (#2205)
    * Fetch min stakes and token contract address from StakingAuRa (#2313)
    * Fetch block reward ratio using validatorRewardPercent getter (#2424)
    * Add missing `banned_until` field in ON CONFLICT clause for staking pools.

--- apps/explorer/lib/explorer/application.ex
+++ apps/explorer/lib/explorer/application.ex
@@ -82,7 +82,7 @@ defmodule Explorer.Application do
       configure(Explorer.Counters.AverageBlockTime),
       configure(Explorer.Counters.Bridge),
       configure(Explorer.Validator.MetadataProcessor),
-      configure(Explorer.Staking.EpochCounter)
+      configure(Explorer.Staking.ContractState)
     ]
     |> List.flatten()
   end
