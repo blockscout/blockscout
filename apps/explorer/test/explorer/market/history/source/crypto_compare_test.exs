32243b0c4ed4bdefc4fd7ca9321762fcf315e5a0
Ayrat Badykov <ayratin555@gmail.com>
2019-07-08 10:43:33 +0300

    fix market history overriding

--- apps/explorer/test/explorer/market/history/source/crypto_compare_test.exs
+++ apps/explorer/test/explorer/market/history/source/crypto_compare_test.exs
@@ -86,5 +86,59 @@ defmodule Explorer.Market.History.Source.CryptoCompareTest do
 
       assert :error == CryptoCompare.fetch_history(3)
     end
+
+    test "rejects empty prices", %{bypass: bypass} do
+      json = """
+      {
+        "Response": "Success",
+        "Type": 100,
+        "Aggregated": false,
+        "Data": [
+          {
+            "time": 1524528000,
+            "close": 0,
+            "high": 9741.91,
+            "low": 8957.68,
+            "open": 0,
+            "volumefrom": 136352.05,
+            "volumeto": 1276464750.74
+          },
+          {
+            "time": 1524614400,
+            "close": 0,
+            "high": 9765.23,
+            "low": 8757.06,
+            "open": 0,
+            "volumefrom": 192797.41,
+            "volumeto": 1779806222.98
+          },
+          {
+            "time": 1524700800,
+            "close": 8804.32,
+            "high": 8965.84,
+            "low": 8669.38,
+            "open": 8873.57,
+            "volumefrom": 74704.5,
+            "volumeto": 661168891
+          }
+        ],
+        "TimeTo": 1524700800,
+        "TimeFrom": 1523836800,
+        "FirstValueInArray": true,
+        "ConversionType": {
+          "type": "direct",
+          "conversionSymbol": ""
+        }
+      }
+      """
+
+      Bypass.expect(bypass, fn conn -> Conn.resp(conn, 200, json) end)
+
+      expected = [
+        %{closing_price: Decimal.from_float(8804.32), date: ~D[2018-04-26], opening_price: Decimal.from_float(8873.57)}
+      ]
+
+      assert {:ok, expected} == CryptoCompare.fetch_history(3)
+    end
   end
 end
