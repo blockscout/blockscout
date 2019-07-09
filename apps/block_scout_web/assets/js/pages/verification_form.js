575afa806d28ae052fb598ea9d8085266ca5c9e2
Ayrat Badykov <ayratin555@gmail.com>
2019-07-09 14:02:43 +0300

    fix external libary function

--- apps/block_scout_web/assets/js/pages/verification_form.js
+++ apps/block_scout_web/assets/js/pages/verification_form.js
@@ -51,6 +51,34 @@ const elements = {
         $('button[data-button-loading="animation"]').click(event => {
           $('#loading').removeClass('d-none')
         })
+
+        $(function () {
+          $('.js-btn-add-contract-libraries').on('click', function () {
+            $('.js-smart-contract-libraries-wrapper').show()
+            $(this).hide()
+          })
+
+          $('.js-smart-contract-form-reset').on('click', function () {
+            $('.js-contract-library-form-group').removeClass('active')
+            $('.js-contract-library-form-group').first().addClass('active')
+            $('.js-smart-contract-libraries-wrapper').hide()
+            $('.js-btn-add-contract-libraries').show()
+            $('.js-add-contract-library-wrapper').show()
+          })
+
+          $('.js-btn-add-contract-library').on('click', function () {
+            let nextContractLibrary = $('.js-contract-library-form-group.active').next('.js-contract-library-form-group')
+
+            if (nextContractLibrary) {
+              nextContractLibrary.addClass('active')
+            }
+
+            if ($('.js-contract-library-form-group.active').length === $('.js-contract-library-form-group').length) {
+              $('.js-add-contract-library-wrapper').hide()
+            }
+          })
+        })
+
         return $el
       }
       return $el
@@ -85,5 +113,32 @@ if ($contractVerificationPage.length) {
 
   $('button[data-button-loading="animation"]').click(event => {
     $('#loading').removeClass('d-none')
+  })
+
+  $(function () {
+    $('.js-btn-add-contract-libraries').on('click', function () {
+      $('.js-smart-contract-libraries-wrapper').show()
+      $(this).hide()
+    })
+
+    $('.js-smart-contract-form-reset').on('click', function () {
+      $('.js-contract-library-form-group').removeClass('active')
+      $('.js-contract-library-form-group').first().addClass('active')
+      $('.js-smart-contract-libraries-wrapper').hide()
+      $('.js-btn-add-contract-libraries').show()
+      $('.js-add-contract-library-wrapper').show()
+    })
+
+    $('.js-btn-add-contract-library').on('click', function () {
+      let nextContractLibrary = $('.js-contract-library-form-group.active').next('.js-contract-library-form-group')
+
+      if (nextContractLibrary) {
+        nextContractLibrary.addClass('active')
+      }
+
+      if ($('.js-contract-library-form-group.active').length === $('.js-contract-library-form-group').length) {
+        $('.js-add-contract-library-wrapper').hide()
+      }
+    })
   })
 }
