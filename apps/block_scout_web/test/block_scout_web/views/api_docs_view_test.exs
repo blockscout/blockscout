286fb077dd83e7e000b0d3ee74b7d868a8e89098
Ayrat Badykov <ayratin555@gmail.com>
2019-07-10 14:00:34 +0300

    setup tests

--- apps/block_scout_web/test/block_scout_web/views/api_docs_view_test.exs
+++ apps/block_scout_web/test/block_scout_web/views/api_docs_view_test.exs
@@ -4,6 +4,14 @@ defmodule BlockScoutWeb.ApiDocsViewTest do
   alias BlockScoutWeb.APIDocsView
 
   describe "blockscout_url/0" do
+    setup do
+      original = Application.get_env(:block_scout_web, BlockScoutWeb.Endpoint)
+
+      on_exit(fn -> Application.put_env(:block_scout_web, BlockScoutWeb.Endpoint, original) end)
+
+      :ok
+    end
+
     test "returns url with scheme and host without port" do
       Application.put_env(:block_scout_web, BlockScoutWeb.Endpoint,
         url: [scheme: "https", host: "blockscout.com", port: 9999, path: "/"]
