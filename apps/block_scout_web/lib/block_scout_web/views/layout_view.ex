430ddd086249afea78db301f38e6ca88222b2f09
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2019-07-11 16:06:22 +0530

    Fix: Networks Updated

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -7,61 +7,13 @@ defmodule BlockScoutWeb.LayoutView do
   @issue_url "https://github.com/poanetwork/blockscout/issues/new"
   @default_other_networks [
     %{
-      title: "POA Core",
-      url: "https://blockscout.com/poa/core"
-    },
-    %{
-      title: "POA Sokol",
-      url: "https://blockscout.com/poa/sokol",
+      title: "TestnetV2",
+      url: "https://explorer.testnet2.matic.network/",
       test_net?: true
     },
     %{
-      title: "xDai Chain",
-      url: "https://blockscout.com/poa/dai"
-    },
-    %{
-      title: "Ethereum Mainnet",
-      url: "https://blockscout.com/eth/mainnet"
-    },
-    %{
-      title: "Kovan Testnet",
-      url: "https://blockscout.com/eth/kovan",
-      test_net?: true
-    },
-    %{
-      title: "Ropsten Testnet",
-      url: "https://blockscout.com/eth/ropsten",
-      test_net?: true
-    },
-    %{
-      title: "Goerli Testnet",
-      url: "https://blockscout.com/eth/goerli",
-      test_net?: true
-    },
-    %{
-      title: "Rinkeby Testnet",
-      url: "https://blockscout.com/eth/rinkeby",
-      test_net?: true
-    },
-    %{
-      title: "Ethereum Classic",
-      url: "https://blockscout.com/etc/mainnet",
-      other?: true
-    },
-    %{
-      title: "Aerum Mainnet",
-      url: "https://blockscout.com/aerum/mainnet",
-      other?: true
-    },
-    %{
-      title: "Callisto Mainnet",
-      url: "https://blockscout.com/callisto/mainnet",
-      other?: true
-    },
-    %{
-      title: "RSK Mainnet",
-      url: "https://blockscout.com/rsk/mainnet",
-      other?: true
+      title: "Alpha Mainnet",
+      url: "https://alpha-mainnet.explorer.matic.network"
     }
   ]
 
