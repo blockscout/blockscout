0895eb5ae993e65fe61ea5d43da57054c0fc7e73
slightlycyborg <collinalexbell@gmail.com>
2019-07-17 23:23:54 +0000

    router

---
+++ apps/block_scout_web/lib/block_scout_web/controllers/chain/transaction_history_chart_controller.ex
@@ -0,0 +1,41 @@
+defmodule BlockScoutWeb.Chain.TransactionHistoryChartController do
+  use BlockScoutWeb, :controller
+
+  alias Explorer.Chain.Transaction.History.TransactionStats
+
+  def show(conn, _params) do
+    with true <- ajax?(conn) do
+      [{:history_size, history_size}]  = Application.get_env(:block_scout_web, __MODULE__, 30)
+
+      latest = Date.utc_today()
+      earliest = Date.add(latest, -1 * history_size)
+
+      transaction_history_data = TransactionStats.by_date_range(earliest, latest)
+      |> extract_history
+      |> encode_transaction_history_data
+
+
+      IO.inspect TransactionStats.by_date_range(earliest, latest)
+
+      json(conn, %{
+        history_data: transaction_history_data,
+      })
+    else
+      _ -> unprocessable_entity(conn)
+    end
+  end
+
+  defp extract_history(db_results) do
+    Enum.map(db_results, fn row ->
+      %{date: row.date, number_of_transactions: row.number_of_transactions} end)
+  end
+
+  defp encode_transaction_history_data(transaction_history_data) do
+    transaction_history_data
+    |> Jason.encode()
+    |> case do
+      {:ok, data} -> data
+      _ -> []
+    end
+  end
+end
