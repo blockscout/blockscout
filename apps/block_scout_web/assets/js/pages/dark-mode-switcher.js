0266a41bc8d45a0b285416e20b2ab1fa17204a37
maxgrapps <maxgrapps@gmail.com>
2019-07-18 20:04:12 +0300

    dark theme added

---
+++ apps/block_scout_web/assets/js/pages/dark-mode-switcher.js
@@ -0,0 +1,10 @@
+import $ from 'jquery'
+
+$('.dark-mode-changer').click(function () {
+  if (localStorage.getItem('current-color-mode') === 'dark') {
+    localStorage.setItem('current-color-mode', 'light')
+  } else {
+  	localStorage.setItem('current-color-mode', 'dark')
+  }
+  document.location.reload(true)
+})
