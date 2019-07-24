ca6c67d6e9896d7ee053c1748ba2e96b526b5568
Ayrat Badykov <ayratin555@gmail.com>
2019-07-24 12:37:24 +0300

    check cache status in chain module

--- apps/block_scout_web/lib/block_scout_web/controllers/api/v1/health_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/v1/health_controller.ex
@@ -1,31 +1,25 @@
 defmodule BlockScoutWeb.API.V1.HealthController do
   use BlockScoutWeb, :controller
 
-  alias Explorer.{Chain, PagingOptions}
+  alias Explorer.Chain
 
   def health(conn, _) do
-    with {:ok, number, timestamp} <- Chain.last_block_status() do
-      send_resp(conn, :ok, result(number, timestamp))
+    with {:ok, number, timestamp} <- Chain.last_db_block_status(),
+         {:ok, cache_number, cache_timestamp} <- Chain.last_cache_block_status() do
+      send_resp(conn, :ok, result(number, timestamp, cache_number, cache_timestamp))
     else
       status -> send_resp(conn, :internal_server_error, error(status))
     end
   end
 
-  def result(number, timestamp) do
-    latest_block_in_cache =
-      [
-        paging_options: %PagingOptions{page_size: 1}
-      ]
-      |> Chain.list_blocks()
-      |> List.last()
-
+  def result(number, timestamp, cache_number, cache_timestamp) do
     %{
       "healthy" => true,
       "data" => %{
-        "db_latest_block_number" => to_string(number),
-        "db_latest_block_inserted_at" => to_string(timestamp),
-        "cache_latest_block_number" => to_string(latest_block_in_cache.number),
-        "cache_latest_block_inserted_at" => to_string(latest_block_in_cache.timestamp)
+        "latest_block_number" => to_string(number),
+        "latest_block_inserted_at" => to_string(timestamp),
+        "cache_latest_block_number" => to_string(cache_number),
+        "cache_latest_block_inserted_at" => to_string(cache_timestamp)
       }
     }
     |> Jason.encode!()
@@ -49,8 +43,8 @@ defmodule BlockScoutWeb.API.V1.HealthController do
       "error_description" =>
         "There are no new blocks in the DB for the last 5 mins. Check the healthiness of Ethereum archive node or the Blockscout DB instance",
       "data" => %{
-        "db_latest_block_number" => to_string(number),
-        "db_latest_block_inserted_at" => to_string(timestamp)
+        "latest_block_number" => to_string(number),
+        "latest_block_inserted_at" => to_string(timestamp)
       }
     }
     |> Jason.encode!()
