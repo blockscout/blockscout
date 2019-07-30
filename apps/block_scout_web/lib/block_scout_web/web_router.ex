596f00661551cbdcedbed791e94ecc686d15a3fb
goodsoft <goodsoft@goodsoft.lv>
2019-07-30 21:43:10 +0300

    Render lists of validators and staking pools (#2215)

--- apps/block_scout_web/lib/block_scout_web/web_router.ex
+++ apps/block_scout_web/lib/block_scout_web/web_router.ex
@@ -36,6 +36,10 @@ defmodule BlockScoutWeb.WebRouter do
 
     get("/uncles", BlockController, :uncle, as: :uncle)
 
+    get("/validators", StakesController, :index, as: :validators, assigns: %{filter: :validator})
+    get("/active_pools", StakesController, :index, as: :active_pools, assigns: %{filter: :active})
+    get("/inactive_pools", StakesController, :index, as: :inactive_pools, assigns: %{filter: :inactive})
+
     resources("/pending_transactions", PendingTransactionController, only: [:index])
 
     resources("/recent_transactions", RecentTransactionsController, only: [:index])
