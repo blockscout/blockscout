ca67e0229712bdceab0e5fe9918882194a2f5aa4
goodsoft <goodsoft@goodsoft.lv>
2019-07-30 22:02:15 +0300

    Make status modals generic and add modal utilities (#2455)
    As status modals are generic enough, they are moved to the main template.
    
    open{Error,Warning,Success}Modal methods just open corresponding modals.
    openQuestionModal allows changing button titles and callbacks.
    It also automatically blocks UI for non-default callback.
    
    For other modals the following methods are added:
    openModal — opens modal, making sure that hiding the previous one doesn't
    break page layout (Bootstrap doesn't provide such functionality out-of-box).
    lockModal — disables all buttons and closing modal, shows spinner on main button.
    unlockModal — reverts the above.

--- apps/block_scout_web/assets/js/lib/modals.js
+++ apps/block_scout_web/assets/js/lib/modals.js
@@ -1,76 +1,140 @@
 import $ from 'jquery'
-import Chart from 'chart.js'
 
-$(function () {
-  $('.js-become-candidate').on('click', function () {
-    $('#becomeCandidateModal').modal()
-  })
+let $currentModal = null
+let modalLocked = false
 
-  $('.js-validator-info-modal').on('click', function () {
-    $('#validatorInfoModal').modal()
-  })
+const spinner =
+  `
+    <span class="loading-spinner-small mr-2">
+      <span class="loading-spinner-block-1"></span>
+      <span class="loading-spinner-block-2"></span>
+    </span>
+  `
 
-  $('.js-move-stake').on('click', function () {
-    $('#errorStatusModal').modal()
-  })
-
-  $('.js-remove-pool').on('click', function () {
-    $('#warningStatusModal').modal()
-  })
-
-  $('.js-copy-address').on('click', function () {
-    $('#successStatusModal').modal()
-  })
-
-  $('.js-stake-stake').on('click', function () {
-    const modal = '#stakeModal'
-    const progress = parseInt($(`${modal} .js-stakes-progress-data-progress`).text())
-    const total = parseInt($(`${modal} .js-stakes-progress-data-total`).text())
-
-    $(modal).modal()
-
-    setupStakesProgress(progress, total, modal)
-  })
-
-  $('.js-withdraw-stake').on('click', function () {
-    const modal = '#withdrawModal'
-    const progress = parseInt($(`${modal} .js-stakes-progress-data-progress`).text())
-    const total = parseInt($(`${modal} .js-stakes-progress-data-total`).text())
-
-    $(modal).modal()
-
-    setupStakesProgress(progress, total, modal)
-  })
-
-  function setupStakesProgress (progress, total, modal) {
-    const stakeProgress = $(`${modal} .js-stakes-progress`)
-    const primaryColor = $('.btn-full-primary').css('background-color')
-    const backgroundColors = [
-      primaryColor,
-      'rgba(202, 199, 226, 0.5)'
-    ]
-    const progressBackground = total - progress
-
-    // eslint-disable-next-line no-unused-vars
-    let myChart = new Chart(stakeProgress, {
-      type: 'doughnut',
-      data: {
-        datasets: [{
-          data: [progress, progressBackground],
-          backgroundColor: backgroundColors,
-          hoverBackgroundColor: backgroundColors,
-          borderWidth: 0
-        }]
-      },
-      options: {
-        cutoutPercentage: 80,
-        legend: {
-          display: false
-        },
-        tooltips: {
-          enabled: false
-        }
-      }
-    })
+$(document.body).on('hide.bs.modal', e => {
+  if (modalLocked) {
+    e.preventDefault()
+    e.stopPropagation()
+    return false
   }
+
+  $currentModal = null
 })
+
+export function openModal ($modal) {
+  if ($currentModal) {
+    modalLocked = false
+
+    $currentModal
+      .one('hidden.bs.modal', () => {
+        $modal.modal('show')
+        $currentModal = $modal
+      })
+      .modal('hide')
+  } else {
+    $modal.modal('show')
+    $currentModal = $modal
+  }
+}
+
+export function lockModal ($modal, $submitButton = null) {
+  $modal.find('.close-modal').attr('disabled', true)
+
+  const $button = $submitButton || $modal.find('.btn-add-full')
+
+  $button
+    .attr('data-text', $button.text())
+    .attr('disabled', true)
+    .html(spinner)
+
+  modalLocked = true
+}
+
+export function unlockModal ($modal, $submitButton = null) {
+  $modal.find('.close-modal').attr('disabled', false)
+
+  const $button = $submitButton || $modal.find('.btn-add-full')
+
+  $button
+    .text($button.attr('data-text'))
+    .attr('disabled', false)
+
+  modalLocked = false
+}
+
+export function openErrorModal (title, text) {
+  const $modal = $('#errorStatusModal')
+  $modal.find('.modal-status-title').text(title)
+  $modal.find('.modal-status-text').text(text)
+  openModal($modal)
+}
+
+export function openWarningModal (title, text) {
+  const $modal = $('#warningStatusModal')
+  $modal.find('.modal-status-title').text(title)
+  $modal.find('.modal-status-text').text(text)
+  openModal($modal)
+}
+
+export function openSuccessModal (title, text) {
+  const $modal = $('#successStatusModal')
+  $modal.find('.modal-status-title').text(title)
+  $modal.find('.modal-status-text').text(text)
+  openModal($modal)
+}
+
+export function openQuestionModal (title, text, acceptCallback = null, exceptCallback = null, acceptText = 'Yes', exceptText = 'No') {
+  const $modal = $('#questionStatusModal')
+
+  $modal.find('.modal-status-title').text(title)
+  $modal.find('.modal-status-text').text(text)
+
+  const $accept = $modal.find('.btn-line.accept')
+  const $except = $modal.find('.btn-line.except')
+
+  $accept
+    .removeAttr('data-dismiss')
+    .unbind('click')
+    .find('.btn-line-text').text(acceptText)
+
+  $except.removeAttr('data-dismiss')
+    .removeAttr('data-dismiss')
+    .unbind('click')
+    .find('.btn-line-text').text(exceptText)
+
+  if (acceptCallback) {
+    $accept.on('click', event => {
+      $accept
+        .unbind('click')
+        .find('.btn-line-text').html(spinner)
+      $except
+        .unbind('click')
+        .removeAttr('data-dismiss')
+
+      modalLocked = true
+      acceptCallback($modal, event)
+    })
+  } else {
+    $accept.attr('data-dismiss', 'modal')
+  }
+
+  if (exceptCallback) {
+    $except.on('click', event => {
+      $modal.find('.close-modal').attr('disabled', true)
+
+      $except
+        .unbind('click')
+        .find('.btn-line-text').html(spinner)
+      $accept
+        .unbind('click')
+        .removeAttr('data-dismiss')
+
+      modalLocked = true
+      exceptCallback($modal, event)
+    })
+  } else {
+    $except.attr('data-dismiss', 'modal')
+  }
+
+  openModal($modal)
+}
