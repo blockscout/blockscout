0dba42376d1239cc9e1c46f22dff9d87cadf6db6
maxgrapps <maxgrapps@gmail.com>
2019-08-07 23:12:35 +0300

    hash middle truncation added

---
+++ apps/block_scout_web/assets/js/pages/address/hash-truncate.js
@@ -0,0 +1,17 @@
+var truncate = function (fullStr, strLen, separator) {
+  if (fullStr.length <= strLen) {
+    return fullStr
+  }
+  separator = separator || '...'
+  var sepLen = separator.length
+  var charsToShow = strLen - sepLen
+  var frontChars = Math.ceil(charsToShow / 2)
+  var backChars = Math.floor(charsToShow / 2) 
+  return fullStr.substr(0, frontChars) + separator + fullStr.substr(fullStr.length - backChars)
+}
+// apply
+var tStrCheck = document.getElementById('tStr')
+if (document.body.contains(tStrCheck)) {
+  var tStr = document.getElementById('tStr').innerHTML
+  document.getElementById('tStr').innerHTML = truncate(tStr, 25)
+}
