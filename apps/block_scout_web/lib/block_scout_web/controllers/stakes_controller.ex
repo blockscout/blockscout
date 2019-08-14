c5f713c2c10ca7ec92d5358c7d5923646832f398
Paul Tsupikoff <goodsoft@goodsoft.lv>
2019-08-14 09:42:25 +0200

    Periodically refresh the table with staking pools (#2557)
    The table is refreshed when new staking epoch starts and after an interval
    configured in apps/block_scout_web/config/config.exs.
    If no interval is configured, the table is only updated on new epoch.

--- apps/block_scout_web/lib/block_scout_web/controllers/stakes_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/stakes_controller.ex
@@ -104,7 +104,8 @@ defmodule BlockScoutWeb.StakesController do
       top: render_top(conn),
       pools_type: filter,
       current_path: current_path(conn),
-      average_block_time: AverageBlockTime.average_block_time()
+      average_block_time: AverageBlockTime.average_block_time(),
+      refresh_interval: Application.get_env(:block_scout_web, BlockScoutWeb.Chain)[:staking_table_refresh_interval]
     )
   end
 
