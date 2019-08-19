e51efc5e062c6c65b626718ea7943698fca4afca
Ayrat Badykov <ayratin555@gmail.com>
2019-08-19 15:53:10 +0300

    mix format

--- apps/explorer/lib/explorer/chain_spec/poa/importer.ex
+++ apps/explorer/lib/explorer/chain_spec/poa/importer.ex
@@ -35,17 +35,16 @@ defmodule Explorer.ChainSpec.POA.Importer do
     block_reward = block_reward_amount()
     emission_funds = emission_funds_amount()
 
-    rewards =
-      [
-        %{
-          block_range: %Range{from: 0, to: @emission_funds_block_start},
-          reward: %Wei{value: block_reward}
-        },
-        %{
-          block_range: %Range{from: @emission_funds_block_start + 1, to: :infinity},
-          reward: %Wei{value: Decimal.add(block_reward, emission_funds)}
-        }
-      ]
+    rewards = [
+      %{
+        block_range: %Range{from: 0, to: @emission_funds_block_start},
+        reward: %Wei{value: block_reward}
+      },
+      %{
+        block_range: %Range{from: @emission_funds_block_start + 1, to: :infinity},
+        reward: %Wei{value: Decimal.add(block_reward, emission_funds)}
+      }
+    ]
 
     {_, nil} = Repo.delete_all(EmissionReward)
     {_, nil} = Repo.insert_all(EmissionReward, rewards)
