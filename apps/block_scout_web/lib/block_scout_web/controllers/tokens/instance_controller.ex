f47e21e5e46e907a1c8c8eaba5d9899a88d6da26
Ayrat Badykov <ayratin555@gmail.com>
2019-08-30 14:54:33 +0300

    fix credo

--- apps/block_scout_web/lib/block_scout_web/controllers/tokens/instance_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/tokens/instance_controller.ex
@@ -7,7 +7,7 @@ defmodule BlockScoutWeb.Tokens.InstanceController do
     with {:ok, hash} <- Chain.string_to_address_hash(token_address_hash),
          {:ok, _token_address} <- Chain.token_from_address_hash(hash, []),
          {:ok, _token_transfer} <-
-           Chain.erc721_token_instance_from_token_id_and_token_address(token_id, hash) |> IO.inspect() do
+           Chain.erc721_token_instance_from_token_id_and_token_address(token_id, hash) do
       redirect(conn, to: token_instance_transfer_path(conn, :index, token_id, token_address_hash))
     else
       _ ->
