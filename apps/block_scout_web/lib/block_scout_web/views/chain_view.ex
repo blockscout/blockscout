d2d99eb60b3170d7f5aa158c150ca7ea3093d510
Ayrat Badykov <ayratin555@gmail.com>
2019-09-02 10:57:15 +0300

    add fallback

--- apps/block_scout_web/lib/block_scout_web/views/chain_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/chain_view.ex
@@ -12,6 +12,10 @@ defmodule BlockScoutWeb.ChainView do
     Decimal.mult(available_supply, usd_value)
   end
 
+  defp market_cap(:standard, exchange_rate) do
+    exchange_rate.market_cap_usd
+  end
+
   defp market_cap(module, exchange_rate) do
     module.market_cap(exchange_rate)
   end
