ce624074a8be81d1a754618f72941280834e34ff
Paul Tsupikoff <goodsoft@goodsoft.lv>
2019-09-03 22:20:17 +0300

    Cut and ellipsize token amounts (#2656)
    Problem: fractional token amounts are too long and break page layout.
    
    Solution: add token amount formatting function that cuts and ellipsizes
    token amounts according to token settings and passed parameters.
    
    By default, amounts are cut to 5 fractional digits, "..." with tooltip
    is added if needed, and token symbol is attached.
    This can be changed by passing additional list of parameters.

--- apps/block_scout_web/lib/block_scout_web/views/stakes_helpers.ex
+++ apps/block_scout_web/lib/block_scout_web/views/stakes_helpers.ex
@@ -2,7 +2,10 @@ defmodule BlockScoutWeb.StakesHelpers do
   @moduledoc """
   Helpers for staking templates
   """
+  alias BlockScoutWeb.CldrHelper.Number
   alias Explorer.Chain.Cache.BlockNumber
+  alias Explorer.Chain.Token
+  alias Phoenix.HTML
   alias Timex.Duration
 
   def amount_ratio(pool) do
@@ -37,4 +40,39 @@ defmodule BlockScoutWeb.StakesHelpers do
   def list_title(:validator), do: "Validators"
   def list_title(:active), do: "Active Pools"
   def list_title(:inactive), do: "Inactive Pools"
+
+  def format_token_amount(amount, token, options \\ [])
+  def format_token_amount(nil, _token, _options), do: "-"
+  def format_token_amount(amount, nil, options), do: format_token_amount(amount, %Token{}, options)
+
+  def format_token_amount(amount, token, options) when is_integer(amount) do
+    amount
+    |> Decimal.new()
+    |> format_token_amount(token, options)
+  end
+
+  def format_token_amount(%Decimal{} = amount, %Token{} = token, options) do
+    symbol = if Keyword.get(options, :symbol, true), do: " #{token.symbol}"
+    digits = Keyword.get(options, :digits, 5)
+    ellipsize = Keyword.get(options, :ellipsize, true)
+    decimals = if token.decimals, do: Decimal.to_integer(token.decimals), else: 0
+
+    reduced =
+      amount.sign
+      |> Decimal.new(amount.coef, amount.exp - decimals)
+      |> Decimal.reduce()
+
+    if digits >= -reduced.exp or not ellipsize do
+      "#{Number.to_string!(reduced, fractional_digits: min(digits, -reduced.exp))}#{symbol}"
+    else
+      HTML.raw(~s"""
+        <span
+          data-placement="top"
+          data-toggle="tooltip"
+          title="#{Number.to_string!(reduced, fractional_digits: -reduced.exp)}#{symbol}">
+          #{Number.to_string!(reduced, fractional_digits: digits)}...#{symbol}
+        </span>
+      """)
+    end
+  end
 end
