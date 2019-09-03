821c3e729127905c357a42881c1e85d08e458cd7
Paul Tsupikoff <goodsoft@goodsoft.lv>
2019-09-03 22:10:09 +0300

    Validate staking contract calls in JS and allow adding <1 POA to existing stake (#2573)
    * Add input validation for "Become a Candidate" dialog
      - Mining address is trimmed before validation
      - Amount is checked against current token balance
    
    * Add input validation for "Make Stake" dialog
      - Amount is checked against current token balance
      - Incrementing stake by less than MIN_DELEGATOR_STAKE is allowed
    
    * Add input validation for "Move Stake" dialog
      - Moving stake is allowed only when resulting stakes are more that minimum
    
    * Add input validation for "Withdraw Stake" dialog
      - Either claim, or withdraw, or claim/withdraw dialogs are displayed
        depending on available amounts
      - Available amounts for immediate and ordered withdrawals are shown
        and taken into account
      - Remaining stake must not go under the minimal delegator stake
      - It is possible to reduce ordered amount, but not below zero
    
    * Validate input data in staking modals in real time (#2638)
      - Inputs in modal dialogs are validated on every input.
      - Errors are displayed on blur.
      - Submit button is enabled/disabled immediately.

--- apps/block_scout_web/assets/js/pages/stakes/withdraw_stake.js
+++ apps/block_scout_web/assets/js/pages/stakes/withdraw_stake.js
@@ -1,6 +1,7 @@
 import $ from 'jquery'
 import { BigNumber } from 'bignumber.js'
-import { openModal, openQuestionModal, lockModal } from '../../lib/modals'
+import { openModal, openErrorModal, openQuestionModal, lockModal } from '../../lib/modals'
+import { setupValidation } from '../../lib/validation'
 import { makeContractCall, setupChart } from './utils'
 
 export function openWithdrawStakeModal (event, store) {
@@ -9,13 +10,15 @@ export function openWithdrawStakeModal (event, store) {
   store.getState().channel
     .push('render_withdraw_stake', { address })
     .receive('ok', msg => {
-      if (msg.claim_html) {
+      if (msg.claim_html && msg.withdraw_html) {
         openQuestionModal(
           'Claim or order', 'Do you want withdraw or claim ordered withdraw?',
           () => setupClaimWithdrawModal(address, store, msg),
           () => setupWithdrawStakeModal(address, store, msg),
           'Claim', 'Withdraw'
         )
+      } else if (msg.claim_html) {
+        setupClaimWithdrawModal(address, store, msg)
       } else {
         setupWithdrawStakeModal(address, store, msg)
       }
@@ -33,14 +36,38 @@ function setupClaimWithdrawModal (address, store, msg) {
 }
 
 function setupWithdrawStakeModal (address, store, msg) {
-  const $modal = $(msg.html)
+  const $modal = $(msg.withdraw_html)
   setupChart($modal.find('.js-stakes-progress'), msg.self_staked_amount, msg.staked_amount)
+  setupValidation(
+    $modal.find('form'),
+    {
+      'amount': value => isAmountValid(value, store, msg)
+    },
+    $modal.find('form button')
+  )
+
+  setupValidation(
+    $modal.find('form'),
+    {
+      'amount': value => isWithdrawAmountValid(value, store, msg)
+    },
+    $modal.find('form button.withdraw')
+  )
+
+  setupValidation(
+    $modal.find('form'),
+    {
+      'amount': value => isOrderWithdrawAmountValid(value, store, msg)
+    },
+    $modal.find('form button.order-withdraw')
+  )
+
   $modal.find('.btn-full-primary.withdraw').click(() => {
-    withdrawStake($modal, address, store)
+    withdrawStake($modal, address, store, msg)
     return false
   })
   $modal.find('.btn-full-primary.order-withdraw').click(() => {
-    orderWithdraw($modal, address, store)
+    orderWithdraw($modal, address, store, msg)
     return false
   })
   openModal($modal)
@@ -50,28 +77,85 @@ function claimWithdraw ($modal, address, store) {
   lockModal($modal)
 
   const stakingContract = store.getState().stakingContract
-
   makeContractCall(stakingContract.methods.claimOrderedWithdraw(address), store)
 }
 
-function withdrawStake ($modal, address, store) {
-  lockModal($modal)
+function withdrawStake ($modal, address, store, msg) {
+  lockModal($modal, $modal.find('.btn-full-primary.withdraw'))
 
   const stakingContract = store.getState().stakingContract
   const decimals = store.getState().tokenDecimals
-
   const amount = new BigNumber($modal.find('[amount]').val().replace(',', '.').trim()).shiftedBy(decimals).integerValue()
 
   makeContractCall(stakingContract.methods.withdraw(address, amount.toString()), store)
 }
 
-function orderWithdraw ($modal, address, store) {
-  lockModal($modal)
+function orderWithdraw ($modal, address, store, msg) {
+  lockModal($modal, $modal.find('.btn-full-primary.order-withdraw'))
 
   const stakingContract = store.getState().stakingContract
   const decimals = store.getState().tokenDecimals
-
+  const orderedWithdraw = new BigNumber(msg.ordered_withdraw)
   const amount = new BigNumber($modal.find('[amount]').val().replace(',', '.').trim()).shiftedBy(decimals).integerValue()
 
+  if (amount.isLessThan(orderedWithdraw.negated())) {
+    openErrorModal('Error', `You cannot reduce withdrawal by more than ${orderedWithdraw.shiftedBy(-decimals)} ${store.getState().tokenSymbol}`)
+    return false
+  }
+
   makeContractCall(stakingContract.methods.orderWithdraw(address, amount.toString()), store)
+}
+
+function isAmountValid (value, store, msg) {
+  const decimals = store.getState().tokenDecimals
+  const minStake = new BigNumber(msg.min_stake)
+  const currentStake = new BigNumber(msg.delegator_staked)
+  const amount = new BigNumber(value.replace(',', '.').trim()).shiftedBy(decimals).integerValue()
+
+  if (!amount.isPositive() && !amount.isNegative()) {
+    return 'Invalid amount'
+  } else if (amount.isLessThan(currentStake) && currentStake.minus(amount).isLessThan(minStake)) {
+    return `A minimum of ${minStake.shiftedBy(-decimals)} ${store.getState().tokenSymbol} is required to remain in the pool, or withdraw the entire amount to leave this pool`
+  }
+
+  return true
+}
+
+function isWithdrawAmountValid (value, store, msg) {
+  const decimals = store.getState().tokenDecimals
+  const minStake = new BigNumber(msg.min_stake)
+  const currentStake = new BigNumber(msg.delegator_staked)
+  const maxAllowed = new BigNumber(msg.max_withdraw_allowed)
+  const amount = new BigNumber(value.replace(',', '.').trim()).shiftedBy(decimals).integerValue()
+
+  if (!amount.isPositive()) {
+    return null
+  } else if (amount.isLessThan(currentStake) && currentStake.minus(amount).isLessThan(minStake)) {
+    return null
+  } else if (!amount.isPositive() || amount.isGreaterThan(maxAllowed)) {
+    return null
+  }
+
+  return true
+}
+
+function isOrderWithdrawAmountValid (value, store, msg) {
+  const decimals = store.getState().tokenDecimals
+  const minStake = new BigNumber(msg.min_stake)
+  const currentStake = new BigNumber(msg.delegator_staked)
+  const orderedWithdraw = new BigNumber(msg.ordered_withdraw)
+  const maxAllowed = new BigNumber(msg.max_ordered_withdraw_allowed)
+  const amount = new BigNumber(value.replace(',', '.').trim()).shiftedBy(decimals).integerValue()
+
+  if (!amount.isPositive() && !amount.isNegative()) {
+    return null
+  } else if (amount.isLessThan(currentStake) && currentStake.minus(amount).isLessThan(minStake)) {
+    return null
+  } else if (amount.isGreaterThan(maxAllowed)) {
+    return null
+  } else if (amount.isLessThan(orderedWithdraw.negated())) {
+    return null
+  }
+
+  return true
 }
