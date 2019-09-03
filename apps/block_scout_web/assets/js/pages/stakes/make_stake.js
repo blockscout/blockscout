873dd362fb19a39f2a0b396d995c4fa888096bd4
Paul Tsupikoff <goodsoft@goodsoft.lv>
2019-09-03 22:10:09 +0300

    Validate staking contract calls in JS and allow adding <1 POA to existing stake (#2573)
    * Add input validation for "Become a Candidate" dialog
      - Mining address is trimmed before validation
      - Amount is checked against current token balance
    
    * Add input validation for "Make Stake" dialog
      - Amount is checked against current token balance
      - Incrementing stake by less than MIN_DELEGATOR_STAKE is allowed
    
    * Add input validation for "Move Stake" dialog
      - Moving stake is allowed only when resulting stakes are more that minimum
    
    * Add input validation for "Withdraw Stake" dialog
      - Either claim, or withdraw, or claim/withdraw dialogs are displayed
        depending on available amounts
      - Available amounts for immediate and ordered withdrawals are shown
        and taken into account
      - Remaining stake must not go under the minimal delegator stake
      - It is possible to reduce ordered amount, but not below zero
    
    * Validate input data in staking modals in real time (#2638)
      - Inputs in modal dialogs are validated on every input.
      - Errors are displayed on blur.
      - Submit button is enabled/disabled immediately.

--- apps/block_scout_web/assets/js/pages/stakes/make_stake.js
+++ apps/block_scout_web/assets/js/pages/stakes/make_stake.js
@@ -1,6 +1,7 @@
 import $ from 'jquery'
 import { BigNumber } from 'bignumber.js'
-import { openModal, openErrorModal, openWarningModal, lockModal } from '../../lib/modals'
+import { openModal, openWarningModal, lockModal } from '../../lib/modals'
+import { setupValidation } from '../../lib/validation'
 import { makeContractCall, setupChart } from './utils'
 
 export function openMakeStakeModal (event, store) {
@@ -16,6 +17,14 @@ export function openMakeStakeModal (event, store) {
     .receive('ok', msg => {
       const $modal = $(msg.html)
       setupChart($modal.find('.js-stakes-progress'), msg.self_staked_amount, msg.staked_amount)
+      setupValidation(
+        $modal.find('form'),
+        {
+          'delegator-stake': value => isDelegatorStakeValid(value, store, msg, address)
+        },
+        $modal.find('form button')
+      )
+
       $modal.find('form').submit(() => {
         makeStake($modal, address, store, msg)
         return false
@@ -30,13 +39,27 @@ function makeStake ($modal, address, store, msg) {
   const stakingContract = store.getState().stakingContract
   const decimals = store.getState().tokenDecimals
 
-  const minStake = new BigNumber(msg.min_delegator_stake)
   const stake = new BigNumber($modal.find('[delegator-stake]').val().replace(',', '.').trim()).shiftedBy(decimals).integerValue()
 
-  if (!stake.isPositive() || stake.isLessThan(minStake)) {
-    openErrorModal('Error', `You cannot stake less than ${minStake.shiftedBy(-decimals)} ${store.getState().tokenSymbol}`)
-    return false
+  makeContractCall(stakingContract.methods.stake(address, stake.toString()), store)
+}
+
+function isDelegatorStakeValid (value, store, msg, address) {
+  const decimals = store.getState().tokenDecimals
+  const minStake = new BigNumber(msg.min_stake)
+  const currentStake = new BigNumber(msg.delegator_staked)
+  const balance = new BigNumber(msg.balance)
+  const stake = new BigNumber(value.replace(',', '.').trim()).shiftedBy(decimals).integerValue()
+  const account = store.getState().account
+
+  if (!stake.isPositive()) {
+    return 'Invalid amount'
+  } else if (stake.plus(currentStake).isLessThan(minStake)) {
+    const staker = (account === address) ? 'candidate' : 'delegate'
+    return `Minimum ${staker} stake is ${minStake.shiftedBy(-decimals)} ${store.getState().tokenSymbol}`
+  } else if (stake.isGreaterThan(balance)) {
+    return 'Insufficient funds'
   }
 
-  makeContractCall(stakingContract.methods.stake(address, stake.toString()), store)
+  return true
 }
