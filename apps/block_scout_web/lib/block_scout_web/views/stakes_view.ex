32bdac8ac78c9644781e9ac3d17a56d0d661f0ea
Ed Man <liberumed@gmail.com>
2019-09-13 21:11:42 +0300

    Fix staking UI issues (#2692)
    2, 3, 4, 5, 6, 7, 12, 13, 19, 26
    
    * Fix items disappearing misbehavior of staking filters
    * Fix items disappearing animation of staking filters
    * Fix not disappearing tooltip in stakes-top container on block number update
    * Fix stakes top stats width to have fixed size on refresh
    * Replace stakes table with flex boxes to fixate size of table header
    * Fix staking modals width
    * Change style for non clickable addresses in delegators list modal
    * Decrease top stats min size to fit in firefox but look nice in logout state
    * Make fixed width for delegators list modal
    * Refactor stakes-address-container to set active and not active styles
    * Reduce gap for modal tile in validation info modal

--- apps/block_scout_web/lib/block_scout_web/views/stakes_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/stakes_view.ex
@@ -1,4 +1,12 @@
 defmodule BlockScoutWeb.StakesView do
   use BlockScoutWeb, :view
   import BlockScoutWeb.StakesHelpers
+
+  def render("scripts.html", %{conn: conn}) do
+    render_scripts(conn, "stakes.js")
+  end
+
+  def render("styles.html", _) do
+    ~E(<link rel="stylesheet" href="/css/stakes.css">)
+  end
 end
