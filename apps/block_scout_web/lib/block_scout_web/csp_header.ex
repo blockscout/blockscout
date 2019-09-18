a87a02ee52ec591a82b759e9f17e0db18263c475
FallenGravity <ethanvb55@gmail.com>
2019-09-18 10:45:31 +0200

    Updates

--- apps/block_scout_web/lib/block_scout_web/csp_header.ex
+++ apps/block_scout_web/lib/block_scout_web/csp_header.ex
@@ -15,7 +15,7 @@ defmodule BlockScoutWeb.CSPHeader do
         default-src 'self';\
         script-src 'self' 'unsafe-inline' 'unsafe-eval';\
         style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com;\
-        img-src 'self' 'unsafe-inline' 'unsafe-eval' data:;\
+        img-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.github.com data:;\
         font-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.gstatic.com data:;\
       "
     })
