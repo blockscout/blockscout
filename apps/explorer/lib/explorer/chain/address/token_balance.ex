79fa24013c401dfb29c69dd091c2d01c4c2bbb42
Ayrat Badykov <ayratin555@gmail.com>
2019-09-18 13:12:24 +0300

    check if value is null in token balances refetch query

--- apps/explorer/lib/explorer/chain/address/token_balance.ex
+++ apps/explorer/lib/explorer/chain/address/token_balance.ex
@@ -82,7 +82,7 @@ defmodule Explorer.Chain.Address.TokenBalance do
       tb in TokenBalance,
       join: t in Token,
       on: tb.token_contract_address_hash == t.contract_address_hash,
-      where: is_nil(tb.value_fetched_at),
+      where: is_nil(tb.value_fetched_at) or is_nil(tb.value),
       where: (tb.address_hash != ^@burn_address_hash and t.type != "ERC-721") or t.type == "ERC-20"
     )
   end
