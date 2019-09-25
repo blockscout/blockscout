50ad94f47b1046dbc5282f31348fa2f26df1c405
pasqu4le <pasqu4le@gmail.com>
2019-09-25 20:18:59 +0200

    Avoid internal_transactions import retries on blocks without valid transactions
    Problem: for some block numbers we fetch and receive internal transactions, but in the database the transactions they are part of either do not exist for that block or are pending.
    This causes a foreign key error and an endless cycle of import retries.
    
    Solution: instead of failing we remove consensus for such problematic blocks, so that they can be refetched with the correct transactions.

--- apps/explorer/lib/explorer/chain/import/runner/internal_transactions.ex
+++ apps/explorer/lib/explorer/chain/import/runner/internal_transactions.ex
@@ -4,9 +4,10 @@ defmodule Explorer.Chain.Import.Runner.InternalTransactions do
   """
 
   require Ecto.Query
+  require Logger
 
   alias Ecto.{Changeset, Multi, Repo}
-  alias Explorer.Chain.{Hash, Import, InternalTransaction, Transaction}
+  alias Explorer.Chain.{Block, Hash, Import, InternalTransaction, Transaction}
   alias Explorer.Chain.Import.Runner
 
   import Ecto.Query, only: [from: 2]
@@ -58,6 +59,14 @@ defmodule Explorer.Chain.Import.Runner.InternalTransactions do
     |> Multi.run(:internal_transactions_indexed_at_transactions, fn repo, %{acquire_transactions: transactions} ->
       update_transactions(repo, transactions, update_transactions_options)
     end)
+    |> Multi.run(
+      :remove_consensus_of_missing_transactions_blocks,
+      fn repo, %{internal_transactions: inserted} = results_map ->
+        # NOTE: for this to work it has to follow the runner `internal_transactions_indexed_at_blocks`
+        block_hashes = Map.get(results_map, :internal_transactions_indexed_at_blocks, [])
+        remove_consensus_of_missing_transactions_blocks(repo, block_hashes, changes_list, inserted)
+      end
+    )
   end
 
   @impl Runner
@@ -76,12 +85,11 @@ defmodule Explorer.Chain.Import.Runner.InternalTransactions do
 
     transactions_map = Map.new(transactions, &{&1.hash, &1})
 
-    # Enforce InternalTransaction ShareLocks order (see docs: sharelocks.md)
-    ordered_changes_list = Enum.sort_by(changes_list, &{&1.transaction_hash, &1.index})
-
     final_changes_list =
-      ordered_changes_list
-      |> reject_pending_transactions(transactions_map)
+      changes_list
+      # Enforce InternalTransaction ShareLocks order (see docs: sharelocks.md)
+      |> Enum.sort_by(&{&1.transaction_hash, &1.index})
+      |> reject_missing_transactions(transactions_map)
 
     {:ok, internal_transactions} =
       Import.insert_changes_list(
@@ -90,16 +98,12 @@ defmodule Explorer.Chain.Import.Runner.InternalTransactions do
         conflict_target: [:transaction_hash, :index],
         for: InternalTransaction,
         on_conflict: on_conflict,
-        returning: [:transaction_hash, :index],
+        returning: true,
         timeout: timeout,
         timestamps: timestamps
       )
 
-    {:ok,
-     for(
-       internal_transaction <- internal_transactions,
-       do: Map.take(internal_transaction, [:id, :index, :transaction_hash])
-     )}
+    {:ok, internal_transactions}
   end
 
   defp default_on_conflict do
@@ -162,15 +166,15 @@ defmodule Explorer.Chain.Import.Runner.InternalTransactions do
       from(
         t in Transaction,
         where: t.hash in ^transaction_hashes,
+        # do not consider pending transactions
+        where: not is_nil(t.block_hash),
         select: map(t, [:hash, :block_hash, :block_number]),
         # Enforce Transaction ShareLocks order (see docs: sharelocks.md)
         order_by: t.hash,
         lock: "FOR UPDATE"
       )
 
-    hashes = repo.all(query)
-
-    {:ok, hashes}
+    {:ok, repo.all(query)}
   end
 
   defp update_transactions(repo, transactions, %{
@@ -183,9 +187,8 @@ defmodule Explorer.Chain.Import.Runner.InternalTransactions do
     update_query =
       from(
         t in Transaction,
+        # pending transactions are already excluded by `acquire_transactions`
         where: t.hash in ^transaction_hashes,
-        # do not try to update pending transactions
-        where: not is_nil(t.block_hash),
         # ShareLocks order already enforced by `acquire_transactions` (see docs: sharelocks.md)
         update: [
           set: [
@@ -221,10 +224,49 @@ defmodule Explorer.Chain.Import.Runner.InternalTransactions do
     end
   end
 
-  defp reject_pending_transactions(ordered_changes_list, transactions_map) do
+  # If not using Parity this is not relevant
+  defp remove_consensus_of_missing_transactions_blocks(_, [], _, _), do: {:ok, []}
+
+  defp remove_consensus_of_missing_transactions_blocks(repo, block_hashes, changes_list, inserted) do
+    inserted_block_numbers = MapSet.new(inserted, & &1.block_number)
+
+    missing_transactions_block_numbers =
+      changes_list
+      |> MapSet.new(& &1.block_number)
+      |> MapSet.difference(inserted_block_numbers)
+      |> MapSet.to_list()
+
+    update_query =
+      from(
+        b in Block,
+        where: b.number in ^missing_transactions_block_numbers,
+        where: b.hash in ^block_hashes,
+        # ShareLocks order already enforced by `internal_transactions_indexed_at_blocks` (see docs: sharelocks.md)
+        update: [set: [consensus: false, internal_transactions_indexed_at: nil]]
+      )
+
+    try do
+      {_num, result} = repo.update_all(update_query, [])
+
+      Logger.debug(fn ->
+        [
+          "consensus removed from blocks with numbers: ",
+          inspect(missing_transactions_block_numbers),
+          " because of missing transactions"
+        ]
+      end)
+
+      {:ok, result}
+    rescue
+      postgrex_error in Postgrex.Error ->
+        {:error, %{exception: postgrex_error, missing_transactions_block_numbers: missing_transactions_block_numbers}}
+    end
+  end
+
+  defp reject_missing_transactions(ordered_changes_list, transactions_map) do
     Enum.reject(ordered_changes_list, fn %{transaction_hash: hash} ->
       transactions_map
-      |> Map.fetch!(hash)
+      |> Map.get(hash, %{})
       |> Map.get(:block_hash)
       |> is_nil()
     end)
