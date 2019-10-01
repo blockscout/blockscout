70803de61a9447666611c73f102a70d5a8f2037e
Eduard Sachava <liberumed@gmail.com>
2019-10-01 16:49:41 +0300

    Adapt Staking DApp UI for mobile devices (#2757)
    * Fix broken dropdowns in Stakes navbar, so bootstrap import happens only once
    
    * Separate table styles on accounts page and staking dapp pages
    
    * Fix tabs switching in mobile view for staking dapp
    
    * Fix pools table styles in mobile view for staking dapp
    
    * Fix one column staking modals styles in mobile view
    
    * Fix two column staking modals styles in mobile view
    
    * Fix three column staking modals styles in mobile view
    
    * Fix claim withdraw modal styles in mobile view
    
    * Make stakes top stats grid take two columns on mobile view
    
    * Fix sticking tooltips on top of modals

--- apps/block_scout_web/lib/block_scout_web/views/stakes_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/stakes_view.ex
@@ -2,10 +2,6 @@ defmodule BlockScoutWeb.StakesView do
   use BlockScoutWeb, :view
   import BlockScoutWeb.StakesHelpers
 
-  def render("scripts.html", %{conn: conn}) do
-    render_scripts(conn, "stakes.js")
-  end
-
   def render("styles.html", _) do
     ~E(<link rel="stylesheet" href="/css/stakes.css">)
   end
