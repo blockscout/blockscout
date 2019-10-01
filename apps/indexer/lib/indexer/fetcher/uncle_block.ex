2dd35f667a23dd90701f1a15e8240f059c364ceb
Victor Baranov <baranov.viktor.27@gmail.com>
2019-10-01 13:15:27 +0300

    Fix uncle_block.ex batch size and concurrency

--- apps/indexer/lib/indexer/fetcher/uncle_block.ex
+++ apps/indexer/lib/indexer/fetcher/uncle_block.ex
@@ -23,8 +23,8 @@ defmodule Indexer.Fetcher.UncleBlock do
 
   @defaults [
     flush_interval: :timer.seconds(3),
-    max_batch_size: 0,
-    max_concurrency: 0,
+    max_batch_size: 10,
+    max_concurrency: 10,
     task_supervisor: Indexer.Fetcher.UncleBlock.TaskSupervisor,
     metadata: [fetcher: :block_uncle]
   ]
