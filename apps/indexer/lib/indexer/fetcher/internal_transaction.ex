53aaf1beb49dc7c6725ed2378efb2187fa05e5ef
Victor Baranov <baranov.viktor.27@gmail.com>
2019-10-01 14:46:58 +0300

    Decrease concurrency to 8 for int xs fetcher

--- apps/indexer/lib/indexer/fetcher/internal_transaction.ex
+++ apps/indexer/lib/indexer/fetcher/internal_transaction.ex
@@ -21,7 +21,7 @@ defmodule Indexer.Fetcher.InternalTransaction do
   @behaviour BufferedTask
 
   @max_batch_size 1
-  @max_concurrency 16
+  @max_concurrency 8
   @defaults [
     flush_interval: :timer.seconds(3),
     max_concurrency: @max_concurrency,
