cbb79679e05ebde5133af3e73c180d0308f202b0
Eduard Sachava <liberumed@gmail.com>
2019-10-16 15:16:33 +0300

    Implement logging in using MetaMask extension (#2408)
    We check Web3 every 0.1s to check for currently selected account.
    This is ugly and MetaMask developers admit that, but it's the only way they
    can recommend.
    
    Login button always redirects to metamask.io website, but if the MetaMask
    is detected, it's onclick is overridden to launch MetaMask's Privacy Mode
    unlock dialog.
    
    When an account is detected, it is sent to websocket and used for rendering
    top panel during further staking information updates.
    
    Putting account into session is not implemented, as it _might_ cause privacy
    issues. In the meanwhile, we'll try to think of more clever way for preventing
    login button blinking during page load.
    
    Note: npm cache is invalidated in CI to work around issue of websocket package
    breaking npm install due to .git directory being present.
    
    Co-Authored-By: Anatoly Nikiforov <anatolyniky@gmail.com>
    Co-Authored-By: Kirill Andreev <hindmost.one@gmail.com>

--- apps/block_scout_web/assets/js/pages/stakes.js
+++ apps/block_scout_web/assets/js/pages/stakes.js
@@ -5,9 +5,12 @@ import _ from 'lodash'
 import { subscribeChannel } from '../socket'
 import { connectElements } from '../lib/redux_helpers.js'
 import { createAsyncLoadStore } from '../lib/async_listing_load'
+import Web3 from 'web3'
 
 export const initialState = {
-  channel: null
+  channel: null,
+  web3: null,
+  account: null
 }
 
 export function reducer (state = initialState, action) {
@@ -18,6 +21,12 @@ export function reducer (state = initialState, action) {
     }
     case 'CHANNEL_CONNECTED': {
       return Object.assign({}, state, { channel: action.channel })
+    }
+    case 'WEB3_DETECTED': {
+      return Object.assign({}, state, { web3: action.web3 })
+    }
+    case 'ACCOUNT_UPDATED': {
+      return Object.assign({}, state, { account: action.account })
     }
     default:
       return state
@@ -35,8 +44,47 @@ if ($stakesPage.length) {
   const channel = subscribeChannel('stakes:staking_update')
   channel.on('staking_update', msg => onStakingUpdate(msg, store))
   store.dispatch({ type: 'CHANNEL_CONNECTED', channel })
+
+  initializeWeb3(store)
 }
 
 function onStakingUpdate (msg, store) {
   $('[data-selector="stakes-top"]').html(msg.top_html)
+
+  if (store.getState().web3) {
+    $('[data-selector="login-button"]').on('click', loginByMetamask)
+  }
+}
+
+function initializeWeb3 (store) {
+  if (window.ethereum) {
+    const web3 = new Web3(window.ethereum)
+    store.dispatch({ type: 'WEB3_DETECTED', web3 })
+
+    setInterval(async function () {
+      const accounts = await web3.eth.getAccounts()
+      const account = accounts[0] ? accounts[0].toLowerCase() : null
+
+      if (account !== store.getState().account) {
+        setAccount(account, store)
+      }
+    }, 100)
+  }
+}
+
+function setAccount (account, store) {
+  store.dispatch({ type: 'ACCOUNT_UPDATED', account })
+  store.getState().channel.push('set_account', account)
+}
+
+async function loginByMetamask (event) {
+  event.stopPropagation()
+  event.preventDefault()
+
+  try {
+    await window.ethereum.enable()
+  } catch (e) {
+    console.log(e)
+    console.error('User denied account access')
+  }
 }
