a3c9a1a2206aca9683324bd3ade145a8a4402b6a
Victor Baranov <baranov.viktor.27@gmail.com>
2019-10-17 17:25:11 +0300

    Increase max concurrency for int txs fetcher

--- apps/indexer/lib/indexer/fetcher/internal_transaction.ex
+++ apps/indexer/lib/indexer/fetcher/internal_transaction.ex
@@ -21,7 +21,7 @@ defmodule Indexer.Fetcher.InternalTransaction do
   @behaviour BufferedTask
 
   @max_batch_size 4
-  @max_concurrency 8
+  @max_concurrency 16
   @defaults [
     flush_interval: :timer.seconds(3),
     max_concurrency: @max_concurrency,
