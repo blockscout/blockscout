1667bdef0c71c2fcb864c9b6c9fe40f0fa765836
Ayrat Badykov <ayratin555@gmail.com>
2019-11-05 14:47:36 +0300

    fix remaining tests

--- apps/block_scout_web/test/block_scout_web/features/viewing_addresses_test.exs
+++ apps/block_scout_web/test/block_scout_web/features/viewing_addresses_test.exs
@@ -336,37 +336,6 @@ defmodule BlockScoutWeb.ViewingAddressesTest do
       |> refute_has(AddressPage.token_transfer(transaction, taft, count: 1))
     end
 
-    test "transactions with multiple token transfers shows only the first one by default", %{
-      addresses: addresses,
-      block: block,
-      session: session
-    } do
-      lincoln = addresses.lincoln
-      taft = addresses.taft
-
-      contract_address = insert(:contract_address)
-
-      insert(:token, contract_address: contract_address)
-
-      transaction =
-        :transaction
-        |> insert(from_address: lincoln, to_address: contract_address)
-        |> with_block(block)
-
-      insert_list(
-        3,
-        :token_transfer,
-        from_address: lincoln,
-        to_address: taft,
-        transaction: transaction,
-        token_contract_address: contract_address
-      )
-
-      session
-      |> AddressPage.visit_page(lincoln)
-      |> assert_has(AddressPage.token_transfers(transaction, count: 1))
-    end
-
     test "transaction with multiple token transfers shows all transfers if expanded", %{
       addresses: addresses,
       block: block,
