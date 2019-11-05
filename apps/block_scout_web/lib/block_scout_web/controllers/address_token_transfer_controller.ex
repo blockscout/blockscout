ccbacf69445fbe90237586df86f6ff2aaa7b939e
Ayrat Badykov <ayratin555@gmail.com>
2019-11-05 12:41:58 +0300

    fix merging conflicts

--- apps/block_scout_web/lib/block_scout_web/controllers/address_token_transfer_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/address_token_transfer_controller.ex
@@ -170,7 +170,6 @@ defmodule BlockScoutWeb.AddressTokenTransferController do
       ) do
     with {:ok, address_hash} <- Chain.string_to_address_hash(address_hash_string),
          {:ok, address} <- Chain.hash_to_address(address_hash) do
-      {transaction_count, validation_count} = transaction_and_validation_count(address_hash)
 
       render(
         conn,
@@ -179,8 +178,7 @@ defmodule BlockScoutWeb.AddressTokenTransferController do
         coin_balance_status: CoinBalanceOnDemand.trigger_fetch(address),
         exchange_rate: Market.get_exchange_rate(Explorer.coin()) || Token.null(),
         current_path: current_path(conn),
-        transaction_count: transaction_count,
-        validation_count: validation_count
+        counters_path: address_path(conn, :address_counters, %{"id" => to_string(address_hash)})
       )
     else
       :error ->
