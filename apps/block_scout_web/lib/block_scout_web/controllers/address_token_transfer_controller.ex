85fd91faacdd0b1b0022caf285d33c82917dba36
Ayrat Badykov <ayratin555@gmail.com>
2019-11-06 13:14:13 +0300

    add filter to token transfers

--- apps/block_scout_web/lib/block_scout_web/controllers/address_token_transfer_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/address_token_transfer_controller.ex
@@ -166,7 +166,7 @@ defmodule BlockScoutWeb.AddressTokenTransferController do
 
   def index(
         conn,
-        %{"address_id" => address_hash_string}
+        %{"address_id" => address_hash_string} = params
       ) do
     with {:ok, address_hash} <- Chain.string_to_address_hash(address_hash_string),
          {:ok, address} <- Chain.hash_to_address(address_hash) do
@@ -176,6 +176,7 @@ defmodule BlockScoutWeb.AddressTokenTransferController do
         address: address,
         coin_balance_status: CoinBalanceOnDemand.trigger_fetch(address),
         exchange_rate: Market.get_exchange_rate(Explorer.coin()) || Token.null(),
+        filter: params["filter"],
         current_path: current_path(conn),
         counters_path: address_path(conn, :address_counters, %{"id" => to_string(address_hash)})
       )
