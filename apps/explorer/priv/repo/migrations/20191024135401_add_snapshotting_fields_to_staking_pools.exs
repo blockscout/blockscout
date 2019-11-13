227d3857fd61fc34d9a373fbf21ff51817c9d7d8
Eduard Sachava <liberumed@gmail.com>
2019-11-13 15:35:56 +0200

    Add validator_reward_ratio field and fix staked_ratio value

--- apps/explorer/priv/repo/migrations/20191024135401_add_snapshotting_fields_to_staking_pools.exs
+++ apps/explorer/priv/repo/migrations/20191024135401_add_snapshotting_fields_to_staking_pools.exs
@@ -5,7 +5,8 @@ defmodule Explorer.Repo.Migrations.AddSnapshottingFieldsToStakingPools do
     alter table(:staking_pools) do
       add(:snapshotted_total_staked_amount, :numeric, precision: 100)
       add(:snapshotted_self_staked_amount, :numeric, precision: 100)
-      add(:snapshotted_staked_ratio, :decimal, precision: 5, scale: 2)
+      add(:validator_reward_ratio, :decimal, precision: 5, scale: 2)
+      add(:snapshotted_validator_reward_ratio, :decimal, precision: 5, scale: 2)
     end
   end
 end
