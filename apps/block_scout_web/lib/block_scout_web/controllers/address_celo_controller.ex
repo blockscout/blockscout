9f821b38f8ab2cee5d11b34cb92feb94b489c080
Sami Mäkelä <mrsmkl@gmail.com>
2019-11-27 16:25:34 +0200

    credo

--- apps/block_scout_web/lib/block_scout_web/controllers/address_celo_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/address_celo_controller.ex
@@ -9,7 +9,6 @@ defmodule BlockScoutWeb.AddressCeloController do
   def index(conn, %{"address_id" => address_hash_string}) do
     with {:ok, address_hash} <- Chain.string_to_address_hash(address_hash_string),
          {:ok, address} <- Chain.hash_to_address(address_hash) do
-      IO.inspect(address)
       {transaction_count, validation_count} = transaction_and_validation_count(address_hash)
 
       render(
