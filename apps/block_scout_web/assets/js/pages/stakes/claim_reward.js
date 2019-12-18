a8d943a746654e5c06a40554eb51a0a7b5ee242e
Vadim <varasev@gmail.com>
2019-12-18 17:14:14 +0300

    Prepare Recalculate button for Claim Reward

--- apps/block_scout_web/assets/js/pages/stakes/claim_reward.js
+++ apps/block_scout_web/assets/js/pages/stakes/claim_reward.js
@@ -54,13 +54,17 @@ function onPoolsFound($modal, $modalBody) {
   const $poolsDropdown = $('[pool-select]', $modalBody)
   const $epochChoiceRadio = $('input[name="epoch_choice"]', $modalBody)
   const $specifiedEpochsText = $('.specified-epochs', $modalBody)
+  let allowedEpochs = []
 
   $poolsDropdown.on('change', () => {
     const data = $('option:selected', this).data()
     const $poolInfo = $('.selected-pool-info', $modalBody)
+    const epochs = data.epochs ? data.epochs : ''
+
+    allowedEpochs = expandEpochsToArray(epochs)
 
     $poolsDropdown.blur()
-    $('textarea', $poolInfo).val(data.epochs ? data.epochs : '')
+    $('textarea', $poolInfo).val(epochs)
     $('#token-reward-sum', $poolInfo).html(data.tokenRewardSum ? data.tokenRewardSum : '0')
     $('#native-reward-sum', $poolInfo).html(data.nativeRewardSum ? data.nativeRewardSum : '0')
     $('#tx-gas-limit', $poolInfo).html(data.gasLimit ? '~' + data.gasLimit : '0')
@@ -71,6 +75,80 @@ function onPoolsFound($modal, $modalBody) {
   })
 
   $epochChoiceRadio.on('change', () => {
-    $specifiedEpochsText.toggleClass('hidden')
+    if ($('#epoch-choice-all', $modalBody).is(':checked')) {
+      $specifiedEpochsText.addClass('hidden')
+      showRecalcButton(false, $modalBody)
+    } else {
+      $specifiedEpochsText.removeClass('hidden')
+      $specifiedEpochsText.trigger('input')
+    }
   })
+
+  $specifiedEpochsText.on('input', () => {
+    const filtered = filterSpecifiedEpochs($specifiedEpochsText.val())
+    const pointedEpochs = expandEpochsToArray(filtered)
+    const needsRecalc = pointedEpochs.length > 0 && !isArrayIncludedToArray(allowedEpochs, pointedEpochs)
+    showRecalcButton(needsRecalc, $modalBody)
+    $specifiedEpochsText.val(filtered)
+  })
+}
+
+function showRecalcButton(show, $modalBody) {
+  const $itemsToStrikeOut = $('#token-reward-sum, #native-reward-sum, #tx-gas-limit', $modalBody)
+  const $recalculateButton = $('button.recalculate', $modalBody)
+  const $submitButton = $('button.submit', $modalBody)
+  if (show) {
+    $itemsToStrikeOut.css('text-decoration', 'line-through')
+    $recalculateButton.removeClass('hidden')
+    $submitButton.addClass('hidden')
+  } else {
+    $itemsToStrikeOut.css('text-decoration', '')
+    $recalculateButton.addClass('hidden')
+    $submitButton.removeClass('hidden')
+  }
+}
+
+function expandEpochsToArray(epochs) {
+  let filtered = epochs.replace(/[-|,]$/g, '').trim()
+  if (filtered == '') return []
+  let ranges = filtered.split(',')
+  ranges = ranges.map((v) => {
+    if (v.indexOf('-') > -1) {
+      v = v.split('-')
+      v[0] = parseInt(v[0])
+      v[1] = parseInt(v[1])
+      v.sort((a, b) => a - b)
+      const min = v[0]
+      const max = v[1]
+      let expanded = []
+      for (let i = min; i <= max; i++) {
+        expanded.push(i)
+      }
+      return expanded
+    } else {
+      return parseInt(v)
+    }
+  })
+  ranges = ranges.reduce((acc, val) => acc.concat(val), []) // similar to ranges.flat()
+  ranges.sort((a, b) => a - b)
+  ranges = [...new Set(ranges)] // make unique
+  ranges = ranges.filter(epoch => epoch != 0)
+  return ranges
+}
+
+function filterSpecifiedEpochs(epochs) {
+  let filtered = epochs
+  filtered = filtered.replace(/[^0-9,-]+/g, '')
+  filtered = filtered.replace(/-{2,}/g, '-')
+  filtered = filtered.replace(/,{2,}/g, ',')
+  filtered = filtered.replace(/,-/g, ',')
+  filtered = filtered.replace(/-,/g, '-')
+  filtered = filtered.replace(/(-[0-9]+)-/g, '$1,')
+  filtered = filtered.replace(/^[,|-|0]/g, '')
+  return filtered
+}
+
+function isArrayIncludedToArray(source, target) {
+  const filtered = target.filter(item => source.indexOf(item) != -1)
+  return filtered.length == source.length
 }
