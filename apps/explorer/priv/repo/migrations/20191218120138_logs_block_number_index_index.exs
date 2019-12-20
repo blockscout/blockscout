06049209f111c2fc1395e1e5c805ea84ca385a99
Victor Baranov <baranov.viktor.27@gmail.com>
2019-12-20 16:09:27 +0300

    Remove dropping of index logs_block_number_index because it never exists in migrations now

--- apps/explorer/priv/repo/migrations/20191218120138_logs_block_number_index_index.exs
+++ apps/explorer/priv/repo/migrations/20191218120138_logs_block_number_index_index.exs
@@ -3,7 +3,5 @@ defmodule Explorer.Repo.Migrations.LogsBlockNumberIndexIndex do
 
   def change do
     create_if_not_exists(index(:logs, ["block_number DESC, index DESC"]))
-
-    drop_if_exists(index(:logs, [:block_number], name: "logs_block_number_index"))
   end
 end
