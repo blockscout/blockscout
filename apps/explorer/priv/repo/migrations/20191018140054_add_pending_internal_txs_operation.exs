7b6f86750b8285346db1c3b8bbf16ff6afb0ca68
Victor Baranov <baranov.viktor.27@gmail.com>
2020-01-05 00:59:06 +0300

    Temp remove part of migration

--- apps/explorer/priv/repo/migrations/20191018140054_add_pending_internal_txs_operation.exs
+++ apps/explorer/priv/repo/migrations/20191018140054_add_pending_internal_txs_operation.exs
@@ -55,48 +55,48 @@ defmodule Explorer.Repo.Migrations.AddPendingInternalTxsOperation do
     DELETE FROM internal_transactions WHERE block_hash IS NULL;
     """)
 
-    execute("""
-    DO $$
-    DECLARE
-        duplicates_count INTEGER := 0;
-        blocks_scanned INTEGER := 0;
-        temprow RECORD;
-    BEGIN
-        FOR temprow IN
-            SELECT number, hash FROM blocks
-        LOOP
-            blocks_scanned := blocks_scanned + 1;
-            IF EXISTS (
-                SELECT 1 FROM transactions WHERE block_hash = temprow.hash
-            ) THEN
-                IF EXISTS (
-                    SELECT block_hash, block_index FROM internal_transactions
-                    WHERE block_hash = temprow.hash
-                    GROUP BY block_hash, block_index HAVING COUNT(*) > 1
-                ) THEN
-                    duplicates_count := duplicates_count + 1;
-                    RAISE NOTICE '% duplicates, blocks scanned %, block #%, block hash is %', duplicates_count, blocks_scanned, temprow.number , temprow.hash;
+    # execute("""
+    # DO $$
+    # DECLARE
+    #     duplicates_count INTEGER := 0;
+    #     blocks_scanned INTEGER := 0;
+    #     temprow RECORD;
+    # BEGIN
+    #     FOR temprow IN
+    #         SELECT number, hash FROM blocks
+    #     LOOP
+    #         blocks_scanned := blocks_scanned + 1;
+    #         IF EXISTS (
+    #             SELECT 1 FROM transactions WHERE block_hash = temprow.hash
+    #         ) THEN
+    #             IF EXISTS (
+    #                 SELECT block_hash, block_index FROM internal_transactions
+    #                 WHERE block_hash = temprow.hash
+    #                 GROUP BY block_hash, block_index HAVING COUNT(*) > 1
+    #             ) THEN
+    #                 duplicates_count := duplicates_count + 1;
+    #                 RAISE NOTICE '% duplicates, blocks scanned %, block #%, block hash is %', duplicates_count, blocks_scanned, temprow.number , temprow.hash;
 
-                    IF NOT EXISTS (
-                        SELECT 1 FROM pending_block_operations
-                        WHERE block_hash = temprow.hash
-                    ) THEN
-                        INSERT INTO pending_block_operations
-                        (block_hash, inserted_at, updated_at, fetch_internal_transactions)
-                        SELECT b.hash, now(), now(), TRUE FROM blocks b
-                        WHERE b.hash = temprow.hash;
-                    END IF;
+    #                 IF NOT EXISTS (
+    #                     SELECT 1 FROM pending_block_operations
+    #                     WHERE block_hash = temprow.hash
+    #                 ) THEN
+    #                     INSERT INTO pending_block_operations
+    #                     (block_hash, inserted_at, updated_at, fetch_internal_transactions)
+    #                     SELECT b.hash, now(), now(), TRUE FROM blocks b
+    #                     WHERE b.hash = temprow.hash;
+    #                 END IF;
 
-                    DELETE FROM internal_transactions
-                    WHERE block_hash = temprow.hash;
+    #                 DELETE FROM internal_transactions
+    #                 WHERE block_hash = temprow.hash;
 
-                    RAISE NOTICE 'DELETED';
-                END IF;
-            END IF;
-        END LOOP;
-        RAISE NOTICE 'SCRIPT FINISHED';
-    END $$;
-    """)
+    #                 RAISE NOTICE 'DELETED';
+    #             END IF;
+    #         END IF;
+    #     END LOOP;
+    #     RAISE NOTICE 'SCRIPT FINISHED';
+    # END $$;
+    # """)
 
     execute("""
     ALTER table internal_transactions
