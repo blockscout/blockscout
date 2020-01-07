b661a009e8c22b8659ed42b53ced9e87e1ecf0a5
Sami Mäkelä <mrsmkl@gmail.com>
2020-01-07 15:57:32 +0200

    show when validator was last elected

---
+++ apps/block_scout_web/lib/block_scout_web/resolvers/celo_util.ex
@@ -0,0 +1,15 @@
+defmodule BlockScoutWeb.Resolvers.CeloUtil do
+  @moduledoc false
+
+  alias Explorer.Chain
+  alias Explorer.Chain.{CeloAccount, CeloValidator}
+
+  def get_usd(%CeloAccount{address: hash}, _, _) do
+    Chain.get_token_balance(hash, "cUSD")
+  end
+
+  def get_elected(%CeloValidator{address: hash}, _, _) do
+    Chain.get_latest_validating_block(hash)
+  end
+
+end
