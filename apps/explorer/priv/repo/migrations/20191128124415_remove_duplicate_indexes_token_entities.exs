99710767c9190b898b63831e57917bec49bcfeff
Victor Baranov <baranov.viktor.27@gmail.com>
2020-01-10 14:13:13 +0300

    improve migrations

--- apps/explorer/priv/repo/migrations/20191128124415_remove_duplicate_indexes_token_entities.exs
+++ apps/explorer/priv/repo/migrations/20191128124415_remove_duplicate_indexes_token_entities.exs
@@ -2,8 +2,8 @@ defmodule Explorer.Repo.Migrations.RemoveDuplicateIndexesTokenEntities do
   use Ecto.Migration
 
   def change do
-    drop(index(:address_token_balances, [:block_number]))
+    drop_if_exists(index(:address_token_balances, [:block_number], name: "address_token_balances_block_number_index"))
 
-    drop(index(:token_instances, [:token_id]))
+    drop_if_exists(index(:token_instances, [:token_id], name: "token_instances_token_id_index"))
   end
 end
