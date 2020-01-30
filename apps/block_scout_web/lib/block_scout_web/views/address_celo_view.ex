5f95b0496c19ece760d75a53bb92ec0921390989
Sami Mäkelä <mrsmkl@gmail.com>
2020-01-30 12:43:14 +0200

    format

--- apps/block_scout_web/lib/block_scout_web/views/address_celo_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/address_celo_view.ex
@@ -1,6 +1,6 @@
 defmodule BlockScoutWeb.AddressCeloView do
   use BlockScoutWeb, :view
-  
+
   def list_with_items?(lst) do
     lst != nil and Ecto.assoc_loaded?(lst) and Enum.count(lst) > 0
   end
