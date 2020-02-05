c7e10af7db16c7459a260b48d03e3aada767f1d3
Victor Baranov <baranov.viktor.27@gmail.com>
2020-02-05 19:12:49 +0300

    Fix migration

--- apps/explorer/priv/repo/migrations/20191018140054_add_pending_internal_txs_operation.exs
+++ apps/explorer/priv/repo/migrations/20191018140054_add_pending_internal_txs_operation.exs
@@ -2,9 +2,9 @@ defmodule Explorer.Repo.Migrations.AddPendingInternalTxsOperation do
   use Ecto.Migration
 
   def change do
-    # alter table(:pending_block_operations) do
-    #   add(:fetch_internal_transactions, :boolean, null: false)
-    # end
+    alter table(:pending_block_operations) do
+      add(:fetch_internal_transactions, :boolean, null: false)
+    end
 
     execute("""
     INSERT INTO pending_block_operations
