e9c4c36756ce30a499b67da9957536d4ce74253a
Victor Baranov <baranov.viktor.27@gmail.com>
2020-02-06 12:44:14 +0300

    Update token_balance.ex

--- apps/indexer/lib/indexer/fetcher/token_balance.ex
+++ apps/indexer/lib/indexer/fetcher/token_balance.ex
@@ -26,8 +26,8 @@ defmodule Indexer.Fetcher.TokenBalance do
 
   @defaults [
     flush_interval: 300,
-    max_batch_size: 100,
-    max_concurrency: 10,
+    max_batch_size: 200,
+    max_concurrency: 4,
     task_supervisor: Indexer.Fetcher.TokenBalance.TaskSupervisor
   ]
 
