b6d0872e808e80256a6e50b83dfd3977178e484d
Victor Baranov <baranov.viktor.27@gmail.com>
2020-02-07 18:20:35 +0300

    Decrese polling polling_period for block if websockets are disabled

--- apps/indexer/lib/indexer/block/realtime/fetcher.ex
+++ apps/indexer/lib/indexer/block/realtime/fetcher.ex
@@ -152,7 +152,7 @@ defmodule Indexer.Block.Realtime.Fetcher do
   defp new_max_number(number, max_number_seen), do: max(number, max_number_seen)
 
   defp schedule_polling do
-    polling_period = 30_000
+    polling_period = 15_000
     # case AverageBlockTime.average_block_time() do
     #   {:error, :disabled} -> 2_000
     #   block_time -> round(Duration.to_milliseconds(block_time) * 2)
