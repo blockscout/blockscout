c58f052784243f74b81770d808e92f18a792a279
Sami Mäkelä <mrsmkl@gmail.com>
2020-03-02 12:48:06 +0200

    cleanupt

--- apps/block_scout_web/lib/block_scout_web/resolvers/coin_balances.ex
+++ apps/block_scout_web/lib/block_scout_web/resolvers/coin_balances.ex
@@ -1,17 +1,7 @@
 defmodule BlockScoutWeb.Resolvers.CoinBalances do
-  #  import BlockScoutWeb.Chain, only: [paging_options: 1]
-
-  #  alias Explorer.Chain
 
   alias Absinthe.Relay.Connection
   alias Explorer.{GraphQL, Repo}
-
-  _ = """
-    def get_by(_, %{address: address_hash} = args, _) do
-      full_options = paging_options(args)
-      {:ok, Chain.address_to_coin_balances(address_hash, full_options)}
-    end
-  """
 
   def get_by(_, %{address: address_hash} = args, _) do
     connection_args = Map.take(args, [:after, :before, :first, :last])
