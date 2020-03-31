e25428a5f681afc3395288a819f582248aa42356
Aitor <1726644+aaitor@users.noreply.github.com>
2020-03-31 18:53:32 +0200

    working environment

--- load-testing/index.js
+++ load-testing/index.js
@@ -4,7 +4,7 @@ const LoadTesting = require('easygraphql-load-tester')
 const { fileLoader } = require('merge-graphql-schemas')
 
 const schema = fs.readFileSync(path.join(__dirname, 'schema.gql'), 'utf8')
-const queries = fileLoader(path.join(__dirname, './graphql', '**/*.graphql'))
+const queries = fileLoader(path.join(__dirname, '**/*.graphql'))
 
 const args = {
   SEARCH_BLOCK: [
@@ -13,13 +13,20 @@ const args = {
   ]
 }
 
+//const queries = [
+//  'query LATEST_BLOCK {latestBlock}',
+//  'query SEARCH_BLOCK($number: Number!) {block(number: $number) { hash, gasUsed, number}}'
+//]
+
 const easyGraphQLLoadTester = new LoadTesting(schema, args)
+
+console.log('Queries: ' + queries)
 
 const testCases = easyGraphQLLoadTester.artillery({
   customQueries: queries,
   onlyCustomQueries: true,
   queryFile: true,
-  selectedQueries: ['SEARCH_BLOCK', 'LATEST_BLOCK'],
+//  selectedQueries: ['SEARCH_BLOCK', 'LATEST_BLOCK'],
   withMutations: true,
 })
 
