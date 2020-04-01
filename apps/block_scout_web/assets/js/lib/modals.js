7c21a15a694eac21ca4ab9f7f49780fe6f465212
Victor Baranov <baranov.viktor.27@gmail.com>
2020-04-01 23:01:14 +0300

    Fix tests: gettext, eslint

--- apps/block_scout_web/assets/js/lib/modals.js
+++ apps/block_scout_web/assets/js/lib/modals.js
@@ -21,11 +21,11 @@ $(document.body).on('hide.bs.modal', e => {
   $currentModal = null
 })
 
-export function currentModal() {
+export function currentModal () {
   return $currentModal
 }
 
-export function openModal($modal, unclosable) {
+export function openModal ($modal, unclosable) {
   // Hide all tooltips before showing a modal,
   // since they are sticking on top of modal
   $('.tooltip').tooltip('hide')
@@ -56,7 +56,7 @@ export function openModal($modal, unclosable) {
   }
 }
 
-export function lockModal($modal, $submitButton = null, spinnerText = '') {
+export function lockModal ($modal, $submitButton = null, spinnerText = '') {
   $modal.find('.close-modal').attr('disabled', true)
 
   const $button = $submitButton || $modal.find('.btn-add-full')
@@ -78,11 +78,11 @@ export function lockModal($modal, $submitButton = null, spinnerText = '') {
   modalLocked = true
 }
 
-export function unlockModal($modal, $submitButton = null) {
+export function unlockModal ($modal, $submitButton = null) {
   $modal.find('.close-modal').attr('disabled', false)
 
   const $button = $submitButton || $modal.find('.btn-add-full')
-  const buttonText = $button.attr('data-text');
+  const buttonText = $button.attr('data-text')
 
   $button.attr('disabled', false)
 
@@ -97,28 +97,28 @@ export function unlockModal($modal, $submitButton = null) {
   modalLocked = false
 }
 
-export function openErrorModal(title, text, unclosable) {
+export function openErrorModal (title, text, unclosable) {
   const $modal = $('#errorStatusModal')
   $modal.find('.modal-status-title').text(title)
   $modal.find('.modal-status-text').html(text)
   openModal($modal, unclosable)
 }
 
-export function openWarningModal(title, text) {
+export function openWarningModal (title, text) {
   const $modal = $('#warningStatusModal')
   $modal.find('.modal-status-title').text(title)
   $modal.find('.modal-status-text').html(text)
   openModal($modal)
 }
 
-export function openSuccessModal(title, text) {
+export function openSuccessModal (title, text) {
   const $modal = $('#successStatusModal')
   $modal.find('.modal-status-title').text(title)
   $modal.find('.modal-status-text').text(text)
   openModal($modal)
 }
 
-export function openQuestionModal(title, text, acceptCallback = null, exceptCallback = null, acceptText = 'Yes', exceptText = 'No') {
+export function openQuestionModal (title, text, acceptCallback = null, exceptCallback = null, acceptText = 'Yes', exceptText = 'No') {
   const $modal = $('#questionStatusModal')
 
   $modal.find('.modal-status-title').text(title)
