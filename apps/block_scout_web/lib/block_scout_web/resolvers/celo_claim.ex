ee0055d7a94f1c81cd024cd663b25940b86957a4
Sami Mäkelä <mrsmkl@gmail.com>
2020-04-06 13:39:25 +0300

    fixing connector

--- apps/block_scout_web/lib/block_scout_web/resolvers/celo_claim.ex
+++ apps/block_scout_web/lib/block_scout_web/resolvers/celo_claim.ex
@@ -1,15 +1,18 @@
 defmodule BlockScoutWeb.Resolvers.CeloClaim do
   @moduledoc false
 
-  alias Explorer.Chain
+  alias Absinthe.Relay.Connection
+  alias Explorer.{Chain, GraphQL, Repo}
   alias Explorer.Chain.{CeloAccount}
 
   def get_by(_, %{hash: hash}, _) do
     {:ok, Chain.get_celo_claims(hash)}
   end
 
-  def get_by(%CeloAccount{address: hash}, _, _) do
-    {:ok, Chain.get_celo_claims(hash)}
+  def get_by(%CeloAccount{address: hash}, args, _) do
+    hash
+    |> GraphQL.address_to_claims_query()
+    |> Connection.from_query(&Repo.all/1, args, [])
   end
 
 end
