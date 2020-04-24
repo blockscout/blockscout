6c6d330a23741f82acfa6ba8cf6d533849afc344
Sami Mäkelä <mrsmkl@gmail.com>
2020-04-24 10:43:56 +0300

    changing this back

--- apps/indexer/lib/indexer/fetcher/internal_transaction.ex
+++ apps/indexer/lib/indexer/fetcher/internal_transaction.ex
@@ -23,7 +23,7 @@ defmodule Indexer.Fetcher.InternalTransaction do
   @behaviour BufferedTask
 
   @max_batch_size 10
-  @max_concurrency 1
+  @max_concurrency 4
   @defaults [
     flush_interval: :timer.seconds(3),
     max_concurrency: @max_concurrency,
