f7602724a1cd8e251147e0e668d7d568e221a42e
Victor Baranov <baranov.viktor.27@gmail.com>
2020-05-01 12:47:49 +0300

    Mix format fix

--- apps/explorer/priv/repo/migrations/20200424070607_drop_block_rewards_address_hash_address_type_block_hash_index.exs
+++ apps/explorer/priv/repo/migrations/20200424070607_drop_block_rewards_address_hash_address_type_block_hash_index.exs
@@ -3,7 +3,9 @@ defmodule Explorer.Repo.Migrations.DropBlockRewardsAddressHashAddressTypeBlockHa
 
   def change do
     drop_if_exists(
-      index(:internal_transactions, [:address_hash, :block_hash, :address_type], name: "block_rewards_address_hash_address_type_block_hash_index")
+      index(:internal_transactions, [:address_hash, :block_hash, :address_type],
+        name: "block_rewards_address_hash_address_type_block_hash_index"
+      )
     )
   end
 end
