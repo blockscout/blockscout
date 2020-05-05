c38d7c4cc12aed3b75c9d841c84437f1db9a79fd
Sami Mäkelä <mrsmkl@gmail.com>
2020-05-05 20:29:59 +0300

    do not show -1 anymore

--- apps/block_scout_web/lib/block_scout_web/views/address_celo_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/address_celo_view.ex
@@ -4,4 +4,9 @@ defmodule BlockScoutWeb.AddressCeloView do
   def list_with_items?(lst) do
     lst != nil and Ecto.assoc_loaded?(lst) and Enum.count(lst) > 0
   end
+
+  def member_number(-1), do: "Not accepted"
+
+  def member_number(n), do: n
+
 end
