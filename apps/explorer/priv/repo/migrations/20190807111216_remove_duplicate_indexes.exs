7974dbee855aa5d4aeab26dbbf2436469593b414
Vadim <varasev@gmail.com>
2020-05-21 15:12:08 +0300

    Reduce DB migrations

--- apps/explorer/priv/repo/migrations/20190807111216_remove_duplicate_indexes.exs
+++ apps/explorer/priv/repo/migrations/20190807111216_remove_duplicate_indexes.exs
@@ -7,7 +7,9 @@ defmodule Explorer.Repo.Migrations.RemoveDuplicateIndexes do
     )
 
     drop_if_exists(
-      index(:staking_pools_delegators, [:address_hash], name: "staking_pools_delegators_address_hash_index")
+      index(:staking_pools_delegators, [:delegator_address_hash],
+        name: "staking_pools_delegators_delegator_address_hash_index"
+      )
     )
 
     drop_if_exists(index(:transactions, [:to_address_hash], name: "transactions_to_address_hash_index"))
