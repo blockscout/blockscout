c673d0fa524a88de1ffd4e38cdd1860ad826194f
Vadim <varasev@gmail.com>
2020-05-21 15:12:08 +0300

    Reduce DB migrations

--- apps/explorer/priv/repo/migrations/20190731172720_add_reward_ratio_to_pool_delegators.exs
+++
@@ -1,9 +0,0 @@
-defmodule Explorer.Repo.Migrations.AddBlockRewardToPoolDelegators do
-  use Ecto.Migration
-
-  def change do
-    alter table(:staking_pools_delegators) do
-      add(:reward_ratio, :decimal, precision: 5, scale: 2)
-    end
-  end
-end
