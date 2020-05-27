2d4d37f1e8d11727ca075eee7d6186b287dbaa2a
Victor Baranov <baranov.viktor.27@gmail.com>
2020-05-27 15:35:34 +0300

    Int txs fetcher: max_batch_size: 30, max_concurrency: 8

--- apps/indexer/lib/indexer/fetcher/internal_transaction.ex
+++ apps/indexer/lib/indexer/fetcher/internal_transaction.ex
@@ -20,8 +20,8 @@ defmodule Indexer.Fetcher.InternalTransaction do
 
   @behaviour BufferedTask
 
-  @max_batch_size 10
-  @max_concurrency 6
+  @max_batch_size 30
+  @max_concurrency 8
   @defaults [
     flush_interval: :timer.seconds(3),
     max_concurrency: @max_concurrency,
