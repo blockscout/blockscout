d18b01ef3bec15ca8d5ca4d5e3ba4a99dc6a2c22
Victor Baranov <baranov.viktor.27@gmail.com>
2020-05-27 16:37:00 +0300

    Update Parity config

--- apps/indexer/config/prod/parity.exs
+++ apps/indexer/config/prod/parity.exs
@@ -16,34 +16,9 @@ config :indexer,
         trace_block: System.get_env("ETHEREUM_JSONRPC_TRACE_URL"),
         trace_replayTransaction: System.get_env("ETHEREUM_JSONRPC_TRACE_URL")
       ],
-      http_options: [
-        recv_timeout: :timer.minutes(180),
-        timeout: :timer.minutes(180),
-        hackney: [pool: :ethereum_jsonrpc]
-      ]
+      http_options: [recv_timeout: :timer.minutes(10), timeout: :timer.minutes(10), hackney: [pool: :ethereum_jsonrpc]]
     ],
     variant: EthereumJSONRPC.Parity
-  ],
-  # Example configuration to override json_rpc_named_arguments for just the realtime block fetcher
-  realtime_overrides: [
-    json_rpc_named_arguments: [
-      transport: EthereumJSONRPC.HTTP,
-      transport_options: [
-        http: EthereumJSONRPC.HTTP.HTTPoison,
-        url: System.get_env("ETHEREUM_JSONRPC_REALTIME_HTTP_URL") || "http://localhost:8545",
-        method_to_url: [
-          eth_getBalance: System.get_env("ETHEREUM_JSONRPC_REALTIME_TRACE_URL") || "http://localhost:8545",
-          trace_block: System.get_env("ETHEREUM_JSONRPC_REALTIME_TRACE_URL") || "http://localhost:8545",
-          trace_replayTransaction: System.get_env("ETHEREUM_JSONRPC_REALTIME_TRACE_URL") || "http://localhost:8545"
-        ],
-        http_options: [
-          recv_timeout: :timer.minutes(30),
-          timeout: :timer.minutes(30),
-          hackney: [pool: :ethereum_jsonrpc]
-        ]
-      ],
-      variant: EthereumJSONRPC.Parity
-    ]
   ],
   subscribe_named_arguments: [
     transport: System.get_env("ETHEREUM_JSONRPC_WS_URL") && EthereumJSONRPC.WebSocket,
