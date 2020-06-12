15bc0d391eab948264f807918356875b96274c6f
Victor Baranov <baranov.viktor.27@gmail.com>
2020-06-12 16:53:19 +0300

    trace_block for BESU

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/besu.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/besu.ex
@@ -5,13 +5,26 @@ defmodule EthereumJSONRPC.Besu do
   require Logger
   import EthereumJSONRPC, only: [id_to_params: 1, integer_to_quantity: 1, json_rpc: 2, request: 1]
 
-  alias EthereumJSONRPC.Parity.{Traces}
+  alias EthereumJSONRPC.Parity.{FetchedBeneficiaries, Traces}
   alias EthereumJSONRPC.{Transaction, Transactions}
 
   @behaviour EthereumJSONRPC.Variant
 
   @impl EthereumJSONRPC.Variant
-  def fetch_beneficiaries(_block_range, _json_rpc_named_arguments), do: :ignore
+  def fetch_beneficiaries(block_numbers, json_rpc_named_arguments)
+      when is_list(block_numbers) and is_list(json_rpc_named_arguments) do
+    id_to_params =
+      block_numbers
+      |> block_numbers_to_params_list()
+      |> id_to_params()
+
+    with {:ok, responses} <-
+           id_to_params
+           |> FetchedBeneficiaries.requests()
+           |> json_rpc(json_rpc_named_arguments) do
+      {:ok, FetchedBeneficiaries.from_responses(responses, id_to_params)}
+    end
+  end
 
   @impl EthereumJSONRPC.Variant
   def fetch_internal_transactions(_transactions_params, _json_rpc_named_arguments), do: :ignore
@@ -86,6 +99,10 @@ defmodule EthereumJSONRPC.Besu do
 
       {:ok, transactions_params}
     end
+  end
+
+  defp block_numbers_to_params_list(block_numbers) when is_list(block_numbers) do
+    Enum.map(block_numbers, &%{block_quantity: integer_to_quantity(&1)})
   end
 
   defp trace_replay_block_transactions_responses_to_internal_transactions_params(responses, id_to_params)
