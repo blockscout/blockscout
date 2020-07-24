ba9119a76b5ac7f5d50fa2751f5014f27c2e4bb3
Sami Mäkelä <mrsmkl@gmail.com>
2020-07-24 14:37:23 +0300

    fixing this...

--- apps/block_scout_web/lib/block_scout_web/controllers/transaction_log_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/transaction_log_controller.ex
@@ -26,6 +26,7 @@ defmodule BlockScoutWeb.TransactionLogController do
           ],
           paging_options(params)
         )
+
       logs_plus_one = Chain.transaction_to_logs(transaction_hash, full_options)
 
       {logs, next_page} = split_list_by_page(logs_plus_one)
