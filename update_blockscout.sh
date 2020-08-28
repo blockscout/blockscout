0e1d3e6008e648bb6b4578f6f1414b3072b72a09
Victor Baranov <baranov.viktor.27@gmail.com>
2020-08-28 12:29:47 +0300

    Update Blockscout script

---
+++ update_blockscout.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+sudo systemctl stop explorer.service
+git pull origin production-sokol-stg
+MIX_ENV=prod mix do deps.get, deps.compile, compile
+MIX_ENV=prod mix do ecto.migrate
+cd apps/block_scout_web/assets; sudo npm install && node_modules/webpack/bin/webpack.js --mode production; cd -
+cd apps/explorer && npm install; cd -
+MIX_ENV=prod mix phx.digest
+sudo systemctl start explorer.service
