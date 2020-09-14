6fc5f2c1db778bf7807553583eb8f132e6430691
Erin Hales <mcbridee093@gmail.com>
2020-09-14 10:00:27 -0600

    added more translations

--- apps/block_scout_web/lib/block_scout_web/views/api/rpc/address_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/api/rpc/address_view.ex
@@ -121,6 +121,7 @@ defmodule BlockScoutWeb.API.RPC.AddressView do
     }
   end
 
+  # TODO: update token name?
   defp prepare_common_token_transfer(token_transfer) do
     %{
       "blockNumber" => to_string(token_transfer.block_number),
