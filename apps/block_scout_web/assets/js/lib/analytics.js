b1c300fea32ab6dcb872b830ccd03cacc23c67bc
Erin Hales <mcbridee093@gmail.com>
2021-01-19 11:15:06 -0700

    refactored analytics.js, added unique userID

--- apps/block_scout_web/assets/js/lib/analytics.js
+++ apps/block_scout_web/assets/js/lib/analytics.js
@@ -1,8 +1,10 @@
-// import { createStore } from './redux_helpers.js'
+import { createStore } from './redux_helpers.js'
 
-// import $ from 'jquery'
+import $ from 'jquery'
 import Analytics from 'analytics'
-// import omit from 'lodash/omit'
+import segmentPlugin from '@analytics/segment'
+import omit from 'lodash/omit'
+import uniqid from 'uniqid'
 
 const analytics = Analytics({
   app: 'Blockscout',
@@ -14,77 +16,69 @@ const analytics = Analytics({
 })
 analytics.page()
 
-// // TODO: create unique user ID and save in store
-// export const initialState = {
-//   userID: '1'
-// }
+export const initialState = {
+  userID: localStorage.getItem('userID')
+}
 
-// export function reducer (state = initialState, action) {
-//   switch (action.type) {
-//     case 'PAGE_LOAD':
-//     case 'ELEMENTS_LOAD': {
-//       return Object.assign({}, state, omit(action, 'type'))
-//     }
-//     case 'SET_USER_ID': {
-//       localStorage.setItem('userID', '2')
-//       return Object.assign({}, state, { userID: '2' })
-//     }
-//     default:
-//       return state
-//   }
-// }
-// console.log('hi')
+export function reducer (state = initialState, action) {
+  switch (action.type) {
+    case 'PAGE_LOAD':
+    case 'ELEMENTS_LOAD': {
+      return Object.assign({}, state, omit(action, 'type'))
+    }
+    case 'SET_USER_ID': {
+      const id = uniqid()
+      localStorage.setItem('userID', id)
+      return Object.assign({}, state, { userID: id })
+    }
+    default:
+      return state
+  }
+}
 
-// const $app = $('[data-page="app-container"]')
-// if ($app.length) {
-//   const store = createStore(reducer)
-//   init()
-// }
+$(function() {
+  const store = createStore(reducer)
+  if (!state.userID) {
+    store.dispatch({ type: 'SET_USER_ID' })
+  }
+  analytics.identify(state.userID)
+  analytics.page()
+  trackEvents()
+})
 
-// function init () {
-//   if (!state.userID) {
-//     // set id
-//   }
-//   analytics.identify(state.userID)
-//   analytics.page()
-//   trackEvents()
-// }
+function trackEvents () {
+  // Page navigation
 
-// function trackEvents () {
-//   // Page navigation
+  // Search box click
+  $('[data-selector="search-bar"]').on('click', function() {
+    analytics.track('search bar click')
+  })
 
-//   // Search box click
-//   $('[data-selector="search-bar"]').on('click', function() {
-//     analytics.track('search bar click')
-//   })
+  // Search submit
+  $('[data-selector="search-bar"]').on('submit', function(e) {
+    e.preventDefault();  //prevent form from submitting
+    analytics.track('search event', {
+      value: e.value
+    })
+  });
 
-//   // Search submit
-//   $(function() {
-//     $('[data-selector="search-bar"]').on('submit', function(e) {
-//       e.preventDefault();  //prevent form from submitting
-//       analytics.track('search event', {
-//         value: e.value
-//       })
-//     });
-//   });
+  // Click on Balance Card Caret
+  $('[data-selector="address-balance-caret"]').on('click', function() {
+    analytics.track('address balance caret click')
+  });
 
-//   // Click on Balance Card Caret
-//   $('[data-selector="address-balance-caret"]').on('click', function() {
-//     analytics.track('address balance caret click')
-//   });
+  // Copy address
+  $('[data-selector="copy-address"]').on('click', function() {
+    analytics.track('copy address click')
+  })
 
-//   // Copy address
-//   $('[data-selector="copy-address"]').on('click', function() {
-//     analytics.track('copy address click')
-//   })
+  // QR code
+  $('[data-selector="qr-code"]').on('click', function() {
+    analytics.track('QR code click')
+  })
 
-//   // QR code
-//   $('[data-selector="qr-code"]').on('click', function() {
-//     analytics.track('QR code click')
-//   })
-
-//   // "view more transfers" click
-//   $('[data-selector="token-transfer-open"]').on('click', function() {
-//     analytics.track('\"View more transfers\" click')
-//   })
-// }
+  // "view more transfers" click
+  $('[data-selector="token-transfer-open"]').on('click', function() {
+    analytics.track('\"View more transfers\" click')
+  })
+}
