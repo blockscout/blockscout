e9553c627b3db5b456d31b887df9123be4dcc8ff
Victor Baranov <baranov.viktor.27@gmail.com>
2021-02-27 12:21:23 +0300

    Fix faucet vulnerability

--- apps/block_scout_web/lib/block_scout_web/controllers/faucet_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/faucet_controller.ex
@@ -1,11 +1,13 @@
 defmodule BlockScoutWeb.FaucetController do
   use BlockScoutWeb, :controller
 
+  alias BlockScoutWeb.Faucet.CaptchaController
   alias Explorer.{Chain, Faucet}
 
   @internal_server_err_msg "Internal server error. Please try again later."
   @send_coins_failed_msg "Sending coins failed. Please try again later."
   @wrong_recipinet_msg "Wrong recipient address"
+  @wrond_captcha_response "Wrong captcha response"
 
   def index(conn, params) do
     []
@@ -17,27 +19,22 @@ defmodule BlockScoutWeb.FaucetController do
   end
 
   def request(conn, %{
-        "receiver" => receiver
+        "receiver" => receiver,
+        "captchaResponse" => captcha_response
       }) do
     case Chain.string_to_address_hash(receiver) do
       {:ok, address_hash} ->
-        last_requested = Faucet.get_last_faucet_request_for_address(receiver)
+        res = CaptchaController.validate_captcha_response(captcha_response)
 
-        today = DateTime.utc_now()
-        yesterday = Timex.shift(today, days: -1)
+        case res do
+          {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
+            check_request_interval_and_send(conn, status_code, body, receiver, address_hash)
 
-        if !last_requested || last_requested < yesterday do
-          send_coins(receiver, address_hash, conn)
-        else
-          dur_to_next_available_request = calc_dur_to_next_available_request(last_requested, yesterday)
-
-          json(conn, %{
-            success: false,
-            message:
-              "You requested #{System.get_env("FAUCET_VALUE")} #{System.get_env("FAUCET_COIN")} within the last 24 hours. Next claim is in #{
-                dur_to_next_available_request
-              }"
-          })
+          _ ->
+            json(conn, %{
+              success: false,
+              message: @wrond_captcha_response
+            })
         end
 
       _ ->
@@ -49,6 +46,36 @@ defmodule BlockScoutWeb.FaucetController do
   end
 
   def request(conn, _), do: not_found(conn)
+
+  defp check_request_interval_and_send(conn, status_code, body, receiver, address_hash) do
+    last_requested = Faucet.get_last_faucet_request_for_address(receiver)
+
+    body_json = Jason.decode!(body)
+
+    if status_code == 200 && Map.get(body_json, "success") do
+      today = DateTime.utc_now()
+      yesterday = Timex.shift(today, days: -1)
+
+      if !last_requested || last_requested < yesterday do
+        send_coins(receiver, address_hash, conn)
+      else
+        dur_to_next_available_request = calc_dur_to_next_available_request(last_requested, yesterday)
+
+        json(conn, %{
+          success: false,
+          message:
+            "You requested #{System.get_env("FAUCET_VALUE")} #{System.get_env("FAUCET_COIN")} within the last 24 hours. Next claim is in #{
+              dur_to_next_available_request
+            }"
+        })
+      end
+    else
+      json(conn, %{
+        success: false,
+        message: @wrond_captcha_response
+      })
+    end
+  end
 
   defp calc_dur_to_next_available_request(last_requested, yesterday) do
     dur_sec = DateTime.diff(last_requested, yesterday, :second)
@@ -70,7 +97,7 @@ defmodule BlockScoutWeb.FaucetController do
       {:error} ->
         json(conn, %{success: false, message: @send_coins_failed_msg})
 
-      _ ->
+      _err ->
         json(conn, %{success: false, message: @send_coins_failed_msg})
     end
   end
