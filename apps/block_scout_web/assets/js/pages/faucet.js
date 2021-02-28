8f6030c8ceddc6a2f8bc169e812d098557bec22a
Victor Baranov <baranov.viktor.27@gmail.com>
2021-02-28 13:32:28 +0300

    Return Sentry for faucet

--- apps/block_scout_web/assets/js/pages/faucet.js
+++ apps/block_scout_web/assets/js/pages/faucet.js
@@ -1,5 +1,6 @@
 import $ from 'jquery'
 import Swal from 'sweetalert2'
+import * as Sentry from '@sentry/browser'
 
 const $csrfToken = $('[name=_csrf_token]')
 const $requestCoinsBtn = $('#requestCoins')
@@ -31,6 +32,7 @@ $(function () {
             text: data.message,
             icon: 'error'
           })
+          Sentry.captureException(data)
         } else {
           $('#receiver').val('')
           const faucetValue = $('#faucetValue').val()
@@ -52,6 +54,7 @@ $(function () {
           icon: 'error'
         })
         $requestCoinsBtn.attr('disabled', false)
+        Sentry.captureException(err)
       })
     } else {
       $requestCoinsBtn.attr('disabled', false)
