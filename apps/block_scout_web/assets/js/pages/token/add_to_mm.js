c5053f0555e3343b071afc6ca55bd738bf4c2d73
Victor Baranov <baranov.viktor.27@gmail.com>
2021-03-02 16:21:00 +0300

    Add to MM button

---
+++ apps/block_scout_web/assets/js/pages/token/add_to_mm.js
@@ -0,0 +1,34 @@
+import $ from 'jquery'
+
+$(document.body)
+  .on('click', '.btn-add-to-mm', event => {
+    const $btn = $(event.target)
+    const tokenAddress = $btn.data('token-address')
+    const tokenSymbol = $btn.data('token-symbol')
+    const tokenDecimals = $btn.data('token-decimals')
+
+    addTokenToMM({ tokenAddress, tokenSymbol, tokenDecimals, tokenImage: null })
+  })
+
+async function addTokenToMM ({ tokenAddress, tokenSymbol, tokenDecimals, tokenImage }) {
+  try {
+    const chainId = await window.ethereum.request({ method: 'eth_chainId' })
+    console.log(chainId)
+    if (chainId === '0x64') {
+      await window.ethereum.request({
+        method: 'wallet_watchAsset',
+        params: {
+          type: 'ERC20', // Initially only supports ERC20, but eventually more!
+          options: {
+            address: tokenAddress, // The address that the token is at.
+            symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+            decimals: tokenDecimals, // The number of decimals in the token
+            image: tokenImage // A string url of the token logo
+          }
+        }
+      })
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
