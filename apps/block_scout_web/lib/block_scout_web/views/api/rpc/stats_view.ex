870ad57081115b4ef431ce3152e095bfc2eeffcd
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-03-21 20:54:24 +0300

    Coinprice api endpoint: handle nil rates

--- apps/block_scout_web/lib/block_scout_web/views/api/rpc/stats_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/api/rpc/stats_view.ex
@@ -28,13 +28,22 @@ defmodule BlockScoutWeb.API.RPC.StatsView do
   end
 
   defp prepare_rates(rates) do
-    timestamp = rates.last_updated |> DateTime.to_unix() |> to_string()
+    if rates do
+      timestamp = rates.last_updated |> DateTime.to_unix() |> to_string()
 
-    %{
-      "coin_btc" => to_string(rates.btc_value),
-      "coin_btc_timestamp" => timestamp,
-      "coin_usd" => to_string(rates.usd_value),
-      "coin_usd_timestamp" => timestamp
-    }
+      %{
+        "coin_btc" => to_string(rates.btc_value),
+        "coin_btc_timestamp" => timestamp,
+        "coin_usd" => to_string(rates.usd_value),
+        "coin_usd_timestamp" => timestamp
+      }
+    else
+      %{
+        "coin_btc" => nil,
+        "coin_btc_timestamp" => nil,
+        "coin_usd" => nil,
+        "coin_usd_timestamp" => nil
+      }
+    end
   end
 end
