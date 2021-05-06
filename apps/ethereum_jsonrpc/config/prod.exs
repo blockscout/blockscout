d4c6089c9cc435e05292806ad98bba85b3cfcc42
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-05-06 10:52:22 +0300

    Decrease logs rotate files number to 5

--- apps/ethereum_jsonrpc/config/prod.exs
+++ apps/ethereum_jsonrpc/config/prod.exs
@@ -5,4 +5,4 @@ config :ethereum_jsonrpc, EthereumJSONRPC.Tracer, env: "production", disabled?: 
 config :logger, :ethereum_jsonrpc,
   level: :info,
   path: Path.absname("logs/prod/ethereum_jsonrpc.log"),
-  rotate: %{max_bytes: 52_428_800, keep: 19}
+  rotate: %{max_bytes: 52_428_800, keep: 5}
--- apps/explorer/config/prod.exs
+++ apps/explorer/config/prod.exs
@@ -13,19 +13,19 @@ config :explorer, Explorer.Tracer, env: "production", disabled?: true
 config :logger, :explorer,
   level: :info,
   path: Path.absname("logs/prod/explorer.log"),
-  rotate: %{max_bytes: 52_428_800, keep: 19}
+  rotate: %{max_bytes: 52_428_800, keep: 5}
 
 config :logger, :reading_token_functions,
   level: :debug,
   path: Path.absname("logs/prod/explorer/tokens/reading_functions.log"),
   metadata_filter: [fetcher: :token_functions],
-  rotate: %{max_bytes: 52_428_800, keep: 19}
+  rotate: %{max_bytes: 52_428_800, keep: 5}
 
 config :logger, :token_instances,
   level: :debug,
   path: Path.absname("logs/prod/explorer/tokens/token_instances.log"),
   metadata_filter: [fetcher: :token_instances],
-  rotate: %{max_bytes: 52_428_800, keep: 19}
+  rotate: %{max_bytes: 52_428_800, keep: 5}
 
 variant =
   if is_nil(System.get_env("ETHEREUM_JSONRPC_VARIANT")) do
