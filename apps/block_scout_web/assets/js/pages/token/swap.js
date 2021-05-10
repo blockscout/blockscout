680de8b11af2a31a86fd4c9e6e2c36aed160440c
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-05-10 12:46:14 +0300

    Sushiswap button, sushiswap menu icon

--- apps/block_scout_web/assets/js/pages/token/swap.js
+++ apps/block_scout_web/assets/js/pages/token/swap.js
@@ -2,7 +2,7 @@ import $ from 'jquery'
 import 'bootstrap'
 
 $(document.body)
-  .on('mouseover', '.btn-swap', event => {
+  .on('mouseover', '.btn-swap.honeypot', event => {
     const $btn = $(event.target)
     const tokenSymbol = $btn.data('token-symbol')
 
@@ -13,3 +13,15 @@ $(document.body)
       placement: 'top'
     }).tooltip('show')
   })
+  .on('mouseover', '.btn-swap.sushi', event => {
+    const $btn = $(event.target)
+    const tokenSymbol = $btn.data('token-symbol')
+
+    $btn.tooltip('dispose')
+    $btn.tooltip({
+      title: `Swap ${tokenSymbol}`,
+      trigger: 'hover',
+      placement: 'top'
+    }).tooltip('show')
+  })
+
