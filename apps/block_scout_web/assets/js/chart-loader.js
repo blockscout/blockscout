171e9059dba03f089a748416532240793e022296
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-06-10 12:20:39 +0300

    Main page chart legend price and tvl in template without js update (prevents flickering)

--- apps/block_scout_web/assets/js/chart-loader.js
+++ apps/block_scout_web/assets/js/chart-loader.js
@@ -1,22 +1,18 @@
 import $ from 'jquery'
 
 import { formatAllUsdValues, updateAllCalculatedUsdValues } from './lib/currency'
-import { createMarketHistoryChart, createGasUsageHistoryChart, createBlockGasHistoryChart } from './lib/history_chart'
+import { createMarketHistoryChart, createGasUsageHistoryChart } from './lib/history_chart'
 import { createCoinBalanceHistoryChart } from './lib/coin_balance_history_chart'
 
 (function () {
   const dashboardChartElement = $('[data-chart="historyChart"]')[0]
   const gasUsageChartElement = $('[data-chart="gasUsageChart"]')[0]
-  const blockGasUsageDoughnutChartElement = $('.blockGasChart')[0]
   const coinBalanceHistoryChartElement = $('[data-chart="coinBalanceHistoryChart"]')[0]
   if (dashboardChartElement) {
     window.dashboardChart = createMarketHistoryChart(dashboardChartElement)
   }
   if (gasUsageChartElement) {
     createGasUsageHistoryChart(gasUsageChartElement)
-  }
-  if (blockGasUsageDoughnutChartElement) {
-    // createBlockGasHistoryChart(blockGasUsageDoughnutChartElement)
   }
   if (coinBalanceHistoryChartElement) {
     window.coinBalanceHistoryChart = createCoinBalanceHistoryChart(coinBalanceHistoryChartElement)
