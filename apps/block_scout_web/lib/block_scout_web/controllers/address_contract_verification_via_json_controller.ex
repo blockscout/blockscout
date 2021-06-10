f7d41877f8ac4ad80da114b77c492b0efa5f01c5
nikitosing <nikitosing4@mail.ru>
2021-06-10 16:49:32 +0700

    Added sourcify verification

--- apps/block_scout_web/lib/block_scout_web/controllers/address_contract_verification_via_json_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/address_contract_verification_via_json_controller.ex
@@ -1,15 +1,24 @@
 defmodule BlockScoutWeb.AddressContractVerificationViaJsonController do
   use BlockScoutWeb, :controller
 
+  alias BlockScoutWeb.AddressContractVerificationController, as: VerificationController
   alias Explorer.Chain.SmartContract
+  alias Explorer.ThirdPartyIntegrations.Sourcify
 
   def new(conn, %{"address_id" => address_hash_string}) do
-    changeset =
-      SmartContract.changeset(
-        %SmartContract{address_hash: address_hash_string},
-        %{}
-      )
+    case Sourcify.check_by_address(address_hash_string) do
+      {:ok, _verified_status} ->
+        VerificationController.get_metadata_and_publish(address_hash_string, conn)
+        redirect(conn, to: address_path(conn, :show, address_hash_string))
 
-    render(conn, "new.html", changeset: changeset, address_hash: address_hash_string)
+      _ ->
+        changeset =
+          SmartContract.changeset(
+            %SmartContract{address_hash: address_hash_string},
+            %{}
+          )
+
+        render(conn, "new.html", changeset: changeset, address_hash: address_hash_string)
+    end
   end
 end
