c10dfec7333a6c8fb1a8a697433a9597dad75e3c
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-06-16 20:10:01 +0300

    Vyper verification: change to linux bundle

--- apps/explorer/lib/explorer/smart_contract/vyper_downloader.ex
+++ apps/explorer/lib/explorer/smart_contract/vyper_downloader.ex
@@ -112,9 +112,9 @@ defmodule Explorer.SmartContract.VyperDownloader do
       Enum.reduce_while(release_assets, "", fn asset, acc ->
         browser_download_url = Map.get(asset, "browser_download_url")
 
-        # todo 
-        # if browser_download_url =~ "linux" do
-        if browser_download_url =~ "darwin" do
+        # darwin is for local tests
+        # if browser_download_url =~ "darwin" do
+        if browser_download_url =~ "linux" do
           {:halt, browser_download_url}
         else
           {:cont, acc}
