3eae6017fac8186352c7bf72518781b3a4c452f7
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-06-17 22:32:05 +0300

    Text banner

---
+++ apps/block_scout_web/lib/block_scout_web/controllers/advertisement/text_ad_controller.ex
@@ -0,0 +1,26 @@
+defmodule BlockScoutWeb.Advertisement.TextAdController do
+  use BlockScoutWeb, :controller
+
+  alias HTTPoison.Response
+
+  def index(conn, params) do
+    # todo
+    ad_api_key = "19260bf627546ab7242"
+    ad_api_url = "https://request-global.czilladx.com/serve/native.php?z=#{ad_api_key}"
+
+    case HTTPoison.get(ad_api_url) do
+      {:ok, %Response{body: body, status_code: 200}} ->
+        case Jason.decode(body) do
+          {:ok, body_decoded} ->
+            title = Map.get(body_decoded, "title")
+            render(conn, "index.html", title: title)
+
+          _ ->
+            render(conn, "empty.html")
+        end
+
+      _ ->
+        render(conn, "empty.html")
+    end
+  end
+end
