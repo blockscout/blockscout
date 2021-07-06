305cf204735d8256a51b76ca0bcdae1d299d59c7
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-07-06 13:07:45 +0300

    Sokol ad imressionURL click

--- apps/block_scout_web/assets/js/lib/ad.js
+++ apps/block_scout_web/assets/js/lib/ad.js
@@ -4,13 +4,14 @@ export function getTextAdData () {
   $.get('https://request-global.czilladx.com/serve/native.php?z=19260bf627546ab7242', function (data) {
     if (data) {
       console.log(data)
-      const { ad: { name, description_short: descriptionShort, thumbnail, url, cta_button: ctaButton } } = data
+      const { ad: { name, description_short: descriptionShort, thumbnail, url, cta_button: ctaButton, impressionUrl } } = data
       $('.ad').removeClass('d-none')
       $('.ad-name').text(name)
       $('.ad-short-description').text(descriptionShort)
       $('.ad-cta-button').text(ctaButton)
       $('.ad-url').attr('href', url)
       $('.ad-img-url').attr('src', thumbnail)
+      $('.ad').data('impressionUrl', impressionUrl)
     } else {
       $('.ad').addClass('d-none')
     }
@@ -19,4 +20,8 @@ export function getTextAdData () {
 
 $(function () {
   getTextAdData()
+
+  $('.ad-url').on('click', function () {
+    $.get($('.ad').data('impressionUrl'))
+  })
 })
