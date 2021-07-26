7dba5d4cc220228764ba1b8a3411b126f8e926db
obasilakis <33531476+obasilakis@users.noreply.github.com>
2021-07-26 13:11:56 +0200

    Obasilakis/stop api from writing to db (#312)
    * Return 0 if coinsupply last fetched counter doesn't exist
    
    * Stop api from writing to db

--- apps/explorer/config/config.exs
+++ apps/explorer/config/config.exs
@@ -140,6 +140,8 @@ config :explorer, Explorer.Integrations.EctoLogger, query_time_ms_threshold: :ti
 
 config :explorer, Explorer.Market.History.Cataloger, enabled: System.get_env("DISABLE_INDEXER") != "true"
 
+config :explorer, Explorer.Chain.Cache.MinMissingBlockNumber, enabled: System.get_env("DISABLE_WRITE_API") != "true"
+
 txs_stats_init_lag =
   System.get_env("TXS_HISTORIAN_INIT_LAG", "0")
   |> Integer.parse()
