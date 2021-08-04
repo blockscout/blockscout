afa277d4f2ad0fabda72d3ab295c61ff2efc1168
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-08-04 13:44:51 +0300

    Add bttom padding for in-house ad in autocomplete dd

--- apps/block_scout_web/assets/js/lib/autocomplete.js
+++ apps/block_scout_web/assets/js/lib/autocomplete.js
@@ -28,7 +28,7 @@ const dataSrc = async (query, id) => {
 const resultsListElement = (list, data) => {
   const info = document.createElement('p')
   const adv = `
-  <div class="ad mb-3" style="visibility: hidden;">
+  <div class="ad dd mb-3" style="visibility: hidden;">
     <span class='ad-prefix'></span>: <img class="ad-img-url" width=20 height=20 /> <b><span class="ad-name"></span></b> - <span class="ad-short-description"></span> <a class="ad-url"><b><span class="ad-cta-button"></span></a></b>
   </div>`
   info.innerHTML = adv
