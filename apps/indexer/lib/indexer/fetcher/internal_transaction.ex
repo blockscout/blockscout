c5f627c350dcba0a4e6c4953dda56984b827cdd0
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-08-18 17:51:46 +0300

    Reduce batch sizes

--- apps/indexer/lib/indexer/fetcher/internal_transaction.ex
+++ apps/indexer/lib/indexer/fetcher/internal_transaction.ex
@@ -20,8 +20,8 @@ defmodule Indexer.Fetcher.InternalTransaction do
 
   @behaviour BufferedTask
 
-  @max_batch_size 20
-  @max_concurrency 48
+  @max_batch_size 10
+  @max_concurrency 30
   @defaults [
     flush_interval: :timer.seconds(3),
     max_concurrency: @max_concurrency,
