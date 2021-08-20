92aba298d33e521058227fe0a0a6bf5dd7d75c6d
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2021-08-20 11:02:05 +0300

    SKALE-4465 Add script for running schain explorer

---
+++ docker/run_schain_explorer.sh
@@ -0,0 +1,15 @@
+set -e
+
+: "${SCHAIN_NAME?Need to set SCHAIN_NAME}"
+: "${ENDPOINT?Need to set ENDPOINT}"
+: "${PORT?Need to set PORT}"
+: "${DB_PORT?Need to set DB_PORT}"
+
+export LOGO=/images/skale_logo.png
+export SUBNETWORK=${SCHAIN_NAME}
+export ETHEREUM_JSONRPC_VARIANT=geth
+export ETHEREUM_JSONRPC_HTTP_URL=${ENDPOINT}
+export NETWORK=SKALE
+
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+make start
