69cd398d19aa42d5f649ee604c72f444441a133f
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-08-20 10:14:57 +0300

    Reduce batch size

--- apps/indexer/lib/indexer/block/catchup/fetcher.ex
+++ apps/indexer/lib/indexer/block/catchup/fetcher.ex
@@ -32,8 +32,8 @@ defmodule Indexer.Block.Catchup.Fetcher do
   # These are all the *default* values for options.
   # DO NOT use them directly in the code.  Get options from `state`.
 
-  @blocks_batch_size 25
-  @blocks_concurrency 32
+  @blocks_batch_size 8
+  @blocks_concurrency 100
   @sequence_name :block_catchup_sequencer
 
   defstruct blocks_batch_size: @blocks_batch_size,
