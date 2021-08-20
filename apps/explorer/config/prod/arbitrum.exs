71bad3735585a6ba1042135c3c74a4bb2cf43e1f
Victor Baranov <baranov.viktor.27@gmail.com>
2021-08-20 11:55:13 +0300

    Merge pull request #4531 from twt--/arbitrum
    Add arbitrum support

---
+++ apps/explorer/config/prod/arbitrum.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+config :explorer,
+  json_rpc_named_arguments: [
+    transport: EthereumJSONRPC.HTTP,
+    transport_options: [
+      http: EthereumJSONRPC.HTTP.HTTPoison,
+      url: System.get_env("ETHEREUM_JSONRPC_HTTP_URL"),
+      http_options: [recv_timeout: :timer.minutes(1), timeout: :timer.minutes(1), hackney: [pool: :ethereum_jsonrpc]]
+    ],
+    variant: EthereumJSONRPC.Arbitrum
+  ],
+  subscribe_named_arguments: [
+    transport: EthereumJSONRPC.WebSocket,
+    transport_options: [
+      web_socket: EthereumJSONRPC.WebSocket.WebSocketClient,
+      url: System.get_env("ETHEREUM_JSONRPC_WS_URL")
+    ],
+    variant: EthereumJSONRPC.Arbitrum
+  ]
