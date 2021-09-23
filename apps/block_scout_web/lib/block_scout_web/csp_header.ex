665f434c2b84a21e4d6edea9db6e688b067bdd8b
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-09-23 15:41:35 +0300

    Add Pendo.io tracking

--- apps/block_scout_web/lib/block_scout_web/csp_header.ex
+++ apps/block_scout_web/lib/block_scout_web/csp_header.ex
@@ -11,14 +11,14 @@ defmodule BlockScoutWeb.CSPHeader do
   def call(conn, _opts) do
     Controller.put_secure_browser_headers(conn, %{
       "content-security-policy" => "\
-        connect-src 'self' #{websocket_endpoints(conn)} https://www.google-analytics.com/ *.poa.network https://request-global.czilladx.com https://raw.githubusercontent.com/; \
+        connect-src 'self' #{websocket_endpoints(conn)} https://www.google-analytics.com/ *.poa.network https://request-global.czilladx.com https://raw.githubusercontent.com/ https://stats.g.doubleclick.net/; \
         default-src 'self';\
-        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.googletagmanager.com https://www.google-analytics.com/ https://www.gstatic.com https://hcaptcha.com https://assets.hcaptcha.com https://newassets.hcaptcha.com https://coinzillatag.com;\
+        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.googletagmanager.com https://www.google-analytics.com/ https://www.gstatic.com *.hcaptcha.com https://assets.hcaptcha.com https://coinzillatag.com *.pendo.io;\
         style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com;\
         img-src 'self' * data:;\
         media-src 'self' * data:;\
         font-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.gstatic.com data:;\
-        frame-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://hcaptcha.com https://assets.hcaptcha.com https://newassets.hcaptcha.com https://request-global.czilladx.com/;\
+        frame-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com *.hcaptcha.com https://request-global.czilladx.com/;\
       "
     })
   end
