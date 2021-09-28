05ff90fd5033f22f0d8e687585a0225156b73973
Valentin Rodygin <valentin@clabs.co>
2021-09-28 09:45:14 +0200

    Reduced the size of js bundles

--- apps/block_scout_web/assets/js/view_specific/raw_trace/code_highlighting.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery'
-import hljs from 'highlight.js'
-
-// only activate highlighting on pages with this selector
-if ($('[data-activate-highlight]').length > 0) {
-  hljs.initHighlightingOnLoad()
-}
