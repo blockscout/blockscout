afc1c1ec95e8ad211a440396920e44e895cf2209
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-10-12 10:56:38 +0300

    Fix CSV export logs test

--- apps/explorer/test/explorer/chain/address_log_csv_explorer_test.exs
+++ apps/explorer/test/explorer/chain/address_log_csv_explorer_test.exs
@@ -74,7 +74,7 @@ defmodule Explorer.Chain.AddressLogCsvExporterTest do
       assert result.index == to_string(log.index)
       assert result.block_number == to_string(log.block_number)
       assert result.block_hash == to_string(log.block_hash)
-      assert result.address == to_string(log.address)
+      assert result.address == String.downcase(to_string(log.address))
       assert result.data == to_string(log.data)
       assert result.first_topic == to_string(log.first_topic)
       assert result.second_topic == to_string(log.second_topic)
