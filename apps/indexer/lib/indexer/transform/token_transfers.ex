0def9f94bb2ef51aad16f608dce1a43a8701e5d3
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-11-01 20:27:15 +0300

    Remove duplicate

--- apps/indexer/lib/indexer/transform/token_transfers.ex
+++ apps/indexer/lib/indexer/transform/token_transfers.ex
@@ -8,7 +8,6 @@ defmodule Indexer.Transform.TokenTransfers do
   alias ABI.TypeDecoder
   alias Explorer.{Chain, Repo}
   alias Explorer.Chain.{Token, TokenTransfer}
-  alias Explorer.Chain.TokenTransfer
   alias Explorer.Token.MetadataRetriever
 
   @burn_address "0x0000000000000000000000000000000000000000"
