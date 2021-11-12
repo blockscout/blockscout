6d231d420830aa200eddc8cc3a1f1efe592b6d0b
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-11-12 14:27:42 +0300

    Extend CSP for Pendo

--- apps/block_scout_web/lib/block_scout_web/csp_header.ex
+++ apps/block_scout_web/lib/block_scout_web/csp_header.ex
@@ -13,7 +13,7 @@ defmodule BlockScoutWeb.CSPHeader do
       "content-security-policy" => "\
         connect-src 'self' #{websocket_endpoints(conn)} https://www.google-analytics.com/ *.poa.network https://request-global.czilladx.com https://raw.githubusercontent.com/trustwallet/assets/ https://stats.g.doubleclick.net/; \
         default-src 'self';\
-        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.googletagmanager.com https://www.google-analytics.com/ https://www.gstatic.com *.hcaptcha.com https://assets.hcaptcha.com https://coinzillatag.com *.pendo.io;\
+        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.googletagmanager.com https://www.google-analytics.com/ https://www.gstatic.com *.hcaptcha.com https://assets.hcaptcha.com https://coinzillatag.com *.pendo.io https://pendo-io-static.storage.googleapis.com/;\
         style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com;\
         img-src 'self' * data:;\
         media-src 'self' * data:;\
