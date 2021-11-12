7e6f2dd731f0fd6fddf772647a73d3c9feea6335
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-11-12 14:56:39 +0300

    Extend CSP for Pendo

--- apps/block_scout_web/lib/block_scout_web/csp_header.ex
+++ apps/block_scout_web/lib/block_scout_web/csp_header.ex
@@ -11,14 +11,16 @@ defmodule BlockScoutWeb.CSPHeader do
   def call(conn, _opts) do
     Controller.put_secure_browser_headers(conn, %{
       "content-security-policy" => "\
-        connect-src 'self' #{websocket_endpoints(conn)} https://www.google-analytics.com/ *.poa.network https://request-global.czilladx.com https://raw.githubusercontent.com/trustwallet/assets/ https://stats.g.doubleclick.net/; \
+        connect-src 'self' #{websocket_endpoints(conn)} https://www.google-analytics.com/ *.poa.network https://request-global.czilladx.com https://raw.githubusercontent.com/trustwallet/assets/ https://stats.g.doubleclick.net/ app.pendo.io pendo-io-static.storage.googleapis.com cdn.pendo.io pendo-static-SUB_ID.storage.googleapis.com data.pendo.io; \
         default-src 'self';\
         script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.googletagmanager.com https://www.google-analytics.com/ https://www.gstatic.com *.hcaptcha.com https://assets.hcaptcha.com https://coinzillatag.com *.pendo.io https://pendo-io-static.storage.googleapis.com/;\
-        style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com;\
-        img-src 'self' * data:;\
+        style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com app.pendo.io cdn.pendo.io pendo-static-SUB_ID.storage.googleapis.com;\
+        img-src 'self' * data: cdn.pendo.io app.pendo.io pendo-static-SUB_ID.storage.googleapis.com data.pendo.io;\
         media-src 'self' * data:;\
         font-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.gstatic.com data:;\
-        frame-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com *.hcaptcha.com https://request-global.czilladx.com/;\
+        frame-ancestors app.pendo.io;\
+        frame-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com *.hcaptcha.com https://request-global.czilladx.com/ app.pendo.io;\
+        child-src app.pendo.io;\
       "
     })
   end
