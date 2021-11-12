ae84a8bfe23ebb33a5b22958b4f5f0ee2645513b
Oleg Sovetnik <sovetnik@oblaka.biz>
2021-11-12 17:59:56 +0300

    Associate watchlist with addresses

--- apps/explorer/lib/explorer/accounts/watchlist_address.ex
+++ apps/explorer/lib/explorer/accounts/watchlist_address.ex
@@ -3,10 +3,12 @@ defmodule Explorer.Accounts.WatchlistAddress do
   import Ecto.Changeset
 
   alias Explorer.Accounts.Watchlist
+  alias Explorer.Chain.Address
+  alias Explorer.Chain.Hash
 
   schema "account_watchlist_addresses" do
-    field :hash, :string
     field :name, :string
+    belongs_to(:address, Address, foreign_key: :address_hash, references: :hash, type: Hash.Address)
     belongs_to(:watchlist, Watchlist)
 
     timestamps()
@@ -15,7 +17,7 @@ defmodule Explorer.Accounts.WatchlistAddress do
   @doc false
   def changeset(watchlist_address, attrs) do
     watchlist_address
-    |> cast(attrs, [:name, :hash])
-    |> validate_required([:name, :hash])
+    |> cast(attrs, [:name, :address_hash])
+    |> validate_required([:name, :address_hash])
   end
 end
--- apps/explorer/priv/repo/migrations/20211105130907_create_account_watchlist_addresses.exs
+++ apps/explorer/priv/repo/migrations/20211105130907_create_account_watchlist_addresses.exs
@@ -4,14 +4,13 @@ defmodule Explorer.Repo.Migrations.CreateAccountWatchlistAddresses do
   def change do
     create table(:account_watchlist_addresses) do
       add(:name, :string)
-      add(:coin, :string)
-      add(:hash, :bytea, null: false)
+      add(:address_hash, references(:addresses, column: :hash, type: :bytea, on_delete: :delete_all))
       add(:watchlist_id, references(:account_watchlists, on_delete: :delete_all))
 
       timestamps()
     end
 
     create(index(:account_watchlist_addresses, [:watchlist_id]))
-    create(index(:account_watchlist_addresses, [:coin]))
+    create(index(:account_watchlist_addresses, [:address_hash]))
   end
 end
