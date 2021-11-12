3b6b2c18c96d0e4663902790bce099ef3f4e5e3c
Enrique Ruiz <enrique@keyko.io>
2021-11-12 09:01:47 +0100

    Eruizgar91/update apps tab (#468)
    * Update names in the apps tab
    
    * update more name
    
    * Get rid off of unused variable
    
    * Fix translation keys

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -220,20 +220,16 @@ defmodule BlockScoutWeb.LayoutView do
     list(:bridges)
   end
 
-  def other_bridges_list do
-    list(:other_bridges)
-  end
-
-  def bridges_alm_list do
-    list(:bridges_alm)
-  end
-
   def defi_list do
     list(:defi)
   end
 
-  def nft_list do
-    list(:nft)
+  def resources_list do
+    list(:resources)
+  end
+
+  def learning_list do
+    list(:learning)
   end
 
   def apps_list do
