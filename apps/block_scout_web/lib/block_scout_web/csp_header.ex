cf24b64ab43395c0ca5a857a3e2dfd202819c358
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-11-15 12:07:38 +0300

    Set subscriptionID for Pendo

--- apps/block_scout_web/lib/block_scout_web/csp_header.ex
+++ apps/block_scout_web/lib/block_scout_web/csp_header.ex
@@ -13,7 +13,7 @@ defmodule BlockScoutWeb.CSPHeader do
       "content-security-policy" => "\
         connect-src 'self' #{websocket_endpoints(conn)} https://www.google-analytics.com/ *.poa.network https://request-global.czilladx.com https://raw.githubusercontent.com/trustwallet/assets/ https://stats.g.doubleclick.net/ app.pendo.io pendo-io-static.storage.googleapis.com cdn.pendo.io pendo-static-6500107995185152.storage.googleapis.com data.pendo.io; \
         default-src 'self';\
-        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.googletagmanager.com https://www.google-analytics.com/ https://www.gstatic.com *.hcaptcha.com https://assets.hcaptcha.com https://coinzillatag.com *.pendo.io https://pendo-io-static.storage.googleapis.com/;\
+        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.googletagmanager.com https://www.google-analytics.com/ https://www.gstatic.com *.hcaptcha.com https://assets.hcaptcha.com https://coinzillatag.com app.pendo.io pendo-io-static.storage.googleapis.com cdn.pendo.io pendo-static-6500107995185152.storage.googleapis.com data.pendo.io;\
         style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com app.pendo.io cdn.pendo.io pendo-static-6500107995185152.storage.googleapis.com;\
         img-src 'self' * data: cdn.pendo.io app.pendo.io pendo-static-6500107995185152.storage.googleapis.com data.pendo.io;\
         media-src 'self' * data:;\
