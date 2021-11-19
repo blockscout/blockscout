e7bdb65244070f34615b5b1faa30f8e4e9b7548c
nikitosing <nikitosing4@mail.ru>
2021-11-19 16:30:21 +0300

    changed controller

--- apps/block_scout_web/lib/block_scout_web/controllers/address_contract_verification_via_standard_json_input_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/address_contract_verification_via_standard_json_input_controller.ex
@@ -9,41 +9,32 @@ defmodule BlockScoutWeb.AddressContractVerificationViaStandardJsonInputControlle
   alias Explorer.ThirdPartyIntegrations.Sourcify
 
   def new(conn, %{"address_id" => address_hash_string}) do
-    address_path =
-      conn
-      |> address_path(:show, address_hash_string)
-      |> Controller.full_path()
-
     if Chain.smart_contract_fully_verified?(address_hash_string) do
+      address_path =
+        conn
+        |> address_path(:show, address_hash_string)
+        |> Controller.full_path()
       redirect(conn, to: address_path)
     else
-      case Sourcify.check_by_address(address_hash_string) do
-        {:ok, _verified_status} ->
-          VerificationController.get_metadata_and_publish(address_hash_string, conn)
-          redirect(conn, to: address_path)
-
-        _ ->
-          changeset =
+      changeset =
             SmartContract.changeset(
               %SmartContract{address_hash: address_hash_string},
               %{}
             )
 
-          compiler_versions =
-            case CompilerVersion.fetch_versions(:solc) do
-              {:ok, compiler_versions} ->
-                compiler_versions
+      compiler_versions =
+        case CompilerVersion.fetch_versions(:solc) do
+          {:ok, compiler_versions} ->
+            compiler_versions
 
-              {:error, _} ->
-                []
-            end
+          {:error, _} ->
+            []
+        end
 
-          render(conn, "new.html",
-            changeset: changeset,
-            address_hash: address_hash_string,
-            compiler_versions: compiler_versions
-          )
-      end
+      render(conn, "new.html",
+        changeset: changeset,
+        address_hash: address_hash_string,
+        compiler_versions: compiler_versions)
     end
   end
 end
