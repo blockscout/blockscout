e8d86ae722e57b10e1614c301b851d3d62de210c
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-12-01 12:12:04 +0400

    Increase timeout for async tasks on address page 60s -> 180s

--- apps/block_scout_web/lib/block_scout_web/controllers/address_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/address_controller.ex
@@ -141,7 +141,7 @@ defmodule BlockScoutWeb.AddressController do
       validation_count_task,
       crc_total_worth_task
     ]
-    |> Task.yield_many(:timer.seconds(60))
+    |> Task.yield_many(:timer.seconds(180))
     |> Enum.map(fn {_task, res} ->
       case res do
         {:ok, result} ->
