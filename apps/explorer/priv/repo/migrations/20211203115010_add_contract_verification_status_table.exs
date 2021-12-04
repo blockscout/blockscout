9eb10bc1e1a92b317db8d35964da438b9275420d
nikitosing <nikitosing4@mail.ru>
2021-12-04 13:06:39 +0300

    fix migrations"

--- apps/explorer/priv/repo/migrations/20211203115010_add_contract_verification_status_table.exs
+++ apps/explorer/priv/repo/migrations/20211203115010_add_contract_verification_status_table.exs
@@ -2,8 +2,8 @@ defmodule Explorer.Repo.Migrations.AddContractVerificationStatusTable do
   use Ecto.Migration
   
   def change do
-    create table("contract_verification_status") do
-      add(:uid, :string, size: 64)
+    create table("contract_verification_status", primary_key: false) do
+      add(:uid, :string, size: 64, primary_key: true)
       add(:status, :int2, null: false)
       add(:address_hash, :bytea, null: false)
 
