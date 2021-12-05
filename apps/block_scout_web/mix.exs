e67cbc8d6f5bd80b66a72d9d569adedeb9c68637
nikitosing <nikitosing4@mail.ru>
2021-12-05 10:41:24 +0300

    ignore waring

--- apps/block_scout_web/mix.exs
+++ apps/block_scout_web/mix.exs
@@ -23,6 +23,7 @@ defmodule BlockScoutWeb.Mixfile do
         credo: :test,
         dialyzer: :test
       ],
+      xref: [exclude: [Explorer.Chain.SmartContract.VerificationStatus]],
       start_permanent: Mix.env() == :prod,
       version: "0.0.1"
     ]
