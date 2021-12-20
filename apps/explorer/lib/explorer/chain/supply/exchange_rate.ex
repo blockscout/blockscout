b330292b738d74c7f15645002103bdb31d257d90
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-12-20 16:20:35 +0400

    cirullating supply minus wpoa balance

--- apps/explorer/lib/explorer/chain/supply/exchange_rate.ex
+++ apps/explorer/lib/explorer/chain/supply/exchange_rate.ex
@@ -6,10 +6,26 @@ defmodule Explorer.Chain.Supply.ExchangeRate do
   use Explorer.Chain.Supply
 
   alias Explorer.ExchangeRates.Token
-  alias Explorer.Market
+  alias Explorer.{Chain, Market}
+
+  @wpoa_address "0xD2CFBCDbDF02c42951ad269dcfFa27c02151Cebd"
 
   def circulating do
-    exchange_rate().available_supply
+    with {:ok, address_hash} <- Chain.string_to_address_hash(@wpoa_address),
+         {:ok, address} <- Chain.hash_to_address(address_hash) do
+      wpoa_total_supply = address.token.total_supply
+      available_supply = exchange_rate().available_supply
+
+      circulating_supply = Decimal.sub(available_supply, wpoa_total_supply)
+
+      if Decimal.cmp(circulating_supply, 0) == :gt do
+        circulating_supply
+      else
+        Decimal.new(0)
+      end
+    else
+      _ -> Decimal.new(0)
+    end
   end
 
   def total do
