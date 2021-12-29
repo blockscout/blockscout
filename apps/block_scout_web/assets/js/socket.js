21362371a971060f018a4dcdfd94d00143b28338
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-12-29 14:52:16 +0300

    /etc/mordor for websocket path

--- apps/block_scout_web/assets/js/socket.js
+++ apps/block_scout_web/assets/js/socket.js
@@ -1,7 +1,7 @@
 import { Socket } from 'phoenix'
 import { locale } from './locale'
 
-const socket = new Socket('/etc/mainnet/socket', { params: { locale: locale } })
+const socket = new Socket('/etc/mordor/socket', { params: { locale: locale } })
 socket.connect()
 
 export default socket
