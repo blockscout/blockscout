0fd6eea9a3f3c39bd1d4364668902fef2f31a0e4
Victor Baranov <baranov.viktor.27@gmail.com>
2022-01-12 19:36:38 +0300

    Merge pull request #5090 from blockscout/vb-rate-limit-by-ip
    Allotted rate limit by IP

--- apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/rpc_translator.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/rpc_translator.ex
@@ -25,11 +25,15 @@ defmodule BlockScoutWeb.API.RPC.RPCTranslator do
   alias Plug.Conn
 
   APILogger.message(
-    "Current global API rate limit #{inspect(Application.get_env(:block_scout_web, :global_api_rate_limit))} reqs/sec"
+    "Current global API rate limit #{inspect(Application.get_env(:block_scout_web, :api_rate_limit)[:global_limit])} reqs/sec"
   )
 
   APILogger.message(
-    "Current API rate limit by key #{inspect(Application.get_env(:block_scout_web, :api_rate_limit_by_key))} reqs/sec"
+    "Current API rate limit by key #{inspect(Application.get_env(:block_scout_web, :api_rate_limit)[:limit_by_key])} reqs/sec"
+  )
+
+  APILogger.message(
+    "Current API rate limit by IP #{inspect(Application.get_env(:block_scout_web, :api_rate_limit)[:limit_by_ip])} reqs/sec"
   )
 
   def init(opts), do: opts
