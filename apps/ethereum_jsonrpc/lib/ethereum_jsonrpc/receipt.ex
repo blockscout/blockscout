522bad869f2ca517938dedd2e83a13e7140330c0
Viktor Baranov <baranov.viktor.27@gmail.com>
2022-01-18 17:02:34 +0300

    Optimism support

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/receipt.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/receipt.ex
@@ -300,7 +300,7 @@ defmodule EthereumJSONRPC.Receipt do
   end
 
   # Arbitrum fields
-  defp entry_to_elixir({key, _}) when key in ~w(returnData returnCode feeStats l1BlockNumber) do
+  defp entry_to_elixir({key, _}) when key in ~w(returnData returnCode feeStats l1BlockNumber l1GasUsed l1GasPrice l1FeeScalar l1Fee) do
     :ignore
   end
 
