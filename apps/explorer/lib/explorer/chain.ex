08caf2a960260e99c9ed3bf7ddd8c416effd29a4
nikitosing <nikitosing4@mail.ru>
2022-02-01 17:22:48 +0300

    Fix rewards tab

--- apps/explorer/lib/explorer/chain.ex
+++ apps/explorer/lib/explorer/chain.ex
@@ -7263,10 +7263,7 @@ defmodule Explorer.Chain do
   def address_to_rewards(address_hash, options \\ []) when is_list(options) do
     paging_options = Keyword.get(options, :paging_options, @default_paging_options)
 
-    with true <- Application.get_env(:block_scout_web, BlockScoutWeb.Chain)[:has_emission_funds],
-         true <- address_has_rewards?(address_hash),
-         %{payout_key: block_miner_payout_address} <- Reward.get_validator_payout_key_by_mining(address_hash),
-         true <- block_miner_payout_address && address_hash == block_miner_payout_address do
+    with true <- Application.get_env(:block_scout_web, BlockScoutWeb.Chain)[:has_emission_funds] do
       address_hash
       |> Reward.fetch_emission_rewards_tuples(paging_options)
     else
