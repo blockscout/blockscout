a206e2ad9c33a2801511c008ebb7f7e17a5c5867
Donald Hutchison <git@toastymofo.net>
2022-02-11 13:40:29 +0100

    Bugfix/verified contract publishing (#555)
    * Failing test.
    
    * Fix test.
    
    * Format.

---
+++ apps/block_scout_web/test/block_scout_web/controllers/address_contract_verification_controller_test.exs
@@ -0,0 +1,24 @@
+defmodule BlockScoutWeb.AddressContractVerificationControllerTest do
+  use BlockScoutWeb.ConnCase, async: true
+
+  alias BlockScoutWeb.AddressContractVerificationController
+
+  describe "sourcify integration" do
+    test "parses sourcify metadata correctly", %{conn: conn} do
+      data = failing_sourcify_metadata()
+
+      %{"params_to_publish" => result} =
+        AddressContractVerificationController.parse_params_from_sourcify(
+          "0x0E7a0c8FAb504dbB94F1e33E0A09ab4506Ea2e9b",
+          data
+        )
+
+      assert result["contract_source_code"] != nil, "Contract source code should be included in params to publish"
+    end
+  end
+
+  defp failing_sourcify_metadata do
+    File.read!("./test/support/fixture/sourcify/cryptopunk.json")
+    |> Jason.decode!()
+  end
+end
