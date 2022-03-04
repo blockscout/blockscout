5e4514e595bf6ad8b64604c560a26082c0a67661
Alex Monso <amonsosanz@gmail.com>
2022-03-04 01:43:56 +0100

    Remove leftover comment

--- bin/install_chrome_headless.sh
+++ bin/install_chrome_headless.sh
@@ -2,7 +2,6 @@ sudo apt-get update
 sudo apt-get -y install xvfb
 export DISPLAY=:99.0
 Xvfb $DISPLAY -screen 0 1024x768x16 -nolisten tcp &
-# sh -e /etc/init.d/xvfb start
 # export CHROMEDRIVER_VERSION=`curl -s http://chromedriver.storage.googleapis.com/LATEST_RELEASE`
 export CHROMEDRIVER_VERSION="99.0.4844.51"
 curl -L -O "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
