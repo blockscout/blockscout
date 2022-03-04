46eab2298f9809462dd08bb7cd071ddd72543dd4
Alex Monso <amonsosanz@gmail.com>
2022-03-04 01:00:34 +0100

    Improve the script install_chrome_headless.sh so it can be run on docker by act

--- bin/install_chrome_headless.sh
+++ bin/install_chrome_headless.sh
@@ -1,11 +1,14 @@
+sudo apt-get update
+sudo apt-get -y install xvfb
 export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
+Xvfb $DISPLAY -screen 0 1024x768x16 -nolisten tcp &
+# sh -e /etc/init.d/xvfb start
 # export CHROMEDRIVER_VERSION=`curl -s http://chromedriver.storage.googleapis.com/LATEST_RELEASE`
-export CHROMEDRIVER_VERSION=`92.0.4515.43`
+export CHROMEDRIVER_VERSION="99.0.4844.51"
 curl -L -O "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
 unzip chromedriver_linux64.zip
 sudo chmod +x chromedriver
 sudo mv chromedriver /usr/local/bin
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
-sudo apt-get install libstdc++6
+sudo apt -y install ./google-chrome-stable_current_amd64.deb
+sudo apt-get -y install libstdc++6
