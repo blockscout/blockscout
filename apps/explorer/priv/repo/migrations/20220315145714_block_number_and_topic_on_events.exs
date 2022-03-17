f60929739b2f648fe2de53d1ee9d500f8d79828f
Donald Hutchison <git@toastymofo.net>
2022-03-17 17:23:15 +0100

    Split migration into two. (#588)
    * Split migration into two.
    
    * Drop pkey constraint on rollback.
    
    * Also drop block_number index.

--- apps/explorer/priv/repo/migrations/20220315145714_block_number_and_topic_on_events.exs
+++ apps/explorer/priv/repo/migrations/20220315145714_block_number_and_topic_on_events.exs
@@ -19,39 +19,12 @@ defmodule Explorer.Repo.Migrations.BlockNumberAndTopicOnEvents do
       update: [set: [block_number: log.block_number, topic: log.first_topic]]
     )
     |> repo().update_all([])
-
-    # use block_number in primary key
-    alter table(:celo_contract_events) do
-      remove(:block_hash)
-      modify(:block_number, :integer, primary_key: true, null: false)
-      modify(:log_index, :integer, primary_key: true, null: false)
-      modify(:topic, :string, null: false)
-    end
-
-    # add indices to block_number and topic
-    create(index(:celo_contract_events, :block_number))
-    create(index(:celo_contract_events, :topic))
   end
 
   def down do
     alter table(:celo_contract_events) do
-      add(:block_hash, :bytea)
-    end
-
-    flush()
-
-    from(e in "celo_contract_events",
-      join: l in "logs",
-      on: {l.block_number, l.index} == {e.block_number, e.log_index},
-      update: [set: [block_hash: l.block_hash]]
-    )
-    |> repo().update_all([])
-
-    alter table(:celo_contract_events) do
       remove(:block_number)
       remove(:topic)
-      modify(:block_hash, :bytea, primary_key: true, null: false)
-      modify(:log_index, :integer, primary_key: true, null: false)
     end
   end
 end
