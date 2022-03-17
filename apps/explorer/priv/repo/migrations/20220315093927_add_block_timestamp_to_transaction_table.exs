828d62ce1c7d40f6a745ccf2e6e68f654715e43a
Viktor Baranov <baranov.viktor.27@gmail.com>
2022-03-17 09:30:53 +0300

    Remove timestamp update from DB migration

--- apps/explorer/priv/repo/migrations/20220315093927_add_block_timestamp_to_transaction_table.exs
+++ apps/explorer/priv/repo/migrations/20220315093927_add_block_timestamp_to_transaction_table.exs
@@ -6,13 +6,6 @@ defmodule Explorer.Repo.Migrations.AddBlockTimestampToTransactionTable do
       add(:block_timestamp, :utc_datetime_usec)
     end
 
-    execute("""
-    UPDATE transactions tx
-    SET block_timestamp = b.timestamp
-    FROM blocks b
-    WHERE b.hash = tx.block_hash;
-    """)
-
     create(index(:transactions, :block_timestamp))
   end
 end
