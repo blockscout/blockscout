eee32f465ceea5c510de0e4f05c7cf6cc9fc6cef
nikitosing <nikitosing4@mail.ru>
2022-03-21 00:28:30 +0300

    Final distribution of changes

--- apps/explorer/lib/explorer/chain/token_transfer.ex
+++ apps/explorer/lib/explorer/chain/token_transfer.ex
@@ -159,7 +159,7 @@ defmodule Explorer.Chain.TokenTransfer do
       from(
         tt in TokenTransfer,
         where: tt.token_contract_address_hash == ^token_address_hash and not is_nil(tt.block_number),
-        preload: [{:transaction, :block}, :token, :from_address, :to_address],
+        preload: [:transaction, :token, :from_address, :to_address],
         order_by: [desc: tt.block_number]
       )
 
@@ -179,7 +179,7 @@ defmodule Explorer.Chain.TokenTransfer do
         where: tt.token_contract_address_hash == ^token_address_hash,
         where: tt.token_id == ^token_id,
         where: not is_nil(tt.block_number),
-        preload: [{:transaction, :block}, :token, :from_address, :to_address],
+        preload: [:transaction, :token, :from_address, :to_address],
         order_by: [desc: tt.block_number]
       )
 
