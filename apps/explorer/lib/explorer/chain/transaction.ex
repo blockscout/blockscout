a806406bea9cf207bbf33e8ae9a8559199d588b5
nikitosing <nikitosing4@mail.ru>
2022-03-21 00:28:30 +0300

    Final distribution of changes

--- apps/explorer/lib/explorer/chain/transaction.ex
+++ apps/explorer/lib/explorer/chain/transaction.ex
@@ -1024,7 +1024,7 @@ defmodule Explorer.Chain.Transaction do
     from(
       t in subquery(query),
       order_by: [desc: t.block_number, desc: t.index],
-      preload: [:from_address, :to_address, :created_contract_address, :block]
+      preload: [:from_address, :to_address, :created_contract_address]
     )
   end
 
@@ -1045,7 +1045,7 @@ defmodule Explorer.Chain.Transaction do
     from(
       t in subquery(query),
       order_by: [desc: t.block_number, desc: t.index],
-      preload: [:from_address, :to_address, :created_contract_address, :block]
+      preload: [:from_address, :to_address, :created_contract_address]
     )
   end
 
