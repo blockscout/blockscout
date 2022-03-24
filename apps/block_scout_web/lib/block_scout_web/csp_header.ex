3b7242a844c438035a016dc9925e15fe42dedb54
Everest <caio.everest@cloudwalk.io>
2022-03-24 13:49:42 -0300

    remove ads

--- apps/block_scout_web/lib/block_scout_web/csp_header.ex
+++ apps/block_scout_web/lib/block_scout_web/csp_header.ex
@@ -13,12 +13,12 @@ defmodule BlockScoutWeb.CSPHeader do
       "content-security-policy" => "\
         connect-src 'self' #{websocket_endpoints(conn)} wss://*.bridge.walletconnect.org/ https://request-global.czilladx.com/ https://raw.githubusercontent.com/trustwallet/assets/ https://registry.walletconnect.org/data/wallets.json https://*.poa.network;\
         default-src 'self';\
-        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://coinzillatag.com https://www.google.com https://www.gstatic.com;\
-        style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com;\
+        script-src 'self' 'sha256-QADITpnBuiXVfTzrfwP9VzAxptWL1j2UNMPM72SNEno=' 'sha256-S+kA9ZZC2ANiKZW1Soge1NhdxH9M4c2UTARyyzpLBio=' 'sha256-fqGNTzWau5hfYKKH3B59dBNOSh8VdaIQ5YXzH+9C0Ys=' 'sha256-89/1LLiXXHmqi3EdeHqEQ7Kz1VVc+6OXCUaen1kiUfg=' 'sha256-IBxcGAt2latSKF1Je/SHpNwTeU5Q1WKiBVNd13xlAiA=' *.google.com *.gstatic.com;\
+        style-src 'self' 'unsafe-inline' fonts.googleapis.com;\
         img-src 'self' * data:;\
         media-src 'self' * data:;\
-        font-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.gstatic.com data:;\
-        frame-src 'self' 'unsafe-inline' 'unsafe-eval' https://request-global.czilladx.com/ https://www.google.com;\
+        font-src 'self' 'unsafe-inline' fonts.gstatic.com data:;\
+        frame-src 'self' 'unsafe-inline' *.google.com;\
       "
     })
   end
--- apps/block_scout_web/lib/block_scout_web/views/advertisement/text_ad_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule BlockScoutWeb.Advertisement.TextAdView do
-  use BlockScoutWeb, :view
-end
