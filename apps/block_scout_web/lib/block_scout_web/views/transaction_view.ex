31866733f57118a584685e5e1dc24f4eda8a626d
Oleg Sovetnik <sovetnik@oblaka.biz>
2022-03-30 01:33:40 +0300

    Show personal_tx_tag on Transaction overview page

--- apps/block_scout_web/lib/block_scout_web/views/transaction_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/transaction_view.ex
@@ -13,7 +13,7 @@ defmodule BlockScoutWeb.TransactionView do
   alias Timex.Duration
 
   import BlockScoutWeb.Gettext
-  import BlockScoutWeb.AddressView, only: [from_address_hash: 1, short_token_id: 2]
+  import BlockScoutWeb.AddressView, only: [from_address_hash: 1, short_token_id: 2, tag_name_to_label: 1]
   import BlockScoutWeb.Tokens.Helpers
 
   @tabs ["token-transfers", "internal-transactions", "logs", "raw-trace"]
