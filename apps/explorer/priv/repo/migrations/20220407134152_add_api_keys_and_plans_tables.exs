dc3d68433bdde3368b29ef7df5863e5f8a337968
nikitosing <nikitosing4@mail.ru>
2022-04-13 16:22:25 +0300

    Review changes

--- apps/explorer/priv/repo/migrations/20220407134152_add_api_keys_and_plans_tables.exs
+++ apps/explorer/priv/repo/migrations/20220407134152_add_api_keys_and_plans_tables.exs
@@ -5,12 +5,12 @@ defmodule Explorer.Repo.Migrations.AddApiKeysAndPlansTables do
     create table(:account_api_plans, primary_key: false) do
       add(:id, :serial, null: false, primary_key: true)
       add(:max_req_per_second, :smallint)
-      add(:name, :string)
+      add(:name, :string, null: false)
 
       timestamps()
     end
 
-    create(unique_index(:account_api_plans, [:id]))
+    create(unique_index(:account_api_plans, [:id, :max_req_per_second, :name]))
 
     execute(
       "INSERT INTO account_api_plans (id, max_req_per_second, name, inserted_at, updated_at) VALUES (1, 10, 'Free Plan', NOW(), NOW());"
@@ -18,8 +18,8 @@ defmodule Explorer.Repo.Migrations.AddApiKeysAndPlansTables do
 
     create table(:account_api_keys, primary_key: false) do
       add(:identity_id, references(:account_identities, column: :id, on_delete: :delete_all), null: false)
-      add(:name, :string)
-      add(:api_key, :uuid, null: false, primary_key: true)
+      add(:name, :string, null: false)
+      add(:value, :uuid, null: false, primary_key: true)
 
       timestamps()
     end
@@ -28,7 +28,7 @@ defmodule Explorer.Repo.Migrations.AddApiKeysAndPlansTables do
       add(:plan_id, references(:account_api_plans, column: :id), default: 1)
     end
 
-    create(unique_index(:account_api_keys, [:api_key]))
+    create(unique_index(:account_api_keys, [:value]))
     create(index(:account_api_keys, [:identity_id]))
   end
 end
