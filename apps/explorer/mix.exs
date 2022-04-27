7d1f70fc8e646677b08835523020efa1897935de
nikitosing <nikitosing4@mail.ru>
2022-04-27 22:40:27 +0300

    Add sentry debugging

--- apps/explorer/mix.exs
+++ apps/explorer/mix.exs
@@ -111,7 +111,8 @@ defmodule Explorer.Mixfile do
       # `Timex.Duration` for `Explorer.Counters.AverageBlockTime.average_block_time/0`
       {:timex, "~> 3.7.1"},
       {:con_cache, "~> 1.0"},
-      {:tesla, "~> 1.3.3"}
+      {:tesla, "~> 1.3.3"},
+      {:sentry, "~> 8.0"}
     ]
   end
 
--- config/prod.exs
+++ config/prod.exs
@@ -24,3 +24,13 @@ config :logger, :account,
   level: :info,
   path: Path.absname("logs/prod/account.log"),
   metadata_filter: [fetcher: :account]
+
+config :sentry,
+  dsn: "https://fd7dceeeb4dd4fe1a0960a26d7d1260f@o1222505.ingest.sentry.io/6366450",
+  environment_name: :prod,
+  enable_source_code_context: true,
+  root_source_code_path: File.cwd!(),
+  tags: %{
+    env: "production"
+  },
+  included_environments: [:prod]
