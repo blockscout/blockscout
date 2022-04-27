5f702f2c25b55e4021383a36ff49aec9565f5fe5
nikitosing <nikitosing4@mail.ru>
2022-04-27 22:40:27 +0300

    Add sentry debugging

--- config/prod.exs
+++ config/prod.exs
@@ -19,3 +19,13 @@ config :logger, :error,
 config :ex_twilio,
   account_sid: {:system, "TWILIO_ACCOUNT_SID"},
   auth_token: {:system, "TWILIO_AUTH_TOKEN"}
+
+config :sentry,
+  dsn: "https://fd7dceeeb4dd4fe1a0960a26d7d1260f@o1222505.ingest.sentry.io/6366450",
+  environment_name: :prod,
+  enable_source_code_context: true,
+  root_source_code_path: File.cwd!(),
+  tags: %{
+    env: "production"
+  },
+  included_environments: [:prod]
