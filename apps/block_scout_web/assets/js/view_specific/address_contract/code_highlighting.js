c1e54d693164f31f11f95fbed00f9fa5a530a3f5
nikitosing <nikitosing4@mail.ru>
2022-04-28 17:04:39 +0300

    Integrate ace editor to display contract sources

--- apps/block_scout_web/assets/js/view_specific/address_contract/code_highlighting.js
+++ apps/block_scout_web/assets/js/view_specific/address_contract/code_highlighting.js
@@ -1,6 +1,25 @@
-import hljs from 'highlight.js/lib/core'
+import ace from '../../lib/ace/src-min/ace'
+import '../../lib/ace/src-min/mode-csharp'
+import '../../lib/ace/src-min/theme-chrome'
+import $ from 'jquery'
 
-// only activate highlighting on pages with this selector
-if (document.querySelectorAll('[data-activate-highlight]').length > 0) {
-  hljs.highlightAll()
+const Mode = ace.require('ace/mode/csharp').Mode
+
+const codeMain = $('#code_viewer_main')
+const code = codeMain.text()
+const editor = ace.edit('code_viewer_main')
+editor.session.setMode(new Mode())
+editor.setTheme('ace/theme/chrome')
+editor.setValue(code, -1)
+editor.setOptions({ maxLines: 40, readOnly: true })
+
+const len = codeMain.data('additional-sources-length')
+for (let i = 0; i < len; i++) {
+  const tag = 'code_viewer_' + i
+  const code = $('#' + tag).text()
+  const editor = ace.edit(tag)
+  editor.session.setMode(new Mode())
+  editor.setTheme('ace/theme/chrome')
+  editor.setValue(code, -1)
+  editor.setOptions({ maxLines: 40, readOnly: true })
 }
