dd4146ef761bc864d77f24f2b13343f3a9e55c13
nikitosing <nikitosing4@mail.ru>
2022-05-03 13:48:04 +0300

    Fix js import

--- apps/block_scout_web/assets/js/view_specific/address_contract/code_highlighting.js
+++ apps/block_scout_web/assets/js/view_specific/address_contract/code_highlighting.js
@@ -1,12 +1,14 @@
-import ace from '../../lib/ace/src-min/ace'
+import '../../lib/ace/src-min/ace'
 import '../../lib/ace/src-min/mode-csharp'
 import '../../lib/ace/src-min/theme-chrome'
 import $ from 'jquery'
 
+/* eslint-disable-next-line */
 const Mode = ace.require('ace/mode/csharp').Mode
 
 const codeMain = $('#code_viewer_main')
 const code = codeMain.text()
+/* eslint-disable-next-line */
 const editor = ace.edit('code_viewer_main')
 editor.session.setMode(new Mode())
 editor.setTheme('ace/theme/chrome')
@@ -17,6 +19,7 @@ const len = codeMain.data('additional-sources-length')
 for (let i = 0; i < len; i++) {
   const tag = 'code_viewer_' + i
   const code = $('#' + tag).text()
+  /* eslint-disable-next-line */
   const editor = ace.edit(tag)
   editor.session.setMode(new Mode())
   editor.setTheme('ace/theme/chrome')
