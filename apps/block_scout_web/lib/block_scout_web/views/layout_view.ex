a34cf0c34ae034c709960762d9b795a630c5de21
Tien Nam Dao <tien.dao2@tiki.vn>
2022-05-06 10:08:39 +0700

    fix: update icon

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -37,12 +37,12 @@ defmodule BlockScoutWeb.LayoutView do
   alias BlockScoutWeb.SocialMedia
 
   def logo do
-    Keyword.get(application_config(), :logo) || "/images/astra-white-logo.svg"
+    Keyword.get(application_config(), :logo) || "/images/astra.svg"
   end
 
   def logo_footer do
     Keyword.get(application_config(), :logo_footer) || Keyword.get(application_config(), :logo) ||
-      "/images/astra-white-logo.svg"
+      "/images/astra.svg"
   end
 
   def logo_text do
