2e09cf12e6341b371fd62b4f212714c0d9bdfd76
Tien Nam Dao <tien.dao2@tiki.vn>
2022-05-09 21:14:19 +0700

    fix: remove col percentage in accounts page

--- docker/init.sh
+++ docker/init.sh
@@ -5,6 +5,7 @@ export ETHEREUM_JSONRPC_HTTP_URL=${HTTP_URL:-http://host.docker.internal:8545/}
 export ETHEREUM_JSONRPC_WS_URL=${WS_URL:-ws://host.docker.internal:8546/}
 export ETHEREUM_JSONRPC_TRACE_URL=${WS_URL:-ws://host.docker.internal:8545/}
 export NETWORK=Testnet
+export SHOW_ADDRESS_MARKETCAP_PERCENTAGE=false
 export HOST_SYSTEM=${HOST_SYSTEM:-$(uname -s)}
 echo $HOST_SYSTEM
 make start
