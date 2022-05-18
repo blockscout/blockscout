6e90d1f7edf2b76ee2c6b6dc8f5cd7e5e6160d8f
nikitosing <nikitosing4@mail.ru>
2022-05-18 23:34:50 +0300

    Add broadcasting of coin balance

--- apps/block_scout_web/assets/js/pages/address.js
+++ apps/block_scout_web/assets/js/pages/address.js
@@ -80,6 +80,15 @@ export function reducer (state = initialState, action) {
         fetchedCoinBalanceBlockNumber: action.msg.fetchedCoinBalanceBlockNumber
       })
     }
+    case 'RECEIVED_NEW_CURRENT_COIN_BALANCE': {
+      if (state.initialBlockNumber && action.msg.currentCoinBalanceBlockNumber < state.initialBlockNumber) return
+      return Object.assign({}, state, {
+        currentCoinBalance: action.msg.currentCoinBalanceHtml,
+        currentCoinBalanceBlockNumber: action.msg.currentCoinBalanceBlockNumberHtml,
+        initialBlockNumber: state.newBlockNumber,
+        newBlockNumber: action.msg.currentCoinBalanceBlockNumber
+      })
+    }
     default:
       return state
   }
@@ -187,6 +196,24 @@ const elements = {
         $('[data-test="address-tokens-panel-crc-total-worth-container"]').addClass('d-none')
       }
     }
+  },
+  '[data-selector="current-coin-balance"]': {
+    render ($el, state, oldState) {
+      if (!state.newBlockNumber || state.newBlockNumber > oldState.newBlockNumber) return
+      $el.empty().append(state.currentCoinBalance)
+      updateAllCalculatedUsdValues()
+    }
+  },
+  '[data-selector="last-balance-update"]': {
+    render ($el, state, oldState) {
+      if (!state.newBlockNumber || state.newBlockNumber > oldState.newBlockNumber) return
+      $el.empty().append(state.currentCoinBalanceBlockNumber)
+    }
+  },
+  '[data-last-balance-update]': {
+    load ($el) {
+      return { initialBlockNumber: numeral($el.data('last-balance-update')).value() }
+    }
   }
 }
 
@@ -240,6 +267,12 @@ if ($addressDetailsPage.length) {
   addressChannel.on('transfer', (msg) => {
     store.dispatch({
       type: 'RECEIVED_NEW_TOKEN_TRANSFER',
+      msg: humps.camelizeKeys(msg)
+    })
+  })
+  addressChannel.on('current_coin_balance', (msg) => {
+    store.dispatch({
+      type: 'RECEIVED_NEW_CURRENT_COIN_BALANCE',
       msg: humps.camelizeKeys(msg)
     })
   })
