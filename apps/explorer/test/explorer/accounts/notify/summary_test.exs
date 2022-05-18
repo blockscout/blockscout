ce5610079a4a6cef1ae41d5d008d37c70e37727a
Oleg Sovetnik <sovetnik@oblaka.biz>
2022-05-18 18:18:16 +0300

    Add summary tests

--- apps/explorer/test/explorer/accounts/notify/summary_test.exs
+++ apps/explorer/test/explorer/accounts/notify/summary_test.exs
@@ -6,7 +6,7 @@ defmodule Explorer.Accounts.Notify.SummaryTest do
 
   alias Explorer.Accounts.Notifier.Summary
   alias Explorer.Chain
-  alias Explorer.Chain.{Address, Transaction, Wei}
+  alias Explorer.Chain.{Address, Token, TokenTransfer, Transaction, Wei}
   alias Explorer.Repo
 
   describe "call" do
@@ -34,6 +34,210 @@ defmodule Explorer.Accounts.Notify.SummaryTest do
                  transaction_hash: tx_hash,
                  tx_fee: fee,
                  type: "COIN"
+               }
+             ]
+    end
+
+    test "ERC-20 Token transfer" do
+      tx =
+        %Transaction{
+          from_address: from_address,
+          to_address: to_address,
+          block_number: block_number,
+          hash: tx_hash
+        } = with_block(insert(:transaction))
+
+      transfer =
+        %TokenTransfer{
+          amount: amount,
+          block_number: block_number,
+          from_address: from_address,
+          to_address: to_address,
+          token: token
+        } =
+        :token_transfer
+        |> insert(transaction: tx)
+        |> Repo.preload([
+          :token
+        ])
+
+      {_, fee} = Chain.fee(tx, :gwei)
+
+      token_decimals = Decimal.to_integer(token.decimals)
+
+      decimals = Decimal.new(Integer.pow(10, token_decimals))
+
+      amount = Decimal.div(transfer.amount, decimals)
+
+      assert Summary.process(transfer) == [
+               %Summary{
+                 amount: amount,
+                 block_number: block_number,
+                 from_address_hash: from_address.hash,
+                 method: "transfer",
+                 name: "Infinite Token",
+                 subject: "ERC-20",
+                 to_address_hash: to_address.hash,
+                 transaction_hash: tx.hash,
+                 tx_fee: fee,
+                 type: "ERC-20"
+               }
+             ]
+    end
+
+    test "ERC-721 Token transfer" do
+      token = insert(:token, type: "ERC-721")
+
+      tx =
+        %Transaction{
+          from_address: from_address,
+          to_address: to_address,
+          block_number: block_number,
+          hash: tx_hash
+        } = with_block(insert(:transaction))
+
+      transfer =
+        %TokenTransfer{
+          amount: amount,
+          block_number: block_number,
+          from_address: from_address,
+          to_address: to_address
+        } =
+        :token_transfer
+        |> insert(
+          transaction: tx,
+          token_id: 42,
+          token_contract_address: token.contract_address
+        )
+        |> Repo.preload([
+          :token
+        ])
+
+      {_, fee} = Chain.fee(tx, :gwei)
+
+      token_decimals = Decimal.to_integer(token.decimals)
+
+      decimals = Decimal.new(Integer.pow(10, token_decimals))
+
+      amount = Decimal.div(transfer.amount, decimals)
+
+      assert Summary.process(transfer) == [
+               %Summary{
+                 amount: 0,
+                 block_number: block_number,
+                 from_address_hash: from_address.hash,
+                 method: "transfer",
+                 name: "Infinite Token",
+                 subject: "42",
+                 to_address_hash: to_address.hash,
+                 transaction_hash: tx.hash,
+                 tx_fee: fee,
+                 type: "ERC-721"
+               }
+             ]
+    end
+
+    test "ERC-1155 single Token transfer" do
+      token = insert(:token, type: "ERC-1155")
+
+      tx =
+        %Transaction{
+          from_address: from_address,
+          to_address: to_address,
+          block_number: block_number,
+          hash: tx_hash
+        } = with_block(insert(:transaction))
+
+      transfer =
+        %TokenTransfer{
+          amount: amount,
+          block_number: block_number,
+          from_address: from_address,
+          to_address: to_address
+        } =
+        :token_transfer
+        |> insert(
+          transaction: tx,
+          token_id: 42,
+          token_contract_address: token.contract_address
+        )
+        |> Repo.preload([
+          :token
+        ])
+
+      {_, fee} = Chain.fee(tx, :gwei)
+
+      token_decimals = Decimal.to_integer(token.decimals)
+
+      decimals = Decimal.new(Integer.pow(10, token_decimals))
+
+      amount = Decimal.div(transfer.amount, decimals)
+
+      assert Summary.process(transfer) == [
+               %Summary{
+                 amount: 0,
+                 block_number: block_number,
+                 from_address_hash: from_address.hash,
+                 method: "transfer",
+                 name: "Infinite Token",
+                 subject: "42",
+                 to_address_hash: to_address.hash,
+                 transaction_hash: tx.hash,
+                 tx_fee: fee,
+                 type: "ERC-1155"
+               }
+             ]
+    end
+
+    test "ERC-1155 multiple Token transfer" do
+      token = insert(:token, type: "ERC-1155")
+
+      tx =
+        %Transaction{
+          from_address: from_address,
+          to_address: to_address,
+          block_number: block_number,
+          hash: tx_hash
+        } = with_block(insert(:transaction))
+
+      transfer =
+        %TokenTransfer{
+          amount: amount,
+          block_number: block_number,
+          from_address: from_address,
+          to_address: to_address
+        } =
+        :token_transfer
+        |> insert(
+          transaction: tx,
+          token_id: nil,
+          token_ids: [23, 42],
+          token_contract_address: token.contract_address
+        )
+        |> Repo.preload([
+          :token
+        ])
+
+      {_, fee} = Chain.fee(tx, :gwei)
+
+      token_decimals = Decimal.to_integer(token.decimals)
+
+      decimals = Decimal.new(Integer.pow(10, token_decimals))
+
+      amount = Decimal.div(transfer.amount, decimals)
+
+      assert Summary.process(transfer) == [
+               %Summary{
+                 amount: 0,
+                 block_number: block_number,
+                 from_address_hash: from_address.hash,
+                 method: "transfer",
+                 name: "Infinite Token",
+                 subject: "23, 42",
+                 to_address_hash: to_address.hash,
+                 transaction_hash: tx.hash,
+                 tx_fee: fee,
+                 type: "ERC-1155"
                }
              ]
     end
