f99610710fa1cb7f116762db020bb2937d070a84
nikitosing <nikitosing4@mail.ru>
2022-05-19 12:51:25 +0300

    Fix address on twin verification; Change redirect routes for verified contracts

--- apps/block_scout_web/lib/block_scout_web/controllers/address_contract_verification_vyper_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/address_contract_verification_vyper_controller.ex
@@ -1,30 +1,41 @@
 defmodule BlockScoutWeb.AddressContractVerificationVyperController do
   use BlockScoutWeb, :controller
 
+  alias BlockScoutWeb.Controller 
+  alias Explorer.Chain
   alias Explorer.Chain.SmartContract
   alias Explorer.SmartContract.{CompilerVersion, Vyper.PublisherWorker}
 
   def new(conn, %{"address_id" => address_hash_string}) do
-    changeset =
-      SmartContract.changeset(
-        %SmartContract{address_hash: address_hash_string},
-        %{}
+    if Chain.smart_contract_fully_verified?(address_hash_string) do
+      address_contract_path =
+        conn
+        |> address_contract_path(:index, address_hash_string)
+        |> Controller.full_path()
+
+      redirect(conn, to: address_contract_path)
+    else
+      changeset =
+        SmartContract.changeset(
+          %SmartContract{address_hash: address_hash_string},
+          %{}
+        )
+
+      compiler_versions =
+        case CompilerVersion.fetch_versions(:vyper) do
+          {:ok, compiler_versions} ->
+            compiler_versions
+
+          {:error, _} ->
+            []
+        end
+
+      render(conn, "new.html",
+        changeset: changeset,
+        compiler_versions: compiler_versions,
+        address_hash: address_hash_string
       )
-
-    compiler_versions =
-      case CompilerVersion.fetch_versions(:vyper) do
-        {:ok, compiler_versions} ->
-          compiler_versions
-
-        {:error, _} ->
-          []
-      end
-
-    render(conn, "new.html",
-      changeset: changeset,
-      compiler_versions: compiler_versions,
-      address_hash: address_hash_string
-    )
+    end
   end
 
   def create(
