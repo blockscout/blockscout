dd465d5325a09807ca5931d5f37564fef1d2b986
Leszek Stachowski <leszkostachowski@gmail.com>
2022-05-30 12:20:44 +0200

    Verified contracts: tooltips (#649)

---
+++ apps/block_scout_web/assets/js/pages/verified_contracts.js
@@ -0,0 +1,20 @@
+import 'bootstrap'
+import $ from 'jquery'
+
+$(function () {
+  $('[data-page="verified_contracts"]').tooltip({
+    selector: '[data-toggle="tooltip-with-link"]',
+    html: true,
+    delay: {
+      show: 0,
+      hide: 3000 // to allow user to be able to click the link before tooltip disappears
+    }
+  })
+
+  $('[data-toggle="tooltip-with-link"]').on('show.bs.tooltip', () => {
+    $('[data-toggle="tooltip-with-link"]').each((_, element) => {
+      // becauase of the options.delay.hide we don't want to keep previous tooltips shown
+      $(element).tooltip('hide')
+    })
+  })
+})
