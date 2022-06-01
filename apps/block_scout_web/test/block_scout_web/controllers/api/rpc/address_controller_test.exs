66751887513ecbb5035855ded280cfa2eb317e8e
Qwerty5Uiop <alex000010@bk.ru>
2022-06-01 21:05:44 +0400

    Clear all data after test

--- apps/block_scout_web/test/block_scout_web/controllers/api/rpc/address_controller_test.exs
+++ apps/block_scout_web/test/block_scout_web/controllers/api/rpc/address_controller_test.exs
@@ -30,9 +30,22 @@ defmodule BlockScoutWeb.API.RPC.AddressControllerTest do
 
     on_exit(fn ->
       Application.put_env(:explorer, AverageBlockTime, enabled: false)
+      Explorer.Repo.delete_all(Explorer.Chain.InternalTransaction)
+      Explorer.Repo.delete_all(Explorer.Chain.Address.CurrentTokenBalance)
+      Explorer.Repo.delete_all(Explorer.Chain.Block.Reward)
       Explorer.Repo.delete_all(Explorer.Chain.Block.SecondDegreeRelation)
+      Explorer.Repo.delete_all(Explorer.Chain.Address.TokenBalance)
+      Explorer.Repo.delete_all(Explorer.Chain.TokenTransfer)
+      Explorer.Repo.delete_all(Explorer.Chain.Log)
+      Explorer.Repo.delete_all(Explorer.Chain.Transaction.Fork)
+      Explorer.Repo.delete_all(Explorer.Chain.Token)
       Explorer.Repo.delete_all(Explorer.Chain.Transaction)
+      Explorer.Repo.delete_all(Explorer.Chain.Address.CoinBalanceDaily)
+      Explorer.Repo.delete_all(Explorer.Chain.StakingPoolsDelegator)
+      Explorer.Repo.delete_all(Explorer.Chain.StakingPool)
       Explorer.Repo.delete_all(Explorer.Chain.Block)
+      Explorer.Repo.delete_all(Explorer.Chain.Address.CoinBalance)
+      Explorer.Repo.delete_all(Explorer.Chain.Address)
     end)
 
     :ok
