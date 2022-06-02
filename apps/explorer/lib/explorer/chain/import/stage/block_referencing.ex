df60752ce2d9e105da6ecdfdf9607dc96fbd8671
Qwerty5Uiop <alex000010@bk.ru>
2022-06-02 21:32:00 +0400

    Fix credo

--- apps/explorer/lib/explorer/chain/import/stage/block_referencing.ex
+++ apps/explorer/lib/explorer/chain/import/stage/block_referencing.ex
@@ -20,11 +20,12 @@ defmodule Explorer.Chain.Import.Stage.BlockReferencing do
 
   @impl Stage
   def multis(runner_to_changes_list, options) do
-    {transactions_multis, runner_to_changes_list} =
+    {transactions_multis, runner_to_changes_list_without_trans} =
       Stage.chunk_every(runner_to_changes_list, Runner.Transactions, @transactions_chunk_size, options)
 
-    {tokens_multis, runner_to_changes_list} = Stage.split_multis([Runner.Tokens], runner_to_changes_list, options)
+    {tokens_multis, result_runner_to_changes_list} =
+      Stage.split_multis([Runner.Tokens], runner_to_changes_list_without_trans, options)
 
-    {transactions_multis ++ tokens_multis, runner_to_changes_list}
+    {transactions_multis ++ tokens_multis, result_runner_to_changes_list}
   end
 end
