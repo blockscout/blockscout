df6f5b3dd7af3771d49f2304423f1be73adad839
Tien Nam Dao <tien.dao2@tiki.vn>
2022-06-15 17:02:43 +0700

    remove cred

--- docker/init.sh
+++ docker/init.sh
@@ -6,6 +6,7 @@ export ETHEREUM_JSONRPC_HTTP_URL=${HTTP_URL:-http://localhost:8545/}
 export ETHEREUM_JSONRPC_WS_URL=${WS_URL:-ws://localhost:8546/}
 export ETHEREUM_JSONRPC_TRACE_URL=${HTTP_URL:-http://localhost:8545/}
 export NETWORK=Testnet
+export SECRET_KEY_BASE=
 export SHOW_ADDRESS_MARKETCAP_PERCENTAGE=false
 export HOST_SYSTEM=${HOST_SYSTEM:-$(uname -s)}
 echo $HOST_SYSTEM
