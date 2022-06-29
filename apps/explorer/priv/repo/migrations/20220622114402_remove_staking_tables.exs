5274128ef4c262db484ca0f746923daae4df4b63
Viktor Baranov <baranov.viktor.27@gmail.com>
2022-06-29 14:50:20 +0300

    Change order of tables deletion in removal of Staking dapp functionality: staking_pools_delegators first, then staking_pools

--- apps/explorer/priv/repo/migrations/20220622114402_remove_staking_tables.exs
+++ apps/explorer/priv/repo/migrations/20220622114402_remove_staking_tables.exs
@@ -2,7 +2,7 @@ defmodule Explorer.Repo.Migrations.RemoveStakingTables do
   use Ecto.Migration
 
   def change do
-    drop_if_exists(table(:staking_pools))
     drop_if_exists(table(:staking_pools_delegators))
+    drop_if_exists(table(:staking_pools))
   end
 end
