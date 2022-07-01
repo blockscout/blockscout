dbff3dbc8d0cfebad37dc21eb9737c3e2061da86
Qwerty5Uiop <alex000010@bk.ru>
2022-07-01 12:16:17 +0400

    Fix migrator pool_size

--- apps/explorer/lib/release_tasks.ex
+++ apps/explorer/lib/release_tasks.ex
@@ -56,7 +56,7 @@ defmodule Explorer.ReleaseTasks do
     IO.puts("Starting repos..")
 
     # Switch pool_size to 2 for ecto > 3.0
-    Enum.each(@repos, & &1.start_link(pool_size: 1))
+    Enum.each(@repos, & &1.start_link(pool_size: 2))
   end
 
   defp stop_services do
