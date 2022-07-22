14f23cd8d46a4912cbf85ccfa04b6d85e5c15a65
Qwerty5Uiop <alex000010@bk.ru>
2022-07-22 15:07:50 +0400

    Local app runtime configs

---
+++ apps/indexer/config/runtime/test.exs
@@ -0,0 +1,13 @@
+import Config
+
+variant =
+  if is_nil(System.get_env("ETHEREUM_JSONRPC_VARIANT")) do
+    "parity"
+  else
+    System.get_env("ETHEREUM_JSONRPC_VARIANT")
+    |> String.split(".")
+    |> List.last()
+    |> String.downcase()
+  end
+
+Code.require_file("#{variant}.exs", "apps/indexer/config/test")
