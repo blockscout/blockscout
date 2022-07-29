a07f784be0843f2c5a5b19f3e57ff82ffd4db8aa
Никита Поздняков <nikitosing4@mail.ru>
2022-07-30 01:04:02 +0700

    Add Guardian DB

--- apps/block_scout_web/config/config.exs
+++ apps/block_scout_web/config/config.exs
@@ -106,6 +106,11 @@ config :block_scout_web, BlockScoutWeb.Guardian,
   issuer: "block_scout_web",
   secret_key: System.get_env("SECRET_KEY_GUARDIAN")
 
+config :guardian, Guardian.DB,
+  repo: Explorer.Repo,
+  schema_name: "guardian_tokens",
+  sweep_interval: 60
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
--- apps/block_scout_web/lib/block_scout_web/application.ex
+++ apps/block_scout_web/lib/block_scout_web/application.ex
@@ -22,7 +22,8 @@ defmodule BlockScoutWeb.Application do
       child_spec(Endpoint, []),
       {Absinthe.Subscription, Endpoint},
       {RealtimeEventHandler, name: RealtimeEventHandler},
-      {BlocksIndexedCounter, name: BlocksIndexedCounter}
+      {BlocksIndexedCounter, name: BlocksIndexedCounter},
+      {Guardian.DB.Token.SweeperServer, []}
     ]
 
     opts = [strategy: :one_for_one, name: BlockScoutWeb.Supervisor, max_restarts: 1_000]
