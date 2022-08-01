b5d8b0086e4d8a0976eb0677ff076e78e8fb2682
Qwerty5Uiop <alex000010@bk.ru>
2022-08-01 16:43:58 +0400

    Drop admin_panel_enabled env from runtime config

--- config/runtime.exs
+++ config/runtime.exs
@@ -72,8 +72,7 @@ config :block_scout_web,
   max_size_to_show_array_as_is: Integer.parse(System.get_env("MAX_SIZE_UNLESS_HIDE_ARRAY", "50")),
   max_length_to_show_string_without_trimming: System.get_env("MAX_STRING_LENGTH_WITHOUT_TRIMMING", "2040"),
   re_captcha_secret_key: System.get_env("RE_CAPTCHA_SECRET_KEY", nil),
-  re_captcha_client_key: System.get_env("RE_CAPTCHA_CLIENT_KEY", nil),
-  admin_panel_enabled: System.get_env("ADMIN_PANEL_ENABLED", "") == "true"
+  re_captcha_client_key: System.get_env("RE_CAPTCHA_CLIENT_KEY", nil)
 
 default_api_rate_limit = 50
 default_api_rate_limit_str = Integer.to_string(default_api_rate_limit)
