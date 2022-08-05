1664dff477e069eb1973a52adb13a47ae4b7eff4
Viktor Baranov <baranov.viktor.27@gmail.com>
2022-08-05 13:41:50 +0300

    Fix merging conflicts

--- apps/explorer/config/config.exs
+++ apps/explorer/config/config.exs
@@ -8,11 +8,7 @@ import Config
 # General application configuration
 config :explorer,
   ecto_repos: [Explorer.Repo],
-<<<<<<< HEAD
   token_functions_reader_max_retries: 1
-=======
-  token_functions_reader_max_retries: 3
->>>>>>> origin/master
 
 config :explorer, Explorer.Counters.AverageBlockTime,
   enabled: true,
