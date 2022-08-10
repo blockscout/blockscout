376963b0be2942d88b4e37cb2dc5eabdff751666
Victor Baranov <baranov.viktor.27@gmail.com>
2022-08-10 13:10:19 +0300

    Merge pull request #5882 from blockscout/dependabot/hex/math-0.7.0
    Bump math from 0.3.1 to 0.7.0

--- apps/block_scout_web/mix.exs
+++ apps/block_scout_web/mix.exs
@@ -91,7 +91,7 @@ defmodule BlockScoutWeb.Mixfile do
       {:junit_formatter, ">= 0.0.0", only: [:test], runtime: false},
       # Log errors and application output to separate files
       {:logger_file_backend, "~> 0.0.10"},
-      {:math, "~> 0.3.0"},
+      {:math, "~> 0.7.0"},
       {:mock, "~> 0.3.0", only: [:test], runtime: false},
       {:number, "~> 1.0.1"},
       {:phoenix, "== 1.5.13"},
