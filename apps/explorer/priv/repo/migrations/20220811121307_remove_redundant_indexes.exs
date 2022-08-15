186178ede987c3ac5d4fd553a027170927ea2aea
Никита Поздняков <nikitosing4@mail.ru>
2022-08-15 16:36:52 +0300

    Remove migration which will be performed manually

--- apps/explorer/priv/repo/migrations/20220811121307_remove_redundant_indexes.exs
+++
@@ -1,8 +0,0 @@
-defmodule Explorer.Repo.Migrations.RemoveRedundantIndexes do
-  use Ecto.Migration
-
-  def change do
-    drop_if_exists(unique_index(:account_custom_abis, [:id]))
-    drop_if_exists(unique_index(:account_api_keys, [:value]))
-  end
-end
