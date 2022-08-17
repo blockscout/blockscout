5f9f57c0ea0224f473422dc4c8b5a9d3989024b7
Tien Nam Dao <tien.dao2@tiki.vn>
2022-08-17 18:43:05 +0700

    fix: update ui + init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 export BLOCKSCOUT_VERSION=$(date '+%Y-%m-%d')
-export COIN=ASA
+export COIN=Astra
 export ETHEREUM_JSONRPC_VARIANT=geth
+export SECRET_KEY_BASE=
+export SHOW_PRICE_CHART=true
+export SHOW_TXS_CHART=true
+export ENABLE_TXS_STATS=true
 export ETHEREUM_JSONRPC_HTTP_URL=${HTTP_URL:-http://localhost:8545/}
 export ETHEREUM_JSONRPC_WS_URL=${WS_URL:-ws://localhost:8546/}
 export ETHEREUM_JSONRPC_TRACE_URL=${HTTP_URL:-http://localhost:8545/}
-export NETWORK=Testnet
-export SECRET_KEY_BASE=
+export LOGO_TEXT=Astra
+export LINK_TO_OTHER_EXPLORERS=false
+export NETWORK=Devnet
 export SHOW_ADDRESS_MARKETCAP_PERCENTAGE=false
+export CHAIN_ID=11115
+
 export HOST_SYSTEM=${HOST_SYSTEM:-$(uname -s)}
-# echo $HOST_SYSTEM
+
+# cd .. && mix phx.server
 make build
 make start
