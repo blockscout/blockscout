4aefc15fa7c97bcfd355266848de58bf3dcf1fc1
Viktor Baranov <baranov.viktor.27@gmail.com>
2022-08-22 15:06:54 +0300

    Remove datepicker from webpack config

--- apps/block_scout_web/assets/webpack.config.js
+++ apps/block_scout_web/assets/webpack.config.js
@@ -63,7 +63,6 @@ const appJs =
       'async-listing-load': './js/lib/async_listing_load',
       'non-critical': './css/non-critical.scss',
       'export-csv': './css/export-csv.scss',
-      'datepicker': './js/lib/datepicker.js',
       'main-page': './css/main-page.scss',
       'tokens': './js/pages/token/search.js',
       'faucet': './js/pages/faucet.js',
