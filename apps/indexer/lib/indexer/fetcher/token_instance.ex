41a1b4cfa665607855c5f984dbc3c2134af3a6af
Donald Hutchison <donald.hutchison@clabs.co>
2022-08-25 16:40:07 +0200

    Fix alias of module.

--- apps/indexer/lib/indexer/fetcher/token_instance.ex
+++ apps/indexer/lib/indexer/fetcher/token_instance.ex
@@ -5,7 +5,6 @@ defmodule Indexer.Fetcher.TokenInstance do
 
   use Indexer.Fetcher
   use Spandex.Decorators
-  use BufferedTask
 
   require Logger
 
@@ -13,6 +12,7 @@ defmodule Indexer.Fetcher.TokenInstance do
   alias Explorer.Token.InstanceMetadataRetriever
   alias Indexer.BufferedTask
 
+  use BufferedTask
 
   @defaults [
     flush_interval: 300,
