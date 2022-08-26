ae7c407c9a7eb0ca970823bd07f2a47fa0a95387
Qwerty5Uiop <alex000010@bk.ru>
2022-08-26 13:48:13 +0400

    Remove acquire_contract_address_tokens step as well

--- apps/explorer/lib/explorer/chain/import/runner/blocks.ex
+++ apps/explorer/lib/explorer/chain/import/runner/blocks.ex
@@ -110,26 +110,10 @@ defmodule Explorer.Chain.Import.Runner.Blocks do
         transactions: transactions
       })
     end)
-    |> Multi.run(:acquire_contract_address_tokens, fn repo, _ ->
-      acquire_contract_address_tokens(repo, consensus_block_numbers)
-    end)
   end
 
   @impl Runner
   def timeout, do: @timeout
-
-  defp acquire_contract_address_tokens(repo, consensus_block_numbers) do
-    query =
-      from(ctb in Address.CurrentTokenBalance,
-        where: ctb.block_number in ^consensus_block_numbers,
-        select: {ctb.token_contract_address_hash, ctb.token_id},
-        distinct: [ctb.token_contract_address_hash, ctb.token_id]
-      )
-
-    contract_address_hashes_and_token_ids = repo.all(query)
-
-    Tokens.acquire_contract_address_tokens(repo, contract_address_hashes_and_token_ids)
-  end
 
   defp fork_transactions(%{
          repo: repo,
