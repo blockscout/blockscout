2cb673fcbecdd4479ac60345ec7fc30e542f5faa
Donald Hutchison <donald.hutchison@clabs.co>
2022-08-29 16:17:28 +0200

    Mix format.

--- apps/explorer/priv/repo/migrations/20220829132236_pg_hero_changes.exs
+++ apps/explorer/priv/repo/migrations/20220829132236_pg_hero_changes.exs
@@ -9,9 +9,17 @@ defmodule Explorer.Repo.Migrations.DropDuplicateIndicesPgHero do
     ["blocks", [:miner_hash], [name: "blocks_miner_hash_index", concurrently: true]],
     ["celo_contract_events", [:block_number], [name: "celo_contract_events_block_number_index", concurrently: true]],
     ["celo_signers", [:address], [name: "celo_signers_address_index", concurrently: true]],
-    ["clabs_contract_event_trackings", [:smart_contract_id], [name: "clabs_contract_event_trackings_smart_contract_id_index", concurrently: true]],
-    ["clabs_tracked_contract_events", [:block_number], [name: "clabs_tracked_contract_events_block_number_index", concurrently: true]],
-    ["logs", [:block_hash, :index], [name: "logs_block_hash_index_index", concurrently: true]],
+    [
+      "clabs_contract_event_trackings",
+      [:smart_contract_id],
+      [name: "clabs_contract_event_trackings_smart_contract_id_index", concurrently: true]
+    ],
+    [
+      "clabs_tracked_contract_events",
+      [:block_number],
+      [name: "clabs_tracked_contract_events_block_number_index", concurrently: true]
+    ],
+    ["logs", [:block_hash, :index], [name: "logs_block_hash_index_index", concurrently: true]]
   ]
 
   def change do
