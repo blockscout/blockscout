1c8e59071937f04d1796a0dd7f12f569867dbddf
Qwerty5Uiop <alex000010@bk.ru>
2022-08-30 20:32:48 +0400

    Fix ERC-1155 tokens fetching

--- apps/explorer/lib/explorer/chain/token_transfer.ex
+++ apps/explorer/lib/explorer/chain/token_transfer.ex
@@ -177,7 +177,7 @@ defmodule Explorer.Chain.TokenTransfer do
       from(
         tt in TokenTransfer,
         where: tt.token_contract_address_hash == ^token_address_hash,
-        where: tt.token_id == ^token_id,
+        where: tt.token_id == ^token_id or ^token_id in tt.token_ids,
         where: not is_nil(tt.block_number),
         preload: [{:transaction, :block}, :token, :from_address, :to_address],
         order_by: [desc: tt.block_number]
@@ -206,7 +206,9 @@ defmodule Explorer.Chain.TokenTransfer do
     query =
       from(
         tt in TokenTransfer,
-        where: tt.token_contract_address_hash == ^token_address_hash and tt.token_id == ^token_id,
+        where:
+          tt.token_contract_address_hash == ^token_address_hash and
+            (tt.token_id == ^token_id or ^token_id in tt.token_ids),
         select: fragment("COUNT(*)")
       )
 
