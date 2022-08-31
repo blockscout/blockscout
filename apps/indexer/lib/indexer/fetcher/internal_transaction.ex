feb84592dd5fe176c458ab28ea26a096de36530c
trung <trung.nguyen25@tiki.vn>
2022-08-31 13:22:03 +0700

    refactor: increase internal txs fetcher batch size

--- apps/indexer/lib/indexer/fetcher/internal_transaction.ex
+++ apps/indexer/lib/indexer/fetcher/internal_transaction.ex
@@ -21,7 +21,7 @@ defmodule Indexer.Fetcher.InternalTransaction do
 
   @behaviour BufferedTask
 
-  @max_batch_size 5
+  @max_batch_size 10
   @max_concurrency 4
   @defaults [
     flush_interval: :timer.seconds(3),
