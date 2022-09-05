55676c7fb6ad653c034bc56e22556b6d64e89e52
trung <trung.nguyen25@tiki.vn>
2022-09-05 10:26:17 +0700

    refactor: tuning fetchers configs

--- apps/indexer/lib/indexer/fetcher/internal_transaction.ex
+++ apps/indexer/lib/indexer/fetcher/internal_transaction.ex
@@ -22,7 +22,7 @@ defmodule Indexer.Fetcher.InternalTransaction do
   @behaviour BufferedTask
 
   @max_batch_size 10
-  @max_concurrency 4
+  @max_concurrency 2
   @defaults [
     flush_interval: :timer.seconds(3),
     max_concurrency: @max_concurrency,
