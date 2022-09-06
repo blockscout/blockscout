a8240b2a784bcc37cbb3a401854c99e7cb53a311
trung <trung.nguyen25@tiki.vn>
2022-09-06 15:10:10 +0700

    refactor: fail fast when error in internal txs rpc call occur

--- apps/indexer/lib/indexer/fetcher/internal_transaction.ex
+++ apps/indexer/lib/indexer/fetcher/internal_transaction.ex
@@ -137,7 +137,7 @@ defmodule Indexer.Fetcher.InternalTransaction do
         )
 
         # re-queue the de-duped entries
-        {:retry, filtered_unique_numbers}
+        # {:retry, filtered_unique_numbers}
 
       :ignore ->
         :ok
