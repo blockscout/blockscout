46f2725c5b43c34c4a666fe3a4ae9671523299ab
Leszek Stachowski <leszek.stachowski@clabs.co>
2022-09-22 16:36:50 +0200

    Epoch rewards: better rounding (#752)

--- apps/block_scout_web/lib/block_scout_web/views/epoch_transaction_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/epoch_transaction_view.ex
@@ -11,5 +11,20 @@ defmodule BlockScoutWeb.EpochTransactionView do
     end
   end
 
-  def wei_to_ether_rounded(amount), do: amount |> Wei.to(:ether) |> then(&Decimal.round(&1, 2))
+  def wei_to_ether_rounded(%Wei{value: value} = amount) do
+    amount
+    |> Wei.to(:ether)
+    |> then(
+      &Decimal.round(
+        &1,
+        cond do
+          Decimal.cmp(value, Decimal.new(10_000_000_000_000)) == :lt -> 2
+          Decimal.cmp(value, Decimal.new(100_000_000_000_000)) == :lt -> 5
+          Decimal.cmp(value, Decimal.new(1_000_000_000_000_000)) == :lt -> 4
+          Decimal.cmp(value, Decimal.new(10_000_000_000_000_000)) == :lt -> 3
+          true -> 2
+        end
+      )
+    )
+  end
 end
