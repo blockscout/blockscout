cf41a579b7c01ee59dc560579d43f3eabbe9ef01
Никита Поздняков <nikitosing4@mail.ru>
2022-10-03 21:13:04 +0300

    Add search API endpoint

--- apps/block_scout_web/lib/block_scout_web/views/search_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/search_view.ex
@@ -16,4 +16,36 @@ defmodule BlockScoutWeb.SearchView do
     |> Regex.replace(safe_result, "<mark class=\'autoComplete_highlight\'>\\g{0}</mark>", global: true)
     |> raw()
   end
+
+  def render("search_results.json", %{search_results: search_results, next_page_params: next_page_params}) do
+    %{"items" => Enum.map(search_results, &prepare_search_result/1), "next_page_params" => next_page_params}
+  end
+
+  def prepare_search_result(%{type: "token"} = search_result) do
+    %{
+      "type" => search_result.type,
+      "name" => search_result.name,
+      "symbol" => search_result.symbol,
+      "address" => search_result.address_hash
+    }
+  end
+
+  def prepare_search_result(%{type: address_or_contract} = search_result)
+      when address_or_contract in ["address", "contract"] do
+    %{"type" => search_result.type, "name" => search_result.name, "address" => search_result.address_hash}
+  end
+
+  def prepare_search_result(%{type: "block"} = search_result) do
+    %{
+      "type" => search_result.type,
+      "block_number" => search_result.block_number,
+      "block_hash" => hash_to_string(search_result.block_hash)
+    }
+  end
+
+  def prepare_search_result(%{type: "transaction"} = search_result) do
+    %{"type" => search_result.type, "tx_hash" => hash_to_string(search_result.tx_hash)}
+  end
+
+  defp hash_to_string(hash), do: "0x" <> Base.encode16(hash, case: :lower)
 end
