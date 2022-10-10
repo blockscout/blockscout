19b9090957d626d09a1c83113387d5eea53ee0ab
perekopskiy <perekopskiy@dlit.dp.ua>
2022-10-10 18:18:58 +0300

    Add ignored field

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/log.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/log.ex
@@ -183,6 +183,11 @@ defmodule EthereumJSONRPC.Log do
     end
   end
 
+  # ZkSync fields
+  defp entry_to_elixir({key, _}) when key in ~w(l1BlockNumber) do
+    :ignore
+  end
+
   defp put_topics(params, topics) when is_map(params) and is_list(topics) do
     params
     |> Map.put(:first_topic, Enum.at(topics, 0))
