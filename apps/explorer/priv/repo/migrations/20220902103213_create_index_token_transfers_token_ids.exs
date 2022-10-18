38a2c9046986da21a5f6af1feefdcdb047352c47
Leszek Stachowski <leszek.stachowski@clabs.co>
2022-10-18 12:42:54 +0200

    Change token_transfers_token_ids_index creation to be concurrent

--- apps/explorer/priv/repo/migrations/20220902103213_create_index_token_transfers_token_ids.exs
+++ apps/explorer/priv/repo/migrations/20220902103213_create_index_token_transfers_token_ids.exs
@@ -1,9 +1,12 @@
 defmodule Explorer.Repo.Migrations.CreateIndexTokenTransfersTokenIds do
   use Ecto.Migration
 
+  @disable_ddl_transaction true
+  @disable_migration_lock true
+
   def up do
     execute("""
-    CREATE INDEX token_transfers_token_ids_index on token_transfers USING GIN ("token_ids")
+    CREATE INDEX CONCURRENTLY token_transfers_token_ids_index on token_transfers USING GIN ("token_ids")
     """)
   end
 
