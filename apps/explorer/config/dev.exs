f1208b911f30b79b589c46be3a531daddebf5b88
Viktor Baranov <baranov.viktor.27@gmail.com>
2022-11-01 12:43:11 +0300

    Fix for concurrent index creation

--- apps/explorer/config/dev.exs
+++ apps/explorer/config/dev.exs
@@ -1,7 +1,9 @@
 import Config
 
 # Configure your database
-config :explorer, Explorer.Repo, timeout: :timer.seconds(80)
+config :explorer, Explorer.Repo,
+  timeout: :timer.seconds(80),
+  migration_lock: nil
 
 # Configure API database
 config :explorer, Explorer.Repo.Replica1, timeout: :timer.seconds(80)
