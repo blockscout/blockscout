cb1799e16049497a4c21ca0dc1646733c1d034d2
POA <33550681+poa@users.noreply.github.com>
2022-11-04 12:59:22 +0300

    Adding transaction_actions table through migrations

---
+++ apps/explorer/priv/repo/migrations/20221104091552_add_transaction_actions_table.exs
@@ -0,0 +1,25 @@
+defmodule Explorer.Repo.Migrations.AddTransactionActionsTable do
+  use Ecto.Migration
+
+  def change do
+    execute(
+      "CREATE TYPE transaction_actions_protocol AS ENUM ('uniswap_v3', 'opensea_v1.1', 'wrapping', 'approval', 'zkbob')",
+      "DROP TYPE transaction_actions_protocol"
+    )
+
+    execute(
+      "CREATE TYPE transaction_actions_type AS ENUM ('mint_nft', 'mint', 'burn', 'collect', 'swap', 'sale', 'cancel', 'transfer', 'wrap', 'unwrap', 'approve', 'revoke', 'withdraw', 'deposit')",
+      "DROP TYPE transaction_actions_type"
+    )
+
+    create table(:transaction_actions, primary_key: false) do
+      add(:hash, references(:transactions, column: :hash, on_delete: :delete_all, on_update: :update_all, type: :bytea), null: false)
+      add(:protocol, :transaction_actions_protocol, null: false)
+      add(:data, :jsonb, null: true)
+      add(:type, :transaction_actions_type, null: false)
+    end
+
+    create(index(:transaction_actions, :hash))
+    create(index(:transaction_actions, [:protocol, :type]))
+  end
+end
