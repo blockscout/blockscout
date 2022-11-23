f751c695534b29400b3c347a672c6f8b3138df80
guoshijiang <guoshijiang2012@163.com>
2022-11-23 18:31:00 +0800

    fix express-status-monitor

--- data-sync-service/src/main.ts
+++ data-sync-service/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  app.use(require('express-status-monitor')());
+  // app.use(require('express-status-monitor')());
   await app.listen(3000);
 }
 bootstrap();
