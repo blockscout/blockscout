fa65f01c62117afa93f0c8bfdd9705cd03b8b7ab
Никита Поздняков <nikitosing4@mail.ru>
2022-12-03 13:37:11 +0300

    Fix env usage

--- apps/explorer/lib/explorer/chain/transaction.ex
+++ apps/explorer/lib/explorer/chain/transaction.ex
@@ -474,7 +474,7 @@ defmodule Explorer.Chain.Transaction do
   def decoded_input_data(%NotLoaded{}, _), do: {:error, :not_loaded}
   def decoded_input_data(%__MODULE__{input: %{bytes: bytes}}, _) when bytes in [nil, <<>>], do: {:error, :no_input_data}
 
-  if Application.get_env(:explorer, :decode_not_a_contract_calls) do
+  if not Application.compile_env(:explorer, :decode_not_a_contract_calls) do
     def decoded_input_data(%__MODULE__{to_address: %{contract_code: nil}}, _), do: {:error, :not_a_contract_call}
   end
 
