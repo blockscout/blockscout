08ffc5464b70e584da620426dc3fa9ac40f94d5f
Valentin Rodygin <valentin@clabs.co>
2022-12-16 17:22:22 +0100

    Removed conditional path

--- apps/block_scout_web/lib/block_scout_web/router.ex
+++ apps/block_scout_web/lib/block_scout_web/router.ex
@@ -62,22 +62,10 @@ defmodule BlockScoutWeb.Router do
     get("/eth-rpc-api-docs", APIDocsController, :eth_rpc)
   end
 
-  url_params = Application.compile_env(:block_scout_web, BlockScoutWeb.Endpoint)[:url]
-  api_path = url_params[:api_path]
-  path = url_params[:path]
+  scope "/verify_smart_contract" do
+    pipe_through(:api)
 
-  if path != api_path do
-    scope to_string(api_path) <> "/verify_smart_contract" do
-      pipe_through(:api)
-
-      post("/contract-verifications", BlockScoutWeb.AddressContractVerificationController, :create)
-    end
-  else
-    scope "/verify_smart_contract" do
-      pipe_through(:api)
-
-      post("/contract-verifications", BlockScoutWeb.AddressContractVerificationController, :create)
-    end
+    post("/contract-verifications", BlockScoutWeb.AddressContractVerificationController, :create)
   end
 
   if Application.compile_env(:block_scout_web, WebRouter)[:enabled] do
