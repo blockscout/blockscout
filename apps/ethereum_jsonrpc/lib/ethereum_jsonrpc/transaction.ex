28ab338b0d206c9230d644e863a71cab0c2ea3b2
POA <33550681+poa@users.noreply.github.com>
2023-01-11 17:52:18 +0300

    Remove l1TxOrigin handling for another version of RPC

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/transaction.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/transaction.ex
@@ -305,6 +305,51 @@ defmodule EthereumJSONRPC.Transaction do
     end
   end
 
+  def elixir_to_params(
+        %{
+          "blockHash" => block_hash,
+          "blockNumber" => block_number,
+          "from" => from_address_hash,
+          "gas" => gas,
+          "gasPrice" => gas_price,
+          "hash" => hash,
+          "input" => input,
+          "nonce" => nonce,
+          "r" => r,
+          "s" => s,
+          "to" => to_address_hash,
+          "transactionIndex" => index,
+          "v" => v,
+          "value" => value,
+          "l1BlockNumber" => l1_block_number
+        } = transaction
+      ) do
+    result = %{
+      block_hash: block_hash,
+      block_number: block_number,
+      from_address_hash: from_address_hash,
+      gas: gas,
+      gas_price: gas_price,
+      hash: hash,
+      index: index,
+      input: input,
+      nonce: nonce,
+      r: r,
+      s: s,
+      to_address_hash: to_address_hash,
+      v: v,
+      value: value,
+      transaction_index: index,
+      l1_block_number: l1_block_number
+    }
+
+    if transaction["creates"] do
+      Map.put(result, :created_contract_address_hash, transaction["creates"])
+    else
+      result
+    end
+  end
+
   # eth_getTransactionReceipt and eth_getTransactionByHash don't return
   # L1 fields by Optimism BedRock node
   def elixir_to_params(
