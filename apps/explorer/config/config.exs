4a3337e84f0671f33a2d8e82e07b16203fe0f929
Viktor Baranov <baranov.viktor.27@gmail.com>
2023-01-26 14:53:28 +0300

    integrate sig provider

--- apps/explorer/config/config.exs
+++ apps/explorer/config/config.exs
@@ -10,7 +10,7 @@ config :explorer,
   ecto_repos: [Explorer.Repo, Explorer.Repo.Account],
   token_functions_reader_max_retries: 3,
   # for not fully indexed blockchains
-  decode_not_a_contract_calls: System.get_env("DECODE_NOT_A_CONTRACT_CALLS") == "true"
+  decode_not_a_contract_calls: System.get_env("DECODE_NOT_A_CONTRACT_CALLS") != "true"
 
 config :explorer, Explorer.Counters.AverageBlockTime,
   enabled: true,
