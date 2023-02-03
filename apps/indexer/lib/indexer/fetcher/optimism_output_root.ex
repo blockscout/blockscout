4c31489bb59401a5e174fbcf5e2d1c73de889a89
POA <33550681+poa@users.noreply.github.com>
2023-02-03 08:02:03 +0300

    Add reorgs handling

--- apps/indexer/lib/indexer/fetcher/optimism_output_root.ex
+++ apps/indexer/lib/indexer/fetcher/optimism_output_root.ex
@@ -9,10 +9,14 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
   require Logger
 
   import Ecto.Query
-  import EthereumJSONRPC, only: [request: 1, json_rpc: 2, fetch_block_number_by_tag: 2, integer_to_quantity: 1, quantity_to_integer: 1]
 
+  import EthereumJSONRPC,
+    only: [request: 1, json_rpc: 2, fetch_block_number_by_tag: 2, integer_to_quantity: 1, quantity_to_integer: 1]
+
+  alias EthereumJSONRPC.Block.ByNumber
   alias Explorer.Chain.OptimismOutputRoot
   alias Explorer.Repo
+  alias Indexer.BoundQueue
 
   @avg_block_time_range_size 100
   @eth_get_logs_range_size 1000
@@ -63,11 +67,28 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
       first_block = max(last_safe_block - @avg_block_time_range_size, 1)
       first_block_timestamp = get_block_timestamp_by_number(first_block, json_rpc_named_arguments)
       last_safe_block_timestamp = get_block_timestamp_by_number(last_safe_block, json_rpc_named_arguments)
-      avg_block_time = ceil((last_safe_block_timestamp - first_block_timestamp) / (last_safe_block - first_block) * 1000)
+
+      avg_block_time =
+        ceil((last_safe_block_timestamp - first_block_timestamp) / (last_safe_block - first_block) * 1000)
 
       start_block = max(start_block_l1, last_l1_block_number)
 
-      {:ok, %{output_oracle: env[:output_oracle], avg_block_time: avg_block_time, start_block: start_block, end_block: last_safe_block}, {:continue, json_rpc_named_arguments}}
+      reorg_monitor_task =
+        Task.Supervisor.async_nolink(Indexer.Fetcher.OptimismOutputRoot.TaskSupervisor, fn ->
+          reorg_monitor(avg_block_time, json_rpc_named_arguments)
+        end)
+
+      # todo: restart process when abnormal exit
+
+      {:ok,
+       %{
+         output_oracle: env[:output_oracle],
+         avg_block_time: avg_block_time,
+         start_block: start_block,
+         end_block: last_safe_block,
+         reorg_monitor_task: reorg_monitor_task,
+         json_rpc_named_arguments: json_rpc_named_arguments
+       }, {:continue, nil}}
     else
       {:start_block_l1_undefined, true} ->
         # the process shoudln't start if the start block is not defined
@@ -86,7 +107,10 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
         :ignore
 
       {:error, error_data} ->
-        Logger.error("Cannot get last L1 transaction from RPC by its hash or last safe block due to RPC error: #{inspect(error_data)}")
+        Logger.error(
+          "Cannot get last L1 transaction from RPC by its hash or last safe block due to RPC error: #{inspect(error_data)}"
+        )
+
         :ignore
 
       {:l1_tx_not_found, true} ->
@@ -103,11 +127,21 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
   end
 
   @impl GenServer
-  def handle_continue(json_rpc_named_arguments, %{output_oracle: output_oracle, avg_block_time: avg_block_time, start_block: start_block, end_block: end_block} = state) do
+  def handle_continue(
+        _,
+        %{
+          output_oracle: output_oracle,
+          avg_block_time: avg_block_time,
+          start_block: start_block,
+          end_block: end_block,
+          json_rpc_named_arguments: json_rpc_named_arguments
+        } = state
+      ) do
     time_before = Timex.now()
 
     chunks_number = ceil((end_block - start_block + 1) / @eth_get_logs_range_size)
     chunk_range = Range.new(0, max(chunks_number - 1, 0), 1)
+
     last_written_block =
       chunk_range
       |> Enum.reduce_while(start_block - 1, fn current_chank, _ ->
@@ -115,14 +149,17 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
         chunk_end = min(chunk_start + @eth_get_logs_range_size - 1, end_block)
 
         if chunk_end >= chunk_start do
-          {:ok, results} = get_logs(chunk_start, chunk_end, output_oracle, @output_proposed_event, json_rpc_named_arguments)
+          {:ok, results} =
+            get_logs(chunk_start, chunk_end, output_oracle, @output_proposed_event, json_rpc_named_arguments)
+
           # todo: write to db...
         end
 
+        reorg_block = reorg_block_pop()
+
         if !is_nil(reorg_block) && reorg_block > 0 do
           Repo.delete_all(from(r in OptimismOutputRoot, where: r.l1_block_number >= ^reorg_block))
-          # todo: reset reorg block in memory...
-          {:halt, (if reorg_block <= chunk_end, do: reorg_block - 1, else: chunk_end)}
+          {:halt, if(reorg_block <= chunk_end, do: reorg_block - 1, else: chunk_end)}
         else
           {:cont, chunk_end}
         end
@@ -136,7 +173,91 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
       :timer.sleep(max(avg_block_time - Timex.diff(Timex.now(), time_before, :milliseconds), 0))
     end
 
-    {:noreply, %{state | start_block: new_start_block, end_block: new_end_block}, {:continue, json_rpc_named_arguments}}
+    {:noreply, %{state | start_block: new_start_block, end_block: new_end_block}, {:continue, nil}}
+  end
+
+  @impl GenServer
+  def handle_info({ref, _result}, %{reorg_monitor_task: %Task{ref: ref}} = state) do
+    Process.demonitor(ref, [:flush])
+    {:noreply, %{state | reorg_monitor_task: nil}}
+  end
+
+  def handle_info(
+        {:DOWN, ref, :process, pid, reason},
+        %{
+          reorg_monitor_task: %Task{pid: pid, ref: ref},
+          avg_block_time: avg_block_time,
+          json_rpc_named_arguments: json_rpc_named_arguments
+        } = state
+      ) do
+    if reason === :normal do
+      {:noreply, %{state | reorg_monitor_task: nil}}
+    else
+      Logger.metadata(fetcher: :optimism_output_root)
+      Logger.error(fn -> "Reorgs monitor task exited due to #{inspect(reason)}. Rerunning..." end)
+
+      task =
+        Task.Supervisor.async_nolink(Indexer.Fetcher.OptimismOutputRoot.TaskSupervisor, fn ->
+          reorg_monitor(avg_block_time, json_rpc_named_arguments)
+        end)
+
+      {:noreply, %{state | reorg_monitor_task: task}}
+    end
+  end
+
+  defp reorg_monitor(avg_block_time, json_rpc_named_arguments) do
+    Logger.metadata(fetcher: :optimism_output_root)
+
+    # infinite loop
+    # credo:disable-for-next-line
+    Enum.reduce_while(Stream.iterate(0, &(&1 + 1)), 0, fn _i, prev_latest ->
+      {:ok, latest} = fetch_block_number_by_tag("latest", json_rpc_named_arguments)
+
+      if latest < prev_latest do
+        reorg_block_push(latest)
+      end
+
+      :timer.sleep(avg_block_time)
+
+      {:cont, latest}
+    end)
+
+    :ok
+  end
+
+  defp reorg_block_pop do
+    case BoundQueue.pop_front(reorg_queue_get()) do
+      {:ok, {block_number, updated_queue}} ->
+        :ets.insert(:op_output_roots_reorgs, {:queue, updated_queue})
+        block_number
+
+      {:error, :empty} ->
+        nil
+    end
+  end
+
+  defp reorg_block_push(block_number) do
+    {:ok, updated_queue} = BoundQueue.push_back(reorg_queue_get(), block_number)
+    :ets.insert(:op_output_roots_reorgs, {:queue, updated_queue})
+  end
+
+  defp reorg_queue_get do
+    if :ets.whereis(:op_output_roots_reorgs) == :undefined do
+      :ets.new(:op_output_roots_reorgs, [
+        :set,
+        :named_table,
+        :public,
+        read_concurrency: true,
+        write_concurrency: true
+      ])
+    end
+
+    with info when info != :undefined <- :ets.info(:op_output_roots_reorgs),
+         [{_, value}] <- :ets.lookup(:op_output_roots_reorgs, :queue) do
+      value
+    else
+      _ -> %BoundQueue{}
+    end
   end
 
   defp get_last_l1_item do
@@ -163,17 +284,21 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
       })
 
     json_rpc(req, json_rpc_named_arguments)
+
+    # todo: repeat 3 times when error
   end
 
   defp get_block_timestamp_by_number(number, json_rpc_named_arguments) do
     {:ok, block} =
       %{id: 0, number: number}
-      |> EthereumJSONRPC.Block.ByNumber.request(false)
+      |> ByNumber.request(false)
       |> json_rpc(json_rpc_named_arguments)
 
     block
     |> Map.get("timestamp")
     |> quantity_to_integer()
+
+    # todo: repeat 3 times when error
   end
 
   defp get_logs(from_block, to_block, address, topic0, json_rpc_named_arguments) do
@@ -181,11 +306,22 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
       request(%{
         id: 0,
         method: "eth_getLogs",
-        params: [%{:fromBlock => integer_to_quantity(from_block), :toBlock => integer_to_quantity(to_block), :address => address, :topics => [topic0]}]
+        params: [
+          %{
+            :fromBlock => integer_to_quantity(from_block),
+            :toBlock => integer_to_quantity(to_block),
+            :address => address,
+            :topics => [topic0]
+          }
+        ]
       })
 
     json_rpc(req, json_rpc_named_arguments)
+
+    # todo: repeat 3 times when error
   end
+
+  # todo: repeat 3 times when error in fetch_block_number_by_tag
 
   defp parse_integer(integer_string) when is_binary(integer_string) do
     case Integer.parse(integer_string) do
@@ -202,7 +338,6 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
       transport_options: [
         http: EthereumJSONRPC.HTTP.HTTPoison,
         url: optimism_rpc_l1,
-        # method_to_url: [],
         http_options: [
           recv_timeout: :timer.minutes(10),
           timeout: :timer.minutes(10),
@@ -210,6 +345,7 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
         ]
       ],
       variant: EthereumJSONRPC.Nethermind
+      # todo: try to remove variant
     ]
   end
 
