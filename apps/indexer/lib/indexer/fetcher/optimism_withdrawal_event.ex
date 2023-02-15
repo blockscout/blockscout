ade41b1b06c037bbf2a6fe62d4d553f8c8e38cad
POA <33550681+poa@users.noreply.github.com>
2023-02-15 16:58:30 +0300

    Add API v2 for Optimism Withdrawals

--- apps/indexer/lib/indexer/fetcher/optimism_withdrawal_event.ex
+++ apps/indexer/lib/indexer/fetcher/optimism_withdrawal_event.ex
@@ -134,6 +134,7 @@ defmodule Indexer.Fetcher.OptimismWithdrawalEvent do
           json_rpc_named_arguments: json_rpc_named_arguments
         } = state
       ) do
+    # credo:disable-for-next-line
     time_before = Timex.now()
 
     chunks_number = ceil((end_block - start_block + 1) / Optimism.get_logs_range_size())
