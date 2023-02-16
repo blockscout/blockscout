b4365fd23ea75e7646a610b6fec7519952be3d25
sl1depengwyn <max.pengwyn@gmail.com>
2023-02-16 22:32:15 +0300

    Reuse basic auth configuration function

--- apps/indexer/config/prod/nethermind.exs
+++ apps/indexer/config/prod/nethermind.exs
@@ -1,15 +1,10 @@
 import Config
 
-basic_auth_user = System.get_env("ETHEREUM_JSONRPC_USER", "")
-basic_auth_pass = System.get_env("ETHEREUM_JSONRPC_PASSWORD", nil)
+~w(config config_helper.exs)
+|> Path.join()
+|> Code.eval_file()
 
-hackney_opts =
-  [pool: :ethereum_jsonrpc]
-  |> (&if(System.get_env("ETHEREUM_JSONRPC_HTTP_INSECURE", "") == "true", do: [:insecure] ++ &1, else: &1)).()
-  |> (&if(basic_auth_user != "" && !is_nil(basic_auth_pass),
-        do: [basic_auth: {basic_auth_user, basic_auth_pass}] ++ &1,
-        else: &1
-      )).()
+hackney_opts = ConfigHelper.hackney_options()
 
 config :indexer,
   block_interval: :timer.seconds(5),
