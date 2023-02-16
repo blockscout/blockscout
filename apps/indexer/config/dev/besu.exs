3b5dc3db62700dff6cb5693e476613fa244edfb0
Viktor Baranov <baranov.viktor.27@gmail.com>
2023-02-16 15:12:59 +0300

    Review comment: Allow blank password in basi auth of JSON RPC endpoint

--- apps/indexer/config/dev/besu.exs
+++ apps/indexer/config/dev/besu.exs
@@ -1,12 +1,12 @@
 import Config
 
 basic_auth_user = System.get_env("ETHEREUM_JSONRPC_USER", "")
-basic_auth_pass = System.get_env("ETHEREUM_JSONRPC_PASSWORD", "")
+basic_auth_pass = System.get_env("ETHEREUM_JSONRPC_PASSWORD", nil)
 
 hackney_opts =
   [pool: :ethereum_jsonrpc]
   |> (&if(System.get_env("ETHEREUM_JSONRPC_HTTP_INSECURE", "") == "true", do: [:insecure] ++ &1, else: &1)).()
-  |> (&if(basic_auth_user != "" && basic_auth_pass != "",
+  |> (&if(basic_auth_user != "" && !is_nil(basic_auth_pass),
         do: [basic_auth: {basic_auth_user, basic_auth_pass}] ++ &1,
         else: &1
       )).()
