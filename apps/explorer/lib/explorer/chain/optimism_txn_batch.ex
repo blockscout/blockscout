13390b86196ec0bda31f274e6c687f4edd0824a9
POA <33550681+poa@users.noreply.github.com>
2023-02-16 17:14:38 +0300

    Add op_transaction_batches table

---
+++ apps/explorer/lib/explorer/chain/optimism_txn_batch.ex
@@ -0,0 +1,34 @@
+defmodule Explorer.Chain.OptimismTxnBatch do
+  @moduledoc "Models a batch of transactions for Optimism."
+
+  use Explorer.Schema
+
+  alias Explorer.Chain.Hash
+
+  @required_attrs ~w(l2_block_number tx_count epoch_number l1_tx_hashes l1_tx_timestamp)a
+
+  @type t :: %__MODULE__{
+          l2_block_number: non_neg_integer(),
+          tx_count: non_neg_integer(),
+          epoch_number: non_neg_integer(),
+          l1_tx_hashes: [Hash.t()],
+          l1_timestamp: DateTime.t()
+        }
+
+  @primary_key false
+  schema "op_transaction_batches" do
+    field(:l2_block_number, :integer, primary_key: true)
+    field(:tx_count, :integer)
+    field(:epoch_number, :integer)
+    field(:l1_tx_hashes, {:array, Hash.Full})
+    field(:l1_timestamp, :utc_datetime_usec)
+
+    timestamps()
+  end
+
+  def changeset(%__MODULE__{} = batches, attrs \\ %{}) do
+    batches
+    |> cast(attrs, @required_attrs)
+    |> validate_required(@required_attrs)
+  end
+end
