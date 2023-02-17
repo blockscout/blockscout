99b6e5da3dabaa2a8362c2bbab8a06d0c0443f67
Viktor Baranov <baranov.viktor.27@gmail.com>
2023-02-17 10:27:11 +0300

    Remove warning from config helper

--- config/config_helper.exs
+++ config/config_helper.exs
@@ -3,12 +3,11 @@ defmodule ConfigHelper do
     basic_auth_user = System.get_env("ETHEREUM_JSONRPC_USER", "")
     basic_auth_pass = System.get_env("ETHEREUM_JSONRPC_PASSWORD", nil)
 
-    hackney_opts =
-      [pool: :ethereum_jsonrpc]
-      |> (&if(System.get_env("ETHEREUM_JSONRPC_HTTP_INSECURE", "") == "true", do: [:insecure] ++ &1, else: &1)).()
-      |> (&if(basic_auth_user != "" && !is_nil(basic_auth_pass),
-            do: [basic_auth: {basic_auth_user, basic_auth_pass}] ++ &1,
-            else: &1
-          )).()
+    [pool: :ethereum_jsonrpc]
+    |> (&if(System.get_env("ETHEREUM_JSONRPC_HTTP_INSECURE", "") == "true", do: [:insecure] ++ &1, else: &1)).()
+    |> (&if(basic_auth_user != "" && !is_nil(basic_auth_pass),
+          do: [basic_auth: {basic_auth_user, basic_auth_pass}] ++ &1,
+          else: &1
+        )).()
   end
 end
