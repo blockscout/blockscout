d6efb4237a0c0d18737d0c072d2ba8a2964ee72e
sl1depengwyn <max.pengwyn@gmail.com>
2023-02-20 21:26:10 +0300

    Create `op_deposits` table

---
+++ apps/explorer/lib/explorer/chain/optimism_deposit.ex
@@ -0,0 +1,34 @@
+defmodule Explorer.Chain.OptimismDeposit do
+  @moduledoc "Models a deposit for Optimism."
+
+  use Explorer.Schema
+
+  alias Explorer.Chain.Hash
+
+  @required_attrs ~w(l1_block_number l1_block_timestamp l1_tx_hash l1_tx_origin l2_tx_hash)a
+
+  @type t :: %__MODULE__{
+          l1_block_number: non_neg_integer(),
+          l1_block_timestamp: DateTime.t(),
+          l1_tx_hash: Hash.t(),
+          l1_tx_origin: Hash.t(),
+          l2_tx_hash: Hash.t()
+        }
+
+  @primary_key false
+  schema "op_output_roots" do
+    field(:l1_block_number, :integer)
+    field(:l1_block_timestamp, :utc_datetime_usec)
+    field(:l1_tx_hash, Hash.Full)
+    field(:l1_tx_origin, Hash.Full)
+    field(:l2_tx_hash, Hash.Full, primary_key: true)
+
+    timestamps()
+  end
+
+  def changeset(%__MODULE__{} = deposit, attrs \\ %{}) do
+    deposit
+    |> cast(attrs, @required_attrs)
+    |> validate_required(@required_attrs)
+  end
+end
---
+++ apps/explorer/priv/repo/migrations/20230220202107_create_op_deposits.exs
@@ -0,0 +1,17 @@
+defmodule Explorer.Repo.Migrations.CreateOpDeposits do
+  use Ecto.Migration
+
+  def change do
+    create table(:op_deposits, primary_key: false) do
+      add(:l1_block_number, :bigint, null: false)
+      add(:l1_block_timestamp, :"timestamp without time zone", null: false)
+      add(:l1_tx_hash, :bytea, null: false)
+      add(:l1_tx_origin, :bytea, null: false)
+      add(:l2_tx_hash, :bytea, null: false, primary_key: true)
+
+      timestamps(null: false, type: :utc_datetime_usec)
+    end
+
+    create(index(:op_deposits, [:l1_block_number]))
+  end
+end
