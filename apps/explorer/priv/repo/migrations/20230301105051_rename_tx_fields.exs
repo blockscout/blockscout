411b8c6483e91aebd8aa459439c29781dd493549
POA <33550681+poa@users.noreply.github.com>
2023-03-01 14:59:28 +0300

    Rename *tx* fields to *transaction* fields

--- apps/explorer/priv/repo/migrations/20230301105051_rename_tx_fields.exs
+++ apps/explorer/priv/repo/migrations/20230301105051_rename_tx_fields.exs
@@ -1,7 +1,9 @@
-defmodule Explorer.Repo.Migrations.RenameTxHashFields do
+defmodule Explorer.Repo.Migrations.RenameTxFields do
   use Ecto.Migration
 
   def change do
+    rename(table(:op_transaction_batches), :l1_tx_hashes, to: :l1_transaction_hashes)
+    rename(table(:op_transaction_batches), :l1_tx_timestamp, to: :l1_transaction_timestamp)
     rename(table(:op_output_roots), :l1_tx_hash, to: :l1_transaction_hash)
     rename(table(:op_withdrawals), :l2_tx_hash, to: :l2_transaction_hash)
     rename(table(:op_withdrawal_events), :l1_tx_hash, to: :l1_transaction_hash)
