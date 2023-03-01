0d5c3e70d3c45da4bfc54d79c4bf3b89d75a0440
POA <33550681+poa@users.noreply.github.com>
2023-03-01 16:41:23 +0300

    Add an indexer helper

--- apps/indexer/lib/indexer/helpers.ex
+++ apps/indexer/lib/indexer/helpers.ex
@@ -3,6 +3,16 @@ defmodule Indexer.Helpers do
   Auxiliary common functions for indexers.
   """
 
+  alias Explorer.Chain.Hash
+
+  def address_hash_to_string(hash) when is_binary(hash) do
+    hash
+  end
+
+  def address_hash_to_string(hash) do
+    Hash.to_string(hash)
+  end
+
   def is_address_correct?(address) when is_binary(address) do
     String.match?(address, ~r/^0x[[:xdigit:]]{40}$/i)
   end
