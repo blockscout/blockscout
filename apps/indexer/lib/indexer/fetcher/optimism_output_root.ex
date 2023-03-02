a1b2adcbf32f746d2a52c1923d4ac3259bf4966e
POA <33550681+poa@users.noreply.github.com>
2023-03-02 16:23:51 +0300

    Refactor init function for two modules

--- apps/indexer/lib/indexer/fetcher/optimism_output_root.ex
+++ apps/indexer/lib/indexer/fetcher/optimism_output_root.ex
@@ -14,10 +14,8 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
 
   alias Explorer.{Chain, Repo}
   alias Explorer.Chain.OptimismOutputRoot
-  alias Indexer.{BoundQueue, Helpers}
+  alias Indexer.BoundQueue
   alias Indexer.Fetcher.Optimism
-
-  @block_check_interval_range_size 100
 
   # 32-byte signature of the event OutputProposed(bytes32 indexed outputRoot, uint256 indexed l2OutputIndex, uint256 indexed l2BlockNumber, uint256 l1Timestamp)
   @output_proposed_event "0xa7aaf2512769da4e444e3de247be2564225c2e7a8f74cfe528e46e17d24868e2"
@@ -43,87 +41,14 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
 
     env = Application.get_all_env(:indexer)[__MODULE__]
 
-    with {:start_block_l1_undefined, false} <- {:start_block_l1_undefined, is_nil(env[:start_block_l1])},
-         optimism_l1_rpc <- Application.get_env(:indexer, :optimism_l1_rpc),
-         {:rpc_l1_undefined, false} <- {:rpc_l1_undefined, is_nil(optimism_l1_rpc)},
-         {:output_oracle_valid, true} <- {:output_oracle_valid, Helpers.is_address_correct?(env[:output_oracle])},
-         start_block_l1 <- Optimism.parse_integer(env[:start_block_l1]),
-         false <- is_nil(start_block_l1),
-         true <- start_block_l1 > 0,
-         {last_l1_block_number, last_l1_transaction_hash} <- get_last_l1_item(),
-         {:start_block_l1_valid, true} <-
-           {:start_block_l1_valid, start_block_l1 <= last_l1_block_number || last_l1_block_number == 0},
-         json_rpc_named_arguments <- json_rpc_named_arguments(optimism_l1_rpc),
-         {:ok, last_l1_tx} <- Optimism.get_transaction_by_hash(last_l1_transaction_hash, json_rpc_named_arguments),
-         {:l1_tx_not_found, false} <- {:l1_tx_not_found, !is_nil(last_l1_transaction_hash) && is_nil(last_l1_tx)},
-         {:ok, last_safe_block} <- Optimism.get_block_number_by_tag("safe", json_rpc_named_arguments),
-         first_block <- max(last_safe_block - @block_check_interval_range_size, 1),
-         {:ok, first_block_timestamp} <- Optimism.get_block_timestamp_by_number(first_block, json_rpc_named_arguments),
-         {:ok, last_safe_block_timestamp} <-
-           Optimism.get_block_timestamp_by_number(last_safe_block, json_rpc_named_arguments) do
-      block_check_interval =
-        ceil((last_safe_block_timestamp - first_block_timestamp) / (last_safe_block - first_block) * 1000 / 2)
-
-      Logger.info("Block check interval is calculated as #{block_check_interval} ms.")
-
-      start_block = max(start_block_l1, last_l1_block_number)
-
-      reorg_monitor_task =
-        Task.Supervisor.async_nolink(Indexer.Fetcher.OptimismOutputRoot.TaskSupervisor, fn ->
-          reorg_monitor(block_check_interval, json_rpc_named_arguments)
-        end)
-
-      {:ok,
-       %{
-         output_oracle: env[:output_oracle],
-         block_check_interval: block_check_interval,
-         start_block: start_block,
-         end_block: last_safe_block,
-         reorg_monitor_task: reorg_monitor_task,
-         json_rpc_named_arguments: json_rpc_named_arguments
-       }, {:continue, nil}}
-    else
-      {:start_block_l1_undefined, true} ->
-        # the process shoudln't start if the start block is not defined
-        :ignore
-
-      {:rpc_l1_undefined, true} ->
-        Logger.error("L1 RPC URL is not defined.")
-        :ignore
-
-      {:output_oracle_valid, false} ->
-        Logger.error("Output Oracle address is invalid or not defined.")
-        :ignore
-
-      {:start_block_l1_valid, false} ->
-        Logger.error("Invalid L1 Start Block value. Please, check the value and op_output_roots table.")
-        :ignore
-
-      {:error, error_data} ->
-        Logger.error(
-          "Cannot get last L1 transaction from RPC by its hash, last safe block, or block timestamp by its number due to RPC error: #{inspect(error_data)}"
-        )
-
-        :ignore
-
-      {:l1_tx_not_found, true} ->
-        Logger.error(
-          "Cannot find last L1 transaction from RPC by its hash. Probably, there was a reorg on L1 chain. Please, check op_output_roots table."
-        )
-
-        :ignore
-
-      _ ->
-        Logger.error("Output Roots Start Block is invalid or zero.")
-        :ignore
-    end
+    Optimism.init(env, env[:output_oracle], __MODULE__)
   end
 
   @impl GenServer
   def handle_continue(
         _,
         %{
-          output_oracle: output_oracle,
+          contract_address: output_oracle,
           block_check_interval: block_check_interval,
           start_block: start_block,
           end_block: end_block,
@@ -245,7 +170,7 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
     end)
   end
 
-  defp reorg_monitor(block_check_interval, json_rpc_named_arguments) do
+  def reorg_monitor(block_check_interval, json_rpc_named_arguments) do
     Logger.metadata(fetcher: :optimism_output_root)
 
     # infinite loop
@@ -301,7 +226,7 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
     end
   end
 
-  defp get_last_l1_item do
+  def get_last_l1_item do
     query =
       from(root in OptimismOutputRoot,
         select: {root.l1_block_number, root.l1_transaction_hash},
@@ -312,20 +237,5 @@ defmodule Indexer.Fetcher.OptimismOutputRoot do
     query
     |> Repo.one()
     |> Kernel.||({0, nil})
-  end
-
-  defp json_rpc_named_arguments(optimism_l1_rpc) do
-    [
-      transport: EthereumJSONRPC.HTTP,
-      transport_options: [
-        http: EthereumJSONRPC.HTTP.HTTPoison,
-        url: optimism_l1_rpc,
-        http_options: [
-          recv_timeout: :timer.minutes(10),
-          timeout: :timer.minutes(10),
-          hackney: [pool: :ethereum_jsonrpc]
-        ]
-      ]
-    ]
   end
 end
