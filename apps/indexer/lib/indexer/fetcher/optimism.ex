fa5b27af0427ba3f0586ffa3bb244123f005ba85
POA <33550681+poa@users.noreply.github.com>
2023-03-02 16:23:51 +0300

    Refactor init function for two modules

--- apps/indexer/lib/indexer/fetcher/optimism.ex
+++ apps/indexer/lib/indexer/fetcher/optimism.ex
@@ -11,7 +11,9 @@ defmodule Indexer.Fetcher.Optimism do
   alias ABI.TypeDecoder
   alias EthereumJSONRPC.Block.ByNumber
   alias Explorer.Chain.Data
+  alias Indexer.Helpers
 
+  @block_check_interval_range_size 100
   @eth_get_logs_range_size 1000
 
   def get_block_number_by_tag(tag, json_rpc_named_arguments, retries_left \\ 3) do
@@ -156,6 +158,104 @@ defmodule Indexer.Fetcher.Optimism do
 
   def get_logs_range_size do
     @eth_get_logs_range_size
+  end
+
+  def json_rpc_named_arguments(optimism_l1_rpc) do
+    [
+      transport: EthereumJSONRPC.HTTP,
+      transport_options: [
+        http: EthereumJSONRPC.HTTP.HTTPoison,
+        url: optimism_l1_rpc,
+        http_options: [
+          recv_timeout: :timer.minutes(10),
+          timeout: :timer.minutes(10),
+          hackney: [pool: :ethereum_jsonrpc]
+        ]
+      ]
+    ]
+  end
+
+  def init(env, contract_address, caller) when caller in [Indexer.Fetcher.OptimismWithdrawalEvent, Indexer.Fetcher.OptimismOutputRoot] do
+    {contract_name, table_name, start_block_note} =
+      if caller == Indexer.Fetcher.OptimismWithdrawalEvent do
+        {"Optimism Portal", "op_withdrawal_events", "Withdrawals L1"}
+      else
+        {"Output Oracle", "op_output_roots", "Output Roots"}
+      end
+
+    with {:start_block_l1_undefined, false} <- {:start_block_l1_undefined, is_nil(env[:start_block_l1])},
+         optimism_l1_rpc <- Application.get_env(:indexer, :optimism_l1_rpc),
+         {:rpc_l1_undefined, false} <- {:rpc_l1_undefined, is_nil(optimism_l1_rpc)},
+         {:contract_is_valid, true} <- {:contract_is_valid, Helpers.is_address_correct?(contract_address)},
+         start_block_l1 <- parse_integer(env[:start_block_l1]),
+         false <- is_nil(start_block_l1),
+         true <- start_block_l1 > 0,
+         {last_l1_block_number, last_l1_transaction_hash} <- caller.get_last_l1_item(),
+         {:start_block_l1_valid, true} <-
+           {:start_block_l1_valid, start_block_l1 <= last_l1_block_number || last_l1_block_number == 0},
+         json_rpc_named_arguments <- json_rpc_named_arguments(optimism_l1_rpc),
+         {:ok, last_l1_tx} <- get_transaction_by_hash(last_l1_transaction_hash, json_rpc_named_arguments),
+         {:l1_tx_not_found, false} <- {:l1_tx_not_found, !is_nil(last_l1_transaction_hash) && is_nil(last_l1_tx)},
+         {:ok, last_safe_block} <- get_block_number_by_tag("safe", json_rpc_named_arguments),
+         first_block <- max(last_safe_block - @block_check_interval_range_size, 1),
+         {:ok, first_block_timestamp} <- get_block_timestamp_by_number(first_block, json_rpc_named_arguments),
+         {:ok, last_safe_block_timestamp} <- get_block_timestamp_by_number(last_safe_block, json_rpc_named_arguments) do
+      block_check_interval =
+        ceil((last_safe_block_timestamp - first_block_timestamp) / (last_safe_block - first_block) * 1000 / 2)
+
+      Logger.info("Block check interval is calculated as #{block_check_interval} ms.")
+
+      start_block = max(start_block_l1, last_l1_block_number)
+
+      reorg_monitor_task =
+        Task.Supervisor.async_nolink(Module.concat(caller, TaskSupervisor), fn ->
+          caller.reorg_monitor(block_check_interval, json_rpc_named_arguments)
+        end)
+
+      {:ok,
+       %{
+         contract_address: contract_address,
+         block_check_interval: block_check_interval,
+         start_block: start_block,
+         end_block: last_safe_block,
+         reorg_monitor_task: reorg_monitor_task,
+         json_rpc_named_arguments: json_rpc_named_arguments
+       }, {:continue, nil}}
+    else
+      {:start_block_l1_undefined, true} ->
+        # the process shoudln't start if the start block is not defined
+        :ignore
+
+      {:rpc_l1_undefined, true} ->
+        Logger.error("L1 RPC URL is not defined.")
+        :ignore
+
+      {:contract_is_valid, false} ->
+        Logger.error("#{contract_name} contract address is invalid or not defined.")
+        :ignore
+
+      {:start_block_l1_valid, false} ->
+        Logger.error("Invalid L1 Start Block value. Please, check the value and #{table_name} table.")
+        :ignore
+
+      {:error, error_data} ->
+        Logger.error(
+          "Cannot get last L1 transaction from RPC by its hash, last safe block, or block timestamp by its number due to RPC error: #{inspect(error_data)}"
+        )
+
+        :ignore
+
+      {:l1_tx_not_found, true} ->
+        Logger.error(
+          "Cannot find last L1 transaction from RPC by its hash. Probably, there was a reorg on L1 chain. Please, check #{table_name} table."
+        )
+
+        :ignore
+
+      _ ->
+        Logger.error("#{start_block_note} Start Block is invalid or zero.")
+        :ignore
+    end
   end
 
   def log_blocks_chunk_handling(chunk_start, chunk_end, start_block, end_block, items_count, layer) do
