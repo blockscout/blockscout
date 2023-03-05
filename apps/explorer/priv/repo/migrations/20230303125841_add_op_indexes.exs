8807c88951a41673a84b433f21b759ac67a54e96
POA <33550681+poa@users.noreply.github.com>
2023-03-05 13:22:19 +0300

    Add an index for l1_block_number field

--- apps/explorer/priv/repo/migrations/20230303125841_add_op_indexes.exs
+++ apps/explorer/priv/repo/migrations/20230303125841_add_op_indexes.exs
@@ -1,7 +1,8 @@
-defmodule Explorer.Repo.Migrations.AddOpWithdrawalEventsIndex do
+defmodule Explorer.Repo.Migrations.AddOpIndexes do
   use Ecto.Migration
 
   def change do
+    create(index(:op_output_roots, [:l1_block_number]))
     create(index(:op_withdrawal_events, [:l1_block_number]))
   end
 end
