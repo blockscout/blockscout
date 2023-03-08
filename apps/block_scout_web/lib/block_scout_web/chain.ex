f8b4b18df54b715b0e4bd7ebbaebb9b6fef4a9dc
POA <33550681+poa@users.noreply.github.com>
2023-03-08 07:48:52 +0300

    Reduce storage consumption for optimism transaction batches

--- apps/block_scout_web/lib/block_scout_web/chain.ex
+++ apps/block_scout_web/lib/block_scout_web/chain.ex
@@ -29,7 +29,6 @@ defmodule BlockScoutWeb.Chain do
     InternalTransaction,
     Log,
     OptimismOutputRoot,
-    OptimismTxnBatch,
     SmartContract,
     Token,
     Token.Instance,
@@ -491,12 +490,12 @@ defmodule BlockScoutWeb.Chain do
     %{"index" => index}
   end
 
-  defp paging_params(%OptimismTxnBatch{l2_block_number: block_number}) do
-    %{"block_number" => block_number}
-  end
-
   defp paging_params(%{msg_nonce: nonce}) do
     %{"nonce" => nonce}
+  end
+
+  defp paging_params(%{l2_block_number: block_number}) do
+    %{"block_number" => block_number}
   end
 
   # clause for search results pagination
