f21c24139198c72bf3f8e500169dbe915d728c01
POA <33550681+poa@users.noreply.github.com>
2023-03-20 15:25:29 +0300

    Return total count of L2 entity by a separate API request

--- apps/block_scout_web/lib/block_scout_web/api_router.ex
+++ apps/block_scout_web/lib/block_scout_web/api_router.ex
@@ -172,9 +172,13 @@ defmodule BlockScoutWeb.ApiRouter do
 
     scope "/optimism" do
       get("/txn-batches", V2.OptimismController, :txn_batches)
+      get("/txn-batches-count", V2.OptimismController, :txn_batches_count)
       get("/output-roots", V2.OptimismController, :output_roots)
+      get("/output-roots-count", V2.OptimismController, :output_roots_count)
       get("/deposits", V2.OptimismController, :deposits)
+      get("/deposits-count", V2.OptimismController, :deposits_count)
       get("/withdrawals", V2.OptimismController, :withdrawals)
+      get("/withdrawals-count", V2.OptimismController, :withdrawals_count)
     end
   end
 
