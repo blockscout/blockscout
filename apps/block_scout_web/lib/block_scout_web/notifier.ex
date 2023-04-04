7fa99ba6b082f282ba80e648db5f35cffc73ed58
varasev <33550681+varasev@users.noreply.github.com>
2023-04-04 15:04:11 +0300

    Add Optimism BedRock Deposits to the main page in API (#7200)
    * Add Optimism BedRock Deposits to the main page in API
    
    * Update changelog
    
    * Pass the number of deposits instead of only one item per once
    
    ---------
    
    Co-authored-by: POA <33550681+poa@users.noreply.github.com>

--- apps/block_scout_web/lib/block_scout_web/notifier.ex
+++ apps/block_scout_web/lib/block_scout_web/notifier.ex
@@ -225,6 +225,10 @@ defmodule BlockScoutWeb.Notifier do
     Endpoint.broadcast("addresses:#{to_string(address_hash)}", "changed_bytecode", %{})
   end
 
+  def handle_event({:chain_event, :optimism_deposits, :realtime, deposits}) do
+    broadcast_optimism_deposits(deposits, "optimism_deposits:new_deposits", "deposits")
+  end
+
   def handle_event(_), do: nil
 
   def fetch_compiler_version(compiler) do
@@ -397,6 +401,10 @@ defmodule BlockScoutWeb.Notifier do
         transaction: transaction
       })
     end
+  end
+
+  defp broadcast_optimism_deposits(deposits, deposit_channel, event) do
+    Endpoint.broadcast(deposit_channel, event, %{deposits: deposits})
   end
 
   defp broadcast_token_transfer(token_transfer) do
