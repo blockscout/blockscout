4398d5e896351cca2d91902118dc7d94528c9fc3
Nikita Pozdniakov <nikitosing4@mail.ru>
2023-04-05 11:25:42 +0300

    Add API_RATE_LIMIT_UI_V2_TOKEN_TTL_IN_SECONDS

--- apps/block_scout_web/lib/block_scout_web/controllers/api/v2/api_key_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/v2/api_key_controller.ex
@@ -8,12 +8,11 @@ defmodule BlockScoutWeb.API.V2.APIKeyController do
 
   def get_key(conn, %{"recaptcha_response" => recaptcha_response}) do
     helper = Application.get_env(:block_scout_web, :captcha_helper)
+    ttl = Application.get_env(:block_scout_web, :api_rate_limit)[:api_v2_token_ttl_seconds]
 
     with {:recaptcha, true} <- {:recaptcha, helper.recaptcha_passed?(recaptcha_response)} do
       conn
-      |> json(
-        key: Crypto.encrypt(conn.secret_key_base, "", %{ip: AccessHelper.conn_to_ip_string(conn)}, max_age: 18_000)
-      )
+      |> json(key: Crypto.encrypt(conn.secret_key_base, "", %{ip: AccessHelper.conn_to_ip_string(conn)}, max_age: ttl))
     end
   end
 end
--- config/runtime.exs
+++ config/runtime.exs
@@ -94,7 +94,8 @@ config :block_scout_web, :api_rate_limit,
   static_api_key: System.get_env("API_RATE_LIMIT_STATIC_API_KEY"),
   whitelisted_ips: System.get_env("API_RATE_LIMIT_WHITELISTED_IPS"),
   is_blockscout_behind_proxy: ConfigHelper.parse_bool_env_var("API_RATE_LIMIT_IS_BLOCKSCOUT_BEHIND_PROXY"),
-  api_v2_ui_limit: ConfigHelper.parse_integer_env_var("API_RATE_LIMIT_UI_V2_WITH_TOKEN", 5)
+  api_v2_ui_limit: ConfigHelper.parse_integer_env_var("API_RATE_LIMIT_UI_V2_WITH_TOKEN", 5),
+  api_v2_token_ttl_seconds: ConfigHelper.parse_integer_env_var("API_RATE_LIMIT_UI_V2_TOKEN_TTL_IN_SECONDS", 18000)
 
 # Configures History
 price_chart_config =
