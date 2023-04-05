dfa54922c7bd226a179c547b94b65888347a1749
Nikita Pozdniakov <nikitosing4@mail.ru>
2023-04-05 14:52:32 +0300

    Handle case when no recaptcha_response provided in /api/v2/key

--- apps/block_scout_web/lib/block_scout_web/controllers/api/v2/api_key_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/v2/api_key_controller.ex
@@ -6,11 +6,13 @@ defmodule BlockScoutWeb.API.V2.APIKeyController do
 
   action_fallback(BlockScoutWeb.API.V2.FallbackController)
 
-  def get_key(conn, %{"recaptcha_response" => recaptcha_response}) do
+  def get_key(conn, params) do
     helper = Application.get_env(:block_scout_web, :captcha_helper)
     ttl = Application.get_env(:block_scout_web, :api_rate_limit)[:api_v2_token_ttl_seconds]
 
-    with {:recaptcha, true} <- {:recaptcha, helper.recaptcha_passed?(recaptcha_response)} do
+    with recaptcha_response <- params["recaptcha_response"],
+         {:recaptcha, false} <- {:recaptcha, is_nil(recaptcha_response)},
+         {:recaptcha, true} <- {:recaptcha, helper.recaptcha_passed?(recaptcha_response)} do
       conn
       |> json(key: Crypto.encrypt(conn.secret_key_base, "", %{ip: AccessHelper.conn_to_ip_string(conn)}, max_age: ttl))
     end
