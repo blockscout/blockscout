ecd7146f3f451e45f69bc08f8206da1c87347037
POA <33550681+poa@users.noreply.github.com>
2023-04-05 17:51:11 +0300

    mix format

--- config/runtime.exs
+++ config/runtime.exs
@@ -365,7 +365,6 @@ config :explorer, :datadog, port: ConfigHelper.parse_integer_env_var("DATADOG_PO
 config :explorer, Explorer.Chain.Cache.TransactionActionTokensData,
   max_cache_size: ConfigHelper.parse_integer_env_var("INDEXER_TX_ACTIONS_MAX_TOKEN_CACHE_SIZE", 100_000)
 
-
 ###############
 ### Indexer ###
 ###############
