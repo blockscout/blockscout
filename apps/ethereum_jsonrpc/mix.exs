5c9abcba8889294139743dfb2c91131f8b5b76a7
Viktor Baranov <baranov.viktor.27@gmail.com>
2023-04-11 22:14:18 +0300

    Blockscout 5.1.3

--- apps/ethereum_jsonrpc/mix.exs
+++ apps/ethereum_jsonrpc/mix.exs
@@ -23,7 +23,7 @@ defmodule EthereumJsonrpc.MixProject do
         dialyzer: :test
       ],
       start_permanent: Mix.env() == :prod,
-      version: "5.1.2"
+      version: "5.1.3"
     ]
   end
 
--- apps/explorer/mix.exs
+++ apps/explorer/mix.exs
@@ -24,7 +24,7 @@ defmodule Explorer.Mixfile do
         dialyzer: :test
       ],
       start_permanent: Mix.env() == :prod,
-      version: "5.1.2",
+      version: "5.1.3",
       xref: [exclude: [BlockScoutWeb.WebRouter.Helpers]]
     ]
   end
