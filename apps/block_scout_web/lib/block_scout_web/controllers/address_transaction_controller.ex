ab41f7e821193adeeb8b6d8677d5d3e12dce3977
Viktor Baranov <baranov.viktor.27@gmail.com>
2023-04-13 20:39:07 +0300

    Support reCAPTCHA v3

--- apps/block_scout_web/lib/block_scout_web/controllers/address_transaction_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/address_transaction_controller.ex
@@ -14,13 +14,14 @@ defmodule BlockScoutWeb.AddressTransactionController do
   alias BlockScoutWeb.{AccessHelper, Controller, TransactionView}
   alias Explorer.{Chain, Market}
 
-  alias Explorer.Chain.{
+  alias Explorer.Chain.CSVExport.{
     AddressInternalTransactionCsvExporter,
     AddressLogCsvExporter,
     AddressTokenTransferCsvExporter,
-    AddressTransactionCsvExporter,
-    Wei
+    AddressTransactionCsvExporter
   }
+
+  alias Explorer.Chain.Wei
 
   alias Explorer.ExchangeRates.Token
   alias Indexer.Fetcher.CoinBalanceOnDemand
@@ -171,10 +172,10 @@ defmodule BlockScoutWeb.AddressTransactionController do
     |> Application.get_env(:captcha_helper)
   end
 
-  defp put_resp_params(conn, file_name) do
+  defp put_resp_params(conn) do
     conn
     |> put_resp_content_type("application/csv")
-    |> put_resp_header("content-disposition", "attachment; filename=#{file_name}")
+    |> put_resp_header("content-disposition", "attachment;")
     |> put_resp_cookie("csv-downloaded", "true", max_age: 86_400, http_only: false)
     |> send_chunked(200)
   end
@@ -187,8 +188,7 @@ defmodule BlockScoutWeb.AddressTransactionController do
            "to_period" => to_period,
            "recaptcha_response" => recaptcha_response
          },
-         csv_export_module,
-         file_name
+         csv_export_module
        )
        when is_binary(address_hash_string) do
     with {:ok, address_hash} <- Chain.string_to_address_hash(address_hash_string),
@@ -196,7 +196,7 @@ defmodule BlockScoutWeb.AddressTransactionController do
          {:recaptcha, true} <- {:recaptcha, captcha_helper().recaptcha_passed?(recaptcha_response)} do
       address
       |> csv_export_module.export(from_period, to_period)
-      |> Enum.reduce_while(put_resp_params(conn, file_name), fn chunk, conn ->
+      |> Enum.reduce_while(put_resp_params(conn), fn chunk, conn ->
         case Conn.chunk(conn, chunk) do
           {:ok, conn} ->
             {:cont, conn}
@@ -217,7 +217,7 @@ defmodule BlockScoutWeb.AddressTransactionController do
     end
   end
 
-  defp items_csv(conn, _, _, _), do: not_found(conn)
+  defp items_csv(conn, _, _), do: not_found(conn)
 
   def token_transfers_csv(conn, params) do
     items_csv(
@@ -228,8 +228,7 @@ defmodule BlockScoutWeb.AddressTransactionController do
         "to_period" => params["to_period"],
         "recaptcha_response" => params["recaptcha_response"]
       },
-      AddressTokenTransferCsvExporter,
-      "token_transfers.csv"
+      AddressTokenTransferCsvExporter
     )
   end
 
@@ -247,8 +246,7 @@ defmodule BlockScoutWeb.AddressTransactionController do
         "to_period" => to_period,
         "recaptcha_response" => recaptcha_response
       },
-      AddressTransactionCsvExporter,
-      "transactions.csv"
+      AddressTransactionCsvExporter
     )
   end
 
@@ -266,8 +264,7 @@ defmodule BlockScoutWeb.AddressTransactionController do
         "to_period" => to_period,
         "recaptcha_response" => recaptcha_response
       },
-      AddressInternalTransactionCsvExporter,
-      "internal_transactions.csv"
+      AddressInternalTransactionCsvExporter
     )
   end
 
@@ -285,8 +282,7 @@ defmodule BlockScoutWeb.AddressTransactionController do
         "to_period" => to_period,
         "recaptcha_response" => recaptcha_response
       },
-      AddressLogCsvExporter,
-      "logs.csv"
+      AddressLogCsvExporter
     )
   end
 end
