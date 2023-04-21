77d775c0e12ba8df04e3b723d002f98fa1d7ac8b
Viktor Baranov <baranov.viktor.27@gmail.com>
2023-04-21 17:45:23 +0300

    Fix tokensupply API v1 endpoint: handle nil total_supply

--- apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/stats_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/stats_controller.ex
@@ -12,7 +12,7 @@ defmodule BlockScoutWeb.API.RPC.StatsController do
     with {:contractaddress_param, {:ok, contractaddress_param}} <- fetch_contractaddress(params),
          {:format, {:ok, address_hash}} <- to_address_hash(contractaddress_param),
          {:token, {:ok, token}} <- {:token, Chain.token_from_address_hash(address_hash)} do
-      render(conn, "tokensupply.json", total_supply: Decimal.to_string(token.total_supply))
+      render(conn, "tokensupply.json", total_supply: token.total_supply && Decimal.to_string(token.total_supply))
     else
       {:contractaddress_param, :error} ->
         render(conn, :error, error: "Query parameter contract address is required")
