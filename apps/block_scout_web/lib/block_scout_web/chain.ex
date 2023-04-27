49f84c25ff8df553e4d88d65264d31c62c46c05e
POA <33550681+poa@users.noreply.github.com>
2023-04-27 17:16:56 +0300

    Fix paging for Optimism Withdrawals in API v2

--- apps/block_scout_web/lib/block_scout_web/chain.ex
+++ apps/block_scout_web/lib/block_scout_web/chain.ex
@@ -484,14 +484,6 @@ defmodule BlockScoutWeb.Chain do
     %{"index" => index}
   end
 
-  defp paging_params(%{msg_nonce: nonce}) do
-    %{"nonce" => nonce}
-  end
-
-  defp paging_params(%{l2_block_number: block_number}) do
-    %{"block_number" => block_number}
-  end
-
   # clause for search results pagination
   defp paging_params(%{
          address_hash: address_hash,
