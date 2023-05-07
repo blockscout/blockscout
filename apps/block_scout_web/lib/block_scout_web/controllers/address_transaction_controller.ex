81235ffa342e4e0a689b0fddcf1f884e85ae91f2
bretep <717695-bretep@users.noreply.gitlab.com>
2023-05-06 22:17:46 -0700

    Update docker ENV variable Re-order clause so recaptcha_response is checked

--- apps/block_scout_web/lib/block_scout_web/controllers/address_transaction_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/address_transaction_controller.ex
@@ -185,42 +185,6 @@ defmodule BlockScoutWeb.AddressTransactionController do
          %{
            "address_id" => address_hash_string,
            "from_period" => from_period,
-           "to_period" => to_period
-         },
-         csv_export_module
-       )
-       when is_binary(address_hash_string) do
-    with {:ok, address_hash} <- Chain.string_to_address_hash(address_hash_string),
-         {:ok, address} <- Chain.hash_to_address(address_hash),
-         true <- Application.get_env(:block_scout_web, :recaptcha)[:is_disabled] do
-      address
-      |> csv_export_module.export(from_period, to_period)
-      |> Enum.reduce_while(put_resp_params(conn), fn chunk, conn ->
-        case Conn.chunk(conn, chunk) do
-          {:ok, conn} ->
-            {:cont, conn}
-
-          {:error, :closed} ->
-            {:halt, conn}
-        end
-      end)
-    else
-      :error ->
-        unprocessable_entity(conn)
-
-      {:error, :not_found} ->
-        not_found(conn)
-
-      false ->
-        not_found(conn)
-    end
-  end
-
-  defp items_csv(
-         conn,
-         %{
-           "address_id" => address_hash_string,
-           "from_period" => from_period,
            "to_period" => to_period,
            "recaptcha_response" => recaptcha_response
          },
@@ -249,6 +213,42 @@ defmodule BlockScoutWeb.AddressTransactionController do
         not_found(conn)
 
       {:recaptcha, false} ->
+        not_found(conn)
+    end
+  end
+
+  defp items_csv(
+         conn,
+         %{
+           "address_id" => address_hash_string,
+           "from_period" => from_period,
+           "to_period" => to_period
+         },
+         csv_export_module
+       )
+       when is_binary(address_hash_string) do
+    with {:ok, address_hash} <- Chain.string_to_address_hash(address_hash_string),
+         {:ok, address} <- Chain.hash_to_address(address_hash),
+         true <- Application.get_env(:block_scout_web, :recaptcha)[:is_disabled] do
+      address
+      |> csv_export_module.export(from_period, to_period)
+      |> Enum.reduce_while(put_resp_params(conn), fn chunk, conn ->
+        case Conn.chunk(conn, chunk) do
+          {:ok, conn} ->
+            {:cont, conn}
+
+          {:error, :closed} ->
+            {:halt, conn}
+        end
+      end)
+    else
+      :error ->
+        unprocessable_entity(conn)
+
+      {:error, :not_found} ->
+        not_found(conn)
+
+      false ->
         not_found(conn)
     end
   end
