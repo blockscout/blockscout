366bfb04fd32e11668e7a7b835af3f40943b3f88
Qwerty5Uiop <alex000010@bk.ru>
2023-05-08 19:43:47 +0400

    Fix block referencing import stage

--- apps/explorer/lib/explorer/chain/import/stage/block_referencing.ex
+++ apps/explorer/lib/explorer/chain/import/stage/block_referencing.ex
@@ -25,9 +25,9 @@ defmodule Explorer.Chain.Import.Stage.BlockReferencing do
     {transactions_multis, runner_to_changes_list_without_trans} =
       Stage.chunk_every(runner_to_changes_list, Runner.Transactions, @transactions_chunk_size, options)
 
-    {tokens_multis, result_runner_to_changes_list} =
-      Stage.split_multis([Runner.Tokens], runner_to_changes_list_without_trans, options)
+    {rest_multis, result_runner_to_changes_list} =
+      Stage.split_multis(runners() -- [Runner.Transactions], runner_to_changes_list_without_trans, options)
 
-    {transactions_multis ++ tokens_multis, result_runner_to_changes_list}
+    {transactions_multis ++ rest_multis, result_runner_to_changes_list}
   end
 end
