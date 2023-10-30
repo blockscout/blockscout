53ae48b57fc786361a30defb900f9e577c2c0822
POA <33550681+poa@users.noreply.github.com>
2023-10-30 15:00:09 +0300

    Small refactor of `Indexer.Fetcher.OptimismTxnBatch`

--- apps/indexer/lib/indexer/fetcher/optimism_txn_batch.ex
+++ apps/indexer/lib/indexer/fetcher/optimism_txn_batch.ex
@@ -50,12 +50,12 @@ defmodule Indexer.Fetcher.OptimismTxnBatch do
   def handle_continue(_, state) do
     Logger.metadata(fetcher: @fetcher_name)
     # two seconds pause needed to avoid exceeding Supervisor restart intensity when DB issues
-    Process.send_after(self(), :init_with_sleep, 2000)
+    Process.send_after(self(), :init_with_delay, 2000)
     {:noreply, state}
   end
 
   @impl GenServer
-  def handle_info(:init_with_sleep, %{json_rpc_named_arguments_l2: json_rpc_named_arguments_l2} = state) do
+  def handle_info(:init_with_delay, %{json_rpc_named_arguments_l2: json_rpc_named_arguments_l2} = state) do
     env = Application.get_all_env(:indexer)[__MODULE__]
 
     with {:start_block_l1_undefined, false} <- {:start_block_l1_undefined, is_nil(env[:start_block_l1])},
