639da3ed613599807218eb35a71753263e59b5be
varasev <33550681+varasev@users.noreply.github.com>
2023-11-13 20:16:43 +0300

    Hotfix for optimism_withdrawal_transaction_status function

--- apps/explorer/lib/explorer/chain.ex
+++ apps/explorer/lib/explorer/chain.ex
@@ -1849,7 +1849,8 @@ defmodule Explorer.Chain do
             hash: w.hash,
             l2_timestamp: l2_block.timestamp,
             l1_transaction_hash: we.l1_transaction_hash
-          }
+          },
+          limit: 1
         )
       )
 
