284b49dbf5ced38f908e2d3c9e10013002d95423
Viktor Baranov <baranov.viktor.27@gmail.com>
2023-11-14 12:47:14 +0300

    Fix tests after rebasing

--- apps/block_scout_web/test/block_scout_web/controllers/api/v2/address_controller_test.exs
+++ apps/block_scout_web/test/block_scout_web/controllers/api/v2/address_controller_test.exs
@@ -2681,5 +2681,29 @@ defmodule BlockScoutWeb.API.V2.AddressControllerTest do
                                               _options ->
       {:ok, "0x0000000000000000000000000000000000000000000000000000000000000001"}
     end)
+    |> expect(:json_rpc, fn %{
+                              id: 0,
+                              method: "eth_getStorageAt",
+                              params: [
+                                _,
+                                "0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50",
+                                "latest"
+                              ]
+                            },
+                            _options ->
+      {:ok, "0x0000000000000000000000000000000000000000000000000000000000000000"}
+    end)
+    |> expect(:json_rpc, fn %{
+                              id: 0,
+                              method: "eth_getStorageAt",
+                              params: [
+                                _,
+                                "0x7050c9e0f4ca769c69bd3a8ef740bc37934f8e2c036e5a723fd8ee048ed3f8c3",
+                                "latest"
+                              ]
+                            },
+                            _options ->
+      {:ok, "0x0000000000000000000000000000000000000000000000000000000000000000"}
+    end)
   end
 end
