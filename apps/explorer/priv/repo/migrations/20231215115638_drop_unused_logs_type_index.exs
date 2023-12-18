02725cabfa026b603c8dad38a831cda9a99436ef
Viktor Baranov <baranov.viktor.27@gmail.com>
2023-12-18 17:59:56 +0300

    Remove log type column

--- apps/explorer/priv/repo/migrations/20231215115638_drop_unused_logs_type_index.exs
+++ apps/explorer/priv/repo/migrations/20231215115638_drop_unused_logs_type_index.exs
@@ -3,5 +3,9 @@ defmodule Explorer.Repo.Migrations.DropUnusedLogsTypeIndex do
 
   def change do
     drop(index(:logs, [:type], name: :logs_type_index))
+
+    alter table(:logs) do
+      remove(:type)
+    end
   end
 end
