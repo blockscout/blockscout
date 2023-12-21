06c25369eed8b7a42b2cda6abf6850ba71a259f1
Victor Baranov <baranov.viktor.27@gmail.com>
2023-12-21 19:07:03 +0300

    Drop unused token_id column from token_transfers table and indexes based on this column (#9005)
    * Drop unused token_id column from token_transfers table and indexes on this column
    
    * Remove token ids migration
    
    * Refactor DB migration
    
    * Update CHANGE LOG entry

--- apps/explorer/config/runtime/test.exs
+++ apps/explorer/config/runtime/test.exs
@@ -33,8 +33,6 @@ config :explorer, Explorer.Market.History.Cataloger, enabled: false
 
 config :explorer, Explorer.Tracer, disabled?: false
 
-config :explorer, Explorer.TokenTransferTokenIdMigration.Supervisor, enabled: false
-
 config :explorer, Explorer.TokenInstanceOwnerAddressMigration.Supervisor, enabled: false
 
 config :explorer, Explorer.Migrator.TransactionsDenormalization, enabled: false
