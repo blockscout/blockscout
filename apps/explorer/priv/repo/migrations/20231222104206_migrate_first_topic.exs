b2875c2f879f97a7ca37f33a18946032773c8ad6
Viktor Baranov <baranov.viktor.27@gmail.com>
2023-12-28 11:30:12 +0300

    Update migration

--- apps/explorer/priv/repo/migrations/20231222104206_migrate_first_topic.exs
+++ apps/explorer/priv/repo/migrations/20231222104206_migrate_first_topic.exs
@@ -4,7 +4,8 @@ defmodule Explorer.Repo.Migrations.MigrateFirstTopic do
   def change do
     execute("""
       INSERT INTO log_first_topics(hash, inserted_at, updated_at)
-      SELECT DISTINCT first_topic, now(), now() FROM logs;
+      SELECT DISTINCT first_topic, now(), now() FROM logs
+      WHERE first_topic IS NOT NULL;
     """)
 
     execute("""
