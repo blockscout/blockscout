144d56693ae00ae5f3688d0751226fa7498a28c1
Viktor Baranov <baranov.viktor.27@gmail.com>
2024-01-16 13:39:59 +0300

    Change token_type to id in the index

--- apps/explorer/priv/repo/migrations/20240114181404_enhanced_unfetched_token_balances_index.exs
+++ apps/explorer/priv/repo/migrations/20240114181404_enhanced_unfetched_token_balances_index.exs
@@ -3,7 +3,7 @@ defmodule Explorer.Repo.Migrations.EnhancedUnfetchedTokenBalancesIndex do
 
   def up do
     execute("""
-      CREATE INDEX unfetched_address_token_balances_index on address_token_balances(token_type)
+      CREATE INDEX unfetched_address_token_balances_index on address_token_balances(id)
       WHERE (
           ((address_hash != '\\x0000000000000000000000000000000000000000' AND token_type = 'ERC-721') OR token_type = 'ERC-20' OR token_type = 'ERC-1155') AND (value_fetched_at IS NULL OR value IS NULL)
       );
