b8df64b9eac79f4a9c35fa665356fdf78d3f93be
Qwerty5Uiop <105209995+Qwerty5Uiop@users.noreply.github.com>
2024-01-26 14:14:23 +0400

    Fix pending transactions sanitizer (#9261)
    * Fix pending transactions sanitizer
    
    * Add block consensus filters to list_transactions and list_token_transfers

--- apps/explorer/lib/explorer/etherscan.ex
+++ apps/explorer/lib/explorer/etherscan.ex
@@ -4,7 +4,7 @@ defmodule Explorer.Etherscan do
   """
 
   import Ecto.Query,
-    only: [from: 2, where: 3, or_where: 3, union: 2, subquery: 1, order_by: 3, limit: 2, offset: 2, preload: 3]
+    only: [from: 2, where: 3, union: 2, subquery: 1, order_by: 3, limit: 2, offset: 2, preload: 3]
 
   import Explorer.Chain.SmartContract, only: [burn_address_hash_string: 0]
 
@@ -474,6 +474,7 @@ defmodule Explorer.Etherscan do
         from(
           t in Transaction,
           where: not is_nil(t.block_hash),
+          where: t.block_consensus == true,
           order_by: [{^options.order_by_direction, t.block_number}],
           limit: ^options.page_size,
           offset: ^offset(options),
@@ -486,6 +487,7 @@ defmodule Explorer.Etherscan do
         from(
           t in Transaction,
           inner_join: b in assoc(t, :block),
+          where: b.consensus == true,
           order_by: [{^options.order_by_direction, t.block_number}],
           limit: ^options.page_size,
           offset: ^offset(options),
@@ -515,10 +517,12 @@ defmodule Explorer.Etherscan do
   end
 
   defp where_address_match(query, address_hash, _) do
-    query
-    |> where([t], t.to_address_hash == ^address_hash)
-    |> or_where([t], t.from_address_hash == ^address_hash)
-    |> or_where([t], t.created_contract_address_hash == ^address_hash)
+    where(
+      query,
+      [t],
+      t.to_address_hash == ^address_hash or t.from_address_hash == ^address_hash or
+        t.created_contract_address_hash == ^address_hash
+    )
   end
 
   @token_transfer_fields ~w(
@@ -564,7 +568,9 @@ defmodule Explorer.Etherscan do
         from(
           tt in subquery(tt_specific_token_query),
           inner_join: t in Transaction,
-          on: tt.transaction_hash == t.hash and tt.block_number == t.block_number and tt.block_hash == t.block_hash,
+          on:
+            tt.transaction_hash == t.hash and tt.block_number == t.block_number and tt.block_hash == t.block_hash and
+              t.block_consensus == true,
           order_by: [{^options.order_by_direction, tt.block_number}, {^options.order_by_direction, tt.token_log_index}],
           select: %{
             token_contract_address_hash: tt.token_contract_address_hash,
@@ -598,6 +604,7 @@ defmodule Explorer.Etherscan do
           inner_join: t in Transaction,
           on: tt.transaction_hash == t.hash and tt.block_number == t.block_number and tt.block_hash == t.block_hash,
           inner_join: b in assoc(t, :block),
+          where: b.consensus == true,
           order_by: [{^options.order_by_direction, tt.block_number}, {^options.order_by_direction, tt.token_log_index}],
           select: %{
             token_contract_address_hash: tt.token_contract_address_hash,
