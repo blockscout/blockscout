f857b6b39be5a7d9db67545591333ed080a9b441
Kirill Fedoseev <kirill@blockscout.com>
2024-01-30 18:38:46 +0400

    fix: too many connections in tests

--- apps/explorer/config/test.exs
+++ apps/explorer/config/test.exs
@@ -60,7 +60,8 @@ for repo <- [
     ownership_timeout: :timer.minutes(1),
     timeout: :timer.seconds(60),
     queue_target: 1000,
-    log: false
+    log: false,
+    pool_size: 1
 end
 
 config :logger, :explorer,
