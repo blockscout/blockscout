a0e801ac413bfc1f103ad6bc74c5eb5e5f2e69b5
Viktor Baranov <baranov.viktor.27@gmail.com>
2024-02-21 12:23:16 +0300

    Rename some Noves.fi proxy endpoints

--- apps/block_scout_web/lib/block_scout_web/api_router.ex
+++ apps/block_scout_web/lib/block_scout_web/api_router.ex
@@ -326,8 +326,16 @@ defmodule BlockScoutWeb.ApiRouter do
     scope "/proxy" do
       scope "/noves-fi" do
         get("/transactions/:transaction_hash_param", V2.Proxy.NovesFiController, :transaction)
+        # todo: remove in the future
         get("/transactions/:transaction_hash_param/describe", V2.Proxy.NovesFiController, :describe_transaction)
-        get("/addresses/:address_hash_param/transactions", V2.Proxy.NovesFiController, :address_transactions)
+        get("/transactions/:transaction_hash_param/description", V2.Proxy.NovesFiController, :describe_transaction)
+
+        get(
+          "/addresses/:address_hash_param/transaction-descriptions",
+          V2.Proxy.NovesFiController,
+          :address_transactions
+        )
+
         get("/transactions", V2.Proxy.NovesFiController, :describe_transactions)
       end
 
