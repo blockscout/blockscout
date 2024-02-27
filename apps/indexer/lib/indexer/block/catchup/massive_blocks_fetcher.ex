784db2860b41794f5a802d836e3a9fe87c08eb99
Qwerty5Uiop <alex000010@bk.ru>
2024-02-27 14:57:23 +0400

    Massive blocks fetcher

---
+++ apps/indexer/lib/indexer/block/catchup/massive_blocks_fetcher.ex
@@ -0,0 +1,68 @@
+defmodule Indexer.Block.Catchup.MassiveBlocksFetcher do
+  @moduledoc """
+  Fetches and indexes blocks by numbers from massive_blocks table.
+  """
+
+  use GenServer
+
+  require Logger
+
+  alias Explorer.Utility.MassiveBlock
+  alias Indexer.Block.Fetcher
+
+  @increased_interval 10000
+
+  @spec start_link(term()) :: GenServer.on_start()
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_) do
+    send_new_task()
+
+    {:ok, %{block_fetcher: generate_block_fetcher()}}
+  end
+
+  @impl true
+  def handle_info(:task, state) do
+    case MassiveBlock.get_last_block_number() do
+      nil ->
+        send_new_task(@increased_interval)
+
+      number ->
+        process_block(state.block_fetcher, number)
+        send_new_task()
+    end
+
+    {:noreply, state}
+  end
+
+  defp process_block(block_fetcher, number) do
+    case Fetcher.fetch_and_import_range(block_fetcher, number..number, %{timeout: :infinity}) do
+      {:ok, _result} ->
+        MassiveBlock.delete_block_number(number)
+
+      {:error, error} ->
+        Logger.error("MassiveBlockFetcher failed: #{inspect(error)}")
+    end
+  end
+
+  defp generate_block_fetcher do
+    receipts_batch_size = Application.get_env(:indexer, :receipts_batch_size)
+    receipts_concurrency = Application.get_env(:indexer, :receipts_concurrency)
+    json_rpc_named_arguments = Application.get_env(:indexer, :json_rpc_named_arguments)
+
+    %Fetcher{
+      broadcast: :catchup,
+      callback_module: Indexer.Block.Catchup.Fetcher,
+      json_rpc_named_arguments: json_rpc_named_arguments,
+      receipts_batch_size: receipts_batch_size,
+      receipts_concurrency: receipts_concurrency
+    }
+  end
+
+  defp send_new_task(interval \\ 0) do
+    Process.send_after(self(), :task, interval)
+  end
+end
