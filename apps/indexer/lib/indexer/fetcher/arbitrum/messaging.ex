b540efaa4dc8339c731bd973d9da642424a6d5c8
Alexander Kolotov <alexandr.kolotov@gmail.com>
2024-03-07 02:28:05 +0000

    status for historical l2-to-l1 messages

--- apps/indexer/lib/indexer/fetcher/arbitrum/messaging.ex
+++ apps/indexer/lib/indexer/fetcher/arbitrum/messaging.ex
@@ -9,6 +9,8 @@ defmodule Indexer.Fetcher.Arbitrum.Messaging do
 
   import Explorer.Helper, only: [decode_data: 2]
 
+  alias Indexer.Fetcher.Arbitrum.Utils.Db
+
   require Logger
 
   # 32-byte signature of the event L2ToL1Tx(address caller, address indexed destination, uint256 indexed hash, uint256 indexed position, uint256 arbBlockNum, uint256 ethBlockNum, uint256 timestamp, uint256 callvalue, bytes data)
@@ -42,26 +44,45 @@ defmodule Indexer.Fetcher.Arbitrum.Messaging do
   TBD
   """
   def filter_l2_to_l1_messages(logs) do
-    logs
-    |> Enum.filter(fn event ->
-      event.first_topic == @l2_to_l1_event
-    end)
-    |> Enum.map(fn event ->
-      Logger.info("L2 to L1 message #{event.transaction_hash} found")
+    filtered_logs =
+      logs
+      |> Enum.filter(fn event ->
+        event.first_topic == @l2_to_l1_event
+      end)
 
-      {message_id, caller, blocknum, timestamp} = l2_to_l1_event_parse(event)
+    if Enum.empty?(filtered_logs) do
+      []
+    else
+      highest_committed_block =
+        case Db.highest_committed_block() do
+          nil -> -1
+          value -> value
+        end
 
-      %{
-        direction: :from_l2,
-        message_id: message_id,
-        originator_address: caller,
-        originating_tx_hash: event.transaction_hash,
-        origination_timestamp: timestamp,
-        originating_tx_blocknum: blocknum,
-        status: :initiated
-      }
-      |> complete_to_params()
-    end)
+      highest_confirmed_block =
+        case Db.highest_confirmed_block() do
+          nil -> -1
+          value -> value
+        end
+
+      filtered_logs
+      |> Enum.map(fn event ->
+        Logger.info("L2 to L1 message #{event.transaction_hash} found")
+
+        {message_id, caller, blocknum, timestamp} = l2_to_l1_event_parse(event)
+
+        %{
+          direction: :from_l2,
+          message_id: message_id,
+          originator_address: caller,
+          originating_tx_hash: event.transaction_hash,
+          origination_timestamp: timestamp,
+          originating_tx_blocknum: blocknum,
+          status: status_l2_to_l1_message(blocknum, highest_committed_block, highest_confirmed_block)
+        }
+        |> complete_to_params()
+      end)
+    end
   end
 
   defp complete_to_params(incomplete) do
@@ -93,5 +114,13 @@ defmodule Indexer.Fetcher.Arbitrum.Messaging do
     position = quantity_to_integer(event.fourth_topic)
 
     {position, caller, arb_block_num, Timex.from_unix(timestamp)}
+  end
+
+  defp status_l2_to_l1_message(msg_block, highest_committed_block, highest_confirmed_block) do
+    cond do
+      highest_confirmed_block >= msg_block -> :confirmed
+      highest_committed_block >= msg_block -> :sent
+      false -> :initiated
+    end
   end
 end
