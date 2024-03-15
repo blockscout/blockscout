16797ec1b906e8126b2963f4661c798fcff963a1
Qwerty5Uiop <105209995+Qwerty5Uiop@users.noreply.github.com>
2024-03-15 18:19:50 +0400

    Separate errors by type in EndpointAvailabilityObserver (#9511)
    * Separate errors by type in EndpointAvailabilityObserver
    
    * Add eth_call fallback url
    
    * Update url unavailable warning message
    
    * Update switching back from fallback url message

--- apps/indexer/config/dev/arbitrum.exs
+++ apps/indexer/config/dev/arbitrum.exs
@@ -19,6 +19,7 @@ config :indexer,
       http: EthereumJSONRPC.HTTP.HTTPoison,
       url: System.get_env("ETHEREUM_JSONRPC_HTTP_URL") || "http://localhost:8545",
       fallback_url: System.get_env("ETHEREUM_JSONRPC_FALLBACK_HTTP_URL"),
+      fallback_eth_call_url: System.get_env("ETHEREUM_JSONRPC_FALLBACK_ETH_CALL_URL"),
       method_to_url: [
         eth_call: ConfigHelper.eth_call_url("http://localhost:8545")
       ],
