3e90794b60cc22bea9a0696d660627492add1b69
Fedor Ivanov <ivnfedor@gmail.com>
2024-04-01 19:48:51 +0000

    feat: add a cache for core celo contracts

---
+++ apps/explorer/lib/explorer/chain/cache/celo_core_contracts.ex
@@ -0,0 +1,121 @@
+defmodule Explorer.Chain.Cache.CeloCoreContracts do
+  @moduledoc """
+  Cache for Celo core contract addresses.
+  """
+
+  require Logger
+
+  # import Ecto.Query,
+  #   only: [
+  #     from: 2
+  #   ]
+
+  alias Explorer.SmartContract.Reader
+  alias Explorer.Celo.AbiHandler
+
+  use Explorer.Chain.MapCache,
+    name: :celo_core_contracts,
+    key: :contract_addresses,
+    key: :async_task,
+    global_ttl: :timer.minutes(60),
+    ttl_check_interval: :timer.seconds(1),
+    callback: &async_task_on_deletion(&1)
+
+  @registry_proxy_contract_address "0x000000000000000000000000000000000000ce10"
+  @nil_address "0x0000000000000000000000000000000000000000"
+
+  @contract_atoms [
+    :gold_token
+  ]
+
+  defp handle_fallback(:contract_addresses) do
+    # This will get the task PID if one exists and launch a new task if not
+    # See next `handle_fallback` definition
+    get_async_task()
+
+    {:return, %{}}
+  end
+
+  defp handle_fallback(:async_task) do
+    # If this gets called it means an async task was requested, but none exists
+    # so a new one needs to be launched
+    {:ok, task} =
+      Task.start(fn ->
+        try do
+          {failed_contracts, contracts} =
+            fetch_core_contract_addresses()
+            |> Enum.split_with(fn {_, %{address: address}} -> address in [nil, @nil_address] end)
+
+          Enum.each(failed_contracts, fn {name, _} ->
+            Logger.error("Could not fetch address for contract with name #{name}")
+          end)
+
+          contracts
+          |> Enum.map(fn
+            {atom, %{address: address}} ->
+              {atom, address}
+          end)
+          |> Enum.into(get_contract_addresses())
+          |> set_contract_addresses()
+        rescue
+          e ->
+            Logger.error([
+              "Could not update Celo core contract addresses",
+              Exception.format(:error, e, __STACKTRACE__)
+            ])
+        end
+
+        set_async_task(nil)
+      end)
+
+    {:update, task}
+  end
+
+  def fetch_core_contract_addresses() do
+    @contract_atoms
+    |> Enum.map(fn atom ->
+      name = to_contract_name(atom)
+      {atom, %{name: name, address: fetch_address_for_contract_name(name)}}
+    end)
+    |> Enum.into(%{})
+  end
+
+  def fetch_address_for_contract_name(contract_name) do
+    with abi <- AbiHandler.get_abi(),
+         # 42404e07 = keccak(_getImplementation())
+         %{"42404e07" => {:ok, [implementation_address]}} <-
+           Reader.query_contract(
+             @registry_proxy_contract_address,
+             abi,
+             %{"42404e07" => []},
+             false
+           ),
+         # 853db323 = keccak(getAddressForString(string))
+         %{"853db323" => {:ok, [contract_address]}} <-
+           Reader.query_contract(
+             implementation_address,
+             abi,
+             %{"853db323" => [contract_name]},
+             false
+           ) do
+      contract_address
+    else
+      _ -> nil
+    end
+  end
+
+  defp to_contract_name(contract) do
+    case contract do
+      :gold_token -> "GoldToken"
+      _ -> nil
+    end
+  end
+
+  # By setting this as a `callback` an async task will be started each time the
+  # `gas_prices` expires (unless there is one already running)
+  defp async_task_on_deletion({:delete, _, :contract_addresses}) do
+    get_async_task()
+  end
+
+  defp async_task_on_deletion(_data), do: nil
+end
