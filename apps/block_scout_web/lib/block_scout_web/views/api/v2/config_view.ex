1c56a3124ec84206e7ec55be8d5813c20215dc8b
Viktor Baranov <baranov.viktor.27@gmail.com>
2024-04-02 12:39:31 +0300

    chore: Remove /config/json-rpc-url API endpoint

--- apps/block_scout_web/lib/block_scout_web/views/api/v2/config_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/api/v2/config_view.ex
@@ -1,10 +1,4 @@
 defmodule BlockScoutWeb.API.V2.ConfigView do
-  def render("json_rpc_url.json", %{url: url}) do
-    %{
-      "json_rpc_url" => url
-    }
-  end
-
   def render("backend_version.json", %{version: version}) do
     %{
       "backend_version" => version
