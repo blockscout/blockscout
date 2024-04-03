803fcce1feaebd6885d239d27dfe010941b2a371
Qwerty5Uiop <alex000010@bk.ru>
2024-04-03 11:10:13 +0400

    fix: Broadcast realtime coin balances

--- apps/indexer/lib/indexer/fetcher/coin_balance/realtime.ex
+++ apps/indexer/lib/indexer/fetcher/coin_balance/realtime.ex
@@ -44,7 +44,7 @@ defmodule Indexer.Fetcher.CoinBalance.Realtime do
               tracer: Tracer
             )
   def run(entries, json_rpc_named_arguments) do
-    Helper.run(entries, json_rpc_named_arguments, false)
+    Helper.run(entries, json_rpc_named_arguments, :realtime)
   end
 
   defp defaults do
