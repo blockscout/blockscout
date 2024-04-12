abc1a99e328a4f08479f0bd17a3e6f05772495b5
Maxim Filonov <53992153+sl1depengwyn@users.noreply.github.com>
2024-04-12 19:05:57 +0300

    Add csv export

--- apps/block_scout_web/lib/block_scout_web/api_router.ex
+++ apps/block_scout_web/lib/block_scout_web/api_router.ex
@@ -349,6 +349,7 @@ defmodule BlockScoutWeb.ApiRouter do
 
     scope "/advanced-filters" do
       get("/", V2.AdvancedFilterController, :list)
+      get("/csv", V2.AdvancedFilterController, :list_csv)
       get("/methods", V2.AdvancedFilterController, :list_methods)
     end
   end
--- apps/block_scout_web/lib/block_scout_web/views/api/v2/advanced_filter_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/api/v2/advanced_filter_view.ex
@@ -2,7 +2,9 @@ defmodule BlockScoutWeb.API.V2.AdvancedFilterView do
   use BlockScoutWeb, :view
 
   alias BlockScoutWeb.API.V2.{Helper, TokenView, TransactionView}
-  alias Explorer.Chain.Transaction
+  alias Explorer.Chain.{Address, Transaction}
+  alias Explorer.Market
+  alias Explorer.Market.MarketHistory
 
   def render("advanced_filters.json", %{advanced_filters: advanced_filters, next_page_params: next_page_params}) do
     {decoded_transactions, _, _} =
@@ -21,6 +23,80 @@ defmodule BlockScoutWeb.API.V2.AdvancedFilterView do
 
   def render("methods.json", %{methods: methods}) do
     methods
+  end
+
+  def to_csv_format(advanced_filters) do
+    exchange_rate = Market.get_coin_exchange_rate()
+
+    date_to_prices =
+      Enum.reduce(advanced_filters, %{}, fn af, acc ->
+        date = DateTime.to_date(af.timestamp)
+
+        if Map.has_key?(acc, date) do
+          acc
+        else
+          market_history = MarketHistory.price_at_date(date)
+
+          Map.put(
+            acc,
+            date,
+            {market_history && market_history.opening_price, market_history && market_history.closing_price}
+          )
+        end
+      end)
+
+    row_names = [
+      "TxHash",
+      "Type",
+      "MethodId",
+      "UtcTimestamp",
+      "FromAddress",
+      "ToAddress",
+      "Value",
+      "TokenContractAddressHash",
+      "TokenDecimals",
+      "TokenSymbol",
+      "BlockNumber",
+      "Fee",
+      "CurrentPrice",
+      "TxDateOpeningPrice",
+      "TxDateClosingPrice"
+    ]
+
+    af_lists =
+      advanced_filters
+      |> Stream.map(fn advanced_filter ->
+        method_id =
+          case advanced_filter.input do
+            %{bytes: <<method_id::binary-size(4), _::binary>>} -> method_id
+            _ -> nil
+          end
+
+        {opening_price, closing_price} = date_to_prices[DateTime.to_date(advanced_filter.timestamp)]
+
+        [
+          to_string(advanced_filter.hash),
+          advanced_filter.type,
+          method_id,
+          advanced_filter.timestamp,
+          Address.checksum(advanced_filter.from_address.hash),
+          Address.checksum(advanced_filter.to_address.hash),
+          advanced_filter.value,
+          if(advanced_filter.type != "coin_transfer",
+            do: advanced_filter.token_transfer.token.contract_address_hash,
+            else: nil
+          ),
+          if(advanced_filter.type != "coin_transfer", do: advanced_filter.token_transfer.token.decimals, else: nil),
+          if(advanced_filter.type != "coin_transfer", do: advanced_filter.token_transfer.token.symbol, else: nil),
+          advanced_filter.block_number,
+          advanced_filter.fee,
+          exchange_rate.usd_value,
+          opening_price,
+          closing_price
+        ]
+      end)
+
+    Stream.concat([row_names], af_lists)
   end
 
   defp prepare_advanced_filter(advanced_filter, decoded_input) do
