c9f86e19f8ef932cf8cd4bf072877491965efb67
Fedor Ivanov <ivnfedor@gmail.com>
2024-04-16 08:34:22 +0000

    fix: compile errors for celo view on default chain type

--- apps/block_scout_web/lib/block_scout_web/views/api/v2/celo_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/api/v2/celo_view.ex
@@ -5,30 +5,40 @@ defmodule BlockScoutWeb.API.V2.CeloView do
   alias Ecto.Association.NotLoaded
   alias Explorer.Chain.Transaction
 
-  def extend_transaction_json_response(out_json, %Transaction{gas_token: %NotLoaded{}}), do: out_json
-
   def extend_transaction_json_response(out_json, %Transaction{} = transaction) do
+    case {
+      Map.get(transaction, "gas_token_contract_address"),
+      Map.get(transaction, "gas_token")
+    } do
+      {_, %NotLoaded{}} ->
+        out_json
+
+      {nil, _} ->
+        out_json |> add_gas_token_field(nil)
+
+      {gas_token_contract_address, nil} ->
+        Logger.error(
+          "Transaction #{transaction.hash} has a gas token contract address '#{gas_token_contract_address}' but no associated token found in the database"
+        )
+
+        out_json |> add_gas_token_field(nil)
+
+      {gas_token_contract_address, gas_token} ->
+        out_json
+        |> add_gas_token_field(
+          TokenView.render("token.json", %{
+            token: gas_token,
+            contract_address_hash: gas_token_contract_address
+          })
+        )
+    end
+  end
+
+  defp add_gas_token_field(out_json, token_json) do
     out_json
     |> Map.put(
       "gas_token",
-      render_token(transaction)
+      token_json
     )
-  end
-
-  defp render_token(%Transaction{gas_token_contract_address: nil}), do: nil
-
-  defp render_token(%Transaction{gas_token: nil} = transaction) do
-    Logger.error(
-      "Transaction #{transaction.hash} has a gas token contract address '#{transaction.gas_token_contract_address}' but no associated token found in the database"
-    )
-
-    nil
-  end
-
-  defp render_token(%Transaction{} = transaction) do
-    TokenView.render("token.json", %{
-      token: transaction.gas_token,
-      contract_address_hash: transaction.gas_token_contract_address
-    })
   end
 end
