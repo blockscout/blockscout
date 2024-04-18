31c2f6f732fdea64957b0370b7a6f5a4d918f7e3
Viktor Baranov <baranov.viktor.27@gmail.com>
2024-04-18 14:15:58 +0300

    Separate index creation and deletion to different migrations

--- apps/explorer/priv/repo/migrations/20240404102511_drop_outdated_index_for_token_holders_list.exs
+++ apps/explorer/priv/repo/migrations/20240404102511_drop_outdated_index_for_token_holders_list.exs
@@ -1,11 +1,11 @@
-defmodule Explorer.Repo.Migrations.EnhanceIndexForTokenHoldersList do
+defmodule Explorer.Repo.Migrations.DropOutdatedIndexForTokenHoldersList do
   use Ecto.Migration
   @disable_ddl_transaction true
   @disable_migration_lock true
 
   def change do
-    create_if_not_exists(
-      index(:address_current_token_balances, ["token_contract_address_hash, value DESC, address_hash DESC"],
+    drop_if_exists(
+      index(:address_current_token_balances, [:token_contract_address_hash],
         where: "address_hash != '\\x0000000000000000000000000000000000000000' AND value > 0",
         concurrently: true
       )
