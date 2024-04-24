070cee92b36b1dd5f8adf84f598367f75fca1114
Fedor Ivanov <ivnfedor@gmail.com>
2024-04-24 14:35:05 +0000

    feat: add pending epoch operations table

---
+++ apps/explorer/lib/explorer/chain/celo/helper.ex
@@ -0,0 +1,11 @@
+defmodule Explorer.Chain.Celo.Helper do
+  @moduledoc """
+  Common helper functions for Celo.
+  """
+
+  @blocks_per_epoch 17280
+
+  def blocks_per_epoch, do: @blocks_per_epoch
+
+  def epoch_block?(block_number), do: rem(block_number, @blocks_per_epoch) == 0
+end
