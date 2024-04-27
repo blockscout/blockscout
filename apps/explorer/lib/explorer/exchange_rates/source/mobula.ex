e00f51fe052c91b9fba266371edddd1cf7b5f11d
xyeuu <teoconstant3@gmail.com>
2024-04-28 00:00:41 +0200

    adding mobula history url

--- apps/explorer/lib/explorer/exchange_rates/source/mobula.ex
+++ apps/explorer/lib/explorer/exchange_rates/source/mobula.ex
@@ -90,6 +90,24 @@ defmodule Explorer.ExchangeRates.Source.Mobula do
     "#{base_url()}/market/data&asset=#{symbol}"
   end
 
+  def history_source_url do
+    id = config(:secondary_coin_id)
+
+    if id, do: "#{base_url()}/market/history?asset=#{id}", else: "#{base_url()}/market/history?asset=#{Explorer.coin()}"
+  end
+
+  @spec history_url(non_neg_integer()) :: String.t()
+  def history_url(previous_days) do
+
+    now = DateTime.utc_now()
+    date_days_ago = DateTime.add(now, -previous_days, :day)
+    timestamp_ms = DateTime.to_unix(date_days_ago) * 1000
+
+    Logger.info("Fetching price history from Mobula: #{history_source_url()} for the last #{previous_days} days. Timestamp: #{timestamp_ms} ms. Now: #{DateTime.to_unix(now)} ms. Date days ago: #{DateTime.to_unix(date_days_ago)} ms.")
+
+    "#{history_source_url()}&from=#{timestamp_ms}"
+  end
+
   @impl Source
   def headers do
     if api_key() do
