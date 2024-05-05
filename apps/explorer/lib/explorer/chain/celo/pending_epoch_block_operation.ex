a965a1caa20618c20374d12c7c2fa1899e5fa834
Fedor Ivanov <ivnfedor@gmail.com>
2024-05-05 11:22:45 +0000

    feat: add query to stream pending epoch block operations

--- apps/explorer/lib/explorer/chain/celo/pending_epoch_block_operation.ex
+++ apps/explorer/lib/explorer/chain/celo/pending_epoch_block_operation.ex
@@ -5,7 +5,9 @@ defmodule Explorer.Chain.Celo.PendingEpochBlockOperation do
 
   use Explorer.Schema
 
+  import Explorer.Chain, only: [add_fetcher_limit: 2]
   alias Explorer.Chain.{Block, Hash}
+  alias Explorer.Repo
 
   @required_attrs ~w(block_hash)a
 
@@ -52,4 +54,41 @@ defmodule Explorer.Chain.Celo.PendingEpochBlockOperation do
   #     select: pending_ops.block_hash
   #   )
   # end
+
+  @doc """
+  Returns a stream of all blocks with unfetched epochs, using
+  the `celo_pending_epoch_block_operations` table.
+
+      iex> unfetched = insert(:block, number: 1 * blocks_per_epoch())
+      iex> insert(:celo_pending_epoch_block_operation, block: unfetched)
+      iex> {:ok, blocks} = PendingEpochBlockOperation.stream_epoch_blocks_with_unfetched_rewards(
+      ...>   [],
+      ...>   fn block, acc ->
+      ...>     [block | acc]
+      ...>   end
+      ...> )
+      iex> [{unfetched.number, unfetched.hash}] == blocks
+      true
+  """
+  @spec stream_epoch_blocks_with_unfetched_rewards(
+          initial :: accumulator,
+          reducer :: (entry :: term(), accumulator -> accumulator),
+          limited? :: boolean()
+        ) :: {:ok, accumulator}
+        when accumulator: term()
+  def stream_epoch_blocks_with_unfetched_rewards(initial, reducer, limited? \\ false)
+      when is_function(reducer, 2) do
+    query =
+      from(
+        op in __MODULE__,
+        join: block in assoc(op, :block),
+        select: %{block_number: block.number, block_hash: block.hash},
+        where: block.consensus == true,
+        order_by: [desc: block.number]
+      )
+
+    query
+    |> add_fetcher_limit(limited?)
+    |> Repo.stream_reduce(initial, reducer)
+  end
 end
