29a955b4feba4d9553a7a2ee0ff33dfde4af5ec0
Fedor Ivanov <ivnfedor@gmail.com>
2024-05-07 08:31:17 +0000

    feat: add utils for logs parsing

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/logs.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/logs.ex
@@ -18,4 +18,51 @@ defmodule EthereumJSONRPC.Logs do
   def to_elixir(logs) when is_list(logs) do
     Enum.map(logs, &Log.to_elixir/1)
   end
+
+  def request(id, params) when is_integer(id) and is_map(params) do
+    EthereumJSONRPC.request(%{
+      id: 0,
+      method: "eth_getLogs",
+      params: [params]
+    })
+  end
+
+  def from_responses(responses) do
+    responses
+    |> reduce_responses()
+    |> case do
+      {:ok, logs} ->
+        {
+          :ok,
+          logs
+          |> to_elixir()
+          |> elixir_to_params()
+        }
+
+      {:error, reasons} ->
+        {:error, reasons}
+    end
+  end
+
+  defp reduce_responses(responses) do
+    responses
+    |> Enum.reduce(
+      {:ok, []},
+      fn
+        %{result: result}, {:ok, logs}
+        when is_list(result) ->
+          {:ok, result ++ logs}
+
+        %{result: _}, {:error, _} = error ->
+          error
+
+        %{error: reason}, {:ok, _} ->
+          {:error, [reason]}
+
+        %{error: reason}, {:error, reasons}
+        when is_list(reasons) ->
+          {:error, [reason | reasons]}
+      end
+    )
+  end
 end
