f62974ee24b70a401a8567f5c3fcd33bef1c69b7
Fedor Ivanov <ivnfedor@gmail.com>
2024-05-13 13:32:48 +0000

    feat: fetch and import epoch logs

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/logs.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/logs.ex
@@ -4,7 +4,7 @@ defmodule EthereumJSONRPC.Logs do
   [`eth_getTransactionReceipt`](https://github.com/ethereum/wiki/wiki/JSON-RPC/e8e0771b9f3677693649d945956bc60e886ceb2b#eth_gettransactionreceipt).
   """
 
-  alias EthereumJSONRPC.Log
+  alias EthereumJSONRPC.{Log, Transport}
 
   @type elixir :: [Log.elixir()]
   @type t :: [Log.t()]
@@ -19,6 +19,7 @@ defmodule EthereumJSONRPC.Logs do
     Enum.map(logs, &Log.to_elixir/1)
   end
 
+  @spec request(id :: integer(), params :: map()) :: Transport.request()
   def request(id, params) when is_integer(id) and is_map(params) do
     EthereumJSONRPC.request(%{
       id: 0,
