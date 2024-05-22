d6a9b01e20ba34eb6ab6b1fd31e03e730be17fbe
Fedor Ivanov <ivnfedor@gmail.com>
2024-05-22 08:35:37 +0000

    feat: add epoch helper functions

--- apps/explorer/lib/explorer/chain/celo/helper.ex
+++ apps/explorer/lib/explorer/chain/celo/helper.ex
@@ -7,5 +7,15 @@ defmodule Explorer.Chain.Celo.Helper do
 
   def blocks_per_epoch, do: @blocks_per_epoch
 
+  defguard is_epoch_block(block_number)
+           when is_integer(block_number) and
+                  rem(block_number, @blocks_per_epoch) == 0
+
+  @spec epoch_block?(non_neg_integer) :: boolean
   def epoch_block?(block_number), do: rem(block_number, @blocks_per_epoch) == 0
+
+  @spec block_number_to_epoch_number(block_number :: non_neg_integer) :: non_neg_integer
+  def block_number_to_epoch_number(block_number) do
+    (block_number / @blocks_per_epoch) |> Float.ceil() |> trunc()
+  end
 end
