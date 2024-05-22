decbd2511f81fa20a132a1ba1ad74237fb34d7c6
Fedor Ivanov <ivnfedor@gmail.com>
2024-05-22 21:22:03 +0000

    refactor: always include `gas_token` to the response

--- apps/block_scout_web/lib/block_scout_web/views/api/v2/celo_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/api/v2/celo_view.ex
@@ -6,39 +6,34 @@ defmodule BlockScoutWeb.API.V2.CeloView do
   alias Explorer.Chain.Transaction
 
   def extend_transaction_json_response(out_json, %Transaction{} = transaction) do
-    case {
-      Map.get(transaction, :gas_token_contract_address),
-      Map.get(transaction, :gas_token)
-    } do
-      {_, %NotLoaded{}} ->
-        out_json
+    token_json =
+      case {
+        Map.get(transaction, :gas_token_contract_address),
+        Map.get(transaction, :gas_token)
+      } do
+        {_, %NotLoaded{}} ->
+          nil
 
-      {nil, _} ->
-        out_json |> add_gas_token_field(nil)
+        {nil, _} ->
+          nil
 
-      {gas_token_contract_address, nil} ->
-        Logger.error(
-          "Transaction #{transaction.hash} has a gas token contract address '#{gas_token_contract_address}' but no associated token found in the database"
-        )
+        {gas_token_contract_address, gas_token} ->
+          if is_nil(gas_token) do
+            Logger.error(fn ->
+              [
+                "Transaction #{transaction.hash} has a gas token contract address ",
+                "'#{gas_token_contract_address}' ",
+                "but no associated token found in the database"
+              ]
+            end)
+          end
 
-        out_json |> add_gas_token_field(nil)
-
-      {gas_token_contract_address, gas_token} ->
-        out_json
-        |> add_gas_token_field(
           TokenView.render("token.json", %{
             token: gas_token,
             contract_address_hash: gas_token_contract_address
           })
-        )
-    end
-  end
+      end
 
-  defp add_gas_token_field(out_json, token_json) do
-    out_json
-    |> Map.put(
-      "gas_token",
-      token_json
-    )
+    Map.put(out_json, "gas_token", token_json)
   end
 end
