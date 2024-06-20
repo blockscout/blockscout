e037eee870ff0e8d5905d35aedbead2eb36fd017
Fedor Ivanov <ivnfedor@gmail.com>
2024-06-20 18:53:35 +0300

    chore: update `ex_abi` to fix type decoder for huge lists

--- apps/ethereum_jsonrpc/mix.exs
+++ apps/ethereum_jsonrpc/mix.exs
@@ -77,7 +77,8 @@ defmodule EthereumJsonrpc.MixProject do
       # Convert unix timestamps in JSONRPC to DateTimes
       {:timex, "~> 3.7.1"},
       # Encode/decode function names and arguments
-      {:ex_abi, "~> 0.4"},
+      # {:ex_abi, "~> 0.4"},
+      {:ex_abi, git: "https://github.com/fedor-ivn/ex_abi", branch: "fix-type-decoder-huge-lists", override: true},
       # `:verify_fun` for `Socket.Web.connect`
       {:ssl_verify_fun, "~> 1.1"},
       # `EthereumJSONRPC.WebSocket`
