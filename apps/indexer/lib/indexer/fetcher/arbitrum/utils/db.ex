1c8a368cef6463ac258768a1edf3443e623f22c3
Alexander Kolotov <alexandr.kolotov@gmail.com>
2024-07-09 15:47:00 -0600

    missing comments added

--- apps/indexer/lib/indexer/fetcher/arbitrum/utils/db.ex
+++ apps/indexer/lib/indexer/fetcher/arbitrum/utils/db.ex
@@ -787,7 +787,22 @@ defmodule Indexer.Fetcher.Arbitrum.Utils.Db do
     not Enum.empty?(Reader.get_anytrust_keyset(keyset_hash))
   end
 
-  @spec get_da_info_by_batch_number(non_neg_integer()) :: map() | nil
+  @doc """
+    Retrieves Data Availability (DA) information for a specific Arbitrum batch number.
+
+    This function queries the database for DA information stored in the
+    `DaMultiPurposeRecord`. It specifically looks for records where
+    the `data_type` is 0, which corresponds to batch-specific DA information.
+
+    ## Parameters
+    - `batch_number`: The Arbitrum batch number.
+
+    ## Returns
+    - A map containing the DA information for the specified batch number. This map
+      corresponds to the `data` field of the `DaMultiPurposeRecord`.
+    - An empty map (`%{}`) if no DA information is found for the given batch number.
+  """
+  @spec get_da_info_by_batch_number(non_neg_integer()) :: map()
   def get_da_info_by_batch_number(batch_number) do
     Reader.get_da_info_by_batch_number(batch_number)
   end
@@ -851,6 +866,25 @@ defmodule Indexer.Fetcher.Arbitrum.Utils.Db do
     |> db_record_to_map(log, true)
   end
 
+  # Converts an Arbitrum-related database record to a map with specified keys and optional encoding.
+  #
+  # This function is used to transform various Arbitrum-specific database records
+  # (such as LifecycleTransaction, BatchBlock, or Message) into maps containing
+  # only the specified keys. It's particularly useful for preparing data for
+  # import or further processing of Arbitrum blockchain data.
+  #
+  # Parameters:
+  #   - `required_keys`: A list of atoms representing the keys to include in the
+  #     output map.
+  #   - `record`: The database record or struct to be converted.
+  #   - `encode`: Boolean flag to determine if Hash and Data types should be
+  #     encoded to strings (default: false). When true, Hash and Data are
+  #     converted to string representations; otherwise, their raw bytes are used.
+  #
+  # Returns:
+  #   - A map containing only the required keys from the input record. Hash and
+  #     Data types are either encoded to strings or left as raw bytes based on
+  #     the `encode` parameter.  @spec db_record_to_map([atom()], map(), boolean()) :: map()
   defp db_record_to_map(required_keys, record, encode \\ false) do
     required_keys
     |> Enum.reduce(%{}, fn key, record_as_map ->
