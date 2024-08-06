3ab691d0e0c3bad274ef5f597d8624ae54e26660
nikitosing <32202610+nikitosing@users.noreply.github.com>
2024-08-06 20:34:54 +0300

    fix: Fix :checkout_timeout error on NFT fetching (#10429)

--- apps/indexer/lib/indexer/application.ex
+++ apps/indexer/lib/indexer/application.ex
@@ -44,7 +44,9 @@ defmodule Indexer.Application do
         ) +
         token_instance_fetcher_pool_size(Indexer.Fetcher.TokenInstance.LegacySanitize, nil) +
         token_instance_fetcher_pool_size(Indexer.Fetcher.TokenInstance.SanitizeERC1155, nil) +
-        token_instance_fetcher_pool_size(Indexer.Fetcher.TokenInstance.SanitizeERC721, nil)
+        token_instance_fetcher_pool_size(Indexer.Fetcher.TokenInstance.SanitizeERC721, nil) + 1
+
+    # + 1 (above in pool_size calculation) for the Indexer.Fetcher.OnDemand.TokenInstanceMetadataRefetch
 
     base_children = [
       :hackney_pool.child_spec(:token_instance_fetcher, max_connections: pool_size),
