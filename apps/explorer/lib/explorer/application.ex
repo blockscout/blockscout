a202f453d884f402edf8c78b891fd17dac546fac
Qwerty5Uiop <alex000010@bk.ru>
2024-08-26 19:38:26 +0400

    chore: Reindex incorrect internal transactions migration

--- apps/explorer/lib/explorer/application.ex
+++ apps/explorer/lib/explorer/application.ex
@@ -147,7 +147,8 @@ defmodule Explorer.Application do
         configure_chain_type_dependent_process(Explorer.Chain.Cache.BlackfortValidatorsCounters, :blackfort),
         configure_chain_type_dependent_process(Explorer.Chain.Cache.StabilityValidatorsCounters, :stability),
         configure_mode_dependent_process(Explorer.Migrator.SanitizeMissingTokenBalances, :indexer),
-        configure_mode_dependent_process(Explorer.Migrator.SanitizeReplacedTransactions, :indexer)
+        configure_mode_dependent_process(Explorer.Migrator.SanitizeReplacedTransactions, :indexer),
+        configure_mode_dependent_process(Explorer.Migrator.ReindexInternalTransactionsWithIncompatibleStatus, :indexer)
       ]
       |> List.flatten()
 
