2312a95fe2783fc68ea25acbc99e5bdc0ef87a69
Viktor Baranov <baranov.viktor.27@gmail.com>
2024-09-02 17:30:06 +0300

    fix: Fix gettext usage warning

--- apps/block_scout_web/lib/block_scout_web.ex
+++ apps/block_scout_web/lib/block_scout_web.ex
@@ -25,7 +25,7 @@ defmodule BlockScoutWeb do
       import BlockScoutWeb.Controller
       import BlockScoutWeb.Router.Helpers
       import BlockScoutWeb.Routers.WebRouter.Helpers, except: [static_path: 2]
-      import BlockScoutWeb.Gettext
+      use Gettext, backend: BlockScoutWeb.Gettext
       import BlockScoutWeb.ErrorHelper
       import BlockScoutWeb.Routers.AccountRouter.Helpers, except: [static_path: 2]
       import Plug.Conn
@@ -49,13 +49,14 @@ defmodule BlockScoutWeb do
       import BlockScoutWeb.{
         CurrencyHelper,
         ErrorHelper,
-        Gettext,
         Router.Helpers,
         TabHelper,
         Tokens.Helper,
         Views.ScriptHelper,
         WeiHelper
       }
+
+      use Gettext, backend: BlockScoutWeb.Gettext
 
       import BlockScoutWeb.Routers.AccountRouter.Helpers, except: [static_path: 2]
 
@@ -78,7 +79,7 @@ defmodule BlockScoutWeb do
     quote do
       use Phoenix.Channel
 
-      import BlockScoutWeb.Gettext
+      use Gettext, backend: BlockScoutWeb.Gettext
     end
   end
 
