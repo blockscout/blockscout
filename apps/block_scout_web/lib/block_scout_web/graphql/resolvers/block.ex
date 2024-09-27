e66d345b96a2d3b2cdece4ad5fbd996cef44e099
Fedor Ivanov <ivnfedor@gmail.com>
2024-09-27 17:17:05 +0300

    feat: add verbosity to GraphQL token transfers query (#10770)
    * feat: GraphQL query for token transfers with verbose token, transaction, and block info
    
    * feat: extend GraphQL schema to support legacy `tokenTransferTxs` query
    
    * fix: cspell warnings
    
    * chore: add spec and doc for `token_transfers_by_address_hash/4`
    
    * chore: remove duplicate function declaration
    
    * fix: do not treat examples as doctests

--- apps/block_scout_web/lib/block_scout_web/graphql/resolvers/block.ex
+++ apps/block_scout_web/lib/block_scout_web/graphql/resolvers/block.ex
@@ -3,14 +3,27 @@ defmodule BlockScoutWeb.GraphQL.Resolvers.Block do
 
   alias BlockScoutWeb.GraphQL.Resolvers.Helper
   alias Explorer.Chain
+  alias Explorer.Chain.Transaction
+
+  @api_true [api?: true]
 
   def get_by(_, %{number: number}, resolution) do
     with {:api_enabled, true} <- {:api_enabled, resolution.context.api_enabled},
-         {:ok, _} = result <- Chain.number_to_block(number) do
+         {:ok, _} = result <- Chain.number_to_block(number, @api_true) do
       result
     else
       {:api_enabled, false} -> {:error, Helper.api_is_disabled()}
       {:error, :not_found} -> {:error, "Block number #{number} was not found."}
+    end
+  end
+
+  def get_by(%Transaction{block_hash: hash}, _, resolution) do
+    with {:api_enabled, true} <- {:api_enabled, resolution.context.api_enabled},
+         {:ok, _} = result <- Chain.hash_to_block(hash, @api_true) do
+      result
+    else
+      {:api_enabled, false} -> {:error, Helper.api_is_disabled()}
+      {:error, :not_found} -> {:error, "Block hash #{to_string(hash)} was not found."}
     end
   end
 end
