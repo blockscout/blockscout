30c61cc22252a0aec7b9fe78598c3fc50313bb0b
EvgenKor <evgen2k7@yandex.ru>
2024-10-04 15:33:55 +0300

    Added indexer dependency to explorer

--- apps/explorer/mix.exs
+++ apps/explorer/mix.exs
@@ -25,7 +25,7 @@ defmodule Explorer.Mixfile do
       ],
       start_permanent: Mix.env() == :prod,
       version: "6.8.0",
-      xref: [exclude: [BlockScoutWeb.Routers.WebRouter.Helpers, Indexer.Helper]]
+      xref: [exclude: [BlockScoutWeb.Routers.WebRouter.Helpers, Indexer.Helper, Indexer.Fetcher.Arbitrum.Messaging, Indexer.Fetcher.Arbitrum.Utils.Rpc]]
     ]
   end
 
