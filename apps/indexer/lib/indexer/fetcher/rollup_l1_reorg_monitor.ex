64579742cb05dcc9e43389e0fdad822ee7b95c55
POA <33550681+poa@users.noreply.github.com>
2024-10-08 15:55:20 +0300

    Add some specs and docs

--- apps/indexer/lib/indexer/fetcher/rollup_l1_reorg_monitor.ex
+++ apps/indexer/lib/indexer/fetcher/rollup_l1_reorg_monitor.ex
@@ -1,9 +1,10 @@
 defmodule Indexer.Fetcher.RollupL1ReorgMonitor do
   @moduledoc """
-  A module to catch L1 reorgs and make queue of the reorg blocks (if there are multiple reorgs).
+  A module to monitor and catch L1 reorgs and make queue of the reorg blocks
+  (if there are multiple reorgs) for rollup modules using this monitor.
 
   A rollup module uses the queue to detect a reorg and to do required actions.
-  In case of reorg, the block number is popped from the queue for that rollup module.
+  In case of reorg, the block number is popped from the queue by that rollup module.
   """
 
   use GenServer
@@ -64,6 +65,25 @@ defmodule Indexer.Fetcher.RollupL1ReorgMonitor do
     GenServer.start_link(__MODULE__, args, Keyword.put_new(gen_server_options, :name, __MODULE__))
   end
 
+  @doc """
+    This function initializes L1 blocks reorg monitor for the current rollup
+    defined by CHAIN_TYPE. If the current chain is not a rollup, the module just doesn't start.
+
+    The monitor is launched for certain modules of the rollup defined in @modules_can_use_reorg_monitor attribute
+    if a module starts (it can be switched off by configuration parameters). Whether each module starts or not
+    is defined by the `requires_l1_reorg_monitor?` function of that module.
+
+    The monitor starts an infinite loop of `eth_getBlockByNumber` requests sending them every
+    `block_check_interval` milliseconds to retrieve the latest block number. To read the latest
+    block number, RPC node of Layer 1 is used, which URL is defined by `l1_rpc_url` function of the rollup module.
+    The `block_check_interval` is determined by the `get_block_check_interval` helper function.
+    After the `block_check_interval` is defined, the function sends `:reorg_monitor` message to the GenServer
+    to start the monitor loop.
+
+    ## Returns
+    - `{:ok, state}` with the determined parameters for the monitor loop if at least one rollup module is launched.
+    - `:ignore` if the monitor is not needed.
+  """
   @impl GenServer
   def init(_args) do
     Logger.metadata(fetcher: @fetcher_name)
@@ -113,6 +133,24 @@ defmodule Indexer.Fetcher.RollupL1ReorgMonitor do
     end
   end
 
+  @doc """
+    Implements the monitor loop which requests RPC node for the latest block every
+    `block_check_interval` milliseconds using `eth_getBlockByNumber` request.
+
+    In case of reorg, the reorg block number is pushed into rollup module's queue.
+    The block numbers are then popped by the rollup module from its queue and
+    used to do some actions needed after reorg.
+
+    ## Parameters
+    - `:reorg_monitor`: The message triggering the next monitoring iteration.
+    - `state`: The current state of the process, containing parameters for the
+               monitoring (such as `block_check_interval`, `json_rpc_named_arguments`,
+               the list of rollup modules in need of monitoring, the previous latest
+               block number).
+
+    ## Returns
+    - `{:noreply, state}` where `state` contains the updated previous latest block number.
+  """
   @impl GenServer
   def handle_info(
         :reorg_monitor,
@@ -136,14 +174,14 @@ defmodule Indexer.Fetcher.RollupL1ReorgMonitor do
   end
 
   @doc """
-  Pops the number of reorg block from the front of the queue for the specified rollup module.
+    Pops the number of reorg block from the front of the queue for the specified rollup module.
 
-  ## Parameters
-  - `module`: The module for which the block number is popped from the queue.
+    ## Parameters
+    - `module`: The module for which the block number is popped from the queue.
 
-  ## Returns
-  - The popped block number.
-  - `nil` if the reorg queue is empty.
+    ## Returns
+    - The popped block number.
+    - `nil` if the reorg queue is empty.
   """
   @spec reorg_block_pop(module()) :: non_neg_integer() | nil
   def reorg_block_pop(module) do
@@ -160,14 +198,14 @@ defmodule Indexer.Fetcher.RollupL1ReorgMonitor do
   end
 
   @doc """
-  Pushes the number of reorg block to the back of the queue for the specified rollup module.
+    Pushes the number of reorg block to the back of the queue for the specified rollup module.
 
-  ## Parameters
-  - `block_number`: The reorg block number.
-  - `module`: The module for which the block number is pushed to the queue.
+    ## Parameters
+    - `block_number`: The reorg block number.
+    - `module`: The module for which the block number is pushed to the queue.
 
-  ## Returns
-  - Nothing is returned.
+    ## Returns
+    - Nothing is returned.
   """
   @spec reorg_block_push(non_neg_integer(), module()) :: any()
   def reorg_block_push(block_number, module) do
@@ -176,6 +214,15 @@ defmodule Indexer.Fetcher.RollupL1ReorgMonitor do
     :ets.insert(table_name, {:queue, updated_queue})
   end
 
+  # Reads a block number queue instance from the ETS table associated with the queue.
+  # The table name depends on the module name and formed by the `reorg_table_name` function.
+  #
+  # ## Parameters
+  # - `table_name`: The ETS table name of the queue.
+  #
+  # ## Returns
+  # - `BoundQueue` instance for the queue. The queue may be empty (then %BoundQueue{} is returned).
+  @spec reorg_queue_get(atom()) :: BoundQueue.t(any())
   defp reorg_queue_get(table_name) do
     if :ets.whereis(table_name) == :undefined do
       :ets.new(table_name, [
@@ -195,6 +242,14 @@ defmodule Indexer.Fetcher.RollupL1ReorgMonitor do
     end
   end
 
+  # Forms an ETS table name for the block number queue for the given module name.
+  #
+  # ## Parameters
+  # - `module`: The module name (instance) for which the ETS table name should be formed.
+  #
+  # ## Returns
+  # - An atom defining the table name.
+  @spec reorg_table_name(module()) :: atom()
   defp reorg_table_name(module) do
     :"#{module}#{:_reorgs}"
   end
