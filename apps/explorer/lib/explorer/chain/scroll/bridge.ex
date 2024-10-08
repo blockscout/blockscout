f3094a64a85544dd72b23e5930bbda25ff564cf0
POA <33550681+poa@users.noreply.github.com>
2024-10-08 16:47:16 +0300

    Add to_import for Explorer.Chain.Scroll.Bridge

--- apps/explorer/lib/explorer/chain/scroll/bridge.ex
+++ apps/explorer/lib/explorer/chain/scroll/bridge.ex
@@ -18,6 +18,29 @@ defmodule Explorer.Chain.Scroll.Bridge do
   @required_attrs ~w(type message_hash)a
 
   @typedoc """
+    Descriptor of the Scroll bridge message:
+    * `type` - Type of the bridge operation (:deposit or :withdrawal).
+    * `index` - Index of the deposit or index of the withdrawal (can be nil).
+    * `l1_transaction_hash` - L1 transaction hash of the bridge operation (can be nil).
+    * `l2_transaction_hash` - L2 transaction hash of the bridge operation (can be nil).
+    * `amount` - Amount of the operation in native token (can be nil).
+    * `block_number` - Block number of deposit operation for `l1_transaction_hash`
+                       or withdrawal operation for `l2_transaction_hash` (can be nil).
+    * `block_timestamp` - Timestamp of the block `block_number` (can be nil).
+    * `message_hash` - Unique hash of the operation (message).
+  """
+  @type to_import :: %{
+          type: :deposit | :withdrawal,
+          index: non_neg_integer() | nil,
+          l1_transaction_hash: Hash.t() | nil,
+          l2_transaction_hash: Hash.t() | nil,
+          amount: Decimal.t() | nil,
+          block_number: non_neg_integer() | nil,
+          block_timestamp: DateTime.t() | nil,
+          message_hash: Hash.t()
+        }
+
+  @typedoc """
     * `type` - Type of the bridge operation (:deposit or :withdrawal).
     * `index` - Index of the deposit or index of the withdrawal (can be nil).
     * `l1_transaction_hash` - L1 transaction hash of the bridge operation (can be nil).
