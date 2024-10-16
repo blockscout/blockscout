c432de1e9f8856ce785a354d05947fa2e78dccbf
Nikita Pozdniakov <nikitosing4@mail.ru>
2024-10-16 11:04:32 +0300

    Migration rewritten

--- apps/explorer/lib/explorer/migrator/sanitize_duplicated_log_index_logs.ex
+++ apps/explorer/lib/explorer/migrator/sanitize_duplicated_log_index_logs.ex
@@ -1,19 +1,45 @@
 defmodule Explorer.Migrator.SanitizeDuplicatedLogIndexLogs do
-  # remember last block processed
-  # run iff chain_type is zkevm and rsk
+  @moduledoc """
+  Module responsible for sanitizing duplicated log index logs in the database.
+
+  The migration process involves identifying and updating duplicated log index logs, updating the corresponding token transfers and token instances.
+  """
 
   use Explorer.Migrator.FillingMigration
 
   import Ecto.Query
 
+  alias Explorer.Chain.Cache.BackgroundMigrations
   alias Explorer.Chain.Log
-  alias Explorer.Chain.Transaction
+  alias Explorer.Chain.TokenTransfer
   alias Explorer.Chain.Token.Instance
   alias Explorer.Migrator.FillingMigration
   alias Explorer.Repo
+
   require Logger
 
   @migration_name "sanitize_duplicated_log_index_logs"
+
+  def init(_) do
+    """
+    CREATE TYPE log_id AS (
+      tx_hash bytea,
+      block_hash bytea,
+      log_index integer
+    );
+    """
+    |> Repo.query!([], timeout: :infinity)
+
+    """
+    CREATE TYPE nft_id AS (
+      block_number bigint,
+      log_index integer
+    );
+    """
+    |> Repo.query!([], timeout: :infinity)
+
+    {:ok, %{}, {:continue, :ok}}
+  end
 
   @impl FillingMigration
   def migration_name, do: @migration_name
@@ -28,8 +54,18 @@ defmodule Explorer.Migrator.SanitizeDuplicatedLogIndexLogs do
       unprocessed_data_query(block_number, block_number + limit)
       |> Repo.all(timeout: :infinity)
       |> Enum.group_by(& &1.block_hash)
+      |> Map.to_list()
 
     {ids, Map.put(state, :block_number_to_process, block_number + limit)}
+  end
+
+  @doc """
+  Stub implementation to satisfy FillingMigration behaviour
+  """
+  @impl FillingMigration
+  @spec unprocessed_data_query() :: nil
+  def unprocessed_data_query do
+    nil
   end
 
   def unprocessed_data_query(block_number_start, block_number_end) do
@@ -38,6 +74,17 @@ defmodule Explorer.Migrator.SanitizeDuplicatedLogIndexLogs do
   end
 
   @impl FillingMigration
+  @doc """
+  Updates a batch of logs grouped by block.
+
+  ## Parameters
+
+    - logs_by_block: A map where the keys are block identifiers and the values are lists of logs associated with those blocks.
+
+  ## Returns
+
+    :ok
+  """
   def update_batch(logs_by_block) do
     logs_to_update =
       logs_by_block
@@ -49,19 +96,44 @@ defmodule Explorer.Migrator.SanitizeDuplicatedLogIndexLogs do
       logs_to_update
       |> Enum.reduce({[], [], %{}}, fn {log, new_index}, {ids, logs, ids_to_new_index} ->
         id = {log.transaction_hash, log.block_hash, log.index}
-        {[id | ids], [%Log{log | index: new_index} | logs], Map.put(ids_to_new_index, id, new_index)}
+
+        {[id | ids],
+         [
+           %Log{log | index: new_index} |> Map.from_struct() |> Map.drop([:block, :address, :transaction, :__meta__])
+           | logs
+         ], Map.put(ids_to_new_index, id, new_index)}
+      end)
+
+    prepared_ids =
+      Enum.map(ids, fn {tx_hash, block_hash, log_index} ->
+        {tx_hash.bytes, block_hash.bytes, log_index}
       end)
 
     Repo.transaction(fn ->
       Log
-      |> where([log], {log.transaction_hash, log.block_hash, log.index} in ^ids)
+      |> where(
+        [log],
+        fragment(
+          "(?, ?, ?) = ANY(?::log_id[])",
+          log.transaction_hash,
+          log.block_hash,
+          log.index,
+          ^prepared_ids
+        )
+      )
       |> Repo.delete_all(timeout: :infinity)
 
-      token_transfers =
+      {_, token_transfers} =
         TokenTransfer
         |> where(
           [token_transfer],
-          {token_transfer.transaction_hash, token_transfer.block_hash, token_transfer.index} in ^ids
+          fragment(
+            "(?, ?, ?) = ANY(?::log_id[])",
+            token_transfer.transaction_hash,
+            token_transfer.block_hash,
+            token_transfer.log_index,
+            ^prepared_ids
+          )
         )
         |> select([token_transfer], token_transfer)
         |> Repo.delete_all(timeout: :infinity)
@@ -71,17 +143,36 @@ defmodule Explorer.Migrator.SanitizeDuplicatedLogIndexLogs do
       token_transfers
       |> Enum.map(fn token_transfer ->
         id = token_transfer_to_index(token_transfer)
+
         %TokenTransfer{token_transfer | log_index: ids_to_new_index[id]}
+        |> Map.from_struct()
+        |> Map.drop([
+          :token_id,
+          :index_in_batch,
+          :reverse_index_in_batch,
+          :token_decimals,
+          :from_address,
+          :to_address,
+          :token_contract_address,
+          :block,
+          :instances,
+          :token,
+          :transaction,
+          :__meta__
+        ])
       end)
-      |> Repo.insert_all(TokenTransfer, timeout: :infinity)
+      |> (&Repo.insert_all(TokenTransfer, &1, timeout: :infinity)).()
 
       nft_instances_params =
-        logs
-        |> Enum.map(fn log -> {log.block_number, log.index} end)
+        token_transfers
+        |> Enum.filter(&(&1.token_type == "ERC-721"))
+        |> Enum.map(fn token_transfer -> {token_transfer.block_number, token_transfer.log_index} end)
 
       nft_updates_map =
-        Enum.reduce(token_transfers, %{}, fn token_transfer, acc ->
-          if token_transfer.consensus == true do
+        token_transfers
+        |> Enum.filter(&(&1.token_type == "ERC-721"))
+        |> Enum.reduce(%{}, fn token_transfer, acc ->
+          if token_transfer.block_consensus do
             id = token_transfer_to_index(token_transfer)
             Map.put(acc, {token_transfer.block_number, token_transfer.log_index}, ids_to_new_index[id])
           else
@@ -90,16 +181,38 @@ defmodule Explorer.Migrator.SanitizeDuplicatedLogIndexLogs do
         end)
 
       Instance
-      |> where([nft], {nft.owner_updated_at_block, nft.owner_updated_at_log_index} in ^nft_instances_params)
+      |> where(
+        [nft],
+        fragment(
+          "(?, ?) = ANY(?::nft_id[])",
+          nft.owner_updated_at_block,
+          nft.owner_updated_at_log_index,
+          ^nft_instances_params
+        )
+      )
       |> Repo.all(timeout: :infinity)
       |> Enum.map(fn nft ->
         %Instance{
           nft
           | owner_updated_at_log_index: nft_updates_map[{nft.owner_updated_at_block, nft.owner_updated_at_log_index}]
         }
+        |> Map.from_struct()
+        |> Map.drop([
+          :current_token_balance,
+          :is_unique,
+          :owner,
+          :token,
+          :__meta__
+        ])
       end)
-      |> Repo.insert_all(on_conflict: {:replace, [:owner_updated_at_log_index]}, timeout: :infinity)
+      |> (&Repo.insert_all(Instance, &1,
+            conflict_target: [:token_contract_address_hash, :token_id],
+            on_conflict: {:replace, [:owner_updated_at_log_index]},
+            timeout: :infinity
+          )).()
     end)
+
+    :ok
   end
 
   defp process_block({block_hash, logs}) do
@@ -122,10 +235,29 @@ defmodule Explorer.Migrator.SanitizeDuplicatedLogIndexLogs do
 
   @impl FillingMigration
   def update_cache do
-    BackgroundMigrations.set_transactions_denormalization_finished(true)
+    BackgroundMigrations.set_sanitize_duplicated_log_index_logs_finished(true)
   end
 
   defp token_transfer_to_index(token_transfer) do
     {token_transfer.transaction_hash, token_transfer.block_hash, token_transfer.log_index}
+  end
+
+  @impl FillingMigration
+
+  @doc """
+  Callback function that is executed when the migration process finishes.
+  """
+  def on_finish do
+    """
+    DROP TYPE log_id;
+    """
+    |> Repo.query!([], timeout: :infinity)
+
+    """
+    DROP TYPE nft_id;
+    """
+    |> Repo.query!([], timeout: :infinity)
+
+    :ok
   end
 end
--- apps/explorer/test/support/factory.ex
+++ apps/explorer/test/support/factory.ex
@@ -819,19 +819,18 @@ defmodule Explorer.Factory do
     from_address_hash = address_hash_from_zero_padded_hash_string(log.second_topic)
 
     # `to_address` is the only thing that isn't created from the token_transfer_log_factory
-    to_address = build(:address, hash: to_address_hash)
-    from_address = build(:address, hash: from_address_hash)
     contract_code = Map.fetch!(contract_code_info(), :bytecode)
 
     token_address = insert(:contract_address, contract_code: contract_code)
     token = insert(:token, contract_address: token_address)
+    block = build(:block)
 
     %TokenTransfer{
-      block: build(:block),
+      block: block,
       amount: Decimal.new(1),
-      block_number: block_number(),
-      from_address: from_address,
-      to_address: to_address,
+      block_number: block.number,
+      from_address_hash: from_address_hash,
+      to_address_hash: to_address_hash,
       token_contract_address: token_address,
       token_type: token.type,
       transaction: log.transaction,
