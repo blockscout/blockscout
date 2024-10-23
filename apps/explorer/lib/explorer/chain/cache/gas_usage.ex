76b3f902eebcc5998c26c19e16fd2b11daefa13b
Maxim Filonov <53992153+sl1depengwyn@users.noreply.github.com>
2024-10-23 20:51:08 +0300

    fix: Handle stalled async task in MapCache

--- apps/explorer/lib/explorer/chain/cache/gas_usage.ex
+++ apps/explorer/lib/explorer/chain/cache/gas_usage.ex
@@ -37,9 +37,10 @@ defmodule Explorer.Chain.Cache.GasUsage do
   end
 
   defp handle_fallback(:sum) do
-    # This will get the task PID if one exists and launch a new task if not
+    # This will get the task PID if one exists, check if it's running and launch
+    # a new task if task doesn't exist or it's not running.
     # See next `handle_fallback` definition
-    get_async_task()
+    safe_get_async_task()
 
     {:return, nil}
   end
@@ -73,7 +74,7 @@ defmodule Explorer.Chain.Cache.GasUsage do
 
   # By setting this as a `callback` an async task will be started each time the
   # `sum` expires (unless there is one already running)
-  defp async_task_on_deletion({:delete, _, :sum}), do: get_async_task()
+  defp async_task_on_deletion({:delete, _, :sum}), do: safe_get_async_task()
 
   defp async_task_on_deletion(_data), do: nil
 
