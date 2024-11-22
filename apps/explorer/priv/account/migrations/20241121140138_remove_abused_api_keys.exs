9957507f03848f190c5734c70452f5e2eed0bfec
Maxim Filonov <53992153+sl1depengwyn@users.noreply.github.com>
2024-11-22 11:09:25 +0300

    Fix migration
    Co-Authored-By: Victor Baranov <baranov.viktor.27@gmail.com>

--- apps/explorer/priv/account/migrations/20241121140138_remove_abused_api_keys.exs
+++ apps/explorer/priv/account/migrations/20241121140138_remove_abused_api_keys.exs
@@ -8,14 +8,13 @@ defmodule Explorer.Repo.Account.Migrations.RemoveAbusedApiKeys do
                                 inserted_at,
                                 ROW_NUMBER() OVER (
                                     PARTITION BY identity_id
-                                    ORDER BY inserted_at DESC
                                     ) as row_number
                         FROM account_api_keys)
     DELETE
     FROM account_api_keys
     WHERE value IN (SELECT value
                     FROM ranked_keys
-                    WHERE row_number > 5)
+                    WHERE row_number > 3)
     """)
   end
 end
