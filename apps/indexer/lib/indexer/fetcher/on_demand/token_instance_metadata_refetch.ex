8f4669dd3c0253bfd3e08d630dd3513bae8d167f
Nikita Pozdniakov <nikitosing4@mail.ru>
2024-12-02 13:25:06 +0300

    fix: Wrong usage of env in TokenInstanceMetadataRefetch

--- apps/indexer/lib/indexer/fetcher/on_demand/token_instance_metadata_refetch.ex
+++ apps/indexer/lib/indexer/fetcher/on_demand/token_instance_metadata_refetch.ex
@@ -46,7 +46,7 @@ defmodule Indexer.Fetcher.OnDemand.TokenInstanceMetadataRefetch do
   end
 
   defp fetch_and_broadcast_metadata(token_instance, _state) do
-    from_base_uri? = Application.get_env(:indexer, __MODULE__)[:base_uri_retry?]
+    from_base_uri? = Application.get_env(:indexer, TokenInstanceHelper)[:base_uri_retry?]
 
     token_id = TokenInstanceHelper.prepare_token_id(token_instance.token_id)
     contract_address_hash_string = to_string(token_instance.token_contract_address_hash)
