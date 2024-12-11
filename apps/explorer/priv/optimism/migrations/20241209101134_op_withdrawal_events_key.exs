4d67c657d3d88985dd1c3a5642a7f2aa146b7a77
POA <33550681+poa@users.noreply.github.com>
2024-12-11 12:38:10 +0400

    Improve migration

--- apps/explorer/priv/optimism/migrations/20241209101134_op_withdrawal_events_key.exs
+++ apps/explorer/priv/optimism/migrations/20241209101134_op_withdrawal_events_key.exs
@@ -1,7 +1,7 @@
 defmodule Explorer.Repo.Optimism.Migrations.OPWithdrawalEventsKey do
   use Ecto.Migration
 
-  def change do
+  def up do
     drop(table(:op_withdrawal_events))
 
     create table(:op_withdrawal_events, primary_key: false) do
@@ -9,6 +9,24 @@ defmodule Explorer.Repo.Optimism.Migrations.OPWithdrawalEventsKey do
       add(:l1_event_type, :withdrawal_event_type, null: false, primary_key: true)
       add(:l1_timestamp, :"timestamp without time zone", null: false)
       add(:l1_transaction_hash, :bytea, null: false, primary_key: true)
+      add(:l1_block_number, :bigint, null: false)
+      add(:game_index, :integer, null: true)
+
+      timestamps(null: false, type: :utc_datetime_usec)
+    end
+
+    create(index(:op_withdrawal_events, :l1_timestamp))
+    create(index(:op_withdrawal_events, :l1_block_number))
+  end
+
+  def down do
+    drop(table(:op_withdrawal_events))
+
+    create table(:op_withdrawal_events, primary_key: false) do
+      add(:withdrawal_hash, :bytea, null: false, primary_key: true)
+      add(:l1_event_type, :withdrawal_event_type, null: false, primary_key: true)
+      add(:l1_timestamp, :"timestamp without time zone", null: false)
+      add(:l1_transaction_hash, :bytea, null: false)
       add(:l1_block_number, :bigint, null: false)
       add(:game_index, :integer, null: true)
 
