536619655ab5af8f5355692894074c53700951dd
POA <33550681+poa@users.noreply.github.com>
2024-12-12 12:20:25 +0400

    Improve migration

--- apps/explorer/priv/optimism/migrations/20241209101134_op_withdrawal_events_key.exs
+++ apps/explorer/priv/optimism/migrations/20241209101134_op_withdrawal_events_key.exs
@@ -2,38 +2,25 @@ defmodule Explorer.Repo.Optimism.Migrations.OPWithdrawalEventsKey do
   use Ecto.Migration
 
   def up do
-    drop(table(:op_withdrawal_events))
+    execute("TRUNCATE TABLE op_withdrawal_events;")
 
-    create table(:op_withdrawal_events, primary_key: false) do
-      add(:withdrawal_hash, :bytea, null: false, primary_key: true)
-      add(:l1_event_type, :withdrawal_event_type, null: false, primary_key: true)
-      add(:l1_timestamp, :"timestamp without time zone", null: false)
-      add(:l1_transaction_hash, :bytea, null: false, primary_key: true)
-      add(:l1_block_number, :bigint, null: false)
-      add(:game_index, :integer, null: true)
+    drop(constraint("op_withdrawal_events", "op_withdrawal_events_pkey"))
 
-      timestamps(null: false, type: :utc_datetime_usec)
+    alter table(:op_withdrawal_events) do
+      modify(:withdrawal_hash, :bytea, primary_key: true)
+      modify(:l1_event_type, :withdrawal_event_type, primary_key: true)
+      modify(:l1_transaction_hash, :bytea, primary_key: true)
     end
-
-    create(index(:op_withdrawal_events, :l1_timestamp))
-    create(index(:op_withdrawal_events, :l1_block_number))
   end
 
   def down do
-    drop(table(:op_withdrawal_events))
+    execute("TRUNCATE TABLE op_withdrawal_events;")
 
-    create table(:op_withdrawal_events, primary_key: false) do
-      add(:withdrawal_hash, :bytea, null: false, primary_key: true)
-      add(:l1_event_type, :withdrawal_event_type, null: false, primary_key: true)
-      add(:l1_timestamp, :"timestamp without time zone", null: false)
-      add(:l1_transaction_hash, :bytea, null: false)
-      add(:l1_block_number, :bigint, null: false)
-      add(:game_index, :integer, null: true)
+    drop(constraint("op_withdrawal_events", "op_withdrawal_events_pkey"))
 
-      timestamps(null: false, type: :utc_datetime_usec)
+    alter table(:op_withdrawal_events) do
+      modify(:withdrawal_hash, :bytea, primary_key: true)
+      modify(:l1_event_type, :withdrawal_event_type, primary_key: true)
     end
-
-    create(index(:op_withdrawal_events, :l1_timestamp))
-    create(index(:op_withdrawal_events, :l1_block_number))
   end
 end
