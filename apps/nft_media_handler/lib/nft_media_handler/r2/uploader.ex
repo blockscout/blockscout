a3b8ee8a32971296be41c1f4f89e847ffd82dfb4
nikitosing <32202610+nikitosing@users.noreply.github.com>
2024-12-12 12:56:42 +0300

    fix: Refactor CDN upload functions, prevent saving partially uploaded… (#11400)
    * fix: Refactor CDN upload functions, prevent saving partially uploaded thumbnails
    
    * Process review comments
    
    * Update changelog

--- apps/nft_media_handler/lib/nft_media_handler/r2/uploader.ex
+++ apps/nft_media_handler/lib/nft_media_handler/r2/uploader.ex
@@ -18,7 +18,7 @@ defmodule NFTMediaHandler.R2.Uploader do
     - `{:ok, result}`: If the upload is successful, returns a tuple with `:ok` and the result.
     - `{:error, reason}`: If the upload fails, returns a tuple with `:error` and the reason for the failure.
   """
-  @spec upload_image(binary(), binary(), binary()) :: {:error, any()} | {:ok, any()}
+  @spec upload_image(binary(), binary(), binary()) :: {:ok, any()} | {:error, any()}
   def upload_image(file_binary, file_name, r2_folder) do
     r2_config = Application.get_env(:ex_aws, :s3)
     file_path = Path.join(r2_folder, file_name)
@@ -26,5 +26,26 @@ defmodule NFTMediaHandler.R2.Uploader do
     r2_config[:bucket_name]
     |> S3.put_object(file_path, file_binary)
     |> ExAws.request()
+  end
+
+  @doc """
+  Uploads a list of images to the specified R2 folder.
+
+  ## Parameters
+
+    - images: A list of images to be uploaded.
+    - r2_folder: The destination folder in R2 where the images will be uploaded.
+  """
+  @spec upload_images(list(), binary()) :: {:ok, any()} | {:error, any()}
+  def upload_images(images, r2_folder) do
+    Enum.reduce_while(images, {:ok, nil}, fn {_pixel_size, file_binary, file_name}, _acc ->
+      case upload_image(file_binary, file_name, r2_folder) do
+        {:ok, _} ->
+          {:cont, {:ok, nil}}
+
+        {:error, reason} ->
+          {:halt, {:error, reason}}
+      end
+    end)
   end
 end
