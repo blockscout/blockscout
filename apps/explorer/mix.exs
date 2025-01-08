c10c1e96652e099d046fb33ad015357b4ee91d7c
Viktor Baranov <baranov.viktor.27@gmail.com>
2025-01-08 19:48:34 +0300

    v6.10.1

--- apps/explorer/mix.exs
+++ apps/explorer/mix.exs
@@ -24,7 +24,7 @@ defmodule Explorer.Mixfile do
         dialyzer: :test
       ],
       start_permanent: Mix.env() == :prod,
-      version: "6.10.0",
+      version: "6.10.1",
       xref: [exclude: [BlockScoutWeb.Routers.WebRouter.Helpers, Indexer.Helper]]
     ]
   end
--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule BlockScout.Mixfile do
     [
       # app: :block_scout,
       # aliases: aliases(config_env()),
-      version: "6.10.0",
+      version: "6.10.1",
       apps_path: "apps",
       deps: deps(),
       dialyzer: dialyzer(),
