3e4c8583b9f48b882ee884da1c2bb270c018261e
Anticripto.bicio <orlanduisayala@gmail.com>
2025-01-13 09:26:12 -0600

    Create wallet.js

---
+++ dynamic V2.0/wreapped_walletconect/wallet.js
@@ -0,0 +1,16 @@
+ const locale = {
+  en: {
+    dyn_login: {
+      title: {
+        all_wallet_list: "Find your favorite",
+      },
+    },
+  },
+  it: {
+    dyn_login: {
+      title: {
+        all_wallet_list: "Trova il tuo preferito",
+      },
+    },
+  },
+};
