068518cae9f524513e6dc7d0735899e90c99f00a
Anticripto.bicio <orlanduisayala@gmail.com>
2025-01-13 09:36:01 -0600

    Create exampleUsingicons.js

---
+++ dynamic V2.0/wreapped_walletconect/exampleUsingicons.js
@@ -0,0 +1,21 @@
+ import { BitcoinIcon } from '@dynamic-labs/iconic';
+
+<DynamicContextProvider
+  settings={{
+    overrides: {
+      views: [
+        {
+          type: 'wallet-list',
+          tabs: {
+            items: [
+              {
+                text: 'Ethereum',
+                icon: <BitcoinIcon />
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }}
+/>
