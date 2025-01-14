4b51ab89e754b2885a92c334f6cdc286a8614693
Nikita Pozdniakov <nikitosing4@mail.ru>
2025-01-14 09:51:21 +0300

    Add spec & doc

--- apps/explorer/lib/explorer/chain.ex
+++ apps/explorer/lib/explorer/chain.ex
@@ -3146,6 +3146,19 @@ defmodule Explorer.Chain do
     Wei.to(value, unit)
   end
 
+  @doc """
+  Retrieves the bytecode of a smart contract.
+
+  ## Parameters
+
+    - `address_or_hash` (binary() | Hash.Address.t()): The address hash of the smart contract.
+    - `options` (api?()): keyword to determine target DB (read replica or primary).
+
+  ## Returns
+
+  - `binary()`: The bytecode of the smart contract.
+  """
+  @spec smart_contract_bytecode(binary() | Hash.Address.t()) :: binary()
   def smart_contract_bytecode(address_hash, options \\ []) do
     query =
       from(
