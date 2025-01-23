5a189e601c97d4daf31b8aae213ed5adad619860
Joe Wagner <njwjs722@gmail.com>
2025-01-23 10:03:13 -0700

    update stop.sh so it works with testnet

--- docker-compose/stop.sh
+++ docker-compose/stop.sh
@@ -1,1 +1,11 @@
-docker compose down
+#!/bin/bash
+
+if (($# != 1)); then
+    echo "Arguments: <Specify the network config to use. Can be 'localnet' (without quote) or any public network"
+else
+    if [[ "$1" == "localnet" ]]; then
+        docker compose down
+    else
+        docker compose --profile publicnet down
+    fi
+fi
