c1f28711f9163558f935481ca791f00158cc1cd1
Nikita Pozdniakov <nikitosing4@mail.ru>
2025-02-07 13:33:49 +0300

    fix: Hanlde invalid BLACKFORT_VALIDATOR_API_URL

--- apps/indexer/lib/indexer/fetcher/stability/validator.ex
+++ apps/indexer/lib/indexer/fetcher/stability/validator.ex
@@ -67,4 +67,9 @@ defmodule Indexer.Fetcher.Stability.Validator do
 
     {:noreply, state}
   end
+
+  @spec trigger_update_validators_list() :: :ok
+  def trigger_update_validators_list do
+    GenServer.cast(__MODULE__, :update_validators_list)
+  end
 end
