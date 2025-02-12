3797a08027027c8eac60da05fce1f317cb3f0d6b
Fedor Ivanov <ivnfedor@gmail.com>
2025-02-12 13:48:06 +0300

    chore: add doc

--- apps/block_scout_web/lib/block_scout_web/graphql/schema_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/graphql/schema_controller.ex
@@ -1,9 +1,17 @@
 defmodule BlockScoutWeb.GraphQL.SchemaController do
+  @moduledoc """
+  Controller for serving the GraphQL schema in SDL format.
+  """
+
   use BlockScoutWeb, :controller
 
   @graphql_schema BlockScoutWeb.GraphQL.Schema
                   |> Absinthe.Schema.to_sdl()
 
+  @doc """
+  The GraphQL schema in SDL format, pre-built at compile time to avoid runtime
+  overhead.
+  """
   def index(conn, _params) do
     conn
     |> put_resp_content_type("text/plain")
