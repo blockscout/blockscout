5677d48892d518053328740a3757e6c281f2acf5
Viktor Baranov <baranov.viktor.27@gmail.com>
2025-03-19 17:30:35 +0300

    Remove clause for rendering token_balances.json in TokenView

--- apps/block_scout_web/lib/block_scout_web/views/api/v2/token_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/api/v2/token_view.ex
@@ -46,17 +46,6 @@ defmodule BlockScoutWeb.API.V2.TokenView do
     |> chain_type_fields(%{address: token.contract_address, field_prefix: nil})
   end
 
-  def render("token_balances.json", %{
-        token_balances: token_balances,
-        next_page_params: next_page_params,
-        token: token
-      }) do
-    %{
-      "items" => Enum.map(token_balances, &prepare_token_balance(&1, token)),
-      "next_page_params" => next_page_params
-    }
-  end
-
   def render("token_holders.json", %{
         token_balances: token_balances,
         next_page_params: next_page_params
@@ -102,15 +91,6 @@ defmodule BlockScoutWeb.API.V2.TokenView do
 
   def exchange_rate(%{fiat_value: fiat_value}) when not is_nil(fiat_value), do: to_string(fiat_value)
   def exchange_rate(_), do: nil
-
-  defp prepare_token_balance(token_balance, token) do
-    %{
-      "address" => Helper.address_with_info(nil, token_balance.address, token_balance.address_hash, false),
-      "value" => token_balance.value,
-      "token_id" => token_balance.token_id,
-      "token" => render("token.json", %{token: token})
-    }
-  end
 
   defp prepare_token_holder(token_balance) do
     %{
