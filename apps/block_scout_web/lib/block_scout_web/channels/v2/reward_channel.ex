cc051840bfcbf9a3542f21f790346fed35f32f9c
Qwerty5Uiop <alex000010@bk.ru>
2025-03-25 13:37:14 +0400

    Return address hash validation for rewards channel v2

--- apps/block_scout_web/lib/block_scout_web/channels/v2/reward_channel.ex
+++ apps/block_scout_web/lib/block_scout_web/channels/v2/reward_channel.ex
@@ -4,7 +4,10 @@ defmodule BlockScoutWeb.V2.RewardChannel do
   """
   use BlockScoutWeb, :channel
 
-  def join("rewards:" <> _address_hash_string, _params, socket) do
-    {:ok, %{}, socket}
+  def join("rewards:" <> address_hash_string, _params, socket) do
+    case valid_address_hash_and_not_restricted_access?(address_hash_string) do
+      :ok -> {:ok, %{}, socket}
+      reason -> {:error, %{reason: reason}}
+    end
   end
 end
