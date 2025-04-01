7dbc6c8613827d24b8eef34111e2618e6051fb7c
Nikita Pozdniakov <nikitosing4@mail.ru>
2025-04-01 14:16:35 +0300

    Process review comment

--- apps/explorer/lib/explorer/metadata_uri_validator.ex
+++ apps/explorer/lib/explorer/metadata_uri_validator.ex
@@ -66,7 +66,7 @@ defmodule Explorer.MetadataURIValidator do
     with {:empty_host, %URI{host: host, scheme: scheme}} when host not in ["", nil] <- {:empty_host, URI.parse(uri)},
          {:disallowed_protocol, false} <- {:disallowed_protocol, scheme not in allowed_uri_protocols()},
          {:nxdomain, ip_list} when not is_nil(ip_list) <- {:nxdomain, host_to_ip_list(host)},
-         {:blacklist, true} <- {:blacklist, Enum.all?(ip_list, &allowed_ip?/1)} do
+         {:blacklist, false} <- {:blacklist, not Enum.all?(ip_list, &allowed_ip?/1)} do
       :ok
     else
       {reason, _} ->
