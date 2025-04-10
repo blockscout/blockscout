b6b91649ee61810cffa539d0e1f2946f22c0daf7
Anticripto.bicio <orlanduisayala@gmail.com>
2025-04-10 12:12:16 -0600

    Update Config.js
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- Config.js
+++ Config.js
@@ -1,6 +1,5 @@
 module.exports = {
   buildbear: {
--    url: process.env.BUILDBEAR_RPC_URL
-+    url: process.env.BUILDBEAR_RPC_URL || "https://rpc.buildbear.io/curly-sandman-10c6e11a"
+    url: process.env.BUILDBEAR_RPC_URL
   }
 }
