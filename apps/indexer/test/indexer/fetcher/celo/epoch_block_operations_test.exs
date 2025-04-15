764f9330405978b7f0fb7e2020a73945b12be3c0
Fedor Ivanov <ivnfedor@gmail.com>
2025-04-15 11:34:58 +0300

    fix: remove test

--- apps/indexer/test/indexer/fetcher/celo/epoch_block_operations_test.exs
+++ apps/indexer/test/indexer/fetcher/celo/epoch_block_operations_test.exs
@@ -36,29 +36,5 @@ defmodule Indexer.Fetcher.Celo.EpochBlockOperationsTest do
                  )
       end
     end
-
-    test "correctly buffers blocks when :l2_migration_block is set", %{
-      json_rpc_named_arguments: json_rpc_named_arguments
-    } do
-      last_l1_epoch_block_number = 1 * blocks_per_epoch()
-      l1_block = insert(:block, number: last_l1_epoch_block_number)
-      l2_block = insert(:block, number: 2 * blocks_per_epoch())
-      insert(:celo_pending_epoch_block_operation, block: l1_block)
-      insert(:celo_pending_epoch_block_operation, block: l2_block)
-
-      Application.put_env(:explorer, :celo, l2_migration_block: last_l1_epoch_block_number)
-
-      assert [
-               %{
-                 block_number: l1_block.number,
-                 block_hash: l1_block.hash
-               }
-             ] ==
-               EpochBlockOperations.init(
-                 [],
-                 fn block_number, acc -> [block_number | acc] end,
-                 json_rpc_named_arguments
-               )
-    end
   end
 end
