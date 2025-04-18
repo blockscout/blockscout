f243db8182a86c551f12eab6fb9c729a6f06278b
Viktor Baranov <baranov.viktor.27@gmail.com>
2025-04-18 17:02:38 +0300

    Temp return common health status based on blocks only

--- apps/block_scout_web/lib/block_scout_web/controllers/api/health_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/health_controller.ex
@@ -52,7 +52,9 @@ defmodule BlockScoutWeb.API.HealthController do
 
         base_health_status
         |> put_in([:metadata, :batches], batches_indexing_status)
-        |> Map.put(:healthy, indexing_status.blocks.new.healthy and batches_indexing_status.healthy)
+        # todo: return this when "latest block" metric starts remain non-empty all time
+        #|> Map.put(:healthy, indexing_status.blocks.new.healthy and batches_indexing_status.healthy)
+        |> Map.put(:healthy, indexing_status.blocks.new.healthy)
       else
         base_health_status
         |> Map.put(:healthy, indexing_status.blocks.new.healthy)
