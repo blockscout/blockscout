094efa905946d0bb1b4068fc37a55a0b1708f245
Viktor Baranov <baranov.viktor.27@gmail.com>
2025-04-18 14:15:21 +0300

    Refactor health monitor values filling in the DB

--- apps/explorer/lib/explorer/chain/health/monitor.ex
+++ apps/explorer/lib/explorer/chain/health/monitor.ex
@@ -40,99 +40,96 @@ defmodule Explorer.Chain.Health.Monitor do
   defp perform_work do
     json_rpc_named_arguments = Application.get_env(:explorer, :json_rpc_named_arguments)
 
-    with {latest_block_number_from_db, latest_block_timestamp_from_db} <- HealthHelper.last_db_block(),
-         {latest_block_number_from_cache, latest_block_timestamp_from_cache} <-
-           HealthHelper.last_cache_block(),
-         {:ok, latest_block_number_from_node} <-
-           EndpointAvailabilityChecker.fetch_latest_block_number(json_rpc_named_arguments) do
-      now = DateTime.utc_now()
+    now = DateTime.utc_now()
 
-      base_params = [
-        %{
-          counter_type: "health_latest_block_number_from_db",
-          value: latest_block_number_from_db,
-          inserted_at: now,
-          updated_at: now
-        },
-        %{
-          counter_type: "health_latest_block_timestamp_from_db",
-          value: DateTime.to_unix(latest_block_timestamp_from_db),
-          inserted_at: now,
-          updated_at: now
-        },
-        %{
-          counter_type: "health_latest_block_number_from_cache",
-          value: latest_block_number_from_cache,
-          inserted_at: now,
-          updated_at: now
-        },
-        %{
-          counter_type: "health_latest_block_timestamp_from_cache",
-          value: DateTime.to_unix(latest_block_timestamp_from_cache),
-          inserted_at: now,
-          updated_at: now
-        },
-        %{
-          counter_type: "health_latest_block_number_from_node",
-          value: quantity_to_integer(latest_block_number_from_node),
-          inserted_at: now,
-          updated_at: now
-        }
-      ]
+    db_and_cache_params =
+      with {latest_block_number_from_db, latest_block_timestamp_from_db} <- HealthHelper.last_db_block(),
+           {latest_block_number_from_cache, latest_block_timestamp_from_cache} <-
+             HealthHelper.last_cache_block() do
+        [
+          counter("health_latest_block_number_from_db", latest_block_number_from_db, now, now),
+          counter(
+            "health_latest_block_timestamp_from_db",
+            DateTime.to_unix(latest_block_timestamp_from_db),
+            now,
+            now
+          ),
+          counter("health_latest_block_number_from_cache", latest_block_number_from_cache, now, now),
+          counter(
+            "health_latest_block_timestamp_from_cache",
+            DateTime.to_unix(latest_block_timestamp_from_cache),
+            now,
+            now
+          )
+        ]
+      end
 
-      batch_info =
-        case Application.get_env(:explorer, :chain_type) do
-          :arbitrum ->
-            get_latest_batch_info_from_module(ArbitrumReaderCommon)
+    base_params = maybe_add_block_from_node_to_params?(db_and_cache_params, json_rpc_named_arguments, now)
 
-          :zksync ->
-            get_latest_batch_info_from_module(ZkSyncReader)
+    batch_info =
+      case Application.get_env(:explorer, :chain_type) do
+        :arbitrum ->
+          get_latest_batch_info_from_module(ArbitrumReaderCommon)
 
-          :optimism ->
-            get_latest_batch_info_from_module(OptimismReader)
+        :zksync ->
+          get_latest_batch_info_from_module(ZkSyncReader)
 
-          :polygon_zkevm ->
-            get_latest_batch_info_from_module(PolygonZkevmReader)
+        :optimism ->
+          get_latest_batch_info_from_module(OptimismReader)
 
-          :scroll ->
-            get_latest_batch_info_from_module(ScrollReader)
+        :polygon_zkevm ->
+          get_latest_batch_info_from_module(PolygonZkevmReader)
 
-          _ ->
-            nil
-        end
+        :scroll ->
+          get_latest_batch_info_from_module(ScrollReader)
 
-      params =
-        if batch_info do
-          base_params ++
-            [
-              %{
-                counter_type: "health_latest_batch_number_from_db",
-                value: batch_info.number,
-                inserted_at: now,
-                updated_at: now
-              },
-              %{
-                counter_type: "health_latest_batch_timestamp_from_db",
-                value: DateTime.to_unix(batch_info.timestamp),
-                inserted_at: now,
-                updated_at: now
-              },
-              %{
-                counter_type: "health_latest_batch_average_time_from_db",
-                value: batch_info.average_batch_time,
-                inserted_at: now,
-                updated_at: now
-              }
-            ]
-        else
-          base_params
-        end
+        _ ->
+          nil
+      end
 
-      Repo.insert_all(LastFetchedCounter, params,
-        on_conflict: on_conflict(),
-        conflict_target: [:counter_type]
-      )
+    params =
+      if batch_info do
+        base_params ++
+          [
+            counter("health_latest_batch_number_from_db", batch_info.number, now, now),
+            counter("health_latest_batch_timestamp_from_db", DateTime.to_unix(batch_info.timestamp), now, now),
+            counter("health_latest_batch_average_time_from_db", batch_info.average_batch_time, now, now)
+          ]
+      else
+        base_params
+      end
+
+    Repo.insert_all(LastFetchedCounter, params,
+      on_conflict: on_conflict(),
+      conflict_target: [:counter_type]
+    )
+  end
+
+  defp maybe_add_block_from_node_to_params?(params, json_rpc_named_arguments, now) do
+    case EndpointAvailabilityChecker.fetch_latest_block_number(json_rpc_named_arguments) do
+      {:ok, latest_block_number_from_node} ->
+        [
+          counter(
+            "health_latest_block_number_from_node",
+            quantity_to_integer(latest_block_number_from_node),
+            now,
+            now
+          )
+          | params
+        ]
+
+      _ ->
+        params
     end
+  end
+
+  defp counter(counter_type, value, inserted_at, updated_at) do
+    %{
+      counter_type: counter_type,
+      value: value,
+      inserted_at: inserted_at,
+      updated_at: updated_at
+    }
   end
 
   defp on_conflict do
