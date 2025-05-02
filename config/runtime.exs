eb97c96bf58abf8783ecdd00af014958406984c8
Maxim Filonov <53992153+sl1depengwyn@users.noreply.github.com>
2025-05-02 13:02:34 +0300

    feat: Add TAC operation search
    Co-Authored-By: nikitosing <32202610+nikitosing@users.noreply.github.com>

--- config/runtime.exs
+++ config/runtime.exs
@@ -624,6 +624,10 @@ config :explorer, Explorer.MicroserviceInterfaces.MultichainSearch,
   api_key: System.get_env("MICROSERVICE_MULTICHAIN_SEARCH_API_KEY"),
   service_url: System.get_env("MICROSERVICE_MULTICHAIN_SEARCH_URL")
 
+config :explorer, Explorer.MicroserviceInterfaces.TACOperationLifecycle,
+  enabled: ConfigHelper.parse_bool_env_var("MICROSERVICE_TAC_OPERATION_LIFECYCLE_ENABLED", "true"),
+  service_url: System.get_env("MICROSERVICE_TAC_OPERATION_LIFECYCLE_URL")
+
 config :explorer, :air_table_public_tags,
   table_url: System.get_env("ACCOUNT_PUBLIC_TAGS_AIRTABLE_URL"),
   api_key: System.get_env("ACCOUNT_PUBLIC_TAGS_AIRTABLE_API_KEY")
