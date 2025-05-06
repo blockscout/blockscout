0c77abe286cd7f90c799b1f8cbc127415a0930a0
POA <33550681+poa@users.noreply.github.com>
2025-05-06 07:33:46 +0000

    Clear OP interop messages table

--- apps/explorer/priv/optimism/migrations/20250501120524_op_interop_transfer_fields.exs
+++ apps/explorer/priv/optimism/migrations/20250501120524_op_interop_transfer_fields.exs
@@ -2,6 +2,8 @@ defmodule Explorer.Repo.Optimism.Migrations.OPInteropTransferFields do
   use Ecto.Migration
 
   def change do
+    execute("TRUNCATE TABLE op_interop_messages;")
+
     alter table(:op_interop_messages) do
       add(:transfer_token_address_hash, :bytea, null: true, default: nil)
       add(:transfer_from_address_hash, :bytea, null: true, default: nil)
