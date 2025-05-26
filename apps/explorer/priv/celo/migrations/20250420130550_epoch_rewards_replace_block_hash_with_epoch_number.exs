08103135ac2369be990c2a1d7c1096c65ebbe831
Fedor Ivanov <ivnfedor@gmail.com>
2025-05-26 17:10:02 +0300

    fix: add deletion of epoch rewards that were fetched after l2 transition

--- apps/explorer/priv/celo/migrations/20250420130550_epoch_rewards_replace_block_hash_with_epoch_number.exs
+++ apps/explorer/priv/celo/migrations/20250420130550_epoch_rewards_replace_block_hash_with_epoch_number.exs
@@ -2,6 +2,17 @@ defmodule Explorer.Repo.Celo.Migrations.EpochRewardsReplaceBlockHashWithEpochNum
   use Ecto.Migration
 
   def change do
+    l2_migration_block_number = Application.get_env(:explorer, :celo)[:l2_migration_block]
+
+    if l2_migration_block_number do
+      execute("""
+      DELETE FROM celo_epoch_rewards er
+      USING blocks b
+      WHERE er.block_hash = b.hash
+        AND b.number >= #{l2_migration_block_number}
+      """)
+    end
+
     alter table(:celo_epoch_rewards) do
       add(:epoch_number, references(:celo_epochs, column: :number, on_delete: :delete_all))
     end
