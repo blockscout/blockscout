68ab9ad6ef1cc0fb27f0f94382cb5399ad53c258
Victor Baranov <baranov.viktor.27@gmail.com>
2025-09-17 18:35:50 +0300

    fix: Soften deposits deletion condition

--- apps/indexer/lib/indexer/fetcher/beacon/deposit.ex
+++ apps/indexer/lib/indexer/fetcher/beacon/deposit.ex
@@ -93,9 +93,16 @@ defmodule Indexer.Fetcher.Beacon.Deposit do
 
   @impl GenServer
   def handle_cast({:lost_consensus, block_number}, %__MODULE__{} = state) do
+    max_reorg_depth_block_number = block_number + 64
+
     {_deleted_deposits_count, deleted_deposits} =
       Repo.delete_all(
-        from(d in Deposit, where: d.block_number >= ^block_number, select: d.index),
+        from(
+          d in Deposit,
+          where: d.block_number > ^block_number,
+          where: d.block_number <= ^max_reorg_depth_block_number,
+          select: d.index
+        ),
         timeout: :infinity
       )
 
