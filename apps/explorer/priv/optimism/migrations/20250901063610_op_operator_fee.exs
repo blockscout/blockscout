12ba417dbe179c2534173834f3bf31895f9e6999
POA <33550681+poa@users.noreply.github.com>
2025-09-26 12:08:52 +0000

    Fixes for review

--- apps/explorer/priv/optimism/migrations/20250901063610_op_operator_fee.exs
+++ apps/explorer/priv/optimism/migrations/20250901063610_op_operator_fee.exs
@@ -1,10 +1,25 @@
 defmodule Explorer.Repo.Optimism.Migrations.OPOperatorFee do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:transactions) do
       add(:operator_fee_scalar, :decimal, null: true, default: nil)
       add(:operator_fee_constant, :decimal, null: true, default: nil)
+    end
+
+    execute("""
+      CREATE INDEX transactions_operator_fee_constant_index ON transactions(operator_fee_constant) WHERE operator_fee_constant IS NULL;
+    """)
+  end
+
+  def down do
+    execute("""
+      DROP INDEX transactions_operator_fee_constant_index;
+    """)
+
+    alter table(:transactions) do
+      remove(:operator_fee_constant)
+      remove(:operator_fee_scalar)
     end
   end
 end
