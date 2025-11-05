4c20fc3d89b4ea63c8de975c3a76d8636617ac80
Victor Baranov <baranov.viktor.27@gmail.com>
2025-11-05 23:45:44 +0300

    9.2.2

--- apps/explorer/mix.exs
+++ apps/explorer/mix.exs
@@ -24,7 +24,7 @@ defmodule Explorer.Mixfile do
         dialyzer: :test
       ],
       start_permanent: Mix.env() == :prod,
-      version: "9.2.1",
+      version: "9.2.2",
       xref: [exclude: [BlockScoutWeb.Routers.WebRouter.Helpers, Indexer.Helper, Indexer.Fetcher.InternalTransaction]]
     ]
   end
