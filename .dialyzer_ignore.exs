a0af8e88700c3e31507fdd80ac4bd0a54ec670b7
Victor Baranov <baranov.viktor.27@gmail.com>
2025-11-14 15:02:56 +0300

    Update dialyzer_ignore

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,5 @@
 [
-    {"lib/ethereum_jsonrpc/rolling_window.ex", :improper_list_constr, 171},
+    {"lib/ethereum_jsonrpc/rolling_window.ex", :improper_list_constr},
     {"lib/explorer/smart_contract/solidity/publisher_worker.ex", :pattern_match, 1},
     {"lib/explorer/smart_contract/solidity/publisher_worker.ex", :exact_eq, 8},
     {"lib/explorer/smart_contract/solidity/publisher_worker.ex", :pattern_match, 8},
@@ -9,13 +9,8 @@
     {"lib/explorer/smart_contract/stylus/publisher_worker.ex", :pattern_match, 1},
     {"lib/explorer/smart_contract/stylus/publisher_worker.ex", :exact_eq, 14},
     {"lib/explorer/smart_contract/stylus/publisher_worker.ex", :pattern_match, 14},
+    ~r/lib\/phoenix\/router.ex/,
     {"lib/explorer/chain/search.ex", :pattern_match, 100},
     {"lib/explorer/chain/search.ex", :pattern_match, 282},
-    {"lib/explorer/chain/search.ex", :pattern_match, 379},
-    ~r/lib\/phoenix\/router.ex/,
-    # todo: consider removing lines below after https://github.com/elixir-lang/elixir/issues/14837 is resolved
-    ~r/lib\/block_scout_web.*call_without_opaque/,
-    ~r/lib\/indexer.*call_without_opaque/,
-    ~r/lib\/explorer.*call_without_opaque/,
-    ~r/lib\/utils.*contract_with_opaque/
+    {"lib/explorer/chain/search.ex", :pattern_match, 379}
 ]
