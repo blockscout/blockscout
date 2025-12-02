c470800294dbb1d7b50ff5716cbcbebbab1e6325
Victor Baranov <baranov.viktor.27@gmail.com>
2025-12-02 18:33:04 +0300

    Process AI comment

--- apps/explorer/priv/repo/migrations/20251202092200_update_contract_methods_unique_index.exs
+++ apps/explorer/priv/repo/migrations/20251202092200_update_contract_methods_unique_index.exs
@@ -6,7 +6,7 @@ defmodule Explorer.Repo.Migrations.UpdateContractMethodsUniqueIndex do
       unique_index(:contract_methods, [:identifier, "md5(abi::text)"], name: :contract_methods_identifier_md5_abi_index)
     )
 
-    drop(unique_index(:contract_methods, [:identifier, :abi]), name: :contract_methods_identifier_abi_index)
+    drop(unique_index(:contract_methods, [:identifier, :abi], name: :contract_methods_identifier_abi_index))
   end
 
   def down do
