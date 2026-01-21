11079e275db3c25c72fbc5dec7b483389bcce3d2
Qwerty5Uiop <alex000010@bk.ru>
2026-01-21 15:39:59 +0400

    Swap drop and create a call_has_call_type  constraint

--- apps/explorer/priv/repo/migrations/20250908062439_add_call_type_enum_to_internal_transactions.exs
+++ apps/explorer/priv/repo/migrations/20250908062439_add_call_type_enum_to_internal_transactions.exs
@@ -11,15 +11,15 @@ defmodule Explorer.Repo.Migrations.AddCallTypeEnumToInternalTransactions do
       add(:call_type_enum, :internal_transactions_call_type, null: true)
     end
 
-    drop(constraint(:internal_transactions, :call_has_call_type, check: "type != 'call' OR call_type IS NOT NULL"),
-      validate: false
-    )
-
     create(
       constraint(:internal_transactions, :call_has_call_type_enum,
         check: "type != 'call' OR call_type IS NOT NULL OR call_type_enum IS NOT NULL",
         validate: false
       )
+    )
+
+    drop(constraint(:internal_transactions, :call_has_call_type, check: "type != 'call' OR call_type IS NOT NULL"),
+      validate: false
     )
   end
 end
