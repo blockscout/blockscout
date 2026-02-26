eaf2154b216a3209c1e21f91b986692c7a9d0c23
Victor Baranov <baranov.viktor.27@gmail.com>
2026-02-26 20:49:40 +0300

    Process agent review

--- apps/explorer/test/explorer/migrator/heavy_db_index_operation/drop_transactions_created_contract_address_hash_with_pending_index_a_test.exs
+++ apps/explorer/test/explorer/migrator/heavy_db_index_operation/drop_transactions_created_contract_address_hash_with_pending_index_a_test.exs
@@ -3,9 +3,9 @@ defmodule Explorer.Migrator.HeavyDbIndexOperation.DropTransactionsCreatedContrac
 
   alias Explorer.Chain.Cache.BackgroundMigrations
   alias Explorer.Migrator.{HeavyDbIndexOperation, MigrationStatus}
-  alias Explorer.Migrator.HeavyDbIndexOperation.Helper
   alias Explorer.Migrator.HeavyDbIndexOperation.CreateTransactionsCreatedContractAddressHashWPendingIndex
   alias Explorer.Migrator.HeavyDbIndexOperation.DropTransactionsCreatedContractAddressHashWithPendingIndexA
+  alias Explorer.Migrator.HeavyDbIndexOperation.Helper
 
   describe "Drops heavy index `transactions_created_contract_address_hash_with_pending_index_a`" do
     setup do
@@ -20,7 +20,7 @@ defmodule Explorer.Migrator.HeavyDbIndexOperation.DropTransactionsCreatedContrac
     end
 
     test "Drops heavy DB index with dependency on create migration" do
-      create_migration_name = "heavy_indexes_create_transactions_2nd_created_contract_address_hash_with_pending_index_a"
+      create_migration_name = CreateTransactionsCreatedContractAddressHashWPendingIndex.migration_name()
       drop_migration_name = "heavy_indexes_drop_transactions_created_contract_address_hash_with_pending_index_a"
       index_name = "transactions_created_contract_address_hash_with_pending_index_a"
 
