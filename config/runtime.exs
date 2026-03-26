a7d6b989114f207c2229254461dab17efc6dd273
Victor Baranov <baranov.viktor.27@gmail.com>
2026-03-26 17:51:33 +0300

    feat: add toggle to disable transactions BENS preload

--- config/runtime.exs
+++ config/runtime.exs
@@ -222,7 +222,10 @@ config :block_scout_web, :chart,
 config :block_scout_web, BlockScoutWeb.Chain.Address.CoinBalance,
   coin_balance_history_days: ConfigHelper.parse_integer_env_var("COIN_BALANCE_HISTORY_DAYS", 10)
 
-config :block_scout_web, BlockScoutWeb.API.V2, enabled: ConfigHelper.parse_bool_env_var("API_V2_ENABLED", "true")
+config :block_scout_web, BlockScoutWeb.API.V2,
+  enabled: ConfigHelper.parse_bool_env_var("API_V2_ENABLED", "true"),
+  disable_transactions_bens_preload: ConfigHelper.parse_bool_env_var("DISABLE_TRANSACTIONS_BENS_PRELOAD", "false"),
+  disable_token_transfers_bens_preload: ConfigHelper.parse_bool_env_var("DISABLE_TOKEN_TRANSFERS_BENS_PRELOAD", "false")
 
 config :block_scout_web, BlockScoutWeb.MicroserviceInterfaces.TransactionInterpretation,
   service_url: System.get_env("MICROSERVICE_TRANSACTION_INTERPRETATION_URL"),
