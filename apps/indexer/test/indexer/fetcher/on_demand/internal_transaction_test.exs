f8ccc1ba5a16c963dcb2921a42052a74f269dbf2
Qwerty5Uiop <105209995+Qwerty5Uiop@users.noreply.github.com>
2026-05-01 13:53:29 +0400

    fix: Update OnDemand.InternalTransaction etherscan fields (#14297)

--- apps/indexer/test/indexer/fetcher/on_demand/internal_transaction_test.exs
+++ apps/indexer/test/indexer/fetcher/on_demand/internal_transaction_test.exs
@@ -438,4 +438,48 @@ defmodule Indexer.Fetcher.OnDemand.InternalTransactionTest do
 
     assert [] = InternalTransactionOnDemand.fetch_by_address(address.hash, opts)
   end
+
+  test "etherscan_fetch_by_transaction/2" do
+    transaction = :transaction |> insert() |> with_block()
+    block_timestamp = transaction.block.timestamp
+    transaction_hash = transaction.hash
+
+    expect(EthereumJSONRPC.Mox, :json_rpc, 1, fn
+      [%{id: id, params: _}], _ ->
+        {:ok,
+         [
+           %{
+             id: id,
+             result: %{
+               "type" => "create",
+               "from" => "0x117b358218da5a4f647072ddb50ded038ed63d17",
+               "to" => "0x205a6b72ce16736c9d87172568a9c0cb9304de0d",
+               "value" => "0x0",
+               "gas" => "0x106f5",
+               "gasUsed" => "0x106f5",
+               "input" =>
+                 "0x608060405234801561001057600080fd5b50610150806100206000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c80632e64cec11461003b5780636057361d14610059575b600080fd5b610043610075565b60405161005091906100d9565b60405180910390f35b610073600480360381019061006e919061009d565b61007e565b005b60008054905090565b8060008190555050565b60008135905061009781610103565b92915050565b6000602082840312156100b3576100b26100fe565b5b60006100c184828501610088565b91505092915050565b6100d3816100f4565b82525050565b60006020820190506100ee60008301846100ca565b92915050565b6000819050919050565b600080fd5b61010c816100f4565b811461011757600080fd5b5056fea26469706673582212209a159a4f3847890f10bfb87871a61eba91c5dbf5ee3cf6398207e292eee22a1664736f6c63430008070033",
+               "output" =>
+                 "0x608060405234801561001057600080fd5b50600436106100365760003560e01c80632e64cec11461003b5780636057361d14610059575b600080fd5b610043610075565b60405161005091906100d9565b60405180910390f35b610073600480360381019061006e919061009d565b61007e565b005b60008054905090565b8060008190555050565b60008135905061009781610103565b92915050565b6000602082840312156100b3576100b26100fe565b5b60006100c184828501610088565b91505092915050565b6100d3816100f4565b82525050565b60006020820190506100ee60008301846100ca565b92915050565b6000819050919050565b600080fd5b61010c816100f4565b811461011757600080fd5b5056fea26469706673582212209a159a4f3847890f10bfb87871a61eba91c5dbf5ee3cf6398207e292eee22a1664736f6c63430008070033"
+             }
+           }
+         ]}
+    end)
+
+    Application.put_env(:ethereum_jsonrpc, EthereumJSONRPC.Geth, tracer: "call_tracer", debug_trace_timeout: "5s")
+
+    assert [
+             %{
+               created_contract_address_hash: created_contract_address_hash,
+               from_address_hash: from_address_hash,
+               to_address_hash: nil,
+               block_timestamp: ^block_timestamp,
+               transaction_hash: ^transaction_hash,
+               error: nil
+             }
+           ] = InternalTransactionOnDemand.etherscan_fetch_by_transaction(transaction, %{})
+
+    assert to_string(created_contract_address_hash) == "0x205a6b72ce16736c9d87172568a9c0cb9304de0d"
+    assert to_string(from_address_hash) == "0x117b358218da5a4f647072ddb50ded038ed63d17"
+  end
 end
