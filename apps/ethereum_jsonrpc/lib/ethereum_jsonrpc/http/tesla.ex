aa2cdee102a2616f2ea13f82b93d0dabe900970a
Victor Baranov <baranov.viktor.27@gmail.com>
2026-05-11 11:16:15 +0300

    fix: normalize Tesla timeout middleware exceptions (#14231)

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/http/tesla.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/http/tesla.ex
@@ -3,6 +3,8 @@ defmodule EthereumJSONRPC.HTTP.Tesla do
   Uses `Tesla.Mint` for `EthereumJSONRPC.HTTP`
   """
 
+  require Logger
+
   alias EthereumJSONRPC.HTTP
   alias EthereumJSONRPC.HTTP.Helper
   alias EthereumJSONRPC.Prometheus.Instrumenter
@@ -16,7 +18,7 @@ defmodule EthereumJSONRPC.HTTP.Tesla do
 
     Instrumenter.json_rpc_requests(method)
 
-    case Tesla.post(TeslaHelper.client(options), url, json, headers: headers, opts: TeslaHelper.request_opts(options)) do
+    case do_post(url, json, headers, options) do
       {:ok, %Tesla.Env{body: body, status: status_code, headers: headers}} ->
         with {:ok, decoded_body} <- Jason.decode(body),
              true <- Helper.response_body_has_error?(decoded_body) do
@@ -33,4 +35,34 @@ defmodule EthereumJSONRPC.HTTP.Tesla do
   end
 
   def json_rpc(url, _json, _headers, _options) when is_nil(url), do: {:error, "URL is nil"}
+
+  defp do_post(url, json, headers, options) do
+    Tesla.post(TeslaHelper.client(options), url, json, headers: headers, opts: TeslaHelper.request_opts(options))
+  rescue
+    error ->
+      if timeout_middleware_exception?(__STACKTRACE__) do
+        log_normalized_timeout(:rescue, url, headers, options)
+        {:error, :timeout}
+      else
+        {:error, error}
+      end
+  catch
+    :exit, {:timeout, _} ->
+      log_normalized_timeout(:exit, url, headers, options)
+      {:error, :timeout}
+  end
+
+  defp timeout_middleware_exception?(stacktrace) do
+    Enum.any?(stacktrace, fn
+      {Tesla.Middleware.Timeout, :call, 3, _} -> true
+      {Tesla.Middleware.Timeout, :repass_error, 1, _} -> true
+      _ -> false
+    end)
+  end
+
+  defp log_normalized_timeout(source, url, headers, options) do
+    Logger.warning(
+      "Normalized timeout in do_post/4 source=#{source} url=#{inspect(url)} timeout=#{inspect(options[:timeout])} recv_timeout=#{inspect(options[:recv_timeout])} headers_count=#{length(headers)}"
+    )
+  end
 end
