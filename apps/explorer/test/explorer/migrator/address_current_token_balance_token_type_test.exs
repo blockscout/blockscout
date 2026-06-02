11fd005d3297cf424a0a26cbf55cfd7712cd6c36
Qwerty5Uiop <alex000010@bk.ru>
2026-06-02 14:07:40 +0400

    Fix background migrations tests

--- apps/explorer/test/explorer/migrator/address_current_token_balance_token_type_test.exs
+++ apps/explorer/test/explorer/migrator/address_current_token_balance_token_type_test.exs
@@ -9,6 +9,8 @@ defmodule Explorer.Migrator.AddressCurrentTokenBalanceTokenTypeTest do
 
   describe "Migrate current token balances" do
     test "Set token_type for not processed current token balances" do
+      insert(:block)
+
       Enum.each(0..10, fn _x ->
         current_token_balance = insert(:address_current_token_balance, token_type: nil)
         assert %{token_type: nil} = current_token_balance
