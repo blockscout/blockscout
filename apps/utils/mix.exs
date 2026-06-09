a9dd43dbf23e83cb6efc561f75717d3fed3da69a
Victor Baranov <baranov.viktor.27@gmail.com>
2026-06-09 17:29:48 +0300

    11.2.0

--- apps/utils/mix.exs
+++ apps/utils/mix.exs
@@ -5,7 +5,7 @@ defmodule Utils.MixProject do
   def project do
     [
       app: :utils,
-      version: "11.1.3",
+      version: "11.2.0",
       build_path: "../../_build",
       # config_path: "../../config/config.exs",
       deps_path: "../../deps",
