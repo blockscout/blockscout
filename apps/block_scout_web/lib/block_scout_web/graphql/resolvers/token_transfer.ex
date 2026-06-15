330585899742fc0d7d81d51bdb208d1f57e4f93f
Victor Baranov <baranov.viktor.27@gmail.com>
2026-06-15 17:00:56 +0300

    feat: apply scam token filter to GraphQL token transfer queries

--- apps/block_scout_web/lib/block_scout_web/graphql/resolvers/token_transfer.ex
+++ apps/block_scout_web/lib/block_scout_web/graphql/resolvers/token_transfer.ex
@@ -10,25 +10,30 @@ defmodule BlockScoutWeb.GraphQL.Resolvers.TokenTransfer do
     GraphQL.get_token_transfer(args)
   end
 
-  def get_by(_, %{token_contract_address_hash: token_contract_address_hash} = args, _) do
+  def get_by(_, %{token_contract_address_hash: token_contract_address_hash} = args, resolution) do
     connection_args = Map.take(args, [:after, :before, :first, :last])
 
     replica = Repo.replica()
+    scam_opts = scam_token_opts(resolution)
 
     token_contract_address_hash
-    |> GraphQL.list_token_transfers_query()
+    |> GraphQL.list_token_transfers_query(scam_opts)
     |> Connection.from_query(&replica.all/1, connection_args, options(args))
   end
 
-  def get_by(%Address{hash: address_hash}, args, _) do
+  def get_by(%Address{hash: address_hash}, args, resolution) do
     connection_args = Map.take(args, [:after, :before, :first, :last])
 
     replica = Repo.replica()
+    scam_opts = scam_token_opts(resolution)
 
     address_hash
-    |> TokenTransfer.token_transfers_by_address_hash(nil, nil, [], nil, [])
+    |> TokenTransfer.token_transfers_by_address_hash(nil, nil, [], nil, scam_opts)
     |> Connection.from_query(&replica.all/1, connection_args, options(args))
   end
+
+  defp scam_token_opts(%{context: context}),
+    do: [show_scam_tokens?: Map.get(context, :show_scam_tokens?, false)]
 
   defp options(%{before: _}), do: []
 
